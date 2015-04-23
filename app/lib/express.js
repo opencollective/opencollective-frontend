1ac8cf750b430105ec2ef83b359f3f96df7c7031
Philmod <philippe.modard@gmail.com>
2015-04-22 21:21:41 -0700

    cors

--- app/lib/express.js
+++ app/lib/express.js
@@ -1,9 +1,13 @@
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 module.exports = function(app) {
 
   // Body parser.
   app.use(bodyParser.json());
+
+  // Cors.
+  app.use(cors());
 
 }
 
