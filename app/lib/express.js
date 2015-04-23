de6300a7720b44219b3f27be0ecf2c2da439951e
Philmod <philippe.modard@gmail.com>
2015-04-22 21:24:28 -0700

    decode req.body

--- app/lib/express.js
+++ app/lib/express.js
@@ -1,10 +1,13 @@
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var multer = require('multer');
 
 module.exports = function(app) {
 
   // Body parser.
   app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(multer());
 
   // Cors.
   app.use(cors());
