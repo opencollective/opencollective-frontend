e8cfb84086c5fe6cc4adadfde1991a9c0a430d45
Philmod <philippe.modard@gmail.com>
2015-05-01 21:53:30 -0700

    port

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ else {
  * Start server.
  */
 function start() {
-  var port = app.set('port') || 3060;
+  var port = process.env.PORT || 3060;
   var server = app.listen(port, function () {
     var host = server.address().address;
     var port = server.address().port;
