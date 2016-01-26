23243ecd47a082c90f6be9d34924e597016a631d
Arnaud Benard <arnaudm.benard@gmail.com>
2016-01-25 16:34:26 -0800

    automigrate db on production on postinstall

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,7 @@
 # Rebuild for bcrypt on circleci and other envs
 npm rebuild
 
-# Only run migrations automatically on staging
-if [ "$SEQUELIZE_ENV" = "staging" ]; then
+# Only run migrations automatically on staging and production
+if [ "$SEQUELIZE_ENV" = "staging" ] || [ "$SEQUELIZE_ENV" = "production" ]; then
   npm run db:migrate
 fi
