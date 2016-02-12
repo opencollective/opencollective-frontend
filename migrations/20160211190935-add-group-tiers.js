7114ff57171f2661860ec75c978ba41185c6785c
Arnaud Benard <arnaudm.benard@gmail.com>
2016-02-12 17:36:51 +0100

    added tiers in the group model

---
+++ migrations/20160211190935-add-group-tiers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, DataTypes) {
+    return queryInterface.addColumn('Groups', 'tiers', {
+      type: DataTypes.JSON
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.removeColumn('Groups', 'tiers');
+  }
+};
