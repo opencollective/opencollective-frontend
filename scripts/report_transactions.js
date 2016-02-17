d8aa7569494578337bf49445b3bca7789a81a3d8
Sebastien Dubois <sdubois@protonmail.com>
2016-02-17 16:38:42 +0200

    Compute other counts and amounts

--- scripts/report_transactions.js
+++ scripts/report_transactions.js
@@ -1,13 +1,8 @@
 const app = require('../index');
 const models = app.set('models');
 const moment = require('moment-timezone');
-//const async = require('async');
-
-const done = (err) => {
-  if (err) console.log('err', err);
-  console.log('done!');
-  process.exit();
-};
+const async = require('async');
+const activities = require('../app/constants/activities');
 
 var thisWeekRaw = moment()
     .tz('America/New_York')
@@ -15,27 +10,79 @@ var thisWeekRaw = moment()
     .add(9, 'hours');
 const thisWeek = thisWeekRaw.format();
 const lastWeek = thisWeekRaw.subtract(1, 'week').format();
-//const testDate = '2016-02-15T09:00:00-05:00';
 
-//const createdAtClause = {
-//  $gt: lastWeek,
-//  $lt: thisWeek
-//};
 const createdAtClause = {
-  $gt: lastWeek
+  $gt: lastWeek,
+  $lt: thisWeek
+};
+const donationClause = {
+  createdAt: createdAtClause,
+  tags: {
+    $contains: ['Donation']
+  }
+};
+const expenseClause = {
+  createdAt: createdAtClause,
+  amount: {
+    $lt: 0
+  }
 };
 
-models.Transaction
-    .count({
-      where: {
-        createdAt: createdAtClause,
-        tags: {
-          $contains: ['Donation']
-        }
-      }
-    })
-    .then(donationCount => {
-      console.log("donation count: "+ donationCount);
-      done();
-    })
-    .catch(done);
+async.auto({
+  donationCount: cb => {
+    models.Transaction
+        .count({ where: donationClause })
+        .then(donationCount => cb(null, donationCount))
+        .catch(cb);
+  },
+
+  expenseCount: cb => {
+    models.Transaction
+        .count({ where: expenseClause })
+        .then(expenseCount => cb(null, expenseCount))
+        .catch(cb);
+  },
+
+  stripeReceivedCount: cb => {
+    models.Activity
+        .count({
+          where: {
+            createdAt: createdAtClause,
+            type: activities.WEBHOOK_STRIPE_RECEIVED
+          }
+        })
+        .then(stripeReceivedCount => cb(null, stripeReceivedCount))
+        .catch(cb);
+  },
+
+  donationAmounts: cb => {
+    models.Transaction
+        .aggregate('amount', 'SUM', {
+          plain: false,
+          group: ['currency'],
+          attributes: ['currency'],
+          where: donationClause
+        })
+        .then(currencies => cb(null, currencies))
+        .catch(cb);
+  },
+
+  expenseAmount: cb => {
+    models.Transaction
+        .aggregate('amount', 'SUM', {
+          plain: false,
+          group: ['currency'],
+          attributes: ['currency'],
+          where: expenseClause
+        })
+        .then(currencies => cb(null, currencies))
+        .catch(cb);
+  }
+}, (err, results) => {
+  if (err) {
+    console.log('err', err);
+  } else {
+    console.log('Result: ' + JSON.stringify(results));
+  }
+  process.exit();
+});
