dfb8c6a401d468bb39bb72436450496798a8d827
Aseem Sood <asood123@yahoo.com>
2016-02-18 16:50:01 -0500

    fixing migrations

--- migrations/20160218164134-add-new-constraint-to-UserGroups.js
+++ migrations/20160218164134-add-new-constraint-to-UserGroups.js
@@ -1,21 +1,13 @@
 'use strict';
 
 module.exports = {
-  up: function (queryInterface, Sequelize) {
+  up: function (queryInterface) {
 
     // We need to manually drop the constraint before old index can be removed
     // http://stackoverflow.com/questions/29518786/remove-constraints-in-sequelize-migration
     queryInterface.sequelize.query('ALTER TABLE "UserGroups" DROP CONSTRAINT "UserGroups_pkey";');
 
-    return queryInterface
-    .addColumn('UserGroups', 'id', {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    })
-    .then(function(){
-      return queryInterface.removeIndex('UserGroups', 'UserGroups_pkey');
-    })
+    return queryInterface.removeIndex('UserGroups', 'UserGroups_pkey')
     .then(function(){
       return queryInterface.addIndex('UserGroups', ['GroupId', 'UserId', 'role'], {
         indexName: 'UserGroups_3way',
@@ -28,16 +20,12 @@ module.exports = {
 
     queryInterface.sequelize.query('ALTER TABLE "UserGroups" DROP CONSTRAINT "UserGroups_3way";');
 
-    return queryInterface
-    .removeIndex('UserGroups', 'UserGroups_3way')
+    return queryInterface.removeIndex('UserGroups', 'UserGroups_3way')
     .then(function(){
       return queryInterface.addIndex('UserGroups', ['GroupId', 'UserId'], {
         indexName: 'UserGroups_pkey',
         indicesType: 'UNIQUE'
       });
     })
-    .then(function(){
-      return queryInterface.removeColumn('UserGroups', 'id');
-    });
   }
 };
