c1341d2bb2bed26727a5bfc44c29d17068181f34
Xavier Damman <xdamman@gmail.com>
2016-02-22 12:57:29 -0800

    tests

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -8,10 +8,17 @@ const sponsor = { totalDonations: 500 };
 
 describe("utils", () => {
   
-  it("gets the right tier", (done) => {
+  it("gets the right tier", () => {
     expect(libutils.getTier(backer, group.tiers)).to.equal('backer');
     expect(libutils.getTier(sponsor, group.tiers)).to.equal('sponsor');
-    done();
-  })
+  });
+  
+  it("returns backer as the default tier", () => {
+    expect(libutils.getTier(backer, null)).to.equal('backer');
+  });
+
+  it("returns null if the member didn't make any donation", () => {
+    expect(libutils.getTier({ totalDonations: null }, group.tiers)).to.equal(null);
+  });
   
 });
