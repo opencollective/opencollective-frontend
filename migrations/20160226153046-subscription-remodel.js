345bfdf04a392c5c0ff7dba7aac2b8d8ae30dbaf
Arnaud Benard <arnaudm.benard@gmail.com>
2016-02-26 15:50:01 +0100

    add migrations for new subscription model

---
+++ migrations/20160226153046-subscription-remodel.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, DataTypes) {
+    return queryInterface.createTable('Subscriptions', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+
+      amount: DataTypes.FLOAT,
+
+      currency: {
+        type: DataTypes.STRING,
+        defaultValue: 'USD',
+      },
+
+      interval: DataTypes.STRING,
+
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+
+      data: DataTypes.JSON,
+
+      stripeSubscriptionId: DataTypes.STRING,
+
+      activatedAt: DataTypes.DATE,
+
+      deactivatedAt: DataTypes.DATE,
+
+      createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+      },
+
+      updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+      },
+
+      deletedAt: {
+        type: DataTypes.DATE
+      }
+
+    });
+  },
+
+  down: function (queryInterface) {
+    return queryInterface.dropTable('Subscriptions');
+  }
+};
