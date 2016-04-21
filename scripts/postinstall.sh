6741e05e9accd6d6cb3ba0a0c4eaa96a921f537e
Sebastien Dubois <sdubois@protonmail.com>
2016-04-21 19:52:48 +0200

    Avoid cyclic npm install

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -10,9 +10,5 @@ fi
 
 # pre-install website and app to attempt to get their node_modules cached by circleci
 if [ "$NODE_ENV" = "circleci" ]; then
-  # only applicable if this repo is the one circleci is originally aiming to build/test
-  # (avoid cyclic dependency when API is checked out from client builds)
-  if [ ! -d "$HOME/website" ] && [ ! -d "$HOME/app" ]; then
-    npm run test:e2e:exec website:install app:install
-  fi
+  npm run test:e2e:exec website:install app:install
 fi
