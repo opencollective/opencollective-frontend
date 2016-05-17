3801d3b8bbe74f0175d44edc8b4a19c0e8c2d457
Sebastien Dubois <sdubois@protonmail.com>
2016-05-17 16:45:30 +0300

    Disable execution of e2e tests from API

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -7,8 +7,3 @@ set -e
 if [ "$SEQUELIZE_ENV" = "staging" ] || [ "$SEQUELIZE_ENV" = "production" ]; then
   npm run db:migrate
 fi
-
-# pre-install website and app to attempt to get their node_modules cached by circleci
-if [ "$NODE_ENV" = "circleci" ]; then
-  npm run test:e2e:exec website:install app:install
-fi
