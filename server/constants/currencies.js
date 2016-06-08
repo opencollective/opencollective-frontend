e6a39a3c1675b35d3d1970fa40990c8fde280ea7
Sébastien Dubois <sedubois@users.noreply.github.com>
2016-06-08 14:31:45 +0300

    Add missing currencies AUD and MXN (#420)

--- server/constants/currencies.js
+++ server/constants/currencies.js
@@ -1,8 +1,10 @@
 module.exports = {
-  USD: value => `$${value}`,
+  AUD: value => `${value} AUD`,
   EUR: value => `€${value}`,
   GBP: value => `£${value}`,
+  INR: value => `₹${value}`,
+  MXN: value => `${value} MXN`,
   SEK: value => `kr ${value}`,
-  UYU: value => `$U ${value}`,
-  INR: value => `₹${value}`
+  USD: value => `$${value}`,
+  UYU: value => `$U ${value}`
 };
