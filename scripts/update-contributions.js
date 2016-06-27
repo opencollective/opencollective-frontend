b4d5cedf259b842bc2bd492ddeff29ba8397f3eb
Aseem Sood <asood123@yahoo.com>
2016-06-27 14:41:00 -0400

    add support for githubRepo setting and count total stars across organizations

--- scripts/update-contributions.js
+++ scripts/update-contributions.js
@@ -17,7 +17,8 @@ Group.findAll({
   attributes: [
     'id',
     'name',
-    'settings'
+    'settings',
+    'data'
   ]
 })
   .tap(groups => {
@@ -25,29 +26,54 @@ Group.findAll({
   })
   .each(group => {
     const org = _.get(group, 'settings.githubOrg');
-    if (!org) {
-      log.warn(group.name, `GitHub org not associated`);
+    const repoLink = _.get(group, 'settings.githubRepo');
+    if (!org && !repoLink) {
+      log.warn(group.name, `No GitHub org or repo associated`);
       return;
     }
-    return client.contributorsInOrg({orgs: [org]})
-      .get(org)
-      .then(repos => {
-      const data = {};
-      data.contributorData = _(repos)
-          .map('contributors')
-          .reduce((acc, contributions) => {
-            _.each(contributions, (count, user) => {
-              acc[user] = (acc[user] || 0) + count;
-            });
-            return acc;
-          }, {});
-        data.repoData = _.mapValues(repos, repo => _.omit(repo, 'contributors'));
-        return data;
-      })
-      .then(data => {
-        group.settings = _.assign(group.settings || {}, {
-          githubOrg: org
+    var fetchPromise;
+
+    if (org) {
+      fetchPromise = client.contributorsInOrg({orgs: [org]})
+        .get(org)
+        .then(repos => {
+          const data = {};
+          data.contributorData = _(repos)
+              .map('contributors')
+              .reduce((acc, contributions) => {
+                _.each(contributions, (count, user) => {
+                  acc[user] = (acc[user] || 0) + count;
+                });
+                return acc;
+              }, {});
+          data.repoData = _.mapValues(repos, repo => _.omit(repo, 'contributors'));
+          data.stars = _(repos)
+              .map('stars')
+              .reduce((sum, n) => {
+                return sum + n;
+              }, 0)
+          return data;
         });
+    } else {
+      const split = repoLink.split('/');
+      if (split.length != 2) {
+        log.warn(group.name, 'Incorrect format of githubRepo');
+        return;
+      }
+      const options = {
+        user: split[0],
+        repo: split[1]
+      }
+      fetchPromise = client.contributorsForRepo(options)
+        .then(data => {
+          const contributorData = {};
+          data.map(dataEntry => contributorData[dataEntry.user] = dataEntry.contributions);
+          return {contributorData}
+        });
+    }
+
+    return fetchPromise
+      .then(data => {
         group.data = _.assign(group.data || {}, {
           githubContributors: data.contributorData,
           repos: data.repoData
