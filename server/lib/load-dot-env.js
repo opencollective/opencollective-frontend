42c9736baa2a9557ebfd33ff18750720ed3741f3
Aseem Sood <asood123@yahoo.com>
2016-07-14 10:00:22 -0700

    add a dummy dotenv file

--- server/lib/load-dot-env.js
+++ server/lib/load-dot-env.js
@@ -2,5 +2,7 @@
  * Load .env file
  */
 if (process.env.NODE_ENV === 'development') {
-  require('dotenv').load();
+  const dotenv = require('dotenv');
+  dotenv.config(); // this loads .env with real values. It needs to be first because dotenv doesn't overwrite any values
+  dotenv.config({path: 'default.env'}); // this loads the default file in github with dummy values
 }
