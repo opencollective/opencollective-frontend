ffc8ba76929477f6e9d78c064fe12ce491ba040d
Xavier Damman <xdamman@gmail.com>
2016-08-25 15:40:44 +0200

    removed done() when possible

--- test/email.routes.test.js
+++ test/email.routes.test.js
@@ -95,11 +95,11 @@ describe("email.routes.test", () => {
       .catch(e => console.error);
   });
 
-  it("forwards emails sent to info@:slug.opencollective.com", (done) => {
+  it("forwards emails sent to info@:slug.opencollective.com", () => {
 
     const spy = sandbox.spy(emailLib, 'sendMessage');
 
-    request(app)
+    return request(app)
       .post('/webhooks/mailgun')
       .send(Object.assign({}, webhookBody, {recipient: 'info@testcollective.opencollective.com'}))
       .then((res) => {
@@ -107,15 +107,14 @@ describe("email.routes.test", () => {
         expect(spy.args[0][0]).to.equal('info@testcollective.opencollective.com');
         expect(spy.args[0][1]).to.equal(webhookBody.subject);
         expect(spy.args[0][3].bcc).to.equal(usersData[0].email);
-        done();
       });
   });
   
-  it("forwards the email for approval to the core members", (done) => {
+  it("forwards the email for approval to the core members", () => {
 
     const spy = sandbox.spy(emailLib, 'send');
 
-    request(app)
+   return request(app)
       .post('/webhooks/mailgun')
       .send(webhookBody)
       .then((res) => {
@@ -124,47 +123,43 @@ describe("email.routes.test", () => {
         const emailSentTo = [spy.args[0][3].bcc,spy.args[1][3].bcc];
         expect(emailSentTo.indexOf(usersData[0].email) !== -1).to.be.true;
         expect(emailSentTo.indexOf(usersData[1].email) !== -1).to.be.true;
-        done();
       });
   });
 
-  it("rejects emails sent to unknown mailing list", (done) => {
+  it("rejects emails sent to unknown mailing list", () => {
 
     const unknownMailingListWebhook = Object.assign({}, webhookBody, { recipient: 'unknown@testcollective.opencollective.com' });
 
-    request(app)
+    return request(app)
       .post('/webhooks/mailgun')
       .send(unknownMailingListWebhook)
       .then((res) => {
         expect(res.statusCode).to.equal(200);
         expect(res.body.error.message).to.equal('There is no user subscribed to unknown@testcollective.opencollective.com');
-        done();
       });
   });
 
-  it("approves the email", (done) => {
+  it("approves the email", () => {
 
     const spy = sandbox.spy(emailLib, 'send');
 
-    request(app)
+    return request(app)
       .get(`/services/email/approve?messageId=eyJwIjpmYWxzZSwiayI6Ijc3NjFlZTBjLTc1NGQtNGIwZi05ZDlkLWU1NTgxODJkMTlkOSIsInMiOiI2NDhjZDg1ZTE1IiwiYyI6InNhb3JkIn0=&approver=${encodeURIComponent(usersData[1].email)}`)
       .then((res) => {
         expect(spy.args[0][1]).to.equal('members@testcollective.opencollective.com');
         expect(spy.args[0][2].subject).to.equal('test collective members');
         expect(spy.args[0][3].bcc).to.equal(usersData[0].email);
         expect(spy.args[0][3].from).to.equal('testcollective collective <info@testcollective.opencollective.com>');
-        done();
       });
   });
 
-  it("return 404 if message not found", (done) => {
+  it("return 404 if message not found", () => {
     const messageId = 'eyJwIjpmYWxzZSwiayI6IjY5MTdlYTZlLWVhNzctNGQzOC04OGUxLWMzMTQwMzdmNGRhNiIsInMiOiIwMjNjMzgwYWFlIiwiYyI6InNhaWFkIn0=';
-    request(app)
+    return request(app)
       .get(`/services/email/approve?messageId=${messageId}&approver=xdamman%40gmail.com&mailserver=so`)
       .then((res) => {
         expect(res.statusCode).to.equal(404);
         expect(res.body.error.message).to.contain(messageId);
-        done();
       });
   });
 
@@ -172,59 +167,55 @@ describe("email.routes.test", () => {
 
     const unsubscribeUrl = `/services/email/unsubscribe/${encodeURIComponent(usersData[0].email)}/${groupData.slug}/mailinglist.members/3d87fb0a6ffa99e8c4307f6fcd649dd1`;
 
-    it("returns an error if invalid token", (done) => {
-      request(app)
+    it("returns an error if invalid token", () => {
+      return request(app)
         .get(`/services/email/unsubscribe/${encodeURIComponent(usersData[0].email)}/${groupData.slug}/mailinglist.members/xxxxxxxxxx`)
         .then((res) => {
           expect(res.statusCode).to.equal(400);
           expect(res.body.error.message).to.equal('Invalid token');
-          done();
         });
     });
 
-    it("sends the unsubscribe link in the footer of the email", (done) => {
+    it("sends the unsubscribe link in the footer of the email", () => {
 
     const spy = sandbox.stub(emailLib, 'sendMessage');
 
-    request(app)
+    return request(app)
       .get(`/services/email/approve?messageId=eyJwIjpmYWxzZSwiayI6Ijc3NjFlZTBjLTc1NGQtNGIwZi05ZDlkLWU1NTgxODJkMTlkOSIsInMiOiI2NDhjZDg1ZTE1IiwiYyI6InNhb3JkIn0=&approver=${encodeURIComponent(usersData[1].email)}`)
       .then((res) => {
         const emailBody = spy.args[0][2];
         expect(emailBody).to.contain(unsubscribeUrl);
         expect(emailBody).to.contain("To unsubscribe from members@testcollective.opencollective.com");
-        done();
       });
     });
 
-    it("unsubscribes", (done) => {
+    it("unsubscribes", () => {
       const where = {
         UserId: users[0].id,
         GroupId: group.id,
         type: 'mailinglist.members'
       };
 
-      request(app)
+      return request(app)
         .get(unsubscribeUrl)
         .then(res => {
           console.log("res body", res.body);
           models.Notification.count({ where })
           .then(count => {
             expect(count).to.equal(0);
-            done();
           });
       });
     });
 
-    it("fails to unsubscribe if already unsubscribed", (done) => {
+    it("fails to unsubscribe if already unsubscribed", () => {
 
       const unsubscribeUrl = `/services/email/unsubscribe/${encodeURIComponent(usersData[0].email)}/${groupData.slug}/unknownType/38e32567c52039a97252b1e0537fd848`;
 
-      request(app)
+      return request(app)
         .get(unsubscribeUrl)
         .then(res => {
           expect(res.statusCode).to.equal(400);
           expect(res.body.error.message).to.equal("No notification found for this user, group and type");
-          done();
       });
     });
 
