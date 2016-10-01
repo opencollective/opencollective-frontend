6985b85f2eeb7cc229404c5d1817033113aa8da4
Xavier Damman <xdamman@gmail.com>
2016-10-02 00:22:16 +0200

    making images.routes.test.js pass

--- test/images.routes.test.js
+++ test/images.routes.test.js
@@ -4,10 +4,10 @@ import request from 'supertest';
 import * as utils from '../test/utils';
 import models from '../server/models';
 
+const application = utils.data('application');
 const userData = utils.data('user1');
 
 describe('images.routes.test.js', () => {
-  let application;
   let user;
 
   beforeEach(() => utils.resetTestDB());
@@ -20,7 +20,7 @@ describe('images.routes.test.js', () => {
 
   it('should upload an image to S3', (done) => {
     request(app)
-    .post('/images/')
+    .post(`/images/?api_key=${application.api_key}`)
     .attach('file', 'test/mocks/images/camera.png')
     .set('Authorization', `Bearer ${user.jwt()}`)
     .expect(200)
@@ -32,7 +32,7 @@ describe('images.routes.test.js', () => {
 
   it('should throw an error if no file field is sent', (done) => {
     request(app)
-    .post('/images/')
+    .post(`/images/?api_key=${application.api_key}`)
     .set('Authorization', `Bearer ${user.jwt()}`)
     .expect(400)
     .end(done);
@@ -40,7 +40,7 @@ describe('images.routes.test.js', () => {
 
   it('should upload if the user is not logged in', (done) => {
     request(app)
-    .post('/images/')
+    .post(`/images/?api_key=${application.api_key}`)
     .attach('file', 'test/mocks/images/camera.png')
     .expect(200)
     .end(done);
