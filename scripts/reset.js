290c5d39216e45f54e47a15c4788c788d40da0d5
Aseem Sood <asood123@yahoo.com>
2016-11-03 13:15:35 -0400

    cleaned up reset script. was erroring out

--- scripts/reset.js
+++ scripts/reset.js
@@ -12,64 +12,49 @@ if (process.env.NODE_ENV !== 'development') {
 }
 
 exec('make dropdb && make database')
-.then((result) => {
-  console.log('output', result.stdout);
-
-  return exec('SEQUELIZE_ENV=development npm run db:migrate')
-})
-.then((result) => {
-  console.log('output', result.stdout);
-
-  return models.User.create({
-      email: 'user@opencollective.com',
-      password: 'password'
-    })
-    .then((user) => data.user = user);
-  })
-  .then(() => {
-    return models.Group.create({
-      name: 'OpenCollective Demo',
-      description: 'OpenCollective Demo group',
-      isPublic: true,
-      slug: 'opencollective',
-      mission: 'demo open collective'
-    })
-    .then((group) => group.addUserWithRole(data.user, roles.HOST))
-  })
-  .then(() => {
-    return models.ConnectedAccount.create({
-      provider: 'paypal',
-      // Sandbox api keys
-      clientId: 'AZaQpRstiyI1ymEOGUXXuLUzjwm3jJzt0qrI__txWlVM29f0pTIVFk5wM9hLY98w5pKCE7Rik9QYvdYA',
-      secret: 'EILQQAMVCuCTyNDDOWTGtS7xBQmfzdMcgSVZJrCaPzRbpGjQFdd8sylTGE-8dutpcV0gJkGnfDE0PmD8'
-    })
-    .then((connectedAccount) => connectedAccount.setUser(data.user));
-  })
-  .then(() => {
-    return models.User.create({
-      email: 'stripeuser@opencollective.com',
-      password: 'password'
-    })
-    .then((user) => data.user = user);
-  })
-  .then(() => {
-    return models.Group.create({
-      name: 'OpenCollective Demo with stripe',
-      description: 'OpenCollective Demo group with stripe',
-      isPublic: true,
-      slug: 'oc-stripe',
-      mission: 'oc with stripe'
-    })
-    .then((group) => group.addUserWithRole(data.user, roles.HOST))
-  });
-})
-.then(function() {
+.tap(result => console.log("Output", result.stdout))
+.then(() => exec('SEQUELIZE_ENV=development npm run db:migrate'))
+.tap(result => console.log("Output", result.stdout))
+.then(() => models.User.create({
+  email: 'user@opencollective.com',
+  password: 'password'
+}))
+.tap(user => data.user = user)
+.then(() => models.Group.create({
+  name: 'OpenCollective Demo',
+  description: 'OpenCollective Demo group',
+  isPublic: true,
+  slug: 'opencollective',
+  mission: 'demo open collective'
+}))
+.then(group => group.addUserWithRole(data.user, roles.HOST))
+.then(() => models.ConnectedAccount.create({
+  provider: 'paypal',
+  // Sandbox api keys
+  clientId: 'AZaQpRstiyI1ymEOGUXXuLUzjwm3jJzt0qrI__txWlVM29f0pTIVFk5wM9hLY98w5pKCE7Rik9QYvdYA',
+  secret: 'EILQQAMVCuCTyNDDOWTGtS7xBQmfzdMcgSVZJrCaPzRbpGjQFdd8sylTGE-8dutpcV0gJkGnfDE0PmD8'
+}))
+.then((connectedAccount) => connectedAccount.setUser(data.user))
+.then(() => models.User.create({
+  email: 'stripeuser@opencollective.com',
+  password: 'password'
+}))
+.then((user) => data.user = user)
+.then(() => models.Group.create({
+  name: 'OpenCollective Demo with stripe',
+  description: 'OpenCollective Demo group with stripe',
+  isPublic: true,
+  slug: 'oc-stripe',
+  mission: 'oc with stripe'
+}))
+.then((group) => group.addUserWithRole(data.user, roles.HOST))
+.then(() => {
   console.log('Please login on development with `user@opencollective.com` and `password` or ');
   console.log('Please login on development with `stripeuser@opencollective.com` and `password`');
   console.log('Script successful');
   process.exit();
 })
-.fail(function(err) {
+.fail(err => {
   console.error('Script error');
   console.log('error', err);
   console.log('stack', err.stack);
