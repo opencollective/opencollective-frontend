51af2173db2ac0ad7d59af6b0dec0beb465e77c5
Xavier Damman <xdamman@gmail.com>
2016-11-17 15:35:02 +0100

    also returning the group info

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -2,7 +2,7 @@
  * Get a transaction
  */
 export const getOne = (req, res) => {
-  req.transaction.getHost()
-    .then(host => Object.assign({}, req.transaction.info, { host: host.info }))
+  Promise.all([req.transaction.getHost(), req.transaction.getGroup()])
+    .then(results => Object.assign({}, req.transaction.info, { host: results[0].info, group: results[1].info }))
     .then(transaction => res.send(transaction))
 };
