5e0ba096d0116449b0fab6096908eafed2e2bc8a
asood123 <asood123@users.noreply.github.com>
2016-12-05 11:04:59 -0600

    Update setup.js

--- test/setup.js
+++ test/setup.js
@@ -4,7 +4,7 @@ if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'development';
 }
 if (process.env.NODE_ENV === "development") {
-  console.log("Setting PG_DATABASE=opencollective_test")
+  console.log("Setting PG_DATABASE=opencollective_test");
   process.env.PG_DATABASE = "opencollective_test";
 
   dotEnv.load();
