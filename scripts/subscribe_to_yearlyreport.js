7d5d6f31d07b6781147c2fcefaa0fa0f4f775a43
Xavier Damman <xdamman@gmail.com>
2016-12-27 21:26:34 +0100

    making sure we never run da script again by mistake

--- scripts/subscribe_to_yearlyreport.js
+++ scripts/subscribe_to_yearlyreport.js
@@ -26,12 +26,18 @@ const init = () => {
 const processRow = (row) => {
   const type = 'user.yearlyreport';
   debug(`Subscribing UserId ${row.id} to ${type}`);
+
+  return;
+
+  /* Don't run this again because it would add duplicate rows
+   * For some reason, the unique key on type,userid,groupid doesn't complain if groupid is null
   return Notification.create({
     UserId: row.id,
     type
   })
   .then(notification => console.log(`> UserId ${row.id} is now subscribed to ${type}`))
   .catch(() => console.error(`UserId ${row.id} already subscribed to ${type}`));
+  */
 };
 
 init();
