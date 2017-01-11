cb61e797cbb1602bf98bde823f344b1174f0ffc9
asood123 <asood123@users.noreply.github.com>
2017-01-11 10:06:28 -0800

    Update email.js

--- server/lib/email.js
+++ server/lib/email.js
@@ -243,7 +243,7 @@ const sendMessageFromActivity = (activity, notification) => {
       return generateEmailFromTemplateAndSend('group.expense.created', notification.User.email, data);
     case activities.GROUP_EXPENSE_APPROVED:
       data.actions = {
-        view: notification.User.generateLoginLink(`/${data.group.slug}/transactions/expenses#exp${data.expense.id}`)
+        viewExpenseUrl: notification.User.generateLoginLink(`/${data.group.slug}/transactions/expenses#exp${data.expense.id}`)
       }
       return generateEmailFromTemplateAndSend('group.expense.approved.for.host', notification.User.email, data);
     default:
