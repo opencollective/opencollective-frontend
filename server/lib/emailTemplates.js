214fd27dda80c2677074bd9368f53ab56e5333e6
Aseem Sood <asood123@yahoo.com>
2017-01-22 18:08:10 -0500

    rename renewalnotice to charge_date_notice

--- server/lib/emailTemplates.js
+++ server/lib/emailTemplates.js
@@ -43,7 +43,7 @@ const subscriptions = fs.readFileSync(`${templatesPath}/partials/subscriptions.h
 const toplogo = fs.readFileSync(`${templatesPath}/partials/toplogo.hbs`, 'utf8');
 const relatedgroups = fs.readFileSync(`${templatesPath}/partials/relatedgroups.hbs`, 'utf8');
 const collectivecard = fs.readFileSync(`${templatesPath}/partials/collectivecard.hbs`, 'utf8');
-const renewalnotice = fs.readFileSync(`${templatesPath}/partials/renewalnotice.hbs`, 'utf8');
+const chargeDateNotice = fs.readFileSync(`${templatesPath}/partials/charge_date_notice.hbs`, 'utf8');
 
 handlebars.registerPartial('header', header);
 handlebars.registerPartial('footer', footer);
@@ -52,7 +52,7 @@ handlebars.registerPartial('subscriptions', subscriptions);
 handlebars.registerPartial('toplogo', toplogo);
 handlebars.registerPartial('collectivecard', collectivecard);
 handlebars.registerPartial('relatedgroups', relatedgroups);
-handlebars.registerPartial('renewalnotice', renewalnotice);
+handlebars.registerPartial('charge_date_notice', chargeDateNotice);
 
 handlebars.registerHelper('sign', (value) => {
   if (value >= 0) return '+';
