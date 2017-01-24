b8ba3dbade7540163ebcd68479d821e6e04d92df
Xavier Damman <xdamman@gmail.com>
2017-01-24 18:01:06 +0100

    getEvent to return an object. getEvents to return an array

--- server/graphql/query.js
+++ server/graphql/query.js
@@ -15,23 +15,38 @@ const queries = {
    * Given a collective slug and an event slug, returns the event
    */
   getEvent: {
-    type: new GraphQLList(EventType),
+    type: EventType,
     args: {
       eventSlug: {
         type: GraphQLString,
-        description: 'Event slug. If omitted, we return all events from that collectiveSlug'
+        description: 'Event slug'
       },
       collectiveSlug: {
         type: new GraphQLNonNull(GraphQLString)
       }
     },
     resolve(_, args) {
-      const where = {};
-      if (args.eventSlug) {
-        where.slug = args.eventSlug;
-      } 
+      return models.Event.findOne({
+        where: { slug: args.eventSlug },
+        include: [{
+          model: models.Group,
+          where: { slug: args.collectiveSlug }
+        }]
+      })
+    }
+  },
+  /*
+   * Given a collective slug, returns the events
+   */
+  getEvents: {
+    type: new GraphQLList(EventType),
+    args: {
+      collectiveSlug: {
+        type: new GraphQLNonNull(GraphQLString)
+      }
+    },
+    resolve(_, args) {
       return models.Event.findAll({
-        where,
         include: [{
           model: models.Group,
           where: { slug: args.collectiveSlug }
