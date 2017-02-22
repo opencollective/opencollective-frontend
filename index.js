203fb67ca5f29ad07b0c57c9a317c9252b3bc003
Xavier Damman <xdamman@gmail.com>
2017-02-22 18:23:53 +0100

    using process.env.PORT

--- index.js
+++ index.js
@@ -40,6 +40,6 @@ app.use('*', (req, res) => {
     return res.sendfile('./build/index.html');
 });
 
-const NODE_PORT = process.env.NODE_PORT || 4000;
-app.listen(NODE_PORT);
-console.log("Listening on port", NODE_PORT);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT);
+console.log("Listening on port", PORT);
