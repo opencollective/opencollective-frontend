f152cb446789753887fe78073b6fff64ffdacbf4
Xavier Damman <xdamman@gmail.com>
2017-02-28 13:21:33 +0100

    update text

--- scripts/git_clean.sh
+++ scripts/git_clean.sh
@@ -2,13 +2,11 @@
 if [[ `git checkout master` ]]; then 
   for BRANCH in `git branch | grep -v "*"`; do
     if [[ `git diff master..$BRANCH` ]]; then
-      echo "- $BRANCH has unmerged changes"; 
+      echo "- $BRANCH needs to be fast-forward";
       git checkout $BRANCH;
       if [[ `git merge master --ff-only` ]]; then
         git checkout master;
         git branch -d $BRANCH;
-      else
-        echo " - $BRANCH cannot be fast-forward with master";
       fi;
     else 
       git branch -D $BRANCH;
