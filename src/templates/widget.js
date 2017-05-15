f212686eb86dae9a3e5e7737c6ed3a2af75a2fcb
Xavier Damman <xdamman@gmail.com>
2017-05-15 19:03:25 -0400

    not using class to avoid having to load a polyfill

--- src/templates/widget.js
+++ src/templates/widget.js
@@ -9,29 +9,16 @@
     buttons: []
   };
 
-  class OpenCollectiveButton {
+  function OpenCollectiveButton(anchor) {
 
-    constructor(anchor) {
-      this.anchor = anchor;
-      this.styles = window.getComputedStyle(anchor.parentNode, null);
+    this.anchor = anchor;
+    this.styles = window.getComputedStyle(anchor.parentNode, null);
 
-      const attributes = this.getAttributes();
-      const color = attributes.color || 'white';
-      console.log("Attributes", attributes, color);
-      const html = `<center><iframe src="{{host}}/{{collectiveSlug}}/donate/button?color=${color}" width="300" height=50 frameborder=0></iframe></center>`;
-
-      this.el = document.createElement('div');
-      this.el.className = 'opencollective-donate-button';
-      this.el.innerHTML = html;
-
-      this.inject(this.el);
-    }
-
-    getContainerWidth() {
+    this.getContainerWidth = () => {
         return this.anchor.parentNode.getBoundingClientRect().width - parseInt(this.styles.paddingLeft, 10) - parseInt(this.styles.paddingRight, 10);
     }
 
-    getAttributes() {
+    this.getAttributes = () => {
       const attributes = {};
       [].slice.call(this.anchor.attributes).forEach((attr) => {
         attributes[attr.name] = attr.value;
@@ -39,9 +26,19 @@
       return attributes;
     }
 
-    inject(e) {
+    this.inject = (e) => {
       this.anchor.parentNode.insertBefore(e, this.anchor);
     }
+
+    const attributes = this.getAttributes();
+    const color = attributes.color || 'white';
+    const html = `<center><iframe src="{{host}}/{{collectiveSlug}}/donate/button?color=${color}" width="300" height=50 frameborder=0></iframe></center>`;
+
+    this.el = document.createElement('div');
+    this.el.className = 'opencollective-donate-button';
+    this.el.innerHTML = html;
+
+    this.inject(this.el);
 
   }
 
