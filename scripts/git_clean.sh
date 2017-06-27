01a7a67823fcfe3abf4c48b5670716ed5001f2f6
Xavier Damman <xdamman@gmail.com>
2017-06-27 10:13:47 -0400

    updated npm run git:clean (#901)

--- scripts/git_clean.sh
+++ scripts/git_clean.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 if [[ `git checkout master` ]]; then 
   for BRANCH in `git branch | grep -v "*"`; do
-    if [[ `git diff master..$BRANCH` ]]; then
-      echo "- $BRANCH needs to be fast-forward";
-      git checkout $BRANCH;
-      if [[ `git merge master --ff-only` ]]; then
+    git checkout $BRANCH > /dev/null 2>&1;
+    if [[ `git merge master --no-edit 2> /dev/null` ]]; then
+      echo "> $BRANCH merged with master";
+      if [[ `git diff master` ]]; then
+	echo "> $BRANCH is different than master";
+        git reset --hard > /dev/null;
+      else
+	echo "> Removing branch $BRANCH";
         git checkout master;
-        git branch -d $BRANCH;
+        git branch -D $BRANCH;
       fi;
     else 
-      git branch -D $BRANCH;
+      git reset --hard > /dev/null;
+      echo "Unable to merge $BRANCH with master";
     fi; 
   done;
   git checkout master;
