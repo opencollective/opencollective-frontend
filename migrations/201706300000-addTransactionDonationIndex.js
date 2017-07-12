fef2ab93f2f2b4b27f425df3b4320508c25fa8bb
Xavier Damman <xdamman@gmail.com>
2017-07-12 23:06:31 +0200

    add index to speed up computing stats for homepage

---
+++ migrations/201706300000-addTransactionDonationIndex.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface) {
+    return queryInterface.addIndex('Transactions', ['DonationId'], {
+      indexName: 'DonationId',
+      indicesType: 'INDEX'
+    });
+  },
+
+  down: function (queryInterface) {
+    return queryInterface.removeIndex('Transactions', 'DonationId');
+  }
+};
