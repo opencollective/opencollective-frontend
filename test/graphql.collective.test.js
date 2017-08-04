7a65f122c72da79b45592ea461c5ea9f4638e2f7
Xavier Damman <xdamman@gmail.com>
2017-08-04 17:32:17 -0400

    sort result for better tests

--- test/graphql.collective.test.js
+++ test/graphql.collective.test.js
@@ -103,14 +103,18 @@ describe('Query Tests', () => {
     expect(userCollective.twitterHandle).to.equal('xdamman');
     expect(userCollective.website).to.equal('http://xdamman.com');
     expect(userCollective.memberships).to.have.length(4);
-    expect(userCollective.memberships[0].role).to.equal('BACKER');
-    expect(userCollective.memberships[1].role).to.equal('ADMIN');
-    expect(userCollective.memberships[2].role).to.equal('BACKER');
-    expect(userCollective.memberships[3].role).to.equal('ADMIN');
-    expect(userCollective.memberships[0].collective.slug).to.equal('apex');
+    const memberships = userCollective.memberships;
+    memberships.sort((a, b) => {
+      `${a.slug}${a.role}` - `${b.slug}${b.role}`;
+    })
+    expect(memberships[0].role).to.equal('ADMIN');
+    expect(memberships[1].role).to.equal('ADMIN');
+    expect(memberships[2].role).to.equal('BACKER');
+    expect(memberships[3].role).to.equal('BACKER');
+    expect(memberships[3].collective.slug).to.equal('apex');
     expect(userCollective.createdByUser.firstName).to.equal('Xavier');
     expect(userCollective.createdByUser.email).to.be.null;
-    expect(userCollective.memberships[0].collective.stats).to.deep.equal({
+    expect(memberships[3].collective.stats).to.deep.equal({
       backers: 23,
       yearlyBudget: 329378
     });
