104662b26c256b52af01d7a0221cd9f6da52fcb4
Xavier Damman <xdamman@gmail.com>
2017-09-20 14:06:39 -0400

    debugging

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -110,6 +110,9 @@ class OrderForm extends React.Component {
       const stripePublishableKey = (typeof window !== "undefined" && (window.location.hostname === 'localhost' || window.location.hostname === 'staging.opencollective.com')) ? 'pk_test_5aBB887rPuzvWzbdRiSzV3QB' : 'pk_live_qZ0OnX69UlIL6pRODicRzsZy';
       // eslint-disable-next-line
       Stripe.setPublishableKey(stripePublishableKey);
+      console.log(">>> OrderForm: componentDidMount: window", window);
+      console.log(">>> OrderForm: componentDidMount: hostname", window.location.hostname);
+      console.log(">>> stripePublishableKey", stripePublishableKey);
     }
     this.componentWillReceiveProps(this.props);
   }
