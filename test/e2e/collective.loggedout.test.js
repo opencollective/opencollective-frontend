c79d822589d878a71a17fb2caa157cd86715e107
Xavier Damman <xdamman@gmail.com>
2017-10-07 01:29:39 +0200

    e2e wait more

--- test/e2e/collective.loggedout.test.js
+++ test/e2e/collective.loggedout.test.js
@@ -59,7 +59,7 @@ describe("logged out", () => {
       .wait('.SubmitExpenseBtn')
       .scrollToElement('.SubmitExpenseBtn')
       .click('.SubmitExpenseBtn')
-      .wait(1000)
+      .wait(1500)
       .screenshot({ filePath: path.join(screenshotsDirectory, 'click_submit_new_expense.png')});
 
     console.log(">>> screenshot", screenshot);
