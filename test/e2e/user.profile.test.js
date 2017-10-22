41fac1d7cb5124ac5a264049c5f49c5d342baf7d
Xavier Damman <xdamman@gmail.com>
2017-10-23 00:35:52 +0900

    added tests, css fix, and show total donated per collective on profile page

---
+++ test/e2e/user.profile.test.js
@@ -0,0 +1,34 @@
+import { Chromeless }  from 'chromeless';
+import { expect } from 'chai';
+import { download } from '../utils';
+const WEBSITE_URL = "https://staging.opencollective.com";
+// const WEBSITE_URL = "http://localhost:3030";
+
+describe("logged out", () => {
+  let chromeless;
+
+  before((done) => {
+    chromeless = new Chromeless({
+      remote: true,
+      viewport: { width: 768, height: 1024 }
+    });
+    done();
+  })
+
+  after(async () => await chromeless.end());
+
+  it("loads a profile of a user", async function() {
+    this.timeout(10000);
+    const screenshot = await chromeless
+      .goto(`${WEBSITE_URL}/addyosmani`)
+      .wait('#BACKER')
+      .scrollToElement('#BACKER')
+      .screenshot();
+
+    download("user.profile", screenshot);
+    
+    const numberOfPastEvents = await chromeless.evaluate(() => document.querySelectorAll('#BACKER .CollectiveCard').length);
+    expect(numberOfPastEvents >= 3).to.be.true;
+  });
+
+});
