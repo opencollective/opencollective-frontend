7725b258ab7bed062e9204eebadd3cb31d789f69
Aseem Sood <asood123@users.noreply.github.com>
2017-10-30 16:01:30 -0400

    add excpetion for jshint (#988)

--- newrelic.js
+++ newrelic.js
@@ -4,23 +4,21 @@
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-export default {
-  config: {
+exports.config = { // eslint-disable-line import/no-commonjs
+  /**
+   * Array of application names.
+   */
+  app_name: ['opencollective-prod-api'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '375d52f6c10eab6a34202b07fdd302ec4e10da13',
+  logging: {
     /**
-     * Array of application names.
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
      */
-    app_name: ['opencollective-prod-api'],
-    /**
-     * Your New Relic license key.
-     */
-    license_key: '375d52f6c10eab6a34202b07fdd302ec4e10da13',
-    logging: {
-      /**
-       * Level at which to log. 'trace' is most useful to New Relic when diagnosing
-       * issues with the agent, 'info' and higher will impose the least overhead on
-       * production applications.
-       */
-      level: 'info'
-    }
+    level: 'info'
   }
 }
