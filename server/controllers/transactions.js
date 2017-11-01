27e5410be6e9086a4bbe965229c6d26b0e0c16bd
Aseem Sood <asood123@users.noreply.github.com>
2017-11-01 11:59:10 -0400

    Invoice fix (#993)
    * fetch right data for invoicing
    
    * fix bug to support user->collective case

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -1,26 +1,59 @@
-import models from '../models';
+import models, { sequelize } from '../models';
 
 /**
  * Get a transaction
  */
-export const getOne = (req, res) => {
+export const getOne = (req, res, next) => {
   Promise.all([
-    models.User.findOne({
-      where: { CollectiveId: req.transaction.HostCollectiveId },
-      include: [ { model: models.Collective, as: 'collective' } ]
+    models.Collective.findOne({
+      where: { id: req.transaction.HostCollectiveId },
     }),
     req.transaction.getCollective(),
     req.transaction.getFromCollective(),
-    req.transaction.getCreatedByUser()
+    req.transaction.getCreatedByUser(),
+    
+    // TODO: we shouldn't need two paths to fetch a host
+    // User->Collective relationship needs to be stored in the Members table
+
+    // case 1: in case Host is a USER, fetch that user (needed for billing address)
+    models.User.findOne({
+      where: { CollectiveId: req.transaction.HostCollectiveId }
+    }),
+
+    // case 2: in case Host is an ORG, look up an admin with a billing address 
+    // in the Members table
+    sequelize.query(`
+    WITH admins AS 
+      (SELECT "MemberCollectiveId" FROM "Members" 
+        WHERE "CollectiveId" = :HostCollectiveId 
+        AND (role = 'ADMIN' OR role = 'HOST'))
+    
+    SELECT * FROM "Users" 
+    WHERE "billingAddress" IS NOT NULL 
+      AND "CollectiveId" IN (SELECT * FROM admins);
+
+      `, {
+        type: sequelize.QueryTypes.SELECT,
+        replacements: {
+          HostCollectiveId: req.transaction.HostCollectiveId
+        }}) // fetch all admins of the host and see if any of them have a billingAddress.
   ])
     .then(results => {
-      const host = results[0].collective.info;
+      const host = results[0].info;
       const collective = results[1].card;
       const fromCollective = results[2].card;
       const createdByUser = results[3].public;
-      host.billingAddress = results[0].billingAddress;
+      const userAdmin = results[4];
+      const hostAdmins = results[5];
       createdByUser.billingAddress = results[3].billingAddress;
+
+      if (userAdmin) {
+        host.billingAddress = userAdmin.billingAddress;
+      } else if (hostAdmins.length > 0) {
+        host.billingAddress = hostAdmins[0].billingAddress;
+      }
       return Object.assign({}, req.transaction.info, { host, fromCollective, collective, createdByUser });
     })
     .then(transaction => res.send(transaction))
+    .catch(next)
 };
