c1ddc6c5516fc9a24abde817207e8b5cfebe5158
Xavier Damman <xdamman@gmail.com>
2017-11-10 12:01:19 -0500

    make tests great again

--- test/webhooks.routes.test.js
+++ test/webhooks.routes.test.js
@@ -229,9 +229,9 @@ describe('webhooks.routes.test.js', () => {
 
     it('successfully sends out an invoice by email to donor', () => {
       expect(emailSendSpy.callCount).to.equal(2);
-      expect(emailSendSpy.thirdCall.args[0])
-      expect(emailSendSpy.thirdCall.args[2].firstPayment).to.be.false;
-      expect(emailSendSpy.thirdCall.args[1]).to.equal(user.email);
+      expect(emailSendSpy.secondCall.args[0])
+      expect(emailSendSpy.secondCall.args[2].firstPayment).to.be.false;
+      expect(emailSendSpy.secondCall.args[1]).to.equal(user.email);
     });
   });
 
