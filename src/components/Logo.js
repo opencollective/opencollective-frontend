d95e400486a8a25ac30362e852c39afb9cacffaa
Xavier Damman <xdamman@gmail.com>
2017-11-22 18:30:49 -0500

    also uses proxy for logo of organizations

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,9 @@
 import { defaultImage } from '../constants/collectives';
+import { imagePreview } from '../lib/utils';
 
 export default ({ src, style = {}, height }) => {
   style.height = style.height || height;
-  const image = src || defaultImage.ORGANIZATION;
+  const image = imagePreview(src, defaultImage.ORGANIZATION);
   return (
     <img className="logo" src={image} style={style} />
   );
