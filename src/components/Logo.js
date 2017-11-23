f8220992c8ad7d9b7372fab6696ae7171b0f7092
Xavier Damman <xdamman@gmail.com>
2017-11-23 12:43:56 +0100

    Fix for resize logo with safari

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,12 @@ import { imagePreview } from '../lib/utils';
 
 export default ({ src, style = {}, height }) => {
   style.height = style.height || height;
-  const image = imagePreview(src, defaultImage.ORGANIZATION, { height });
+  const resizeOptions = {};
+  resizeOptions.height = style.height;
+  if (typeof resizeOptions.height === 'string') {
+    resizeOptions.height = Number(resizeOptions.height.replace(/rem/,'')) * 10;
+  }
+  const image = imagePreview(src, defaultImage.ORGANIZATION, resizeOptions);
   return (
     <img className="logo" src={image} style={style} />
   );
