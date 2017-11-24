4c29a6c112b80feffffc3813cc61a65db53141ef
Aseem Sood <asood123@users.noreply.github.com>
2017-11-24 15:31:51 -0500

    add transactions for paid expenses (#1025)

---
+++ scripts/add_transactions_for_paid_expenses.js
@@ -0,0 +1,60 @@
+/*
+ * This script finds all expenses that are paid and don't have a transaction
+ */
+
+import models, { sequelize } from '../server/models';
+import { createFromPaidExpense as createTransactionFromPaidExpense } from '../server/lib/transactions';
+
+const done = (err) => {
+  if (err) console.log('err', err);
+  console.log('done!');
+  process.exit();
+}
+
+const expensesFixed = [];
+
+function run() {
+  /*
+    Find all expenses that are paid, with payout method 'manual' or 'other'
+    and don't have an entry in the Transactions table.
+   */
+  return sequelize.query(`
+    SELECT 
+      e.id AS id
+    FROM "Expenses" e
+    LEFT JOIN "Transactions" t ON t."ExpenseId" = e.id
+    WHERE 
+      e.status ILIKE 'paid' 
+      AND t.id IS NULL 
+      AND e."deletedAt" IS NULL 
+      AND (e."payoutMethod" ILIKE 'other' OR e."payoutMethod" ILIKE 'manual')
+    ORDER BY "ExpenseId" DESC, e."updatedAt"
+    `, { type: sequelize.QueryTypes.SELECT})
+  .then(expenses => {
+    console.log("Expenses found", expenses.length)
+    return expenses
+  })
+  .each(expenseId => {
+    let expense;
+    return models.Expense.findOne({
+      where: {
+        id: expenseId.id
+      },
+      include: [{
+        model: models.Collective,
+        as: 'collective'
+      }]
+    })
+    .then(e => {
+      expense = e;
+      return e.collective.getHostCollective() 
+    })
+    .then(host => createTransactionFromPaidExpense(host, null, expense, null, null, expense.UserId))
+    .then(() => expensesFixed.push(expenseId))
+  })
+  .then(() => console.log("Expenses fixed: ", expensesFixed.length))
+  .then(() => done())
+  .catch(done)
+}
+
+run();
