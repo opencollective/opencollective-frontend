4612f3d32e9702efc6ab3251845b053a3b35159e
Xavier Damman <xdamman@gmail.com>
2017-11-27 11:34:06 +0100

    Merged User/Host Collective back to allow users to host and contribute to collectives

--- src/components/UserCollective.js
+++ src/components/UserCollective.js
@@ -8,6 +8,7 @@ import CollectiveCover from '../components/CollectiveCover';
 import Tier from '../components/Tier';
 import NotificationBar from '../components/NotificationBar';
 import Memberships from '../components/Memberships';
+import CollectivesWithData from '../components/CollectivesWithData';
 import Markdown from 'react-markdown';
 import { defineMessages, FormattedMessage } from 'react-intl';
 import { get, groupBy } from 'lodash';
@@ -44,6 +45,7 @@ class UserCollective extends React.Component {
       'user.collective.memberOf.member.title': { id: 'user.collective.memberOf.member.title', defaultMessage: `I'm a member of {n, plural, one {this collective} other {these collectives}}`},
       'user.collective.memberOf.backer.title': { id: 'user.collective.memberOf.backer.title', defaultMessage: `I'm backing {n, plural, one {this collective} other {these collectives}}`},
       'user.collective.memberOf.follower.title': { id: 'user.collective.memberOf.follower.title', defaultMessage: `I'm following {n, plural, one {this collective} other {these collectives}}`},
+      'organization.collective.memberOf.host.title': { id: 'organization.collective.memberOf.host.title', defaultMessage: `We are hosting {n, plural, one {this collective} other {{n} collectives}}`},
       'organization.collective.memberOf.admin.title': { id: 'organization.collective.memberOf.admin.title', defaultMessage: `We are a core contributor of {n, plural, one {this collective} other {these collectives}}`},
       'organization.collective.memberOf.member.title': { id: 'organization.collective.memberOf.member.title', defaultMessage: `We are a member of {n, plural, one {this collective} other {these collectives}}`},
       'organization.collective.memberOf.backer.title': { id: 'organization.collective.memberOf.backer.title', defaultMessage: `We are backing {n, plural, one {this collective} other {these collectives}}`},
@@ -157,7 +159,7 @@ class UserCollective extends React.Component {
               />
 
             <MenuBar
-              info={intl.formatMessage(this.messages[`${this.collective.isHost ? 'host' : type}.collective.since`], { year: (new Date(this.collective.createdAt)).getFullYear() })}
+              info={intl.formatMessage(this.messages[`${type}.collective.since`], { year: (new Date(this.collective.createdAt)).getFullYear() })}
               actions={actions}
               />
 
@@ -216,21 +218,37 @@ class UserCollective extends React.Component {
                   )}
                 </div>
               </div>
-              { Object.keys(memberOf).map(role => (
-                <section id={role}>
-                    <h1>{intl.formatMessage(this.messages[`${type}.collective.memberOf.${role.toLowerCase()}.title`], { n: memberOf[role].length })}</h1>
-                    { role === 'HOST' && LoggedInUser && LoggedInUser.canEditCollective(this.collective) &&
-                      <div className="adminActions" id="adminActions">
-                        <ul>
-                          <li><Link><a href={`/${this.collective.slug}/collectives/expenses`}><FormattedMessage id="host.collectives.manage" defaultMessage="Manage expenses" /></a></Link></li>
-                        </ul>
-                      </div>
-                      }
-                    }
-                    <Memberships
-                      className={role}
-                      memberships={memberOf[role]}
+              { this.collective.stats.collectives > 0 &&
+                <section id="hosting">
+                  <h1>
+                    {intl.formatMessage(this.messages[`${type}.collective.memberOf.host.title`], { n: this.collective.stats.collectives })}
+                  </h1>
+                  { LoggedInUser && LoggedInUser.canEditCollective(this.collective) &&
+                    <div className="adminActions" id="adminActions">
+                      <ul>
+                        <li><Link><a href={`/${this.collective.slug}/collectives/expenses`}><FormattedMessage id="host.collectives.manage" defaultMessage="Manage expenses" /></a></Link></li>
+                      </ul>
+                    </div>
+                  }
+
+                  <div className="cardsList">
+                    <CollectivesWithData
+                      HostCollectiveId={this.collective.id}
+                      orderBy="balance"
+                      orderDirection="DESC"
+                      limit={20}
                       />
+                  </div>
+                </section>
+              }
+              { Object.keys(memberOf).map(role => role !== 'HOST' && (
+                <section id={role}>
+                  <h1>{intl.formatMessage(this.messages[`${type}.collective.memberOf.${role.toLowerCase()}.title`], { n: memberOf[role].length })}</h1>
+                  }
+                  <Memberships
+                    className={role}
+                    memberships={memberOf[role]}
+                    />
                 </section>
               ))}
 
