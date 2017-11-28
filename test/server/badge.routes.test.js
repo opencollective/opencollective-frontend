0b754119ebac2d9c31b8e195f2e992ea687e8900
Xavier Damman <xdamman@gmail.com>
2017-11-28 10:30:14 +0100

    fix server tests

--- test/server/badge.routes.test.js
+++ test/server/badge.routes.test.js
@@ -1,4 +1,5 @@
 import r2 from 'r2';
+import fetch from 'node-fetch';
 
 const WEBSITE_URL = process.env.WEBSITE_URL || "https://staging.opencollective.com";
 
@@ -59,11 +60,12 @@ describe("badge.routes.test.js", () => {
 
   describe("contributors.svg", () => {
     test("loads the mosaic", async () => {
-      const res = await r2(`${WEBSITE_URL}/tipbox/contributors.svg?width=500`).response;
+      const res = await fetch(`${WEBSITE_URL}/tipbox/contributors.svg?width=500&43`);
       expect(res.status).toEqual(200);
-      expect(Number(res.headers.get('content-length'))).toBeGreaterThan(97000);
       expect(res.headers.get('content-type')).toEqual('image/svg+xml; charset=utf-8');
-      expect(res.headers.get('cache-control')).toEqual('public, max-age=300');
+      expect(res.headers.get('cache-control')).toMatch(/public, max-age=[1-9][0-9]{2,5}/);
+      const text = await res.text();
+      expect(text.length).toBeGreaterThan(97000);
     });
   })
 
