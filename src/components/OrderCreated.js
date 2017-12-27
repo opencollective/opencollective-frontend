02f798a9c70e3e30c1805bce62a34795b863d65f
Xavier Damman <xdamman@gmail.com>
2017-12-27 12:37:03 +0100

    don't crash if collective not provided

--- src/components/OrderCreated.js
+++ src/components/OrderCreated.js
@@ -21,13 +21,15 @@ class OrderCreated extends React.Component {
     this.messages = defineMessages({
       tweet: { id: 'order.created.tweet', defaultMessage: `I've just donated {amount} to {collective}. Consider donating too, every little helps!` }
     })
-    const tweetText = intl.formatMessage(this.messages.tweet, {
-      collective: collective.twitterHandle ? `@${collective.twitterHandle}` : collective.name,
-      amount: formatCurrency(totalAmount, collective.currency, { precision: 0 })
-    });
-    const url = `https://opencollective.com/${collective.slug}?referral=${fromCollective.id}`;
-    this.tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}&url=${encodeURIComponent(url)}`;
-    this.fbUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+    if (collective) {
+      const tweetText = intl.formatMessage(this.messages.tweet, {
+        collective: collective.twitterHandle ? `@${collective.twitterHandle}` : collective.name,
+        amount: formatCurrency(totalAmount, collective.currency, { precision: 0 })
+      });
+      const url = `https://opencollective.com/${collective.slug}?referral=${fromCollective.id}`;
+      this.tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}&url=${encodeURIComponent(url)}`;
+      this.fbUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+    }
   }
 
   open(url, w = 500, h = 300) {
@@ -106,40 +108,46 @@ class OrderCreated extends React.Component {
         `}
         </style>
 
-        <CollectiveCard collective={collective} membership={membership} />
+        { collective &&
+          <CollectiveCard collective={collective} membership={membership} />
+        }
 
         <div className="message">
           <p className="thankyou">
             <FormattedMessage id="collective.user.orderCreated.thankyou" defaultMessage="Thank you for your donation! 🙏" />
           </p>
-          <p>
-            { status === 'orderCreated' && collective &&
-              <FormattedMessage id="collective.user.orderCreated.message" defaultMessage="We have added {collective} to your profile" values={{ collective: collective.name }} />
-            }
-            { status === 'orderProcessing' && collective &&
-              <FormattedMessage id="collective.user.orderProcessing.message" defaultMessage="We are currently processing your donation to {collective}. We will add it to your profile and we will send you a confirmation email once the payment is confirmed." values={{ collective: collective.name }} />
-            }
-          </p>
-          <h2>
-            <FormattedMessage id="collective.user.orderCreated.helpUsRaise.title" defaultMessage="Help us raise more money!" />
-          </h2>
-          <p>
-            <FormattedMessage id="collective.user.orderCreated.helpUsRaise.shareUrl" defaultMessage="Share this URL:" />
+          { collective &&
             <div>
-              <a href={`https://opencollective.com/${collective.slug}/donate?referral=${fromCollective.id}`}>{`https://opencollective.com/${collective.slug}/donate?referral=${fromCollective.id}`}</a>
+              <p>
+                { status === 'orderCreated' && collective &&
+                  <FormattedMessage id="collective.user.orderCreated.message" defaultMessage="We have added {collective} to your profile" values={{ collective: collective.name }} />
+                }
+                { status === 'orderProcessing' && collective &&
+                  <FormattedMessage id="collective.user.orderProcessing.message" defaultMessage="We are currently processing your donation to {collective}. We will add it to your profile and we will send you a confirmation email once the payment is confirmed." values={{ collective: collective.name }} />
+                }
+              </p>
+              <h2>
+                <FormattedMessage id="collective.user.orderCreated.helpUsRaise.title" defaultMessage="Help us raise more money!" />
+              </h2>
+              <p>
+                <FormattedMessage id="collective.user.orderCreated.helpUsRaise.shareUrl" defaultMessage="Share this URL:" />
+                <div>
+                  <a href={`https://opencollective.com/${collective.slug}/donate?referral=${fromCollective.id}`}>{`https://opencollective.com/${collective.slug}/donate?referral=${fromCollective.id}`}</a>
+                </div>
+                <FormattedMessage id="collective.user.orderCreated.helpUsRaise.description" defaultMessage="The total amount that you will help us raise will be shown on your profile." />
+              </p>
+              <p>
+                <Button onClick={() => this.open(this.tweetUrl)}>
+                  <TwitterLogo />
+                  <FormattedMessage id="collective.user.orderCreated.helpUsRaise.tweetUrl" defaultMessage="Share on Twitter" />
+                </Button>
+                <Button onClick={() => this.open(this.fbUrl, 550, 700)}>
+                  <FacebookLogo />
+                  <FormattedMessage id="collective.user.orderCreated.helpUsRaise.fbUrl" defaultMessage="Share on Facebook" />
+                </Button>
+              </p>
             </div>
-            <FormattedMessage id="collective.user.orderCreated.helpUsRaise.description" defaultMessage="The total amount that you will help us raise will be shown on your profile." />
-          </p>
-          <p>
-            <Button onClick={() => this.open(this.tweetUrl)}>
-              <TwitterLogo />
-              <FormattedMessage id="collective.user.orderCreated.helpUsRaise.tweetUrl" defaultMessage="Share on Twitter" />
-            </Button>
-            <Button onClick={() => this.open(this.fbUrl, 550, 700)}>
-              <FacebookLogo />
-              <FormattedMessage id="collective.user.orderCreated.helpUsRaise.fbUrl" defaultMessage="Share on Facebook" />
-            </Button>
-          </p>
+          }
         </div>
       </div>
     );
