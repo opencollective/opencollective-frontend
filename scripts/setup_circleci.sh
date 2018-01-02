9cdfd65aff5154460f83cb9a25f154191304755d
Xavier Damman <xdamman@gmail.com>
2018-01-03 00:42:14 +0100

    more test

--- scripts/setup_circleci.sh
+++ scripts/setup_circleci.sh
@@ -45,15 +45,17 @@ fi
 
 cd "opencollective-api-${BRANCH}"
 echo "> Restoring opencollective_dvl database for e2e testing";
-./scripts/db_restore -d opencollective_dvl -f test/dbdumps/opencollective_dvl.pgsql
+./scripts/db_restore.sh -d opencollective_dvl -f test/dbdumps/opencollective_dvl.pgsql
 
 echo "> Starting api"
 PG_DATABASE=opencollective_dvl npm start &
 
 sleep 5
+curl http://localhost:3060/status
+
 if curl -s --head  --request GET http://localhost:3060/status | grep "200" > /dev/null
 then
   echo "✓ API is running";
 else
-  echo "𐄂 API is not responding";
+  echo "✗ API is not responding";
 fi
