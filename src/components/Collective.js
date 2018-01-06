c5d1f47253d3ff411c97570516debdeec8c4f601
Xavier Damman <xdamman@gmail.com>
2018-01-06 11:35:11 +0100

    Fix matching fund

--- src/components/Collective.js
+++ src/components/Collective.js
@@ -109,7 +109,10 @@ class Collective extends React.Component {
 
   render() {
     const { intl, LoggedInUser, query: { referral } } = this.props;
-
+    const donateParams = { collectiveSlug: this.collective.slug, verb: 'donate' };
+    if (referral) {
+      donateParams.referral = referral;
+    }
     const backersHash = this.collective.stats.backers.organizations > 0 ? '#organizations' : '#backers';
     const actions = [
       {
@@ -142,7 +145,7 @@ class Collective extends React.Component {
       },
       {
         className: 'blue',
-        component: <Link route={'donate'} params={{ collectiveSlug: this.collective.slug, verb: 'donate', referral }}>
+        component: <Link route={'donate'} params={donateParams}>
             <a><b>{intl.formatMessage(this.messages['collective.donate']).toUpperCase()}</b></a>
           </Link>
       }
