4c0976c15cf5a2c5ec0aaaa30a15ae778f17cd37
Xavier Damman <xdamman@gmail.com>
2018-01-09 11:45:11 -0500

    fix tests

--- server/lib/activities.js
+++ server/lib/activities.js
@@ -304,9 +304,8 @@ const linkify = (format, link, text) => {
  * Generates a userString given a user's info
  */
 const getUserString = (format, userCollective, email) => {
-  if (!userCollective) return 'someone';
-
-  const userString = userCollective.name || userCollective.twitterHandle;
+  userCollective = userCollective || {};
+  const userString = userCollective.name || userCollective.twitterHandle || 'someone';
   const link = userCollective.twitterHandle ? `https://twitter.com/${userCollective.twitterHandle}` : userCollective.website;
 
   let returnVal;
