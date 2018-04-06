6bdf94862975ae3acf0a9fcc04732751ab39f481
Lincoln de Sousa <lincoln@comum.org>
2018-04-06 00:40:16 -0400

    Fix customizing db args on db:migrate:dev & db:migrate:dev:undo
    The argument -l (local) was added to the sequelize.sh call in
    package.json for the commands db:migrate:dev, db:migrate:dev:undo,
    db:migration:create and db:seed (not sure if it's working either way).
    
    The command db:clean (runs on postdb:migrate:dev) needed the variables
    to be forwarded in their declaration in the file package.json.

--- scripts/sequelize.sh
+++ scripts/sequelize.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 # This script wraps the sequelize command with babel-node and passes
 # parameters required in every single call.
+
+# If the first parameter is `-l` (local), it will append the
+# configuration with variables that can be passed from the shell. It
+# defaults to the local environment though (hardcoded values).
+[ "$1" = "-l" ] && { LOCAL=1; shift; }
+
+# Important paths
 ROOT="$( dirname "$(readlink "$0/..")" )"
 NODEBIN=${ROOT}/node_modules/.bin
 PATH=${PATH}:$NODEBIN
+
+# Environment
 SQLENV=${SEQUELIZE_ENV:=${NODE_ENV:=development}}
+
+# Parameters & Command
 SEQUELIZE_CONFIG="--models-path server/models/ --config config/sequelize_cli.json --env ${SQLENV}"
-COMMAND="babel-node $NODEBIN/sequelize $@ ${SEQUELIZE_CONFIG}"
+LOCAL_DB_URL="--url postgres://${PG_USERNAME:=opencollective}@${PG_HOST:=localhost}:${PG_PORT:=5432}/${PG_DATABASE:=opencollective_dvl}"
+[ -n "$LOCAL" ] && SEQUELIZE_CONFIG="${SEQUELIZE_CONFIG} ${LOCAL_DB_URL}"
+COMMAND="babel-node $NODEBIN/sequelize ${SEQUELIZE_CONFIG} $@"
+
+# Variables exported for the exec
+export CUSTOM_DB PATH
 cd ${ROOT}
 echo ${COMMAND}
 exec ${COMMAND}
