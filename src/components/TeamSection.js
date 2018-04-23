5327439fc23d585fd2a23731925f5cfc7cfad385
Xavier Damman <xdamman@gmail.com>
2018-04-23 18:01:52 -0400

    Add team section

---
+++ src/components/TeamSection.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withIntl from '../lib/withIntl';
+import { FormattedMessage } from 'react-intl'
+import SectionTitle from './SectionTitle';
+import Member from './Member';
+
+class TeamSection extends React.Component {
+
+  static propTypes = {
+    collective: PropTypes.object.isRequired, // collective.id
+    LoggedInUser: PropTypes.object
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { collective, LoggedInUser } = this.props;
+
+    let action;
+    if (LoggedInUser && LoggedInUser.canEditCollective(collective)) {
+      action = {
+        href: `/${collective.slug}/edit#members`,
+        label: <FormattedMessage id="sections.team.edit" defaultMessage="Edit team members" />
+      }
+    }
+
+    const members = collective.members.filter(m => m.role === 'ADMIN' || m.role === 'MEMBER');
+
+    return (
+      <section id="team">
+        <div className="content" >
+          <SectionTitle section="team" />
+          <div className="Members cardsList">
+            {members.map((member) =>
+              (<Member
+                key={member.id}
+                member={member}
+                collective={collective}
+                LoggedInUser={LoggedInUser}
+              />)
+            )}
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+}
+
+export default withIntl(TeamSection);
