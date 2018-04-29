e62023e879584c6a9938718d4342ef06c751cbe1
François Hodierne <francois@hodierne.net>
2018-04-29 22:04:23 +0200

    remove unused CustomMenu

--- src/components/CustomMenu.js
+++ src/components/CustomMenu.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export class CustomToggle extends React.Component {
   constructor(props, context) {
@@ -18,45 +17,6 @@ export class CustomToggle extends React.Component {
       <a href="" onClick={this.handleClick}>
         {this.props.children}
       </a>
-    );
-  }
-}
-
-export class CustomMenu extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.handleChange = this.handleChange.bind(this);
-
-    this.state = {
-      value: ''
-    };
-  }
-
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
-
-  focusNext() {
-    const input = ReactDOM.findDOMNode(this.input);
-
-    if (input) {
-      input.focus();
-    }
-  }
-
-  render() {
-    const { children } = this.props;
-    const { value } = this.state;
-
-    return (
-      <div className="dropdown-menu" style={{ padding: '5px', width: '140px', 'fontSize': '11px', 'margin-right': '100px'}}>
-        <ul className="list-unstyled">
-          {React.Children.toArray(children).filter(
-            child => !value.trim() || child.props.children.indexOf(value) !== -1
-          )}
-        </ul>
-      </div>
     );
   }
 }
