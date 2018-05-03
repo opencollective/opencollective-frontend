0188e82e70d3f07f4f84e502f82783b9147772e6
Lincoln de Sousa <lincoln@comum.org>
2018-05-03 15:43:15 -0400

    Increase validity of session token from 1 to 3 months

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -11,7 +11,7 @@ const daysToSeconds = (days) => moment.duration({ days }).asSeconds();
 /* Constants that determin token expiration */
 export const TOKEN_EXPIRATION_LOGIN = daysToSeconds(1);
 export const TOKEN_EXPIRATION_CONNECTED_ACCOUNT = daysToSeconds(1);
-export const TOKEN_EXPIRATION_SESSION = daysToSeconds(30);
+export const TOKEN_EXPIRATION_SESSION = daysToSeconds(90);
 
 /** Generate a JWToken with the received parameters */
 export function createJwt(subject, payload, expiresIn) {
