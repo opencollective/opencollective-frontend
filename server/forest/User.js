9e0a9e9f4d742b9573f9b4c4662954143b0c3cad
François Hodierne <francois@hodierne.net>
2018-05-04 16:21:32 +0200

    feat(forest): ability to delete a user, simple case

--- server/forest/User.js
+++ server/forest/User.js
@@ -7,5 +7,16 @@ Liana.collection('User', {
     get: object => {
       return `${object.firstName || ''} ${object.lastName || ''}`.trim();
     }
-  }]
+  }],
+  actions: [
+    {
+      name: 'Delete user and dependencies',
+      fields: [{
+        field: 'Confirmation',
+        type: 'Boolean',
+        description: 'You are about to delete this user and all its dependencies (Memberships, Unpaid Expenses, Payment Methods, User Collective). Check this box to proceed.',
+        isRequired: true
+      }]
+    }
+  ]
 });
