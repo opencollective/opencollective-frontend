d4f667b7ca259fb1d25f80e8934372d148cc1da7
Xavier Damman <xdamman@gmail.com>
2018-05-10 12:21:11 +0200

    converted old jest e2e test to cypress

--- scripts/run_e2e_tests.sh
+++ scripts/run_e2e_tests.sh
@@ -13,6 +13,7 @@ fi
 echo ""
 echo "> Starting server jest tests"
 jest test/server/*
+RETURN_CODE=$?
 echo ""
 echo "> Running cypress tests"
 cypress run --record
@@ -21,9 +22,6 @@ if [ $? -ne 0 ]; then
   exit 1;
 fi
 echo ""
-echo "> Starting e2e jest tests"
-jest test/e2e/* -w 1
-RETURN_CODE=$?
 
 if [ "$NODE_ENV" = "circleci" ]; then
   echo "Killing all node processes"
