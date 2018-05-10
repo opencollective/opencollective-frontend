4d784aa4b59dcd1e27ed43f24370ef0fdeea1e97
HipsterBrown <headhipster@hipsterbrown.com>
2018-05-10 15:34:45 -0400

    test(setup): add chai-jest-snapshot setup for mocha

--- test/setup.js
+++ test/setup.js
@@ -1,3 +1,15 @@
-if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = 'test';
-}
+import chai from 'chai';
+import chaiJestSnapshot from 'chai-jest-snapshot';
+
+chai.use(chaiJestSnapshot);
+
+before(() => {
+  if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'test';
+  }
+  chaiJestSnapshot.resetSnapshotRegistry();
+});
+
+beforeEach(function() {
+  chaiJestSnapshot.configureUsingMochaContext(this);
+});
