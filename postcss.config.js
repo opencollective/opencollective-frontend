da86bf8f484b53713c08b4aed256dfcb47df2c2f
François Hodierne <francois@hodierne.net>
2018-05-14 10:58:10 +0200

    build(postcss): remove unused postcss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: [
-    require('postcss-easy-import')({prefix: '_'}), // keep this first
-    require('autoprefixer')({ /* ...options */ }) // so imports are auto-prefixed too
+    require('postcss-nested'),
   ]
 }
