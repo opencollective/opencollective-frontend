60bfc69b3493de9f029737fc06e53c9376dac188
François Hodierne <francois@hodierne.net>
2018-05-14 09:44:12 +0200

    build(webpack): clean webpack configuration

--- src/next.config.js
+++ src/next.config.js
@@ -3,60 +3,28 @@ import glob from 'glob';
 import webpack from 'webpack';
 
 module.exports = {
-    onDemandEntries: {
-      // Make sure entries are not getting disposed.
-      maxInactiveAge: 1000 * 60 * 60
-    },
-    webpack: (config) => {
+  onDemandEntries: {
+    // Make sure entries are not getting disposed.
+    maxInactiveAge: 1000 * 60 * 60
+  },
+  webpack: (config) => {
     config.plugins.push(
-      new webpack.IgnorePlugin(/react\/addons/),
-      new webpack.IgnorePlugin(/react\/lib\/ExecutionEnvironment/),
-      new webpack.IgnorePlugin(/react\/lib\/ReactContext/),
+      // Ignore __tests__
+      new webpack.IgnorePlugin(/\/__tests__\//),
+      // Only include our supported locales
       new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en|fr|es|ja/),
     );
     config.module.rules.push(
       {
-        test: /\.(css|scss|md)/,
-        loader: 'emit-file-loader',
-        options: {
-          name: 'dist/[path][name].[ext]'
-        }
-      }
-    ,
-      {
         test: /\.css$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader']
+        use: ['babel-loader', 'raw-loader'],
       }
-    ,
+      ,
       {
         test: /\.md$/,
-        use: ['babel-loader', 'raw-loader',
-          { loader: 'markdown-loader',
-            options: {
-              includePaths: ['markdown', 'node_modules']
-                .map((d) => path.join(__dirname, d))
-                .map((g) => glob.sync(g))
-                .reduce((a, c) => a.concat(c), [])
-            }
-          }
-        ]
-
-      }
-    ,
-      {
-        test: /\.s(a|c)ss$/,
-        use: ['babel-loader', 'raw-loader', 'postcss-loader',
-          { loader: 'sass-loader',
-            options: {
-              includePaths: ['styles', 'node_modules']
-                .map((d) => path.join(__dirname, d))
-                .map((g) => glob.sync(g))
-                .reduce((a, c) => a.concat(c), [])
-            }
-          }
-        ]
+        use: ['babel-loader', 'raw-loader', 'markdown-loader'],
       }
     )
     return config
   }
-}
+};
