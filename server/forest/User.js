321bb8a47db59ee8ef818fed895f924618a33350
François Hodierne <francois@hodierne.net>
2018-05-15 11:23:17 +0200

    feat(forest): delete user and merge

--- server/forest/User.js
+++ server/forest/User.js
@@ -17,6 +17,21 @@ Liana.collection('User', {
         description: 'You are about to delete this user and all its dependencies (Memberships, Unpaid Expenses, Payment Methods, User Collective). Check this box to proceed.',
         isRequired: true
       }]
+    },
+    {
+      name: 'Delete user and merge',
+      fields: [{
+        field: 'User ID',
+        type: 'Number',
+        description: 'After deleting, merge data (Memberships, Unpaid Expenses, Payment Methods) into this user.',
+        isRequired: true
+      },
+      {
+        field: 'Confirmation',
+        type: 'Boolean',
+        description: 'You are about to delete this user and all its dependencies. Check this box to proceed.',
+        isRequired: true
+      }]
     }
   ]
 });
