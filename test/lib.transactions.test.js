838ef0b67962c2e1a7ec3dd29ecf0faa15e5ba04
Lincoln de Sousa <lincoln@comum.org>
2018-05-16 17:20:24 -0400

    chore(tests): remove binary dump dep from lib.transactions test
    Related to https://github.com/opencollective/opencollective/issues/1048

--- test/lib.transactions.test.js
+++ test/lib.transactions.test.js
@@ -1,32 +1,37 @@
+/* Test utilities */
 import { expect } from 'chai';
-import models, { Op } from '../server/models';
-import * as utils from '../test/utils';
+import * as utils from './utils';
+import * as store from './features/support/stores';
 
-import * as tlib from '../server/lib/transactions';
+/* Support code */
+import models from '../server/models';
+
+/* Code being tested */
+import * as libtransactions from '../server/lib/transactions';
 
 describe('lib.transactions.test.js', () => {
 
-  const startDate = new Date("2017-02-01");
-  const endDate = new Date("2017-03-01");
-  let transactions;
+  beforeEach(utils.resetTestDB);
 
-  const where = {
-    createdAt: { [Op.gte]: startDate, [Op.lt]: endDate}
-  };
-
-  before(() => utils.loadDB("wwcode_test"));
-
-  beforeEach('get transactions', () => models.Transaction.findAll({ where }).then(ts => {
-    transactions = ts;
-    expect(transactions.length).to.equal(40);
-  }));
-
-  it('exports transactions', (done) => {
-    const csv = tlib.exportTransactions(transactions);
+  it('exports transactions', async () => {
+    // Given a host with a collective
+    const currency = 'USD';
+    const { collective } = await store.newCollectiveWithHost('apex', currency, currency, 10);
+    const { userCollective } = await store.newUser('a new user');
+    // And given some transactions
+    await store.stripeConnectedAccount(collective.HostCollectiveId);
+    await store.stripeOneTimeDonation({ userCollective, collective, currency, amount: 100 });
+    await store.stripeOneTimeDonation({ userCollective, collective, currency, amount: 200 });
+    await store.stripeOneTimeDonation({ userCollective, collective, currency, amount: 300 });
+    await store.stripeOneTimeDonation({ userCollective, collective, currency, amount: 400 });
+    await store.stripeOneTimeDonation({ userCollective, collective, currency, amount: 500 });
+    const transactions = await models.Transaction.findAll({ where: { CollectiveId: collective.id } });
+    expect(transactions.length).to.equal(5);
+    // When the newly created transactions are exported
+    const csv = libtransactions.exportTransactions(transactions);
     const lines = csv.split('\n');
-    expect(lines.length).to.equal(41);
+    expect(lines.length).to.equal(6);
     expect(lines[0].split('","').length).to.equal(12);
-    done();
-  });
+  }); /* End of "exports transactions" */
 
-});
+}); /* End of "lib.transactions.test.js" */
