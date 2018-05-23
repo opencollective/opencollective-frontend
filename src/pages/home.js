42e2d15c6b076a25f8bf35b42d24136784b8ed01
HipsterBrown <headhipster@hipsterbrown.com>
2018-05-23 12:31:22 -0400

    feat(pages/homes): display transactions using TransactionSimple component

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,30 +4,51 @@ import gql from 'graphql-tag'
 
 import withData from '../lib/withData'
 import withIntl from '../lib/withIntl';
+import { transactionFields } from '../graphql/queries';
 
 import Body from '../components/Body';
 import Button from '../components/Button';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import TransactionSimple from '../components/TransactionSimple';
 import { Link } from '../server/pages';
 
 class HomePage extends React.Component {
   render() {
+    const {
+      transactions: {
+        transactions,
+      },
+      loading,
+    } = this.props.data;
+
+    if (loading) {
+      return <p>loading...</p>;
+    }
+
     return (
       <Fragment>
         <style jsx>{`
           .ai-center { align-items: center; }
           .f-subheadline { font-size: 5rem; }
           .flex { display: flex; }
+          .pa3 { padding: 1rem; }
           .text-center { text-align: center; }
           .text-left { text-align: left; }
           .w-50 { width: 50%; }
+          .list { list-style: none; }
+          .item {
+            border: 1px solid rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+            box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.2);
+            margin-bottom: 0.3625rem;
+          }
         `}</style>
         <Header
           title="Home"
         />
         <Body>
-          <div className="flex">
+          <div className="flex pa3">
             <div className="w-50">
               <h1 className="f-subheadline text-left">A new form of association, <br /> <strong>transparent by design.</strong></h1>
 
@@ -47,6 +68,9 @@ class HomePage extends React.Component {
 
             <div className="w-50">
               <p className="text-center">Latest Transactions</p>
+              <ul className="list" style={{ maxHeight: '50rem', overflow: 'scroll' }}>
+                {transactions.map((transaction) => <li key={transaction.id} className="pa3 item"><TransactionSimple {...transaction} /></li>)}
+              </ul>
             </div>
           </div>
 
@@ -59,11 +83,28 @@ class HomePage extends React.Component {
 
 const query = gql`
   query home {
-    allTransactions {
+    transactions {
       transactions {
         amount
+        createdAt
         currency
+        id
         type
+        fromCollective {
+          id
+          image
+          name
+          slug
+        }
+        host {
+          name
+          slug
+        }
+        ... on Order {
+          subscription {
+            interval
+          }
+        }
       }
     }
   }
@@ -72,4 +113,4 @@ const query = gql`
 const addHomeData = graphql(query)
 
 export { HomePage as MockHomePage };
-export default withData(addHomeData(HomePage));
+export default withData(addHomeData(withIntl(HomePage)));
