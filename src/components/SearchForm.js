6c20f74023ec3bae790bc7f4b6db04702100ecf6
HipsterBrown <headhipster@hipsterbrown.com>
2018-06-04 18:18:30 -0400

    feat(components/SearchForm): create styled search form component

---
+++ src/components/SearchForm.js
@@ -0,0 +1,48 @@
+import { Box, Flex } from 'grid-styled';
+import SearchIcon from './SearchIcon';
+
+const SearchInputContainer = Flex.extend`
+  border: solid 1px var(--silver-four);
+  border-radius: 20px;
+`;
+
+const SearchInput = Box.extend`
+  && {
+    background-color: transparent;
+    border: none;
+    font-size: 1.2rem;
+    letter-spacing: 0.1rem;
+  }
+`;
+
+const SearchButton = Flex.extend`
+  && {
+    appearance: none;
+    background-color: transparent;
+    border: none;
+  }
+`;
+
+const SearchForm = () => (
+  <form action="/search" method="GET">
+    <SearchInputContainer
+      alignItems="center"
+      justifyContent="space-between"
+      p={1}
+    >
+      <SearchInput 
+        is="input"
+        type="search" 
+        name="q" 
+        placeholder="Search Open Collective" 
+        py={1} 
+        pl={3}
+      />
+      <SearchButton is="button" mr={1} p={1}>
+        <SearchIcon size={16} fill="#aaaaaa" />
+      </SearchButton>
+    </SearchInputContainer>
+  </form>
+);
+
+export default SearchForm;
