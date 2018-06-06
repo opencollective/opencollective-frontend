b56de81f4cdf56493bd47083311f7bf7a48ca542
HipsterBrown <headhipster@hipsterbrown.com>
2018-06-06 13:25:55 -0400

    feat(components/icons): Github, Mail, Medium, Slack, Twitter

---
+++ src/components/icons/index.js
@@ -0,0 +1,5 @@
+export { default as GithubIcon } from './GithubIcon';
+export { default as MailIcon } from './MailIcon';
+export { default as MediumIcon } from './MediumIcon';
+export { default as SlackIcon } from './SlackIcon';
+export { default as TwitterIcon } from './TwitterIcon';
