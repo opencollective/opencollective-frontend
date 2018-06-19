9a5931fbd0f0b6254b435ea32216fa698067f0c9
HipsterBrown <headhipster@hipsterbrown.com>
2018-06-19 14:28:40 -0400

    feat(pages/home): add 'Movement' sections copy

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,12 +17,31 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import TransactionSimple from '../components/TransactionSimple';
 import { Link } from '../server/pages';
-import { P, H1, H2, H3 } from '../components/Text';
+import { Span, P, H1, H2, H3 } from '../components/Text';
 import ListItem from '../components/ListItem';
 import Hide from '../components/Hide';
 import Container from '../components/Container';
 import StyledLink from '../components/StyledLink';
 import CollectiveStatsCard from '../components/CollectiveStatsCard';
+
+const responsiveAlign = ['center', null, 'left'];
+const sectionHeadingStyles = {
+  fontSize: [28, null, 48],
+  fontWeight: 800,
+  px: [3, null, 4],
+  textAlign: responsiveAlign,
+};
+const sectionSubHeadingStyles = {
+  ...sectionHeadingStyles,
+  color: '#2E3033',
+  fontSize: [20, null, 28],
+  my: 3,
+};
+const sectionDetailStyles = {
+  color: '#2E3033',
+  px: [3, null, 4],
+  textAlign: responsiveAlign,
+};
 
 class HomePage extends React.Component {
   state = {
@@ -164,6 +183,158 @@ class HomePage extends React.Component {
             >
               Discover more collectives
             </StyledLink>
+          </Container>
+
+          <Container bg="#EBF1FA" py={3} my={5}>
+            <Container maxWidth={800} mx="auto">
+              <H2 textAlign="center" fontWeight="900" px={2} lineHeight={[36, null, 58]} fontSize={[null, null, 56]}>
+                Join the movement for a world with more open, transparent, and fluid organizations.
+              </H2>
+              <P color="#6E747A" textAlign="center" fontSize={[16, null, 20]} lineHeight={[24, null, 28]} px={3} my={[3, null, 4]}>
+                There are many ways to participate; from fiscally hosting collectives to spreading the word to whomever you believe will benefit from operating as an open association. Here’s how:
+              </P>
+            </Container>
+
+            <Container bg={['#3385FF', null, 'transparent']} height={2} width={32} mx="auto" my={[5, null, 3]} />
+
+            <Container display="flex" flexDirection={['column', null, 'row']}>
+              <Container w={[1, null, 0.5]}>
+                <H3 {...sectionHeadingStyles}>Create an open collective</H3>
+
+                <P {...sectionSubHeadingStyles}>
+                  A group of people with a shared mission that operates in full transparency. 👀
+                </P>
+
+                <P {...sectionDetailStyles}>
+                  Want to evolve from the democracy of our voices to the democracy of our actions? Create an open collective for your group and leverage the power of the community to live up to your mission. <a href="/discover">Learn more.</a>
+                </P>
+              </Container>
+              <Container w={[1, null, 0.5]}>
+              </Container>
+            </Container>
+
+            <Container bg={['#3385FF', null, 'transparent']} height={2} width={32} mx="auto" my={[5]} />
+
+            <Container display="flex" flexDirection={['column', null, 'row']}>
+              <Container w={[1, null, 0.5]}>
+                <H3 {...sectionHeadingStyles}>Become a sponsor</H3>
+
+                <P {...sectionSubHeadingStyles}>Great companies supporting great collectives with 💙.</P>
+
+                <P {...sectionDetailStyles}>Accelerate collectives on behalf of your organization. <Span fontWeight="bold">You'll get an invoice for every financial contribution your company makes as well as monthly reporting.</Span></P>
+
+                <P {...sectionDetailStyles} my={3}>If you’re looking to onboard and financially support an initiative through Open Collective, <a href="mailto:info@opencollective.com">let us know</a> and we’ll gladly set them up and get them going.</P>
+
+                <Link route="/organizations/new">
+                  <StyledLink
+                    bg="#3385FF"
+                    borderRadius="50px"
+                    color="white"
+                    display="block"
+                    fontSize="1.6rem"
+                    fontWeight="bold"
+                    maxWidth="220px"
+                    mx="auto"
+                    mt={4}
+                    hover={{ color: 'white' }}
+                    py={3}
+                    textAlign="center"
+                    w={[250, null, 320]}
+                  >
+                    Become a sponsor
+                  </StyledLink>
+                </Link>
+              </Container>
+              <Container w={[1, null, 0.5]}>
+              </Container>
+            </Container>
+
+            <Container bg={['#3385FF', null, 'transparent']} height={2} width={32} mx="auto" my={[5]} />
+
+            <Container display="flex" flexDirection={['column', null, 'row']}>
+              <Container w={[1, null, 0.5]}>
+                <H3 {...sectionHeadingStyles}>Become a backer</H3>
+
+                <P {...sectionSubHeadingStyles}>Those who believe that giving back in unified financial support is the way to go. 👊</P>
+
+                <P {...sectionDetailStyles}>Join Open Collective and discover the different initiatives that are seeking your support. Embrace the mission that drives them and contribute to their group efforts. <Span fontWeight="bold">Become part of the movement.</Span></P>
+
+                <Link route="/discover">
+                  <StyledLink
+                    bg="#3385FF"
+                    borderRadius="50px"
+                    color="white"
+                    display="block"
+                    fontSize="1.6rem"
+                    fontWeight="bold"
+                    maxWidth="220px"
+                    mx="auto"
+                    mt={4}
+                    hover={{ color: 'white' }}
+                    py={3}
+                    textAlign="center"
+                    w={[250, null, 320]}
+                  >
+                    Become a backer
+                  </StyledLink>
+                </Link>
+              </Container>
+              <Container w={[1, null, 0.5]}>
+              </Container>
+            </Container>
+
+            <Container bg={['#3385FF', null, 'transparent']} height={2} width={32} mx="auto" my={[5]} />
+
+            <Container display="flex" flexDirection={['column', null, 'row']}>
+              <Container w={[1, null, 0.5]}>
+                <H3 {...sectionHeadingStyles}>Contribute to OC’s development.</H3>
+
+                <P {...sectionSubHeadingStyles}>Building Open Collective together to get further, faster. 🚀</P>
+
+                <P {...sectionDetailStyles}>Are you interested in helping us improve the platform experience? Are you a developer who believes in the movement? Open Collective is open source so anyone can contribute code or report issues publicly.</P>
+
+                <P {...sectionDetailStyles} my={3}>Special thanks to all of you who already contributed in some way.</P>
+
+                <StyledLink {...sectionDetailStyles} color="#3385FF" display="inline-block" my={4} href="https://github.com/opencollective">
+                  Check out our Github organization to find out more
+                </StyledLink>
+              </Container>
+              <Container w={[1, null, 0.5]}>
+              </Container>
+            </Container>
+
+            <Container bg={['#3385FF', null, 'transparent']} height={2} width={32} mx="auto" my={[5]} />
+
+            <Container display="flex" flexDirection={['column', null, 'row']}>
+              <Container w={[1, null, 0.5]}>
+                <H3 {...sectionHeadingStyles}>Create an OC Chapter</H3>
+
+                <P {...sectionSubHeadingStyles}>Providing the legal and fiscal entities open collectives need to raise funds. ⚖️</P>
+
+                <P {...sectionDetailStyles}><Span fontWeight="bold">Expand the movement</Span> by becoming a fiscal host for open collectives in your region or for those focusing on topics that matter to you. Help organize initiatives and provide a means for them to recieve financial support from the Open Collective community.</P>
+
+                <StyledLink
+                  href="https://github.com/opencollective/opencollective/wiki/Becoming-a-Host"
+                  bg="#3385FF"
+                  borderRadius="50px"
+                  color="white"
+                  display="block"
+                  fontSize="1.6rem"
+                  fontWeight="bold"
+                  maxWidth="220px"
+                  mx="auto"
+                  mt={4}
+                  hover={{ color: 'white' }}
+                  py={3}
+                  textAlign="center"
+                  w={[250, null, 320]}
+                >
+                  Create a chapter
+                </StyledLink>
+              </Container>
+              <Container w={[1, null, 0.5]}>
+              </Container>
+            </Container>
           </Container>
         </Body>
         <Footer />
