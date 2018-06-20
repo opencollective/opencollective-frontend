47798c16217361a818fff5a7a0af125136f75f0f
HipsterBrown <headhipster@hipsterbrown.com>
2018-06-20 17:32:25 -0400

    feat(components/Currency): add abbreviate prop, return styled Span

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { FormattedNumber } from 'react-intl';
 
-const Currency = ({value, currency, precision=0}) => (
+import { abbreviateNumber } from '../lib/utils';
+
+import { Span } from './Text';
+
+const Currency = ({
+  abbreviate = false,
+  currency,
+  precision = 0,
+  value,
+  ...styles,
+}) => (
   <FormattedNumber
     value={value / 100}
     currency={currency}
@@ -9,7 +19,12 @@ const Currency = ({value, currency, precision=0}) => (
     currencyDisplay="symbol"
     minimumFractionDigits={precision}
     maximumFractionDigits={precision}
-    />
+  >
+    {formattedNumber => abbreviate ?
+      <Span {...styles}>{formattedNumber.slice(0, 1)}{abbreviateNumber(value/100)}</Span>
+      : <Span {...styles}>{formattedNumber}</Span>
+    }
+  </FormattedNumber>
 );
 
 export default Currency;
