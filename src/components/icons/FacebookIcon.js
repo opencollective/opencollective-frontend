75191da8e3f9bdd0b222e64f449752c3bc761e42
HipsterBrown <headhipster@hipsterbrown.com>
2018-06-28 16:28:00 -0400

    feat(components/icons): add Facebook, LinkedIn icons

---
+++ src/components/icons/FacebookIcon.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const FacebookIcon = ({ size = 16, fill = '#6E747A', ...props }) => (
+  <svg width={size} height={size} viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <title>facebook icon</title>
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g transform="translate(-87.000000, -9584.000000)" fill={fill}>
+        <g transform="translate(-69.000000, 3570.000000)">
+          <g transform="translate(85.000000, 5758.000000)">
+            <g transform="translate(44.000000, 240.000000)">
+              <path d="M42.117,16 L27.883,16 C27.395,16 27,16.395 27,16.883 L27,31.117 C27,31.605 27.395,32 27.883,32 L35.546,32 L35.546,25.804 L33.46,25.804 L33.46,23.39 L35.546,23.39 L35.546,21.607 C35.546,19.541 36.808,18.417 38.652,18.417 C39.535,18.417 40.294,18.481 40.515,18.511 L40.515,20.671 L39.235,20.671 C38.235,20.671 38.04,21.151 38.04,21.851 L38.04,23.391 L40.43,23.391 L40.12,25.811 L38.04,25.811 L38.04,32 L42.117,32 C42.605,32 43,31.605 43,31.117 L43,16.883 C43,16.395 42.605,16 42.117,16" id="facebook"></path>
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+
+export default FacebookIcon;
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -3,3 +3,5 @@ export { default as MailIcon } from './MailIcon';
 export { default as MediumIcon } from './MediumIcon';
 export { default as SlackIcon } from './SlackIcon';
 export { default as TwitterIcon } from './TwitterIcon';
+export { default as FacebookIcon } from './FacebookIcon';
+export { default as LinkedInIcon } from './LinkedInIcon';
