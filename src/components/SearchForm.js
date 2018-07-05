b56343ce2054dc62b7ed6d49f23ffb2326a246db
HipsterBrown <headhipster@hipsterbrown.com>
2018-07-05 15:21:51 +0200

    fix(components/SearchForm): SearchInput should fill container

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -37,6 +37,7 @@ const SearchForm = () => (
         placeholder="Search Open Collective" 
         py={1} 
         pl={3}
+        w={1}
       />
       <SearchButton is="button" mr={1} p={1}>
         <SearchIcon size={16} fill="#aaaaaa" />
