9396190329a56458a5c3a9f1cd9ff307f0e8e455
Xavier Damman <xdamman@gmail.com>
2018-07-14 11:10:07 +0200

    better export

--- migrations/201807060000-fixLedger.js
+++ migrations/201807060000-fixLedger.js
@@ -2,6 +2,7 @@
 const DRY_MODE = false;
 import Promise from 'bluebird';
 import nock from 'nock';
+import moment from 'moment';
 import { get } from 'lodash';
 import { getFxRate } from '../server/lib/currency';
 
@@ -63,29 +64,6 @@ const addPaymentProcessorFee = (transaction) => {
   }
 }
 
-/**
- * 
- * Equivalent of:
- * UPDATE "Transactions" SET "amountInHostCurrency" = "amount"
- * WHERE amount != "amountInHostCurrency" AND currency = "hostCurrency" AND "hostCurrencyFxRate" = 1
- */
-const fixAmountInHostCurrency = (transaction) => {
-  return updateLedgerEntry(transaction, { amountInHostCurrency: transaction.amount });
-}
-
-/**
- * Some transactions have a fxrate in the wrong direction
- * (it should be: amountInHostCurrency = amountInCollectiveCurrency * hostCurrencyFxRate)
- * We not only need to flip it, but also need to recompute the amountInHostCurrency.
- * See for example TransactionGroup 75809cc0-4000-4520-9b62-1957b751d892
- * @param {*} transaction 
- */
-const fixAmountInHostCurrencyWithFxRate = (transaction) => {
-  const amountInHostCurrency = Math.round(transaction.amount * transaction.hostCurrencyFxRate);
-  console.log(">>> fixing amountInHostCurrency", transaction.amount, transaction.currency, "to host currency:", amountInHostCurrency, `(instead of ${transaction.amountInHostCurrency})`, transaction.hostCurrency, "fxrate:", transaction.hostCurrencyFxRate);
-  return updateLedgerEntry(transaction, { amountInHostCurrency });
-}
-
 const verifyTransaction = (tr, accuracy = 0) => {
   if (tr.hostCollectiveCurrency && tr.hostCurrency !== tr.hostCollectiveCurrency) return false; // if there is a discrepency between tr.hostCurrency and tr.host.currency
   if (tr.currency !== tr.hostCurrency) {
@@ -108,11 +86,16 @@ const verifyTransaction = (tr, accuracy = 0) => {
   }
 }
 
-const cols = ['reason', 'host', 'host.currency', 'collective', 'type', 'transaction.amount', 'transaction.currency', 'amountInHostCurrency', 'hostCurrency', 'hostCurrencyFxRate', 'hostFeeInHostCurrency', 'platformFeeInHostCurrency', 'paymentProcessorFeeInHostcurrency', 'totalFeesInCollectiveCurrency', 'netAmountInCollectiveCurrency', 'OrderId', 'ExpenseId', 'TransactionGroup', 'fix', 'netAmountDelta', 'amountInHostCurrencyDelta', 'fixValid'];
-console.log(cols.join('||'));
+const cols = ['date', 'host', 'host.currency', 'collective', 'type', 'transaction.amount', 'transaction.currency', 'amountInHostCurrency', 'update', 'delta', 'hostCurrency', 'hostCurrencyFxRate', 'newHostCurrencyFxRate', 'hostFeeInHostCurrency', 'platformFeeInHostCurrency', 'paymentProcessorFeeInHostcurrency', 'totalFeesInCollectiveCurrency', 'netAmountInCollectiveCurrency', 'update', 'delta', 'OrderId', 'ExpenseId', 'TransactionGroup', 'reason', 'fix', 'fixValid'];
+console.log(cols.join('|'));
 
 const relativeDiffInPercentage = (a, b) => {
   return Math.abs(Math.round(Math.abs(a-b)/Math.min(a,b) * 10000) / 10000);
+}
+
+const isRefundTransaction = (tr) => {
+  if (!tr.RefundTransactionId) return false;
+  return (tr.description.match(/^Refund of /));
 }
 
 const fixTransaction = async (tr) => {
@@ -130,15 +113,15 @@ const fixTransaction = async (tr) => {
     update = addPaymentProcessorFee(tr) || {};
   }
   
-  if (tr.hostFeeInHostCurrency > 0) {
+  if (tr.hostFeeInHostCurrency > 0 && !isRefundTransaction(tr)) {
     reasons.push("hostFeeInHostCurrency should be negative");
     update.hostFeeInHostCurrency = -tr.hostFeeInHostCurrency;
   }
-  if (tr.platformFeeInHostCurrency > 0) {
+  if (tr.platformFeeInHostCurrency > 0 && !isRefundTransaction(tr)) {
     reasons.push("platformFeeInHostCurrency should be negative");
     update.platformFeeInHostCurrency = -tr.platformFeeInHostCurrency;
   }
-  if (tr.paymentProcessorFeeInHostCurrency > 0) {
+  if (tr.paymentProcessorFeeInHostCurrency > 0 && !isRefundTransaction(tr)) {
     reasons.push("paymentProcessorFeeInHostCurrency should be negative");
     update.paymentProcessorFeeInHostCurrency = -tr.paymentProcessorFeeInHostCurrency;
   }
@@ -212,8 +195,17 @@ const fixTransaction = async (tr) => {
       console.error(`Unable to fetch fxrate for transaction id ${tr.id} from ${tr.currency} to ${tr.hostCurrency}, date: ${tr.createdAt}`, e);
     }
   }
-  if (update.hostCurrencyFxRate) {
-    update.amountInHostCurrency = Math.round(tr.amount * update.hostCurrencyFxRate);
+  const newAmountInHostCurrency = Math.round(tr.amount * update.hostCurrencyFxRate);
+  if (update.hostCurrencyFxRate && newAmountInHostCurrency !== tr.amountInHostCurrency) {
+    update.amountInHostCurrency = newAmountInHostCurrency;
+    // if we change the amountInHostCurrency, we need to recompute the hostFees and platformFees since they were computed based on that amount.
+    if (tr.platformFeeInHostCurrency < 0 && tr.platformFeeInHostCurrency !== -Math.round(0.05 * Math.abs(update.amountInHostCurrency))) {
+      update.platformFeeInHostCurrency = -Math.round(0.05 * Math.abs(update.amountInHostCurrency));
+    }
+    const hostFeePercent = Math.abs(Math.round(tr.hostFeeInHostCurrency / tr.amountInHostCurrency * 100) / 100);
+    if (tr.hostFeeInHostCurrency < 0 && tr.hostFeeInHostCurrency !== -Math.abs(Math.round(hostFeePercent * update.amountInHostCurrency))) {
+      update.hostFeeInHostCurrency = -Math.abs(Math.round(hostFeePercent * update.amountInHostCurrency));
+    }
   }
 
   const newTransaction = {
@@ -247,8 +239,8 @@ const fixTransaction = async (tr) => {
   const netAmountDelta = update.netAmountInCollectiveCurrency && Math.abs(tr.netAmountInCollectiveCurrency - update.netAmountInCollectiveCurrency);
   const amountInHostCurrencyDelta = update.amountInHostCurrency && Math.abs(tr.amountInHostCurrency - update.amountInHostCurrency);
   if (DRY_MODE) {
-    const vals = [ reasons.join(', '), tr.host, tr.hostCollectiveCurrency, tr.collective, tr.type, tr.amount, tr.currency, tr.amountInHostCurrency, tr.hostCurrency, tr.hostCurrencyFxRate, tr.hostFeeInHostCurrency, tr.platformFeeInHostCurrency, tr.paymentProcessorFeeInHostCurrency, totalFeesInCollectiveCurrency, tr.netAmountInCollectiveCurrency, tr.OrderId, tr.ExpenseId, tr.TransactionGroup, JSON.stringify(update), netAmountDelta, amountInHostCurrencyDelta, fixValid];
-    console.log(vals.join('||'));
+    const vals = [ moment(tr.createdAt).format("YYYY-MM-DD HH:mm:ss"), tr.host, tr.hostCollectiveCurrency, tr.collective, tr.type, tr.amount, tr.currency, tr.amountInHostCurrency, update.amountInHostCurrency, amountInHostCurrencyDelta, tr.hostCurrency, tr.hostCurrencyFxRate, update.hostCurrencyFxRate, tr.hostFeeInHostCurrency, tr.platformFeeInHostCurrency, tr.paymentProcessorFeeInHostCurrency, totalFeesInCollectiveCurrency, tr.netAmountInCollectiveCurrency, update.netAmountInCollectiveCurrency, netAmountDelta, tr.OrderId, tr.ExpenseId, tr.TransactionGroup, reasons.join(', '), JSON.stringify(update), fixValid];
+    console.log(vals.join('|'));
   } else {
     return updateLedgerEntry(tr, update);
   }
