657294e2d243ab866d3329a4376d4e37676a553e
Xavier Damman <xdamman@gmail.com>
2018-07-20 15:43:53 -0400

    exit tests if jest fails

--- scripts/run_e2e_tests.sh
+++ scripts/run_e2e_tests.sh
@@ -14,10 +14,16 @@ echo ""
 echo "> Starting server jest tests"
 jest test/server/*
 RETURN_CODE=$?
+if [ $RETURN_CODE -ne 0 ]; then
+  echo "Error with jest tests, exiting"
+  exit 1;
+fi
+
 echo ""
 echo "> Running cypress tests"
 cypress run --record
-if [ $? -ne 0 ]; then
+RETURN_CODE=$?
+if [ $RETURN_CODE -ne 0 ]; then
   echo "Error with cypress e2e tests, exiting"
   exit 1;
 fi
