b7e4f2027c0d1bc21337652ee9fd55da336fb137
marcogbarcellos <marcogbarcellos@gmail.com>
2018-07-25 21:44:03 -0400

    feat(migrations/opencollectivebot): creating open collective bot to initally add comments to expenses

---
+++ migrations/20180725202700-populate-collective-with-opencollective-bot.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Promise = require('bluebird');
+
+const insert = (sequelize, table, entry) => {
+  console.log(sequelize);
+  return sequelize.query(`
+    INSERT INTO "${table}" ("${Object.keys(entry).join('","')}") VALUES (:${Object.keys(entry).join(",:")})
+  `, { replacements: entry });
+};
+
+module.exports = {
+  up: (queryInterface, sequelize) => {
+    
+    // Inserting OpenCollective Bot Collective
+    const botData = {
+      name: 'Open Collective bot',
+      mission: 'Support users through the platform',
+      description: 'Open Collective bot that support users through the platform',
+      longDescription: 'Open Collective bot that support users through the platform',
+      currency: 'USD',
+      image: 'https://cldup.com/rdmBCmH20l.png',
+      isActive: true,
+      slug: 'opencollective-company',
+      website: 'https://opencollective.com',
+      type: 'BOT'
+    };
+    return insert(queryInterface.sequelize, "Collectives", botData);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return Promise.resolve(); 
+  }
+};
