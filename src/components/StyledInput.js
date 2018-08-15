e20654633c49dd6fb579ba8008dfde8da13c74d1
HipsterBrown <headhipster@hipsterbrown.com>
2018-08-15 14:15:45 -0400

    feat(components/StyledInput): export TextInput, SubmitInput

--- src/components/StyledInput.js
+++ src/components/StyledInput.js
@@ -14,6 +14,7 @@ import {
   textAlign,
   width,
 } from 'styled-system';
+import { overflow } from './Container';
 
 const StyledInput = styled.input(
   [],
@@ -27,6 +28,7 @@ const StyledInput = styled.input(
   fontWeight,
   maxWidth,
   minWidth,
+  overflow,
   space,
   textAlign,
   width,
@@ -34,6 +36,34 @@ const StyledInput = styled.input(
 
 StyledInput.defaultProps = {
   border: 'none',
+};
+
+export const TextInput = StyledInput.extend`
+  border: 1px solid #cccccc;
+  border-radius: 4px;
+`;
+
+TextInput.defaultProps = {
+  fontSize: '14px',
+  px: 2,
+  py: 1,
+  type: 'text',
+};
+
+export const SubmitInput = StyledInput.extend`
+  border: none;
+  border-radius: 30px;
+  opacity: ${props => props.disabled ? 0.5 : 1};
+`;
+
+SubmitInput.defaultProps = {
+  bg: '#3385ff',
+  color: 'white',
+  fontWeight: 'bold',
+  maxWidth: 250,
+  py: 2,
+  type: 'submit',
+  width: 1,
 };
 
 export default StyledInput;
