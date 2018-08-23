b02683c54b99af8bb46fa970fbe72ce2f31f4bb7
Xavier Damman <xdamman@gmail.com>
2018-08-23 11:23:28 +0200

    upgrade to cypress 3.1.0 -- updated e2e tests

--- cypress/integration/22-collective.edit.test.js
+++ cypress/integration/22-collective.edit.test.js
@@ -5,8 +5,8 @@ const signin = (redirect) => {
   cy.get('.email.inputField input').type('testuser+admin@opencollective.com');
   cy.wait(400)
   cy.get('.email.inputField input').type('{enter}');
-  cy.wait(700)
-}
+  cy.wait(700);
+};
 
 const addTier = (tier) => {
   cy.get('.addTier').click();
@@ -23,13 +23,13 @@ const addTier = (tier) => {
     const action = field.type === 'select' ? 'select' : 'type';
     const value = action === 'type' ? `{selectall}${tier[field.name]}` : tier[field.name];
     cy.get(`.EditTiers .tier:last .${field.name}.inputField ${field.type}`)[action](value);
-  })
-}
+  });
+};
 
-describe("edit collective", () => {
+describe('edit collective', () => {
 
-  it ("edit info", () => {
-    signin(`/testcollective/edit`);
+  it ('edit info', () => {
+    signin('/testcollective/edit');
     cy.get('.name.inputField input').type(' edited');
     cy.get('.description.inputField input').type(' edited');
     cy.get('.twitterHandle.inputField input').type('{selectall}opencollect');
@@ -42,57 +42,57 @@ describe("edit collective", () => {
     cy.get('.CollectivePage .cover h1').contains('edited');
     cy.get('.cover .twitterHandle').contains('@opencollect');
     cy.get('.cover .website').contains('opencollective.com');
-    cy.get('.CollectivePage .longDescription').contains("Testing markdown link to google");
-    cy.get('.CollectivePage .longDescription a').contains("link to google");
-  })
+    cy.get('.CollectivePage .longDescription').contains('Testing markdown link to google');
+    cy.get('.CollectivePage .longDescription a').contains('link to google');
+  });
 
-  it ("edit tiers", () => {
-    signin(`/testcollective/edit`);
-    cy.get('.menuBtnGroup .tiers').click();
-    cy.get('.EditTiers .tier:first .name.inputField input').type("{selectall}Backer edited")
-    cy.get('.EditTiers .tier:first .description.inputField textarea').type("{selectall}New description for backers")
-    cy.get('.EditTiers .tier:first .amount.inputField input').type("{selectall}5")
-    cy.get('.EditTiers .tier:first ._amountType.inputField select').select("flexible");
-    cy.get('.EditTiers .tier:first .currency1.inputField input').type("{selectall}5");
-    cy.get('.EditTiers .tier:first .currency2.inputField input').type("{selectall}10");
-    cy.get('.EditTiers .tier:first .currency3.inputField input').type("{selectall}20");
-    cy.get('.EditTiers .tier:first .currency0.inputField input').type("{selectall}{backspace}");
+  it ('edit tiers', () => {
+    signin('/testcollective/edit');
+    cy.get('.MenuItem.tiers').click();
+    cy.get('.EditTiers .tier:first .name.inputField input').type('{selectall}Backer edited');
+    cy.get('.EditTiers .tier:first .description.inputField textarea').type('{selectall}New description for backers');
+    cy.get('.EditTiers .tier:first .amount.inputField input').type('{selectall}5');
+    cy.get('.EditTiers .tier:first ._amountType.inputField select').select('flexible');
+    cy.get('.EditTiers .tier:first .currency1.inputField input').type('{selectall}5');
+    cy.get('.EditTiers .tier:first .currency2.inputField input').type('{selectall}10');
+    cy.get('.EditTiers .tier:first .currency3.inputField input').type('{selectall}20');
+    cy.get('.EditTiers .tier:first .currency0.inputField input').type('{selectall}{backspace}');
     addTier({
-      name: "Donor (one time donation)",
-      type: "DONATION",
+      name: 'Donor (one time donation)',
+      type: 'DONATION',
       amount: 500,
-      interval: "onetime",
-      description: "New description for donor"
+      interval: 'onetime',
+      description: 'New description for donor',
     });
     addTier({
-      type: "SERVICE",
-      name: "Priority Support",
-      description: "Get priority support from the core contributors",
+      type: 'SERVICE',
+      name: 'Priority Support',
+      description: 'Get priority support from the core contributors',
       amount: 1000,
-      interval: "month",
-      maxQuantity: 10
-    })
+      interval: 'month',
+      maxQuantity: 10,
+    });
     cy.wait(500);
     cy.get('.actions > .btn').click(); // save changes
     cy.get('.backToProfile a').click(); // back to profile
     cy.wait(500);
     cy.get('.TierCard', { timeout: 5000 })
-    cy.screenshot("tierEdited");
+    cy.screenshot('tierEdited');
     cy.get('.TierCard').first().find('.name').contains('Backer edited');
     cy.get('.TierCard').first().find('.description').contains('New description for backers');
     cy.get('.TierCard').first().find('.amount').contains('$5+');
     cy.get('.TierCard').first().find('.interval').contains('per month');
-    cy.screenshot("tierAdded");
+    cy.screenshot('tierAdded');
     cy.get('.CollectivePage .tiers', { timeout: 5000 }).find('.TierCard')
       .should('have.length', 4)
       .last().should('contain', 'Priority Support')
     cy.get('.CollectivePage .tiers').find('.TierCard').first().find('a.action').click();
     cy.get('.OrderForm', { timeout: 20000 })
-    cy.get('.tier .selectPreset label').contains("Select monthly amount")
+    cy.get('.tier .selectPreset label').contains('Select monthly amount');
     cy.get('.tier .presetBtn', { timeout: 5000 })
       .should('have.length', 3)
     cy.visit(`${WEBSITE_URL}/testcollective/edit#tiers`);
-    cy.get('.EditTiers .tier').first().find('._amountType select').select("fixed");
+    cy.get('.EditTiers .tier').first().find('._amountType select').select('fixed');
     cy.get('.EditTiers .tier').last().find('.removeTier').click();
     cy.get('.EditTiers .tier').last().find('.removeTier').click();
     cy.wait(500)
@@ -100,7 +100,6 @@ describe("edit collective", () => {
     cy.get('.backToProfile a').click(); // back to profile
     cy.wait(500)
     cy.get('.CollectivePage .tiers', { timeout: 10000 }).find('.TierCard')
-      .should('have.length', 2)
-  })
-
-})
+      .should('have.length', 2);
+  });
+});
