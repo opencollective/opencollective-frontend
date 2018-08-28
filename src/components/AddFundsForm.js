c3e75f044798c664895d0a2c268a379b7e22f4e1
marcogbarcellos <marcogbarcellos@gmail.com>
2018-08-28 12:45:44 -0400

    fix(components/AddFundsForm): improving validation on add funds form

--- src/components/AddFundsForm.js
+++ src/components/AddFundsForm.js
@@ -74,7 +74,7 @@ class AddFundsForm extends React.Component {
         options: {
           collective: this.props.collective,
           host: this.props.host,
-          paymentMethod: get(this, 'props.host') ? this.props.host.paymentMethods.find(pm => pm.service === 'opencollective') : null,
+          paymentMethod: get(this, 'props.host.paymentMethods') ? this.props.host.paymentMethods.find(pm => pm.service === 'opencollective') : null,
         }
       },
       {
