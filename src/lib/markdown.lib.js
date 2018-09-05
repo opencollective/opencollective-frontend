cb927f32c9078e8ed7ba81617bff92d00098af32
Xavier Damman <xdamman@gmail.com>
2018-09-05 14:31:51 -0400

    Convert youtube and vimeo URLs to embeds in markdown

--- src/lib/markdown.lib.js
+++ src/lib/markdown.lib.js
@@ -25,7 +25,6 @@ import slugify from 'slugify';
  * }
  */
 export function processMarkdown(text) {
-
   const body = { sections: [], params: {} };
   if (!text) {
     body.sections = [];
@@ -38,21 +37,25 @@ export function processMarkdown(text) {
     body.sections.push({
       title: currentTitle,
       id: currentTitle && slugify(currentTitle).toLowerCase(),
-      markdown: paragraph.join('\n').trim()
+      markdown: paragraph.join('\n').trim(),
     });
-  }
+  };
 
   let currentTitle;
   let isHeader = true;
   let paragraph = [];
   lines.forEach((line) => {
-    if (isHeader && line.match(/^[a-z]+:.+/i)) {
+    if (line.length === 0) {
+      return paragraph.push(line);
+    }
+    if (isHeader && line.match(/^[a-z]+:.+/i) && !line.match(/^http/i)) {
       const tokens = line.match(/([^:]+):(.+)/);
       body.params[tokens[1]] = tokens[2].trim();
       return;
     } else {
       isHeader = false;
     }
+
     if (line.length <= 60) {
       const match = line.match(/^# *([^#]{2,60})/i);
       if (match) {
@@ -60,6 +63,29 @@ export function processMarkdown(text) {
         currentTitle = match[1];
         paragraph = [];
         return;
+      }
+    }
+    let videoid;
+    let videoPlatform;
+    const tokens = line.match(/^https?:\/\/(www\.)?(youtu).be\/([^\/\?]+).*$/i) || line.match(/^https?:\/\/(www\.)?(youtu)be\.com\/watch\?v=([^&]*).*$/i) || line.match(/^https?:\/\/(www\.)?(vimeo)\.com\/([0-9]+)/i);
+    if (tokens && tokens.length > 3) {
+      videoPlatform = tokens[2];
+      videoid = tokens[3];
+    }
+
+    if (videoid) {
+      let embed;
+      const dimensions = 'width="640" height="360"';
+      switch (videoPlatform) {
+        case 'vimeo':
+          embed = `<iframe ${dimensions} src="https://player.vimeo.com/video/${videoid}" frameborder="0" allowfullscreen></iframe>`;
+          break;
+        case 'youtu':
+          embed = `<iframe ${dimensions} src="https://www.youtube.com/embed/${videoid}" frameborder="0" allowFullScreen></iframe>`;
+          break;
+      }
+      if (embed) {
+        line = `<div class='video ${videoPlatform}'>${embed}</div>`;
       }
     }
     paragraph.push(line);
