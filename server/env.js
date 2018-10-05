e1007b4dedebe5aeecaa60715a3d852797acb826
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-05 17:21:46 -0400

    feat(env): adding test and development default dotenv files(used only locally)
    chore: updating test script to use NODE_ENV=test by default

--- server/env.js
+++ server/env.js
@@ -6,8 +6,7 @@ if (!process.env.NODE_ENV) {
 
 if (['production', 'circleci'].indexOf(process.env.NODE_ENV) === -1) {
   const dotenv = require('dotenv');
-  dotenv.config(); // this loads .env with real values. It needs to be first because dotenv doesn't overwrite any values
-  dotenv.config({ path: 'default.env' }); // this loads the default file in github with dummy values
+  dotenv.config({ path: `.env.${process.env.NODE_ENV}` }); // this loads the default file in github with dummy values
 }
 
 // Only load newrelic when we explicitly want it
