4ae9a02622081eb37dc64da8938e5deb3c63733a
François Hodierne <francois@hodierne.net>
2018-10-08 09:04:17 +0200

    perf: handle search submit client side
    avoid reloading the page

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,7 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Box, Flex } from 'grid-styled';
-import SearchIcon from './SearchIcon';
 import { fontSize } from 'styled-system';
 import styled from 'styled-components';
+
+import { Router } from '../server/pages';
+
+import SearchIcon from './SearchIcon';
 
 const SearchInputContainer = styled(Flex)`
   border: solid 1px var(--silver-four);
@@ -26,28 +31,53 @@ const SearchButton = styled(Flex)`
   }
 `;
 
-const SearchForm = ({ fontSize }) => (
-  <form action="/search" method="GET">
-    <SearchInputContainer
-      alignItems="center"
-      justifyContent="space-between"
-      p={1}
-    >
-      <SearchInput
-        is="input"
-        type="search"
-        name="q"
-        placeholder="Search Open Collective"
-        py={1}
-        pl={3}
-        width={1}
-        fontSize={fontSize}
-      />
-      <SearchButton is="button" mr={1} p={1}>
-        <SearchIcon size={16} fill="#aaaaaa" />
-      </SearchButton>
-    </SearchInputContainer>
-  </form>
-);
+class SearchForm extends React.Component {
+  static propTypes = {
+    fontSize: PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = { q: '' };
+  }
+
+  onSubmit = () => {
+    Router.pushRoute('search', { q: this.state.q });
+    return false;
+  };
+
+  handleChange = event => {
+    this.setState({ q: event.target.value });
+  };
+
+  render() {
+    const { fontSize } = this.props;
+    return (
+      <form action="/search" method="GET" onSubmit={this.onSubmit}>
+        <SearchInputContainer
+          alignItems="center"
+          justifyContent="space-between"
+          p={1}
+        >
+          <SearchInput
+            is="input"
+            type="search"
+            name="q"
+            placeholder="Search Open Collective"
+            py={1}
+            pl={3}
+            width={1}
+            fontSize={fontSize}
+            onChange={this.handleChange}
+          />
+          <SearchButton is="button" mr={1} p={1}>
+            <SearchIcon size={16} fill="#aaaaaa" />
+          </SearchButton>
+        </SearchInputContainer>
+      </form>
+    );
+  }
+}
 
 export default SearchForm;
