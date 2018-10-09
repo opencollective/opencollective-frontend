a481f3c7c845f7ca7264fd2c96bab21092db06bb
HipsterBrown <headhipster@hipsterbrown.com>
2018-10-09 16:35:56 -0400

    test(graphql.updateOrder): initial coverage

---
+++ test/graphql.updateOrder.test.js
@@ -0,0 +1,232 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { pick } from 'lodash';
+
+import * as utils from '../test/utils';
+import models from '../server/models';
+
+const ordersData = utils.data('orders');
+
+const order = {
+  quantity: 1,
+  interval: null,
+  collective: {
+    id: null,
+  },
+};
+
+const updateOrderQuery = `
+  mutation updateOrder($order: OrderInputType!) {
+    updateOrder(order: $order) {
+      id
+      status
+      createdByUser {
+        id
+      }
+      paymentMethod {
+        id
+      }
+      totalAmount
+      fromCollective {
+        id
+        slug
+        name
+        website
+      }
+      collective {
+        id
+        slug
+        currency
+      }
+      subscription {
+        id
+        amount
+        interval
+        isActive
+        stripeSubscriptionId
+      }
+    }
+  }
+`;
+
+describe('updateOrder', () => {
+  let sandbox, user, user2, collective, paymentMethod, existingOrder;
+
+  before(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  after(() => sandbox.restore());
+
+  beforeEach(async () => {
+    await utils.resetTestDB();
+    user = await models.User.createUserWithCollective(utils.data('user1'));
+    user2 = await models.User.createUserWithCollective(utils.data('user2'));
+    collective = await models.Collective.create(utils.data('collective1'));
+    paymentMethod = await models.PaymentMethod.create({
+      ...utils.data('paymentMethod2'),
+      CreatedByUserId: user2.id,
+    });
+
+    await user.collective.update({ currency: 'EUR' });
+    await user2.collective.update({ currency: 'EUR' });
+    await collective.addHost(user.collective, user);
+
+    await models.ConnectedAccount.create({
+      service: 'stripe',
+      token: 'sktest_123',
+      CollectiveId: user.CollectiveId,
+    });
+
+    existingOrder = await models.Order.create({
+      ...ordersData[0],
+      currency: 'EUR',
+      CreatedByUserId: user2.id,
+      FromCollectiveId: user2.CollectiveId,
+      CollectiveId: collective.id,
+      totalAmount: ordersData[0].amount,
+    });
+
+    utils.stubStripeCreate(sandbox, {
+      charge: { currency: 'eur', status: 'succeeded' },
+    });
+  });
+
+  afterEach(() => {
+    utils.clearbitStubAfterEach(sandbox);
+  });
+
+  it('fails if if no authorization provided', async () => {
+    const res = await utils.graphqlQuery(updateOrderQuery, {
+      order,
+    });
+
+    expect(res.errors).to.exist;
+    expect(res.errors[0].message).to.equal(
+      'You need to be logged in to update an order',
+    );
+  });
+
+  it('fails if no order exists yet', async () => {
+    const res = await utils.graphqlQuery(
+      updateOrderQuery,
+      {
+        order,
+      },
+      user2,
+    );
+
+    expect(res.errors).to.exist;
+    expect(res.errors[0].message).to.equal('Existing order not found');
+  });
+
+  it('fails if no paymentMethod is included', async () => {
+    order.id = existingOrder.id;
+    order.totalAmount = existingOrder.totalAmount;
+
+    const res = await utils.graphqlQuery(
+      updateOrderQuery,
+      {
+        order,
+      },
+      user2,
+    );
+
+    expect(res.errors).to.exist;
+    expect(res.errors[0].message).to.equal(
+      'This order requires a payment method',
+    );
+  });
+
+  it('updates an order as logged in user', async () => {
+    order.id = existingOrder.id;
+    order.totalAmount = existingOrder.totalAmount;
+    order.paymentMethod = pick(paymentMethod, [
+      'id',
+      'uuid',
+      'service',
+      'token',
+      'name',
+    ]);
+
+    utils.stubStripeBalance(
+      sandbox,
+      order.totalAmount,
+      'eur',
+      Math.round(order.totalAmount * 0.05),
+      4500,
+    ); // This is the payment processor fee.
+
+    // When the query is executed
+    const res = await utils.graphqlQuery(updateOrderQuery, { order }, user2);
+
+    // Then there should be no errors
+    expect(res.errors).to.not.exist;
+    expect(res.data.updateOrder.status).to.equal('PAID');
+    expect(res.data.updateOrder.subscription).to.not.exist;
+
+    // And then the creator of the order should be user
+    const orderForCollective = res.data.updateOrder.collective;
+    const transaction = await models.Transaction.findOne({
+      where: {
+        CollectiveId: orderForCollective.id,
+        amount: existingOrder.totalAmount,
+      },
+    });
+    expect(transaction.FromCollectiveId).to.equal(user2.CollectiveId);
+    expect(transaction.CollectiveId).to.equal(orderForCollective.id);
+    expect(transaction.currency).to.equal(orderForCollective.currency);
+    expect(transaction.hostFeeInHostCurrency).to.equal(
+      -(0.1 * order.totalAmount),
+    );
+    expect(transaction.platformFeeInHostCurrency).to.equal(
+      -(0.05 * order.totalAmount),
+    );
+    expect(transaction.data.charge.currency).to.equal(
+      orderForCollective.currency.toLowerCase(),
+    );
+    expect(transaction.data.charge.status).to.equal('succeeded');
+    expect(
+      transaction.data.balanceTransaction.net +
+        transaction.hostFeeInHostCurrency,
+    ).to.equal(transaction.netAmountInCollectiveCurrency);
+    // make sure the payment has been recorded in the connected
+    // Stripe Account of the host
+    expect(transaction.data.charge.currency).to.equal('eur');
+  });
+
+  it('updates an order with a new Subscription', async () => {
+    order.id = existingOrder.id;
+    order.interval = 'month';
+    order.totalAmount = existingOrder.totalAmount;
+    order.paymentMethod = pick(paymentMethod, [
+      'id',
+      'uuid',
+      'service',
+      'token',
+      'name',
+    ]);
+
+    utils.stubStripeBalance(
+      sandbox,
+      order.totalAmount,
+      'eur',
+      Math.round(order.totalAmount * 0.05),
+      4500,
+    ); // This is the payment processor fee.
+
+    // When the query is executed
+    const res = await utils.graphqlQuery(updateOrderQuery, { order }, user2);
+
+    // Then there should be no errors
+    expect(res.errors).to.not.exist;
+    expect(res.data.updateOrder.subscription).to.exist;
+    expect(res.data.updateOrder.status).to.equal('ACTIVE');
+
+    const { subscription } = res.data.updateOrder;
+
+    expect(subscription.interval).to.equal('month');
+    expect(subscription.isActive).to.be.true;
+    expect(subscription.amount).to.equal(order.totalAmount);
+  });
+});
