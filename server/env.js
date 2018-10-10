ab5605a1ca6c2fbc6ae956ad00ea3bd270337905
marcogbarcellos <marcogbarcellos@gmail.com>
2018-10-10 08:58:22 -0400

    fix(env): removing unnecessary comment

--- server/env.js
+++ server/env.js
@@ -6,7 +6,7 @@ if (!process.env.NODE_ENV) {
 
 if (['production', 'circleci'].indexOf(process.env.NODE_ENV) === -1) {
   const dotenv = require('dotenv');
-  dotenv.config({ path: `.env.${process.env.NODE_ENV}` }); // this loads the default file in github with dummy values
+  dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 }
 
 // Only load newrelic when we explicitly want it
