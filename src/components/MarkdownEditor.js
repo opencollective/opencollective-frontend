97b957f09b933187ea50ccb3c8050b0af377d0b3
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-11-05 16:00:39 +0100

    Update react-mde to 6.0.0-alpha.1
    Fix https://github.com/opencollective/opencollective-frontend/issues/395
    
    See https://github.com/andrerpena/react-mde/blob/master/docs-md/ChangeLogMigrating.md for changelog.

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactMde, { ReactMdeCommands } from 'react-mde';
+import ReactMde, { ReactMdeCommands as Commands } from 'react-mde';
+import classNames from 'classnames';
+import * as Showdown from 'showdown';
+import { Heading } from 'styled-icons/fa-solid/Heading.cjs';
+import { Bold } from 'styled-icons/fa-solid/Bold.cjs';
+import { Italic } from 'styled-icons/fa-solid/Italic.cjs';
+import { Link } from 'styled-icons/fa-solid/Link.cjs';
+import { Image } from 'styled-icons/fa-solid/Image.cjs';
+import { ListUl } from 'styled-icons/fa-solid/ListUl.cjs';
+import { ListOl } from 'styled-icons/fa-solid/ListOl.cjs';
 
 import '../../node_modules/react-mde/lib/styles/css/react-mde-all.css';
+
+const EditorIconComponents = {
+  heading: Heading,
+  bold: Bold,
+  italic: Italic,
+  link: Link,
+  image: Image,
+  'list-ul': ListUl,
+  'list-ol': ListOl,
+};
 
 /*
  * Simple editor component that takes placeholder text as a prop
@@ -19,47 +38,64 @@ class MarkdownEditor extends React.Component {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
-      reactMdeValue: { text: props.defaultValue, selection: null },
+      value: props.defaultValue || '',
     };
+    this.converter = new Showdown.Converter();
+    this.commands = [
+      {
+        commands: [
+          Commands.headerCommand,
+          Commands.boldCommand,
+          Commands.italicCommand,
+        ],
+      },
+      {
+        commands: [Commands.linkCommand, Commands.imageCommand],
+      },
+      {
+        commands: [Commands.unorderedListCommand, Commands.orderedListCommand],
+      },
+    ];
   }
 
   handleChange(value) {
-    this.setState({ reactMdeValue: value });
-    this.props.onChange(value.text);
+    this.setState({ value });
+    this.props.onChange(value);
   }
 
   render() {
     return (
       <div
-        className={`${
-          this.props.preview === false ? 'noPreview' : ''
-        } MarkdownEditor`}
+        className={classNames('MarkdownEditor', {
+          noPreview: this.props.preview === false,
+        })}
       >
         <style jsx global>
           {`
-            .MarkdownEditor .react-mde-editor {
-              width: 50%;
-            }
-            .MarkdownEditor.noPreview .react-mde-preview {
+            .MarkdownEditor.noPreview .mde-tabs {
               display: none;
             }
-            .MarkdownEditor .react-mde-preview {
-              width: 50%;
-            }
-            .MarkdownEditor .column .mde-preview .mde-preview-content {
-              padding-top: 5px;
+            .DraftEditor-root,
+            .DraftEditor-editorContainer,
+            .public-DraftEditor-content {
+              height: 100%;
             }
           `}
         </style>
         <ReactMde
-          textAreaProps={{
-            id: 'ta1',
-            name: 'ta1',
-          }}
-          value={this.state.reactMdeValue}
-          className="column"
+          value={this.state.value}
           onChange={this.handleChange}
-          commands={ReactMdeCommands.getDefaultCommands()}
+          commands={this.commands}
+          generateMarkdownPreview={markdown => {
+            // react-mde expect generateMarkdownPreview to return a promise
+            return Promise.resolve(this.converter.makeHtml(markdown));
+          }}
+          buttonContentOptions={{
+            iconProvider: name => {
+              const Component = EditorIconComponents[name];
+              return Component ? <Component size="1em" title={name} /> : name;
+            },
+          }}
         />
       </div>
     );
