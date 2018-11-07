3c5f21b3a3cb54f92fcd4dd46de5f6d2973603ef
marcogbarcellos <marcogbarcellos@gmail.com>
2018-11-07 18:16:26 -0500

    feat(paymentProviders/stripe/creditcard): add method to refund transactions that were already refunded by strype but were not reflected in the OC database

--- server/paymentProviders/stripe/creditcard.js
+++ server/paymentProviders/stripe/creditcard.js
@@ -217,6 +217,47 @@ export default {
     );
   },
 
+  /** Refund a given transaction that was already refunded
+   * in stripe but not in our database
+   */
+  refundTransactionOnlyInDatabase: async (transaction, user) => {
+    /* What's going to be refunded */
+    const chargeId = _.result(transaction.data, 'charge.id');
+
+    /* From which stripe account it's going to be refunded */
+    const collective = await models.Collective.findById(
+      transaction.type === 'CREDIT'
+        ? transaction.CollectiveId
+        : transaction.FromCollectiveId,
+    );
+    const hostStripeAccount = await collective.getHostStripeAccount();
+
+    /* Refund both charge & application fee */
+    const refund = await stripeGateway.retrieveRefundFromChargeId(
+      hostStripeAccount,
+      chargeId,
+    );
+    const balance = await stripeGateway.retrieveBalanceTransaction(
+      hostStripeAccount,
+      refund.balance_transaction,
+    );
+    const fees = stripeGateway.extractFees(balance);
+
+    /* Create negative transactions for the received transaction */
+    const refundTransaction = await paymentsLib.createRefundTransaction(
+      transaction,
+      fees.stripeFee,
+      { refund, balance },
+      user,
+    );
+
+    /* Associate RefundTransactionId to all the transactions created */
+    return paymentsLib.associateTransactionRefundId(
+      transaction,
+      refundTransaction,
+    );
+  },
+
   webhook: (requestBody, event) => {
     const invoice = event.data.object;
     const invoiceLineItems = invoice.lines.data;
