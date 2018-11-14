2342a471e6a0817418472b4562592f2ec3fdf5cd
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-11-14 15:03:21 +0100

    Set UsingVirtualCardFromCollectiveId for existing transactions in migrations

--- migrations/20181107133924-add-virtualcards-emitter-collective-to-transactions.js
+++ migrations/20181107133924-add-virtualcards-emitter-collective-to-transactions.js
@@ -2,10 +2,8 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn(
-      'Transactions',
-      'UsingVirtualCardFromCollectiveId',
-      {
+    return queryInterface
+      .addColumn('Transactions', 'UsingVirtualCardFromCollectiveId', {
         type: Sequelize.INTEGER,
         references: { model: 'Collectives', key: 'id' },
         onDelete: 'SET NULL',
@@ -13,8 +11,18 @@ module.exports = {
         allowNull: true,
         description:
           'References the collective that created the virtual card used for this order',
-      },
-    );
+      })
+      .then(() => queryInterface.sequelize.sync())
+      .then(() => {
+        return queryInterface.sequelize.query(`
+          UPDATE  "Transactions" t
+          SET     "UsingVirtualCardFromCollectiveId" = spm."CollectiveId"
+          FROM    "PaymentMethods" pm, "PaymentMethods" spm
+          WHERE   pm.id 	= t."PaymentMethodId"
+          AND     spm.id 	= pm."SourcePaymentMethodId"
+          AND     pm.type = 'virtualcard'
+        `);
+      });
   },
 
   down: queryInterface => {
