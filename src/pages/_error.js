7e6aeb96780fbe115f3e6a6460ec1e5dab22ccb9
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-11-16 22:29:41 +0100

    Return a 404 error when fetching a collective that don't exists
    Fix #1369

---
+++ src/pages/_error.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import withData from '../lib/withData';
+import withLoggedInUser from '../lib/withLoggedInUser';
+import withIntl from '../lib/withIntl';
+import ErrorPage from '../components/ErrorPage';
+
+/**
+ * This page is shown when NextJS triggers a critical error during server-side
+ * rendering, typically 404 errors.
+ */
+class Error extends React.Component {
+  static propTypes = {
+    statusCode: PropTypes.number.isRequired,
+    url: PropTypes.string,
+    err: PropTypes.object,
+  };
+
+  static getInitialProps({ res, err, req }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode, err, url: req && req.originalUrl };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  async componentDidMount() {
+    const { getLoggedInUser } = this.props;
+    const LoggedInUser = await getLoggedInUser();
+    this.setState({ LoggedInUser });
+  }
+
+  render() {
+    const { statusCode, url } = this.props;
+    const { LoggedInUser } = this.state;
+
+    if (statusCode === 404 && url) {
+      const slugRegex = /^\/([^/?]+)/;
+      const parsedUrl = slugRegex.exec(url);
+      const errorData = parsedUrl && {
+        error: { message: 'No collective found' },
+        variables: { slug: parsedUrl[1] },
+      };
+
+      return <ErrorPage LoggedInUser={LoggedInUser} data={errorData} />;
+    }
+    return <ErrorPage LoggedInUser={LoggedInUser} />;
+  }
+}
+
+export default withData(withIntl(withLoggedInUser(Error)));
