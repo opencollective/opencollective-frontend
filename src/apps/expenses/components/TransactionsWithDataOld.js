966e241752b2bf608abd45c837e027ea5169a136
marcogbarcellos <marcogbarcellos@gmail.com>
2018-11-26 14:27:48 -0500

    feat: add Transactions with data from current prod api transactions table page

---
+++ src/apps/expenses/components/TransactionsWithDataOld.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'react-apollo';
+
+import withIntl from '../../../lib/withIntl';
+import Error from '../../../components/Error';
+import { getOldTransactionsQuery } from '../../../graphql/queries';
+
+import Transactions from './Transactions';
+
+class TransactionsWithDataOld extends React.Component {
+  static propTypes = {
+    collective: PropTypes.object,
+    limit: PropTypes.number,
+    filters: PropTypes.bool,
+    LoggedInUser: PropTypes.object,
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (!this.props.LoggedInUser && nextProps.LoggedInUser) {
+      return this.props.data.refetch();
+    }
+  }
+
+  render() {
+    const {
+      data,
+      LoggedInUser,
+      collective,
+      fetchMore,
+      showCSVlink,
+      filters,
+    } = this.props;
+
+    if (data.error) {
+      console.error('graphql error>>>', data.error.message);
+      return <Error message="GraphQL error" />;
+    }
+
+    const transactions = data.allTransactionsOld;
+    
+    console.log('OLD TRANSACTIONS');
+    console.log(transactions);
+    return (
+      <div className="TransactionsContainer">
+        <Transactions
+          collective={collective}
+          transactions={transactions}
+          refetch={data.refetch}
+          fetchMore={fetchMore}
+          filters={filters}
+          LoggedInUser={LoggedInUser}
+          showCSVlink={showCSVlink}
+        />
+      </div>
+    );
+  }
+}
+
+const TRANSACTIONS_PER_PAGE = 10;
+export const addTransactionsData = graphql(getOldTransactionsQuery, {
+  options(props) {
+    return {
+      variables: {
+        CollectiveId: props.collective.id,
+        offset: 0,
+        limit: props.limit || TRANSACTIONS_PER_PAGE * 2,
+      },
+    };
+  },
+  props: ({ data }) => ({
+    data,
+    fetchMore: () => {
+      return data.fetchMore({
+        variables: {
+          offset: data.allTransactionsOld.length,
+          limit: TRANSACTIONS_PER_PAGE,
+        },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          if (!fetchMoreResult) {
+            return previousResult;
+          }
+          return Object.assign({}, previousResult, {
+            // Append the new posts results to the old one
+            allTransactionsOld: [
+              ...previousResult.allTransactionsOld,
+              ...fetchMoreResult.allTransactionsOld,
+            ],
+          });
+        },
+      });
+    },
+  }),
+});
+
+export default addTransactionsData(withIntl(TransactionsWithDataOld));
