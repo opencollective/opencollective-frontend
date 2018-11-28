c4d18b6fa2729491ebd4b7a8c8b828e52426c12d
HipsterBrown <headhipster@hipsterbrown.com>
2018-11-28 00:26:14 -0500

    feat(pages/search): use Page, cleanup loading

--- src/pages/search.js
+++ src/pages/search.js
@@ -7,12 +7,10 @@ import styled from 'styled-components';
 
 import { Link, Router } from '../server/pages';
 
-import Body from '../components/Body';
+import Page from '../components/Page';
 import Button from '../components/Button';
 import CollectiveCard from '../components/CollectiveCard';
 import ErrorPage from '../components/ErrorPage';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
 import LoadingGrid from '../components/LoadingGrid';
 import StyledLink from '../components/StyledLink';
 import Pagination from '../components/Pagination';
@@ -68,11 +66,6 @@ class SearchPage extends React.Component {
     usePledges: false,
   };
 
-  state = {
-    loadingUserLogin: true,
-    LoggedInUser: undefined,
-  };
-
   refetch = event => {
     event.preventDefault();
 
@@ -93,8 +86,6 @@ class SearchPage extends React.Component {
       data: { error, loading, search },
       term = '',
       usePledges,
-      loadingLoggedInUser,
-      LoggedInUser,
     } = this.props;
 
     if (error) {
@@ -102,160 +93,97 @@ class SearchPage extends React.Component {
     }
 
     const { collectives, limit = 20, offset, total = 0 } = search || {};
-
-    const showCollectives = !loading && term.trim() !== '' && !!collectives;
+    const showCollectives = term.trim() !== '' && !!collectives;
 
     return (
-      <div>
-        <Header
-          title="Search"
-          className={loadingLoggedInUser ? 'loading' : ''}
-          LoggedInUser={LoggedInUser}
-          showSearch={false}
-        />
-        <Body>
-          <Container mx="auto" px={3} width={[1, 0.85]} maxWidth={1200}>
-            <Box width={1}>
-              <form method="GET" onSubmit={this.refetch}>
-                <FormGroup controlId="search" bsSize="large">
-                  <ControlLabel className="h1">Search Open Collective</ControlLabel>
-                  <Flex alignItems="flex-end" my={3}>
-                    <SearchInput type="search" name="q" placeholder="open source" defaultValue={term} />
-                    <SearchButton type="submit">
-                      <span className="fa fa-search" />
-                    </SearchButton>
-                  </Flex>
-                </FormGroup>
-              </form>
-            </Box>
-            <Flex justifyContent={['center', 'center', 'flex-start']} flexWrap="wrap">
-              {loading && (
-                <Flex py={3} width={1} justifyContent="center">
-                  <LoadingGrid />
+      <Page title="Search" showSearch={false}>
+        <Container mx="auto" px={3} width={[1, 0.85]} maxWidth={1200}>
+          <Box width={1}>
+            <form method="GET" onSubmit={this.refetch}>
+              <FormGroup controlId="search" bsSize="large">
+                <ControlLabel className="h1">Search Open Collective</ControlLabel>
+                <Flex alignItems="flex-end" my={3}>
+                  <SearchInput type="search" name="q" placeholder="open source" defaultValue={term} />
+                  <SearchButton type="submit">
+                    <span className="fa fa-search" />
+                  </SearchButton>
                 </Flex>
-              )}
-              {showCollectives &&
-                collectives.map(collective => (
-                  <Flex key={collective.slug} my={3} mx={2}>
-                    <CollectiveCard collective={collective} />
-                  </Flex>
-                ))}
+              </FormGroup>
+            </form>
+          </Box>
+          <Flex justifyContent={['center', 'center', 'flex-start']} flexWrap="wrap">
+            {loading && !collectives && (
+              <Flex py={3} width={1} justifyContent="center">
+                <LoadingGrid />
+              </Flex>
+            )}
+            {showCollectives &&
+              collectives.map(collective => (
+                <Flex key={collective.slug} my={3} mx={2}>
+                  <CollectiveCard collective={collective} />
+                </Flex>
+              ))}
 
-              {/* TODO: add suggested collectives when the result is empty */}
-              {showCollectives && collectives.length === 0 && (
-                <Flex py={3} width={1} justifyContent="center" flexDirection="column" alignItems="center">
-                  <p>
-                    <em>
-                      No collectives found matching your query: &quot;
-                      {term}
-                      &quot;
-                    </em>
-                  </p>
-                  {usePledges && (
-                    <Link route="createPledge" params={{ name: term }} passHref>
-                      <StyledLink
-                        display="block"
-                        fontSize="Paragraph"
-                        fontWeight="bold"
-                        maxWidth="220px"
-                        py={2}
-                        px={4}
-                        textAlign="center"
-                        buttonStyle="primary"
-                      >
-                        Make a pledge
-                      </StyledLink>
-                    </Link>
-                  )}
-                </Flex>
+            {/* TODO: add suggested collectives when the result is empty */}
+            {showCollectives && collectives.length === 0 && (
+              <Flex py={3} width={1} justifyContent="center" flexDirection="column" alignItems="center">
+                <p>
+                  <em>
+                    No collectives found matching your query: &quot;
+                    {term}
+                    &quot;
+                  </em>
+                </p>
+                {usePledges && (
+                  <Link route="createPledge" params={{ name: term }} passHref>
+                    <StyledLink
+                      display="block"
+                      fontSize="Paragraph"
+                      fontWeight="bold"
+                      maxWidth="220px"
+                      py={2}
+                      px={4}
+                      textAlign="center"
+                      buttonStyle="primary"
+                    >
+                      Make a pledge
+                    </StyledLink>
+                  </Link>
+                )}
+              </Flex>
+            )}
+          </Flex>
+          {showCollectives && collectives.length !== 0 && total > limit && (
+            <Container display="flex" justifyContent="center" fontSize="Paragraph" my={3}>
+              <Pagination offset={offset} total={total} limit={limit} />
+            </Container>
+          )}
+
+          {showCollectives && collectives.length !== 0 && (
+            <Flex py={3} width={1} justifyContent="center" flexDirection="column" alignItems="center">
+              <p>
+                <em>If you don&apos;t see the collective you&apos;re searching for:</em>
+              </p>
+              {usePledges && (
+                <Link route="createPledge" params={{ name: term }} passHref>
+                  <StyledLink
+                    display="block"
+                    fontSize="Paragraph"
+                    fontWeight="bold"
+                    maxWidth="220px"
+                    py={2}
+                    px={4}
+                    textAlign="center"
+                    buttonStyle="primary"
+                  >
+                    Make a pledge
+                  </StyledLink>
+                </Link>
               )}
             </Flex>
-            {showCollectives && collectives.length !== 0 && total > limit && (
-              <Container display="flex" justifyContent="center" fontSize="Paragraph" my={3}>
-                <Pagination offset={offset} total={total} limit={limit} />
-              </Container>
-            )}
-
-            {showCollectives && collectives.length !== 0 && (
-              <Flex py={3} width={1} justifyContent="center" flexDirection="column" alignItems="center">
-                <p>
-                  <em>If you don&apos;t see the collective you&apos;re searching for:</em>
-                </p>
-                {usePledges && (
-                  <Link route="createPledge" params={{ name: term }} passHref>
-                    <StyledLink
-                      display="block"
-                      fontSize="Paragraph"
-                      fontWeight="bold"
-                      maxWidth="220px"
-                      py={2}
-                      px={4}
-                      textAlign="center"
-                      buttonStyle="primary"
-                    >
-                      Make a pledge
-                    </StyledLink>
-                  </Link>
-                )}
-              </Flex>
-            )}
-            {showCollectives && collectives.length !== 0 && total > limit && (
-              <Flex justifyContent="center">
-                <ul className="pagination">
-                  {Array(Math.ceil(total / limit))
-                    .fill(1)
-                    .map((n, i) => (
-                      <li
-                        key={i * limit}
-                        className={classNames({
-                          active: i * limit === offset,
-                        })}
-                      >
-                        <Link
-                          href={{
-                            query: {
-                              limit,
-                              offset: i * limit,
-                              q: term,
-                            },
-                          }}
-                        >
-                          <a>{`${n + i}`}</a>
-                        </Link>
-                      </li>
-                    ))}
-                </ul>
-              </Flex>
-            )}
-
-            {showCollectives && collectives.length !== 0 && (
-              <Flex py={3} width={1} justifyContent="center" flexDirection="column" alignItems="center">
-                <p>
-                  <em>If you don&apos;t see the collective you&apos;re searching for:</em>
-                </p>
-
-                {usePledges && (
-                  <Link route="createPledge" params={{ name: term }} passHref>
-                    <StyledLink
-                      display="block"
-                      fontSize="Paragraph"
-                      fontWeight="bold"
-                      maxWidth="220px"
-                      py={2}
-                      px={4}
-                      textAlign="center"
-                      buttonStyle="primary"
-                    >
-                      Make a pledge
-                    </StyledLink>
-                  </Link>
-                )}
-              </Flex>
-            )}
-          </Container>
-        </Body>
-        <Footer />
-      </div>
+          )}
+        </Container>
+      </Page>
     );
   }
 }
