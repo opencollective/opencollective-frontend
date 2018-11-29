f175dd5b9224dce281ec8880f30e321a70bfcfcf
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-11-29 17:50:03 +0100

    Remove includeVirtualCards from getTransactionsQuery
    Should be merged with https://github.com/opencollective/opencollective-api/pull/1547

--- src/apps/expenses/components/TransactionsWithData.js
+++ src/apps/expenses/components/TransactionsWithData.js
@@ -60,7 +60,6 @@ export const addTransactionsData = graphql(getTransactionsQuery, {
         CollectiveId: props.collective.id,
         offset: 0,
         limit: props.limit || TRANSACTIONS_PER_PAGE * 2,
-        includeVirtualCards: true,
       },
     };
   },
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -57,8 +57,8 @@ export const transactionFields = `
 
 /* eslint-disable graphql/template-strings, graphql/no-deprecated-fields, graphql/capitalized-type-name, graphql/named-operations */
 export const getTransactionsQuery = gql`
-query Transactions($CollectiveId: Int!, $type: String, $limit: Int, $offset: Int, $dateFrom: String, $dateTo: String, $includeVirtualCards: Boolean = false) {
-  allTransactions(CollectiveId: $CollectiveId, type: $type, limit: $limit, offset: $offset, dateFrom: $dateFrom, dateTo: $dateTo, includeVirtualCards: $includeVirtualCards) {
+query Transactions($CollectiveId: Int!, $type: String, $limit: Int, $offset: Int, $dateFrom: String, $dateTo: String) {
+  allTransactions(CollectiveId: $CollectiveId, type: $type, limit: $limit, offset: $offset, dateFrom: $dateFrom, dateTo: $dateTo) {
     ${transactionFields}
     refundTransaction {
       ${transactionFields}
