a7d27b09ca21e64c74395568c4a609990a65e38b
HipsterBrown <headhipster@hipsterbrown.com>
2018-11-29 17:35:09 -0500

    docs(styleguide): add Caret

--- src/components/Caret.js
+++ src/components/Caret.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { display, height, width, style } from 'styled-system';
 
@@ -5,6 +6,9 @@ const cursor = style({
   prop: 'cursor',
 });
 
+/**
+ * @see See [styled-system docs](https://github.com/jxnblk/styled-system/blob/master/docs/api.md) for usage of those props
+ */
 const Caret = styled.div`
   position: relative;
 
@@ -35,4 +39,25 @@ const Caret = styled.div`
   }
 `;
 
+Caret.displayName = 'Caret';
+
+Caret.propTypes = {
+  /** Background color to match parent background */
+  bgColor: PropTypes.string,
+  /** Color of the caret stroke */
+  color: PropTypes.string,
+  /** From styled-system: accepts any css 'display' value */
+  display: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.array]),
+  /** Use relative sizing for best results, i.e. 1rem */
+  height: PropTypes.string.isRequired,
+  /** Use relative sizing for best results, i.e. 0.4rem */
+  strokeWidth: PropTypes.string.isRequired,
+};
+
+Caret.defaultProps = {
+  bgColor: 'white',
+  color: 'black',
+};
+
+/** @component */
 export default Caret;
