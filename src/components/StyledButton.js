4c4097ee3e1fe1a5855838b7ec7ad4d15c28047d
HipsterBrown <headhipster@hipsterbrown.com>
2018-11-29 17:34:41 -0500

    docs(styleguide): add StyledButton

--- src/components/StyledButton.js
+++ src/components/StyledButton.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import tag from 'clean-tag';
 import {
@@ -16,9 +17,15 @@ import {
 } from 'styled-system';
 import { buttonSize, buttonStyle } from '../constants/theme';
 
+/**
+ * styled-component button using styled-system
+ *
+ * @see See [styled-system docs](https://github.com/jxnblk/styled-system/blob/master/docs/api.md) for usage of those props
+ */
 const StyledButton = styled(tag.button)`
   appearance: none;
   border: none;
+  cursor: pointer;
 
   ${bgColor}
   ${border}
@@ -37,10 +44,44 @@ const StyledButton = styled(tag.button)`
   ${buttonSize}
 `;
 
+StyledButton.propTypes = {
+  /** @ignore */
+  blacklist: PropTypes.arrayOf(PropTypes.string),
+  /**
+   * Based on the design system theme
+   */
+  buttonSize: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Based on the design system theme
+   */
+  buttonStyle: PropTypes.oneOf(['primary', 'standard']),
+  /**
+   * From styled-system: accepts any css 'display' value
+   */
+  display: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.array]),
+  /**
+   * From styled-system: accepts any css 'font-weight' value
+   */
+  fontWeight: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.array]),
+  /**
+   * From styled-system: accepts any css 'max-width' value
+   */
+  maxWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.array]),
+  /**
+   * From styled-system: accepts any css 'text-align' value
+   */
+  textAlign: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.array]),
+  /**
+   * From styled-system: accepts any css 'width' value
+   */
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.array]),
+};
+
 StyledButton.defaultProps = {
   blacklist: tag.defaultProps.blacklist.concat('buttonStyle', 'buttonSize'),
   buttonSize: 'medium',
   buttonStyle: 'standard',
 };
 
+/** @component */
 export default StyledButton;
