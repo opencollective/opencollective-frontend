d49ef974adc3ee9159091a4e2ac3fbcda0ccebbb
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-11-30 22:13:28 +0100

    Tweak cypress config to make it faster

--- scripts/run_e2e_tests.sh
+++ scripts/run_e2e_tests.sh
@@ -9,6 +9,13 @@ if [ "$NODE_ENV" = "circleci" ]; then
   echo "> Starting frontend server"
   npm start &
   FRONTEND_PID=$!
+  # Record video and upload them if test fail on CI
+  CYPRESS_CONFIG="video=true,videoUploadOnPasses=true"
+  CYPRESS_RECORD="--record"
+else
+  # Never record video in dev
+  CYPRESS_CONFIG="video=false"
+  CYPRESS_RECORD=""
 fi
 echo ""
 echo "> Starting server jest tests"
@@ -23,7 +30,7 @@ echo ""
 echo "> Ensure cypress binary is installed (should normally be cached)"
 cypress install
 echo "> Running cypress tests"
-cypress run --record
+cypress run ${CYPRESS_RECORD} --config ${CYPRESS_CONFIG}
 RETURN_CODE=$?
 if [ $RETURN_CODE -ne 0 ]; then
   echo "Error with cypress e2e tests, exiting"
