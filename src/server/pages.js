0e55621821753fb6ce165f48600ac9b73f2bedbe
Xavier Damman <xdamman@gmail.com>
2018-11-30 10:43:32 +0100

    feat(orders): new /orders and /orders/:OrderId pages

--- src/server/pages.js
+++ src/server/pages.js
@@ -56,6 +56,8 @@ pages
     'expenses',
     '/:parentCollectiveSlug?/:collectiveType(events)?/:collectiveSlug/expenses/:filter(categories|recipients)?/:value?',
   )
+  .add('orders', '/:parentCollectiveSlug?/:collectiveType(events)?/:collectiveSlug/orders')
+  .add('order', '/:parentCollectiveSlug?/:collectiveType(events)?/:collectiveSlug/orders/:OrderId([0-9]+)')
   .add('discover', '/discover');
 
 if (USE_PLEDGES) {
