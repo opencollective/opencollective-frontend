3af9852a5fbb9e63d063e13137c18929f962b0d8
HipsterBrown <headhipster@hipsterbrown.com>
2018-12-03 11:08:34 -0500

    feat(styleguide/ThemeWrapper): include frontend app global styles

--- styleguide/ThemeWrapper.js
+++ styleguide/ThemeWrapper.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from '../src/constants/theme';
+import AppGlobalStyles from '../src/pages/global-styles';
+import { createGlobalStyle } from 'styled-components';
+
+const StyleguideGlobalStyles = createGlobalStyle`
+  body {
+    font-size: 14px;
+  }
+`;
 
 export default class ThemeWrapper extends Component {
   render() {
-    return <ThemeProvider theme={theme}>{this.props.children}</ThemeProvider>;
+    return (
+      <Fragment>
+        <AppGlobalStyles />
+        <StyleguideGlobalStyles />
+        <ThemeProvider theme={theme}>{this.props.children}</ThemeProvider>
+      </Fragment>
+    );
   }
 }
