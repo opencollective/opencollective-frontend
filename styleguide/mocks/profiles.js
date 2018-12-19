883dd9e1042151a1c656db071cd8ccbd099fb7c4
HipsterBrown <headhipster@hipsterbrown.com>
2018-12-19 00:17:23 -0500

    docs(styleguide/examples/ContributeAs): initial coverage

---
+++ styleguide/mocks/profiles.js
@@ -0,0 +1,32 @@
+export const personalProfile = {
+  email: 'example.person@example.com',
+  name: 'Example Person',
+  type: 'USER',
+};
+
+export default {
+  '13': {
+    name: 'Open Collective Inc.',
+    type: 'COLLECTIVE',
+  },
+  '26': {
+    name: 'Sustain',
+    type: 'COLLECTIVE',
+  },
+  '39': {
+    name: 'Democracy Earth Foundation',
+    type: 'COLLECTIVE',
+  },
+  '52': {
+    name: 'Airbnb',
+    type: 'ORGANIZATION',
+  },
+  '65': {
+    name: 'Trivago',
+    type: 'ORGANIZATION',
+  },
+  '65': {
+    name: 'Trivago',
+    type: 'ORGANIZATION',
+  },
+};
