4df01cc1ca98950eeb660c75581332be601d354d
François Hodierne <francois@hodierne.net>
2019-01-08 12:46:57 +0100

    fix(sequelize): read dialectOptions and operatorsAliases from config

--- config/sequelize-cli.js
+++ config/sequelize-cli.js
@@ -1,10 +1,14 @@
 import '../server/env';
 
+import config from 'config';
+import { get } from 'lodash';
+
 import { getDBConf } from '../server/lib/db';
 
 const dbConfig = getDBConf('database');
 
 export default {
   ...dbConfig,
-  operatorsAliases: false,
+  dialectOptions: get(config.database.options, 'dialectOptions', []),
+  operatorsAliases: get(config.database.options, 'operatorsAliases', false),
 };
