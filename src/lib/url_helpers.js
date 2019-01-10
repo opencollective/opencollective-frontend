a62905751ec880a330fb2f209f28080596cb6982
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-01-10 13:43:42 +0100

    feat(ContributionFlow): Submit form with recaptcha and success page

--- src/lib/url_helpers.js
+++ src/lib/url_helpers.js
@@ -1,4 +1,36 @@
+import { pickBy, isEmpty } from 'lodash';
+
 export const invoiceServiceURL = process.env.INVOICES_URL;
+
+// ---- Utils ----
+
+/**
+ * Transorm an object into a query string. Strips undefined values.
+ *
+ * ## Example
+ *
+ *    > objectToQueryString({a: 42, b: "hello", c: undefined})
+ *    "?a=42&b=hello"
+ */
+export const objectToQueryString = options => {
+  const definedOptions = pickBy(options, value => value !== undefined);
+  if (isEmpty(definedOptions)) {
+    return '';
+  }
+
+  const encodeValue = value => {
+    if (Array.isArray(value)) {
+      return value.concat.map(encodeURIComponent).join(',');
+    }
+    return encodeURIComponent(value);
+  };
+
+  return `?${Object.entries(definedOptions)
+    .map(([key, value]) => `${key}=${encodeValue(value)}`)
+    .join('&')}`;
+};
+
+// ---- Routes to other Open Collective services ----
 
 export const collectiveInvoiceURL = (collectiveSlug, invoiceSlug, format) => {
   return `${invoiceServiceURL}/collectives/${collectiveSlug}/${invoiceSlug}.${format}`;
@@ -6,4 +38,24 @@ export const collectiveInvoiceURL = (collectiveSlug, invoiceSlug, format) => {
 
 export const transactionInvoiceURL = transactionUUID => {
   return `${invoiceServiceURL}/transactions/${transactionUUID}/invoice.pdf`;
+};
+
+// ---- Routes to external services ----
+
+/**
+ * @param opts {object} With the following attributes:
+ *  - text: Tweet text
+ *  - url: A URL to share in the tweet
+ *  - via: A Twitter username to associate with the Tweet, such as your site’s Twitter account
+ */
+export const tweetURL = opts => {
+  return `https://twitter.com/intent/tweet${objectToQueryString(opts)}`;
+};
+
+/**
+ * @param opts {object} With the following attributes:
+ *  - u: A URL to share in the tweet
+ */
+export const facebooKShareURL = opts => {
+  return `https://www.facebook.com/sharer/sharer.php${objectToQueryString(opts)}`;
 };
