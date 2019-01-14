368efe225eb42994e62b1cf25749c20940c1f7ea
HipsterBrown <headhipster@hipsterbrown.com>
2019-01-14 16:24:31 -0500

    chore(styleguide.config.js): add Grid && UI sections, custom blockquote style

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,6 +4,11 @@ const fileExistsCaseInsensitive = require('react-styleguidist/scripts/utils/find
 
 module.exports = {
   assetsDir: 'styleguide',
+  compilerConfig: {
+    transforms: {
+      dangerousTaggedTemplateString: true,
+    },
+  },
   getExampleFilename(componentPath) {
     const examplePath = path.join(__dirname, 'styleguide', 'examples', `${path.parse(componentPath).name}.md`);
     const existingFile = fileExistsCaseInsensitive(examplePath);
@@ -12,9 +17,40 @@ module.exports = {
     }
     return false;
   },
+  pagePerSection: true,
+  sections: [
+    {
+      name: 'UI',
+      content: 'styleguide/pages/UI.md',
+      components: 'src/components/*.js',
+    },
+    {
+      name: 'Grid',
+      content: 'styleguide/pages/Grid.md',
+      sections: [
+        {
+          name: 'Box',
+          content: 'styleguide/examples/Box.md',
+        },
+        {
+          name: 'Flex',
+          content: 'styleguide/examples/Flex.md',
+        },
+      ],
+    },
+  ],
   skipComponentsWithoutExample: true,
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'styleguide/ThemeWrapper'),
+  },
+  styles: {
+    Blockquote: {
+      blockquote: {
+        borderLeft: '3px solid grey',
+        margin: '16px 0',
+        padding: '0 32px',
+      },
+    },
   },
   title: 'Open Collective Frontend Style Guide',
   usageMode: 'expand',
