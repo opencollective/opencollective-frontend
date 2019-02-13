0192b4bbec0c7527a12341897d1e06620e8f29fd
flickz <omoyajowo2015@gmail.com>
2019-02-13 13:38:10 +0100

    Remove unused variables

--- src/pages/openSourceApply.js
+++ src/pages/openSourceApply.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import { Flex, Box } from '@rebass/grid';
