127ab16fd01813ce85fe2c596011c90f5e5789d7
flickz <omoyajowo2015@gmail.com>
2019-02-13 08:26:54 +0100

    Add e2e test for createCollective From Github

---
+++ cypress/integration/31-create-collectiveFromGithub.test.js
@@ -0,0 +1,46 @@
+describe('Create collective from Github', () => {
+  it('Should submit create github page', () => {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: '/api/github-repositories?*',
+      response: [
+        {
+          description: 'Adblock Plus browser extension',
+          fork: true,
+          full_name: 'flickz/adblockpluschrome',
+          name: 'adblockpluschrome',
+          owner: { login: 'flickz', type: 'Organization' },
+          stargazers_count: 113,
+        },
+        {
+          description:
+            'A new form of association, transparent by design. Please report issues there. Feature requests and ideas welcome!',
+          fork: true,
+          full_name: 'flickz/jobtweets',
+          name: 'JobTweets',
+          owner: { login: 'flickz', type: 'User' },
+          stargazers_count: 103,
+        },
+      ],
+    });
+    cy.login({ email: 'testuser@opencollective.com', redirect: '/opensource/create?token=foofoo' });
+    cy.contains('Pick a repository');
+    cy.get('[type="radio"]')
+      .first()
+      .check();
+    cy.get('[name="name"]')
+      .first()
+      .type('Adblock plus');
+    cy.get('[name="slug"]')
+      .first()
+      .type('adblock');
+    cy.get('[type="submit"]')
+      .first()
+      .click();
+    cy.wait(3500);
+    cy.location().should(location => {
+      expect(location.search).to.eq('?status=collectiveCreated');
+    });
+  });
+});
