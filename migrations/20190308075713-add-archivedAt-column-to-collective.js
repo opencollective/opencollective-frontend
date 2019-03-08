68e172d80863e43428352f89e0e2ceb66de2c2d0
flickz <omoyajowo2015@gmail.com>
2019-03-08 22:48:02 +0100

    Remove addColumn and removeColumn for archiveAt in collectiveHistories
    I'm removing this because I just realized archiveAt already exist in collectiveHistories table

--- migrations/20190308075713-add-archivedAt-column-to-collective.js
+++ migrations/20190308075713-add-archivedAt-column-to-collective.js
@@ -9,11 +9,9 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const colParams = { type: Sequelize.DATE };
     await queryInterface.addColumn('Collectives', colName, colParams);
-    await queryInterface.addColumn('CollectiveHistories', colName, colParams);
   },
 
   down: async queryInterface => {
     await queryInterface.removeColumn('Collectives', colName);
-    await queryInterface.removeColumn('CollectiveHistories', colName);
   },
 };
