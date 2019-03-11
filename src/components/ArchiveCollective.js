d54b5d586305e258b808f2e044f74682f13a25a5
flickz <omoyajowo2015@gmail.com>
2019-03-11 22:39:49 +0100

    Add ArchiveCollective component functionality - Handle onClick ArchiveCollective. - Display error message when an error occur during archiving process. - Set a loading spinner on "Archive Collective" button during archiving process. - Hide "Archive Collective" button and display MessageBox indicating a collective has already been archived. - Redirect user to collective page after archiving process was successful

--- src/components/ArchiveCollective.js
+++ src/components/ArchiveCollective.js
@@ -1,24 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
+import { addArchiveCollectiveMutation } from '../graphql/mutations';
 import { H2, P } from './Text';
 import Container from './Container';
 import StyledButton from './StyledButton';
+import MessageBox from './MessageBox';
+import Modal from './Modal';
+import { Router } from '../server/pages';
 
-const ArchiveCollective = ({ collective }) => {
+const ArchiveCollective = ({ collective, archiveCollective }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState(null);
+  const [archiving, setArchiving] = useState(false);
+
   const collectiveType = collective.type === 'ORGANIZATION' ? 'Organization' : 'Collective';
+  const handleArchiveCollective = async ({ archiveCollective, id }) => {
+    setShowModal(false);
+    try {
+      setArchiving(true);
+      await archiveCollective(id);
+      setArchiving(false);
+      Router.pushRoute('collective', {
+        slug: collective.slug,
+        status: 'collectiveArchived',
+      });
+    } catch (err) {
+      console.error('>>> archiveCollective error: ', JSON.stringify(err));
+      setError(`An error occur while archiving your ${collectiveType.toLocaleLowerCase()}. Please try again later.`);
+      setArchiving(false);
+    }
+  };
 
   return (
     <Container>
       <H2>Archive this {collectiveType}.</H2>
       <P>Mark this {collectiveType.toLocaleLowerCase()} as archived, delete all tiers and cancel all subscriptions.</P>
-      <StyledButton>Archive this {collectiveType.toLocaleLowerCase()}</StyledButton>
+      {error && <P color="#ff5252">{error}</P>}
+      {!collective.archived && (
+        <StyledButton onClick={() => setShowModal(true)} loading={archiving}>
+          Archive this {collectiveType.toLocaleLowerCase()}
+        </StyledButton>
+      )}
+      {collective.archived && (
+        <MessageBox withIcon type="info">
+          {collectiveType} already archived.
+        </MessageBox>
+      )}
+      <Modal
+        onClose={() => setShowModal(false)}
+        show={showModal}
+        className="confirm-ArchiveCollective"
+        title={`Are you sure you want to archive ${collectiveType.toLocaleLowerCase()}?`}
+      >
+        <Container display="flex" justifyContent="space-between" width={1} mt={4}>
+          <StyledButton onClick={() => setShowModal(false)}>Cancel</StyledButton>
+          <StyledButton
+            buttonStyle="primary"
+            onClick={() => handleArchiveCollective({ archiveCollective, id: collective.id })}
+          >
+            Archive
+          </StyledButton>
+        </Container>
+      </Modal>
     </Container>
   );
 };
 
 ArchiveCollective.propTypes = {
   collective: PropTypes.object.isRequired,
+  archiveCollective: PropTypes.func,
 };
 
-export default ArchiveCollective;
+export default addArchiveCollectiveMutation(ArchiveCollective);
