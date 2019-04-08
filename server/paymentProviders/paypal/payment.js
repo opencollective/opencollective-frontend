bb15439fbb301390ebb11293e2b8f7f4934336a2
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-04-08 22:58:24 +0200

    chore(PayPal): Improve the way we retrieve data in paymentInfo

--- server/paymentProviders/paypal/payment.js
+++ server/paymentProviders/paypal/payment.js
@@ -1,4 +1,5 @@
 import config from 'config';
+import { get } from 'lodash';
 
 import models from '../../models';
 import roles from '../../constants/roles';
@@ -101,10 +102,10 @@ export async function executePayment(order) {
 export async function createTransaction(order, paymentInfo) {
   /* The `* 100` in the next lines convert from PayPal format in
      dollars to Open Collective format in cents */
-  const amountFromPayPal = paymentInfo.transactions.map(t => parseFloat(t.amount.total)) * 100;
-  const paypalFee =
-    paymentInfo.transactions.map(t => t.related_resources.map(r => parseFloat(r.sale.transaction_fee.value))) * 100;
-  const currencyFromPayPal = paymentInfo.transactions.map(t => t.amount.currency)[0];
+  const transaction = paymentInfo.transactions[0];
+  const amountFromPayPal = parseFloat(transaction.amount.total) * 100;
+  const paypalFee = parseFloat(get(transaction, 'related_resources.0.sale.transaction_fee.value', '0.0')) * 100;
+  const currencyFromPayPal = transaction.amount.currency;
 
   const hostFeeInHostCurrency = libpayments.calcFee(amountFromPayPal, order.collective.hostFeePercent);
   const platformFeeInHostCurrency = libpayments.calcFee(amountFromPayPal, constants.OC_FEE_PERCENT);
