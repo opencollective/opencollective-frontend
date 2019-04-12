6c01879d251e2a430ec5fd39c91cd404fd67341c
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-04-12 22:27:38 +0200

    chore(SearchPage): Remove snapshot testing

--- src/pages/__tests__/search.test.js
+++ src/pages/__tests__/search.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import 'jest-styled-components';
 
 import Router from 'next/router';
-import { withMockRouterContext } from '../../../test/mocks/withMockRouter';
 
 import { MockSearchPage } from '../search';
 
@@ -13,92 +11,6 @@ describe('Search Page', () => {
     data: {},
     getLoggedInUser: () => Promise.resolve({}),
   };
-
-  const MockRouter = withMockRouterContext({});
-
-  it('renders loading grid SVG', () => {
-    const loadingProps = {
-      ...props,
-      data: {
-        loading: true,
-      },
-    };
-
-    const tree = renderer.create(<MockSearchPage {...loadingProps} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders empty results message', () => {
-    const emptyResultsProps = {
-      ...props,
-      data: {
-        search: {
-          collectives: [],
-        },
-      },
-      term: 'test',
-    };
-
-    const tree = renderer.create(<MockSearchPage {...emptyResultsProps} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders "Make pledge" button with empty results', () => {
-    const emptyResultsProps = {
-      ...props,
-      data: {
-        search: {
-          collectives: [],
-        },
-      },
-      term: 'test',
-    };
-
-    const tree = renderer.create(<MockSearchPage {...emptyResultsProps} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders error message', () => {
-    const errorProps = {
-      ...props,
-      data: {
-        error: {
-          message: 'Test error',
-        },
-      },
-    };
-
-    const tree = renderer.create(<MockSearchPage {...errorProps} />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders search results with pagination', () => {
-    const resultsProps = {
-      ...props,
-      data: {
-        search: {
-          collectives: [
-            {
-              slug: 'test-collective',
-            },
-          ],
-          limit: 20,
-          offset: 0,
-          total: 100,
-        },
-      },
-      term: 'Test',
-    };
-
-    const tree = renderer
-      .create(
-        <MockRouter>
-          <MockSearchPage {...resultsProps} />
-        </MockRouter>,
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-  });
 
   it('calls Router.push on form submission', () => {
     Router.push = jest.fn();
