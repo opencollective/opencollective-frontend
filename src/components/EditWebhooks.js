a34ac9569c9d117e006c6e7bbff19a33823c62e4
Elijah Oyekunle <eloyekunle@gmail.com>
2019-04-18 13:30:39 +0100

    implement EditWebhook

--- src/components/EditWebhooks.js
+++ src/components/EditWebhooks.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { Button, Form } from 'react-bootstrap';
 import { defineMessages } from 'react-intl';
-import withIntl from '../lib/withIntl';
 import { get } from 'lodash';
+
+import withIntl from '../lib/withIntl';
 import InputField from './InputField';
 import events from '../constants/events';
 
@@ -19,10 +19,6 @@ class EditWebhooks extends React.Component {
     const { intl } = props;
 
     this.state = { webhooks: [...props.webhooks] || [{}] };
-    this.renderWebhook = this.renderWebhook.bind(this);
-    this.addWebhook = this.addWebhook.bind(this);
-    this.removeWebhook = this.removeWebhook.bind(this);
-    this.editWebhook = this.editWebhook.bind(this);
     this.onChange = props.onChange.bind(this);
 
     this.defaultType = this.props.defaultType || 'TICKET';
@@ -34,7 +30,7 @@ class EditWebhooks extends React.Component {
       },
       'webhooks.types.label': {
         id: 'webhooks.types.label',
-        defaultMessage: 'Types',
+        defaultMessage: 'Activities',
       },
       'webhooks.add': {
         id: 'webhooks.add',
@@ -46,14 +42,6 @@ class EditWebhooks extends React.Component {
       },
     });
 
-    const getOptions = arr => {
-      return arr.map(key => {
-        const obj = {};
-        obj[key] = intl.formatMessage(this.messages[key]);
-        return obj;
-      });
-    };
-
     this.fields = [
       {
         name: 'url',
@@ -62,10 +50,10 @@ class EditWebhooks extends React.Component {
         label: intl.formatMessage(this.messages['webhooks.url.label']),
       },
       {
-        name: 'types',
+        name: 'activities',
         type: 'select',
         label: intl.formatMessage(this.messages['webhooks.types.label']),
-        options: events.map(event => {
+        options: events.sort().map(event => {
           return { value: event, label: event };
         }),
         multiple: true,
@@ -74,46 +62,28 @@ class EditWebhooks extends React.Component {
     ];
   }
 
-  editWebhook(index, fieldname, value) {
+  editWebhook = (index, fieldname, value) => {
     const { webhooks } = this.state;
-    webhooks[index] = {
-      ...webhooks[index],
-      role: webhooks[index].role || 'ADMIN',
-    };
-    const obj = {};
-    if (fieldname.indexOf('.') !== -1) {
-      const tokens = fieldname.split('.');
-      const parent = tokens[0];
-      fieldname = tokens[1];
-      obj[parent] = {};
-      obj[parent][fieldname] = value;
-      webhooks[index][parent] = {
-        ...webhooks[index][parent],
-        [fieldname]: value,
-      };
-    } else {
-      webhooks[index][fieldname] = value;
-    }
-
+    webhooks[index][fieldname] = value;
     this.setState({ webhooks });
     this.onChange({ webhooks });
-  }
+  };
 
-  addWebhook(webhook) {
+  addWebhook = webhook => {
     const { webhooks } = this.state;
     webhooks.push(webhook || {});
     this.setState({ webhooks });
-  }
+  };
 
-  removeWebhook(index) {
+  removeWebhook = index => {
     const { webhooks } = this.state;
     if (index < 0 || index > webhooks.length) return;
     webhooks.splice(index, 1);
     this.setState({ webhooks });
     this.onChange({ webhooks });
-  }
+  };
 
-  renderWebhook(webhook, index) {
+  renderWebhook = (webhook, index) => {
     const { intl } = this.props;
 
     return (
@@ -146,7 +116,7 @@ class EditWebhooks extends React.Component {
         </Form>
       </div>
     );
-  }
+  };
 
   render() {
     const { intl } = this.props;
