71644b2c8ed8f8ecf52c52f0891291758d8f01f5
Elijah Oyekunle <eloyekunle@gmail.com>
2019-04-18 18:46:24 +0100

    removed unused var

--- src/components/EditWebhooks.js
+++ src/components/EditWebhooks.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Form } from 'react-bootstrap';
 import { defineMessages } from 'react-intl';
-import { get, keys } from 'lodash';
+import { get } from 'lodash';
 
 import withIntl from '../lib/withIntl';
 import InputField from './InputField';
