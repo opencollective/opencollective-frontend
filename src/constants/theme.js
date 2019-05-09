opencollective/opencollective-frontend

commit f4bd5f8cc70f9011f66380169e02c709fa49da40

feat(CollectivePage): Add Hero component

src/constants/theme.js
@@ -3,7 +3,7 @@ import { transparentize } from 'polished';
 
 export const colors = {
   black: {
-    900: '#141414',
+    900: '#090A0A',
     800: '#313233',
     700: '#4E5052',
     600: '#76777A',
@@ -98,13 +98,16 @@ const theme = {
   fontSizes,
   lineHeights,
   space,
-  breakpoints: ['40em', '52em', '64em'],
+  breakpoints: [
+    '40em', //  640px - mobile
+    '52em', //  832px - tablet
+    '64em', // 1024px - desktop
+    '88em', // 1408px - widescreen
+  ],
   buttons: {
     standard: {
       backgroundColor: 'white',
-      border: '1px solid',
       borderColor: colors.black[300],
-      borderRadius: '100px',
       color: colors.black[600],
 
       '&:hover': {
@@ -133,8 +136,6 @@ const theme = {
 
     primary: {
       backgroundColor: colors.primary[500],
-      border: '1px solid',
-      borderRadius: '100px',
       borderColor: colors.primary[500],
       color: 'white',
 
@@ -160,6 +161,12 @@ const theme = {
         borderColor: colors.primary[50],
         cursor: 'not-allowed',
       },
+    },
+
+    dark: {
+      backgroundColor: colors.black[900],
+      color: colors.white.full,
+      borderColor: colors.black[900],
     },
   },
   buttonSizes: {

src/components/Avatar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { backgroundImage, borders, borderColor, size, themeGet } from 'styled-system';
+import { backgroundImage, backgroundColor, borders, borderColor, size, themeGet } from 'styled-system';
 import tag from 'clean-tag';
 import { Flex } from '@rebass/grid';
 import withFallbackImage from '../lib/withFallbackImage';
@@ -11,6 +11,7 @@ const getInitials = name => name.split(' ').reduce((result, value) => (result +=
 export const StyledAvatar = styled(Flex)`
   align-items: center;
   background-color: ${({ theme, type }) => (type === 'USER' ? themeGet('colors.black.100')({ theme }) : 'none')};
+  ${backgroundColor}
   ${backgroundImage}
   background-position: center center;
   background-repeat: no-repeat;

src/components/AvatarWithHost.js
@@ -1,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import { CollectiveType } from '../constants/collectives';
+import withFallbackImage from '../lib/withFallbackImage';
+import Avatar from './Avatar';
+import LinkCollective from './LinkCollective';
+
+const MainContainer = styled.div`
+  display: inline-block;
+  position: relative;
+`;
+
+const HostLink = styled(LinkCollective)`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  & > div {
+    margin-left: 25%;
+    margin-bottom: -25%;
+  }
+`;
+
+/**
+ * Same as `Avatar`, except this one also displays the host avatar on
+ * the bottom right corner.
+ */
+const AvatarWithHost = ({ collective, host, radius }) => {
+  return (
+    <MainContainer>
+      <LinkCollective collective={collective}>
+        <Avatar
+          type={collective.type}
+          src={collective.image}
+          backgroundColor="#EBEBEB"
+          border="1px solid #efefef"
+          radius={radius}
+          borderRadius={radius / 4}
+        />
+      </LinkCollective>
+      {host && (
+        <HostLink collective={host}>
+          <Avatar
+            type={host.type}
+            src={host.image}
+            border="1px solid #efefef"
+            radius={radius / 4}
+            borderRadius={radius / 16}
+            title={host.name}
+          />
+        </HostLink>
+      )}
+    </MainContainer>
+  );
+};
+
+AvatarWithHost.propTypes = {
+  /** The main collective to display the image for */
+  collective: PropTypes.shape({
+    type: PropTypes.oneOf(Object.keys(CollectiveType)),
+    image: PropTypes.string,
+  }).isRequired,
+
+  /** The host collective (if any) */
+  host: PropTypes.shape({
+    type: PropTypes.oneOf(Object.keys(CollectiveType)),
+    image: PropTypes.string,
+  }),
+
+  /** Size of the main image in pixels. Should ideally be a multiple of 4. */
+  radius: PropTypes.number,
+};
+
+export default withFallbackImage(AvatarWithHost);

src/components/DefinedTerm.js
@@ -1,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { defineMessages } from 'react-intl';
+import styled from 'styled-components';
+
+import withIntl from '../lib/withIntl';
+import StyledTooltip from './StyledTooltip';
+
+/**
+ * All the terms defined here must have a matching translation
+ * in both `TranslatedTerms` and `TranslatedDefinitions`.
+ */
+export const Terms = {
+  FISCAL_HOST: 'FiscalHost',
+};
+
+const TranslatedTerms = defineMessages({
+  [Terms.FISCAL_HOST]: {
+    id: 'Fiscalhost',
+    defaultMessage: 'Fiscal Host',
+  },
+});
+
+const TranslatedDefinitions = defineMessages({
+  [Terms.FISCAL_HOST]: {
+    id: 'Fiscalhost.definition',
+    defaultMessage:
+      'A fiscal host is a legal entity holding the money and responsible for the admin/taxes forms for the collective.',
+  },
+});
+
+const UnderlinedTerm = styled.span`
+  color: #969ba3;
+  border-bottom: 2px dotted #969ba3;
+  padding-bottom: 0.1em;
+  cursor: help;
+`;
+
+/**
+ * Underlines the given word and show a tooltip with the definition when focused
+ * or hovered. Both the term and the definition are translated.
+ */
+const DefinedTerm = ({ intl, term }) => {
+  return (
+    <StyledTooltip content={() => intl.formatMessage(TranslatedDefinitions[term])}>
+      <UnderlinedTerm>{intl.formatMessage(TranslatedTerms[term])}</UnderlinedTerm>
+    </StyledTooltip>
+  );
+};
+
+DefinedTerm.propTypes = {
+  /** The term to be defined */
+  term: PropTypes.oneOf(Object.values(Terms)),
+  /** @ignore from withIntl */
+  intl: PropTypes.object.isRequired,
+};
+
+export default withIntl(DefinedTerm);

src/components/Footer.js
@@ -116,7 +116,7 @@ class Footer extends React.Component {
                   LANGUAGES
                 </Span>
                 <StyledTooltip
-                  content={
+                  content={() => (
                     <FormattedMessage
                       id="Footer.Languages.JoinEffort"
                       defaultMessage="No technical skill is required to contribute to translations. You can join the effort on {crowdinLink} 🌐"
@@ -128,7 +128,7 @@ class Footer extends React.Component {
                         ),
                       }}
                     />
-                  }
+                  )}
                 >
                   <InfoCircle size={16} cursor="pointer" />
                 </StyledTooltip>

src/components/I18nCollectiveTags.js
@@ -1,0 +1,96 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { defineMessages } from 'react-intl';
+import { CollectiveCategory } from '../constants/collectives';
+import withIntl from '../lib/withIntl';
+
+const TranslatedTags = defineMessages({
+  [CollectiveCategory.ASSOCIATION]: {
+    id: 'Tags.ASSOCIATION',
+    defaultMessage: 'Association',
+  },
+  [CollectiveCategory.COLLECTIVE]: {
+    id: 'Tags.COLLECTIVE',
+    defaultMessage: 'Collective',
+  },
+  [CollectiveCategory.CONFERENCE]: {
+    id: 'Tags.CONFERENCE',
+    defaultMessage: 'Conference',
+  },
+  [CollectiveCategory.COOPERATIVE]: {
+    id: 'Tags.COOPERATIVE',
+    defaultMessage: 'Cooperative',
+  },
+  [CollectiveCategory.OPEN_SOURCE]: {
+    id: 'Tags.OPEN_SOURCE',
+    defaultMessage: 'Open source',
+  },
+  [CollectiveCategory.MEDIA]: {
+    id: 'Tags.MEDIA',
+    defaultMessage: 'Media',
+  },
+  [CollectiveCategory.MEETUP]: {
+    id: 'Tags.MEETUP',
+    defaultMessage: 'Meetup',
+  },
+  [CollectiveCategory.MOVEMENT]: {
+    id: 'Tags.MOVEMENT',
+    defaultMessage: 'Movement',
+  },
+  [CollectiveCategory.POLITICS]: {
+    id: 'Tags.POLITICS',
+    defaultMessage: 'Politics',
+  },
+  [CollectiveCategory.TECH_MEETUP]: {
+    id: 'Tags.TECH_MEETUP',
+    defaultMessage: 'Tech meetup',
+  },
+  [CollectiveCategory.US_NONPROFIT]: {
+    id: 'Tags.US_NONPROFIT',
+    defaultMessage: 'US nonprofit',
+  },
+});
+
+/** Translates a list of tags */
+class I18nCollectiveTags extends React.Component {
+  static propTypes = {
+    /** A tag or a list of tags */
+    tags: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]).isRequired,
+    /** A function used to render the tag */
+    children: PropTypes.func.isRequired,
+    /** Ignore tags if translation is missing */
+    ignoreUntranslated: PropTypes.bool,
+    /** @ignore */
+    intl: PropTypes.object,
+  };
+
+  static defaultProps = {
+    ignoreUntranslated: false,
+    /** Default renderer, will render a string list */
+    children: tags => {
+      return tags.map((tag, index, translatedTags) => {
+        if (index === translatedTags.length - 1) {
+          return tag.value;
+        } else {
+          return `${tag.value}, `;
+        }
+      });
+    },
+  };
+
+  render() {
+    const { intl, children, tags, ignoreUntranslated } = this.props;
+    const tagsToTranslate = typeof tags === 'string' ? [tags] : tags;
+    const processedTags = tagsToTranslate.map(tag => {
+      if (TranslatedTags[tag]) {
+        return { value: intl.formatMessage(TranslatedTags[tag]), isTranslated: true };
+      } else {
+        return { value: tag, isTranslated: false };
+      }
+    });
+
+    return children(ignoreUntranslated ? processedTags.filter(t => t.isTranslated) : processedTags);
+  }
+}
+
+export default withIntl(I18nCollectiveTags);

src/components/StyledButton.js
@@ -11,6 +11,7 @@ import {
   fontFamily,
   fontSize,
   fontWeight,
+  height,
   minWidth,
   maxWidth,
   space,
@@ -31,6 +32,8 @@ const StyledButtonContent = styled(tag.button)`
   border: none;
   cursor: pointer;
   outline: 0;
+  border: 1px solid;
+  border-radius: 100px;
 
   ${buttonStyle}
   ${buttonSize}
@@ -63,6 +66,7 @@ const StyledButtonContent = styled(tag.button)`
   ${space}
   ${textAlign}
   ${width}
+  ${height}
 `;
 
 const StyledButton = ({ loading, ...props }) =>
@@ -84,7 +88,7 @@ StyledButton.propTypes = {
   /**
    * Based on the design system theme
    */
-  buttonStyle: PropTypes.oneOf(['primary', 'standard']),
+  buttonStyle: PropTypes.oneOf(['primary', 'standard', 'dark']),
   /**
    * From styled-system: accepts any css 'display' value
    */

src/components/StyledRoundButton.js
@@ -1,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import StyledButton from './StyledButton';
+
+/**
+ * A round button with content centered. Accepts all props from `StyledButton`
+ */
+const StyledRoundButton = styled(({ size, ...props }) => (
+  <StyledButton display="flex" width={size} height={size} p={0} {...props} />
+))`
+  justify-content: center;
+  align-items: center;
+`;
+
+StyledRoundButton.propTypes = {
+  /** From styled-system: accepts any css 'width' value */
+  size: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.array]),
+};
+
+StyledRoundButton.defaultProps = {
+  size: 42,
+};
+
+export default StyledRoundButton;

commit cb0cb033e55b1b99fc59adfdaa53a246e080c09a

fix(StyledLink): Rounded button when using buttonStyle
Fix rounded button issue with StyledLink introduced in https://github.com/opencollective/opencollective-frontend/commit/f4bd5f8cc70f9011f66380169e02c709fa49da40?short_path=7055803#diff-ae9317587611fe2fedcb543e97242b52L105

src/constants/theme.js
@@ -107,7 +107,9 @@ const theme = {
   buttons: {
     standard: {
       backgroundColor: 'white',
+      border: '1px solid',
       borderColor: colors.black[300],
+      borderRadius: '100px',
       color: colors.black[600],
 
       '&:hover': {
@@ -136,7 +138,9 @@ const theme = {
 
     primary: {
       backgroundColor: colors.primary[500],
+      border: '1px solid',
       borderColor: colors.primary[500],
+      borderRadius: '100px',
       color: 'white',
 
       '&:hover': {
@@ -166,7 +170,9 @@ const theme = {
     dark: {
       backgroundColor: colors.black[900],
       color: colors.white.full,
+      border: '1px solid',
       borderColor: colors.black[900],
+      borderRadius: '100px',
     },
   },
   buttonSizes: {