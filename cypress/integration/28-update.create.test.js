39b34ee225deb8c1c1d7926d18d0b76a45c9e508
Elijah Oyekunle <eloyekunle@gmail.com>
2019-05-16 13:37:34 +0100

    test(update): add test for random user viewing updates

--- cypress/integration/28-update.create.test.js
+++ cypress/integration/28-update.create.test.js
@@ -8,7 +8,6 @@ describe('create an update', () => {
     cy.fillInputField('title', 'New update');
     cy.get('.ql-editor').type('This is some bold HTML{selectall}');
     cy.get('.ql-bold').click();
-    cy.get('.custom-checkbox').click(); // Make private
     cy.wait(300);
     cy.get('.actions button').click();
     cy.wait(1000);
@@ -16,7 +15,7 @@ describe('create an update', () => {
     cy.get('.UpdatePage .meta').contains('draft');
     cy.get('.UpdatePage .meta')
       .get('#privateIcon')
-      .should('exist');
+      .should('not.exist');
     cy.get('.UpdatePage .PublishUpdateBtn').contains('Your update will be sent to');
     cy.get('.UpdatePage button.publish').click();
     cy.get('.UpdatePage .meta')
@@ -25,11 +24,22 @@ describe('create an update', () => {
 
     cy.get('.UpdatePage .toggleEditUpdate').click();
     cy.wait(300);
-    cy.get('.custom-checkbox').click(); // Make non-private
+    cy.get('.custom-checkbox').click(); // Make private
     cy.get('.actions button').click();
     cy.wait(1000);
     cy.get('.UpdatePage .meta')
       .get('#privateIcon')
-      .should('not.exist');
+      .should('exist');
+  });
+});
+
+describe('random user cannot see update', () => {
+  it('cannot view private update', () => {
+    cy.visit('/testcollective/updates');
+    cy.get('.title a')
+      .first()
+      .click(); // The update created in the describe block above.
+    cy.wait(500);
+    cy.get('.UpdatePage .body').contains('Become a backer of Test Collective to see this update');
   });
 });
