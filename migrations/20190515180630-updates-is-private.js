9da685672af6c3861d92dcccfefec782611f4981
Elijah Oyekunle <eloyekunle@gmail.com>
2019-05-20 11:14:15 +0100

    fix(migrations): add 'allowNull' to migration

--- migrations/20190515180630-updates-is-private.js
+++ migrations/20190515180630-updates-is-private.js
@@ -11,6 +11,7 @@ module.exports = {
         return queryInterface.addColumn('UpdateHistories', 'isPrivate', {
           type: Sequelize.BOOLEAN,
           defaultValue: false,
+          allowNull: false,
         });
       });
   },
