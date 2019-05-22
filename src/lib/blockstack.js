aa0acf29f44fa18c039a9167ad7d1d1df18a4a3d
Friedger Müffke <mail@friedger.de>
2019-05-23 01:24:32 +0200

    only use blockstack on client

--- src/lib/blockstack.js
+++ src/lib/blockstack.js
@@ -1,40 +1,9 @@
-import { UserSession, AppConfig, SessionDataStore } from 'blockstack';
-import { parseCookies, setCookie, destroyCookie } from 'nookies';
+import { UserSession, AppConfig } from 'blockstack';
 
 export const appConfig = new AppConfig(['email'], 'http://127.0.0.1:3000', null, '/static/manifest.json');
 
-class NookieSessionStore extends SessionDataStore {
-  constructor() {
-    super();
-  }
-
-  getSessionData() {
-    console.log(parseCookies({}));
-    return parseCookies({}).sessionData;
-  }
-
-  setSessionData(sessionData) {
-    setCookie({}, 'sessionData', sessionData, {
-      maxAge: 30 * 24 * 60 * 60,
-      path: '/',
-    });
-  }
-
-  deleteSessionData() {
-    destroyCookie({}, 'sessionData');
-    return true;
-  }
-}
-
 export function createUserSession() {
-  const sessionStore = new NookieSessionStore();
-  return new UserSession({ appConfig, sessionStore });
-}
-
-export function redirectToSignIn(userSession, next) {
-  userSession.redirectToSignIn(`${appConfig.appDomain}/signin/blockstack?${next}`);
-}
-
-export function handlePendingSignIn(userSession, authResponse) {
-  return userSession.handlePendingSignIn(authResponse);
+  const userSession = new UserSession({ appConfig });
+  console.log(userSession);
+  return userSession;
 }
