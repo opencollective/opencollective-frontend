bd77626a613d209b762c91828d10d72cd8c08739
Piet Geursen <pietgeursen@gmail.com>
2019-06-05 12:28:47 +1200

    First pass at creating legal documents table migration.

---
+++ migrations/20190604235807-create-legal-documents-table.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('LegalDocuments', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      document_type: Sequelize.ENUM('US_TAX_FORM'),
+      request_status: Sequelize.ENUM('NOT_REQUESTED', 'REQUESTED', 'RECEIVED', 'ERROR'),
+      year: Sequelize.STRING,
+      document_link: Sequelize.STRING,
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+      collective_id: {
+        type: Sequelize.INTEGER,
+        references: { key: 'id', model: 'Collectives' },
+        onDelete: 'SET NULL', //TODO check if this is correct (and test).
+        onUpdate: 'CASCADE', //TODO check if this is correct (and test).
+      },
+      host_collective_id: {
+        type: Sequelize.INTEGER,
+        references: { key: 'id', model: 'Collectives' },
+        onDelete: 'SET NULL', //TODO check if this is correct (and test).
+        onUpdate: 'CASCADE', //TODO check if this is correct (and test).
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('LegalDocuments');
+  },
+};
