b26a43dbec810f8ee89cabc3b2a29b051dbfa4c8
Friedger Müffke <mail@friedger.de>
2019-06-18 12:32:08 +0200

    use blockstack 19.2.1, fix now.json version 1, revert changes CreateProfile

--- src/components/SignInOrJoinFree.js
+++ src/components/SignInOrJoinFree.js
@@ -64,15 +64,7 @@ class SignInOrJoinFree extends React.Component {
     this.setState({ submitting: true });
 
     const next = data.redirect || '/';
-    let redirectUrl;
-    if (data) {
-      const organizationData = pick(data, ['orgName', 'githubHandle', 'twitterHandle', 'website']);
-      redirectUrl = `${window.location.origin}/signin/blockstack?&next='${next}'&org=${JSON.stringify(
-        organizationData,
-      )}`;
-    } else {
-      redirectUrl = `${window.location.origin}/signin/blockstack?&next='${next}'`;
-    }
+    const redirectUrl = `${window.location.origin}/signin/blockstack?&next='${next}'`;
     blockstack.createUserSession().redirectToSignIn(redirectUrl);
   };
 
