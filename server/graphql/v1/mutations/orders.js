fb7ca7b7f1418e5a690ab95bebaea222f49600a0
flickz <omoyajowo2015@gmail.com>
2019-06-30 22:01:11 +0100

    feat(tier-customField): replace customFields with customData in createOrder mutation

--- server/graphql/v1/mutations/orders.js
+++ server/graphql/v1/mutations/orders.js
@@ -437,7 +437,7 @@ export async function createOrder(order, loaders, remoteUser, reqIp) {
           percentage: taxPercent,
           taxIDNumber: order.taxIDNumber,
         },
-        customFields: order.customFields,
+        customData: order.customData,
       },
       status: status.PENDING, // default status, will get updated after the order is processed
     };
