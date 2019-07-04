5ad0d43cc2a227a96cc7d2bf4b6242a2a0451954
François Hodierne <francois@hodierne.net>
2019-07-04 08:42:10 +0200

    fix: moment.js warning with GraphQL API v1 date format

--- src/components/Moment.js
+++ src/components/Moment.js
@@ -3,14 +3,11 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 import { FormattedDate } from 'react-intl';
 
-const formatDate = value => new Date(value).toISOString();
-
 const Moment = ({ value, relative }) => {
-  const formattedLongDateStr = moment(value).format('LLLL');
+  const date = new Date(value);
+  const formattedLongDateStr = moment(date).format('LLLL');
   return (
-    <span title={formattedLongDateStr}>
-      {relative ? moment(formatDate(value)).fromNow() : <FormattedDate value={value} />}
-    </span>
+    <span title={formattedLongDateStr}>{relative ? moment(date).fromNow() : <FormattedDate value={value} />}</span>
   );
 };
 
