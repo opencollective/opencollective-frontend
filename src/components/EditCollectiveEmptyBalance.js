24c86906f36c4f27018349750a0e19050077d409
Josh Ike <ike.josh@icloud.com>
2019-07-07 09:54:15 +0100

    feature(collective): added confirmation modal for empty balance

--- src/components/EditCollectiveEmptyBalance.js
+++ src/components/EditCollectiveEmptyBalance.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 
@@ -8,11 +8,19 @@ import Container from './Container';
 import StyledButton from './StyledButton';
 import SendMoneyToCollectiveBtn from './SendMoneyToCollectiveBtn';
 import { H2, P } from './Text';
+import Modal, { ModalBody, ModalHeader, ModalFooter } from './StyledModal';
 
 const EditCollectiveEmptyBalance = ({ collective, LoggedInUser }) => {
+  const [modal, setModal] = useState({ type: 'Transfer', show: false, isApproved: false });
+
   if (!collective.host || !collective.host.hostCollective) {
     return null;
   }
+
+  const confirmTransfer = () => {
+    setModal({ ...modal, show: true, isApproved: false });
+  };
+
   return (
     <Container display="flex" flexDirection="column" width={1} alignItems="flex-start" mb={2}>
       <H2>
@@ -33,6 +41,8 @@ const EditCollectiveEmptyBalance = ({ collective, LoggedInUser }) => {
           LoggedInUser={LoggedInUser}
           amount={collective.stats.balance}
           currency={collective.currency}
+          confirmTransfer={confirmTransfer}
+          isTransferApproved={modal.isApproved}
         />
       )}
       {collective.stats.balance === 0 && (
@@ -47,6 +57,48 @@ const EditCollectiveEmptyBalance = ({ collective, LoggedInUser }) => {
           />
         </StyledButton>
       )}
+
+      <Modal
+        show={modal.show}
+        width="570px"
+        height="230px"
+        onClose={() => setModal({ ...modal, show: false, isApproved: false })}
+      >
+        <ModalHeader>
+          <FormattedMessage
+            id="collective.archive.modal.header"
+            values={{ action: modal.type }}
+            defaultMessage={'{action} Balance'}
+          />
+        </ModalHeader>
+        <ModalBody>
+          <P>
+            <FormattedMessage
+              id="collective.archive.modal.body"
+              values={{ collective: collective.host.hostCollective.name, action: modal.type.toLowerCase() }}
+              defaultMessage={'Are you sure you want to {action} to {collective}?'}
+            />
+          </P>
+        </ModalBody>
+        <ModalFooter>
+          <Container display="flex" justifyContent="flex-end">
+            <StyledButton mx={20} onClick={() => setModal({ ...modal, show: false, isApproved: false })}>
+              <FormattedMessage id="collective.archive.cancel.btn" defaultMessage={'Cancel'} />
+            </StyledButton>
+            <StyledButton
+              buttonStyle="primary"
+              data-cy="action"
+              onClick={() => setModal({ ...modal, show: false, isApproved: true })}
+            >
+              <FormattedMessage
+                id="collective.archive.confirm.btn"
+                values={{ action: modal.type }}
+                defaultMessage={'{action}'}
+              />
+            </StyledButton>
+          </Container>
+        </ModalFooter>
+      </Modal>
     </Container>
   );
 };
