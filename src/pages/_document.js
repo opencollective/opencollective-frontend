54c4bf8439ad689d4d9954cb785feb50a84e2b22
François Hodierne <francois@hodierne.net>
2019-07-08 11:01:10 +0200

    refactor: remove feature flags for archive/delete/empty

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -59,9 +59,6 @@ export default class IntlDocument extends Document {
       'GOOGLE_MAPS_API_KEY',
       'RECAPTCHA_SITE_KEY',
       'RECAPTCHA_ENABLED',
-      'SHOW_ARCHIVE_COLLECTIVE',
-      'SHOW_DELETE_COLLECTIVE',
-      'SHOW_EMPTY_BALANCE_COLLECTIVE',
     ]);
   }
 
