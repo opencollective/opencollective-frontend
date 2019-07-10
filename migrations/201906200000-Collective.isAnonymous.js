26d7e0962576404753ba933c903aefc428f5b603
Xavier Damman <xdamman@gmail.com>
2019-07-11 00:10:15 +0200

    fix(migration): add isAnonymous to CollectiveHistories

--- migrations/201906200000-Collective.isAnonymous.js
+++ migrations/201906200000-Collective.isAnonymous.js
@@ -2,13 +2,22 @@
 
 module.exports = {
   up: (queryInterface, sequelize) => {
-    return queryInterface.addColumn('Collectives', 'isAnonymous', {
-      type: sequelize.BOOLEAN,
-      defaultValue: false,
-    });
+    return queryInterface
+      .addColumn('Collectives', 'isAnonymous', {
+        type: sequelize.BOOLEAN,
+        defaultValue: false,
+      })
+      .then(() =>
+        queryInterface.addColumn('CollectiveHistories', 'isAnonymous', {
+          type: sequelize.BOOLEAN,
+          defaultValue: false,
+        }),
+      );
   },
 
   down: (queryInterface, sequelize) => {
-    return queryInterface.removeColumn('Collectives', 'isAnonymous');
+    return queryInterface
+      .removeColumn('Collectives', 'isAnonymous')
+      .then(() => queryInterface.removeColumn('CollectiveHistories', 'isAnonymous'));
   },
 };
