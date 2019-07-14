bde03355d3db5b44be59ed14fd067e6170fb3189
François Hodierne <francois@hodierne.net>
2019-07-15 00:57:17 +0200

    refactor: add api endpoints

---
+++ pages/api/graphql/v2.js
@@ -0,0 +1,15 @@
+import fetch from 'node-fetch';
+import { pick } from 'lodash';
+
+export default async function handle(req, res) {
+  const graphqlUrl = `${process.env.API_URL}/graphql/v2?api_key=${process.env.API_KEY}`;
+
+  const json = await fetch(graphqlUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', ...pick(req.headers, ['authorization']) },
+    body: JSON.stringify(req.body),
+  }).then(result => result.json());
+
+  res.setHeader('Content-Type', 'application/json');
+  res.json(json);
+}
