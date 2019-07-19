8d3ff3988df66907caec88e33b0d73879a5a0ae2
François Hodierne <francois@hodierne.net>
2019-07-19 10:59:15 +0200

    feat: increase authentication token expiry to 75 minutes

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -10,7 +10,7 @@ const daysToSeconds = days => moment.duration({ days }).asSeconds();
 const minutesToSeconds = minutes => moment.duration({ minutes }).asSeconds();
 
 /* Constants that determin token expiration */
-export const TOKEN_EXPIRATION_LOGIN = minutesToSeconds(15);
+export const TOKEN_EXPIRATION_LOGIN = minutesToSeconds(75);
 export const TOKEN_EXPIRATION_CONNECTED_ACCOUNT = daysToSeconds(1);
 export const TOKEN_EXPIRATION_SESSION = daysToSeconds(90);
 
