b4ec40cb464d2a1265082487096f2cb1ad002ae2
Piet Geursen <pietgeursen@gmail.com>
2019-07-23 18:51:54 +1200

    Remove linting error.

--- server/graphql/v1/types.js
+++ server/graphql/v1/types.js
@@ -26,7 +26,6 @@ import models, { Op, sequelize } from '../../models';
 import { getContributorsForTier } from '../../lib/contributors';
 import { strip_tags } from '../../lib/utils';
 import status from '../../constants/expense_status';
-import expenseType from '../../constants/expense_type';
 import orderStatus from '../../constants/order_status';
 import { maxInteger } from '../../constants/math';
 import intervals from '../../constants/intervals';
