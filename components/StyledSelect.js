76fd7bcd026b768877c1f3ada0aa74e73bf1b4f6
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-07-31 15:25:10 +0200

    chore/react-select-fix-tests

--- components/StyledSelect.js
+++ components/StyledSelect.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import styled from 'styled-components';
 import { typography, layout, space } from 'styled-system';
-import Select from 'react-select';
+import Select, { components } from 'react-select';
 import { injectIntl, defineMessages } from 'react-intl';
 
 const Messages = defineMessages({
@@ -18,6 +19,16 @@ const Messages = defineMessages({
   },
 });
 
+// eslint-disable-next-line react/prop-types
+const Option = ({ innerProps, ...props }) => (
+  <components.Option {...props} innerProps={{ ...innerProps, 'data-cy': 'select-option' }} />
+);
+
+/**
+ * A map to override the default components of reac-select
+ */
+const customComponents = { Option };
+
 /**
  * Binds our custom theme and wordings to a regular `react-select`'s `Select`.
  * See https://react-select.com for more documentation.
@@ -27,6 +38,7 @@ const StyledSelect = styled(Select).attrs(({ theme, intl, placeholder, disabled,
   placeholder: placeholder || intl.formatMessage(Messages.placeholder),
   loadingMessage: () => intl.formatMessage(Messages.loading),
   noOptionsMessage: () => intl.formatMessage(Messages.loading),
+  components: customComponents,
   styles: {
     control: (baseStyles, state) => {
       const customStyles = { borderColor: theme.colors.black[400] };
