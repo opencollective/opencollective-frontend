ec3de668473b52c981b946e208d7e799735b1de5
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-08-02 11:48:11 +0200

    feat: Add script to diff Stripe charges with transactions

--- server/paymentProviders/stripe/gateway.js
+++ server/paymentProviders/stripe/gateway.js
@@ -153,6 +153,15 @@ export const extractFees = balance => {
 };
 
 /**
+ * List all charges
+ * @param {number} limit A limit on the number of objects to be returned. Limit can range between 1 and 100, and the default is 10.
+ * @param {string} starting_after A cursor for use in pagination. starting_after is an object ID that defines your place in the list. For instance, if you make a list request and receive 100 objects, ending with obj_foo, your subsequent call can include starting_after=obj_foo in order to fetch the next page of the list.
+ */
+export const listCharges = params => {
+  return appStripe.charges.list(params);
+};
+
+/**
  * Returns true if token is a valid stripe test token.
  * See https://stripe.com/docs/testing#cards
  */
