deb91fa3f93d6767007edeabf63bf3ec7d1020d6
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-08-28 10:10:29 +0200

    chore(NCP/Contributions): Update membership cards to match design

--- components/StyledMembershipCard.js
+++ components/StyledMembershipCard.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Flex } from '@rebass/grid';
-import { get, truncate } from 'lodash';
+import { Flex, Box } from '@rebass/grid';
+import { get } from 'lodash';
+import { FormattedMessage, FormattedDate } from 'react-intl';
 
+import { getCollectiveMainTag } from '../lib/collective.lib';
+import roles from '../lib/constants/roles';
+import { formatCurrency } from '../lib/utils';
 import StyledCard from './StyledCard';
 import LinkCollective from './LinkCollective';
 import Container from './Container';
-import { P } from './Text';
+import { P, Span } from './Text';
 import Avatar from './Avatar';
 import I18nCollectiveTags from './I18nCollectiveTags';
-import { getCollectiveMainTag } from '../lib/collective.lib';
 import StyledTag from './StyledTag';
+import FormattedMoneyAmount from './FormattedMoneyAmount';
 
 const getBackground = collective => {
   const backgroundImage = collective.backgroundImage || get(collective, 'parentCollective.backgroundImage');
@@ -19,32 +23,100 @@ const getBackground = collective => {
     : 'url(/static/images/collective-card-mask.png) bottom, #1776E1';
 };
 
+const formatStrongValue = msg => (
+  <Span textTransform="capitalize" fontSize="LeadParagraph" fontWeight="bold">
+    {msg}
+  </Span>
+);
+
 /**
  * A card to show a user's membership.
  */
-const StyledMembershipCard = ({ toCollective, ...props }) => {
+const StyledMembershipCard = ({ membership, ...props }) => {
+  const { collective, since, stats, role } = membership;
   return (
     <StyledCard width={250} height={360} position="relative" {...props}>
-      <Container style={{ background: getBackground(toCollective) }} backgroundSize="cover" height={100} px={3} pt={26}>
+      <Container style={{ background: getBackground(collective) }} backgroundSize="cover" height={100} px={3} pt={26}>
         <Container border="2px solid white" borderRadius="25%" backgroundColor="white.full" width={68}>
-          <Avatar collective={toCollective} radius={64} />
+          <Avatar collective={collective} radius={64} />
         </Container>
       </Container>
-      <Flex flexDirection="column" justifyContent="space-between">
+      <Flex flexDirection="column" justifyContent="space-between" height={260}>
         <Container p={3}>
-          <LinkCollective collective={toCollective}>
+          <LinkCollective collective={collective}>
             <P fontSize="LeadParagraph" fontWeight="bold" color="black.800">
-              {toCollective.name}
+              {collective.name}
             </P>
           </LinkCollective>
           <StyledTag display="inline-block" my={2}>
             <I18nCollectiveTags
-              tags={getCollectiveMainTag(get(toCollective, 'host.id'), toCollective.tags, toCollective.type)}
+              tags={getCollectiveMainTag(get(collective, 'host.id'), collective.tags, collective.type)}
             />
           </StyledTag>
-          <P fontSize="Caption" color="black.700" title={toCollective.description}>
-            {truncate(toCollective.description, { length: 240 })}
-          </P>
+        </Container>
+        <Container p={3}>
+          {role === roles.BACKER ? (
+            <Box mb={2}>
+              <P fontSize="Caption">
+                <FormattedMessage
+                  id="Membership.ContributorSince"
+                  defaultMessage="{contributorType} since"
+                  values={{
+                    contributorType: (
+                      <FormattedMessage id="Member.Role.BACKER" defaultMessage="Financial Contributor" />
+                    ),
+                  }}
+                />
+                <Span display="block" fontSize="LeadParagraph" fontWeight="bold">
+                  <FormattedDate value={since} month="long" year="numeric" />
+                </Span>
+              </P>
+              <P mt={3}>
+                <FormattedMessage id="membership.totalDonations.title" defaultMessage="amount contributed">
+                  {msg => (
+                    <Span textTransform="capitalize" fontSize="Caption">
+                      {msg}
+                    </Span>
+                  )}
+                </FormattedMessage>
+                <Span display="block" fontSize="LeadParagraph" fontWeight="bold">
+                  {/** Ideally we should breakdown amounts donated per currency, but for now
+                    API only returns the total amount in USD. */
+                  formatCurrency(stats.totalDonations, 'USD', { precision: 0 })}
+                </Span>
+              </P>
+            </Box>
+          ) : (
+            <Box mb={2}>
+              <P fontSize="Caption">
+                <FormattedMessage
+                  id="Membership.ContributorsCount"
+                  defaultMessage="{count, plural, one {<strong>1</strong> Contributor} other {<strong>{count}</strong> Contributors}} "
+                  values={{
+                    count: collective.stats.backers.all,
+                    strong: formatStrongValue,
+                  }}
+                />
+              </P>
+              <P mt={3} fontSize="Caption">
+                <FormattedMessage
+                  id="StyledMembershipCard.YearlyBudget"
+                  defaultMessage="{amount} yearly budget"
+                  values={{
+                    amount: (
+                      <Span fontWeight="bold">
+                        <FormattedMoneyAmount
+                          amount={collective.stats.yearlyBudget}
+                          currency={collective.currency || 'USD'}
+                          amountStyles={{ fontSize: 'LeadParagraph' }}
+                        />
+                      </Span>
+                    ),
+                  }}
+                />
+              </P>
+            </Box>
+          )}
         </Container>
       </Flex>
     </StyledCard>
@@ -52,22 +124,34 @@ const StyledMembershipCard = ({ toCollective, ...props }) => {
 };
 
 StyledMembershipCard.propTypes = {
-  role: PropTypes.string.isRequired,
-  since: PropTypes.string.isRequired,
-  toCollective: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    slug: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    backgroundImage: PropTypes.string,
-    tags: PropTypes.arrayOf(PropTypes.string),
-    host: PropTypes.shape({
-      id: PropTypes.number,
+  membership: PropTypes.shape({
+    role: PropTypes.string.isRequired,
+    since: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      totalDonations: PropTypes.numer,
     }),
-    parentCollective: PropTypes.shape({
+    collective: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      slug: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      description: PropTypes.string,
+      currency: PropTypes.string,
       backgroundImage: PropTypes.string,
+      tags: PropTypes.arrayOf(PropTypes.string),
+      host: PropTypes.shape({
+        id: PropTypes.number,
+      }),
+      parentCollective: PropTypes.shape({
+        backgroundImage: PropTypes.string,
+      }),
+      stats: PropTypes.shape({
+        yearlyBudget: PropTypes.numer,
+        backers: PropTypes.shape({
+          all: PropTypes.number,
+        }),
+      }),
     }),
-  }),
+  }).isRequired,
 };
 
 export default StyledMembershipCard;
