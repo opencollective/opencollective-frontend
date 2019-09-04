2734e7eb03677c26d1ff2eb80e06c0bfac2b706f
François Hodierne <francois@hodierne.net>
2019-09-04 09:29:26 +0200

    feat(backyourstack): add a transaction for accurate backyourstack accounting

--- server/lib/backyourstack/dispatcher.js
+++ server/lib/backyourstack/dispatcher.js
@@ -3,6 +3,7 @@ import uuidV4 from 'uuid/v4';
 import debugLib from 'debug';
 import moment from 'moment';
 import { map } from 'bluebird';
+import { pick } from 'lodash';
 
 import models from '../../models';
 import status from '../../constants/order_status';
@@ -33,23 +34,63 @@ function fetchDependencies(jsonUrl) {
   return fetch(jsonUrl).then(res => res.json());
 }
 
+async function createPaymentMethod(originalCreditTransaction) {
+  const shareableAmount =
+    originalCreditTransaction.amountInHostCurrency +
+    originalCreditTransaction.hostFeeInHostCurrency +
+    originalCreditTransaction.platformFeeInHostCurrency +
+    originalCreditTransaction.paymentProcessorFeeInHostCurrency;
+
+  const paymentMethodTransaction = {
+    ...pick(originalCreditTransaction, ['currency', 'hostCurrency', 'CreatedByUserId']),
+    description: 'BackYourStack dispatch Payment Method',
+    amount: shareableAmount,
+    CollectiveId: originalCreditTransaction.FromCollectiveId,
+    FromCollectiveId: originalCreditTransaction.CollectiveId,
+    paymentProcessorFeeInHostCurrency: 0,
+    platformFeeInHostCurrency: 0,
+    hostFeeInHostCurrency: 0,
+  };
+
+  await models.Transaction.createDoubleEntry(paymentMethodTransaction);
+
+  return models.PaymentMethod.create({
+    initialBalance: shareableAmount,
+    currency: originalCreditTransaction.currency,
+    CollectiveId: originalCreditTransaction.FromCollectiveId,
+    name: 'BackYourStack dispatch Payment Method',
+    service: 'opencollective',
+    type: 'prepaid',
+    uuid: uuidV4(),
+    data: { HostCollectiveId: originalCreditTransaction.HostCollectiveId },
+  });
+}
+
 export async function dispatchFunds(order) {
   const debug = debugLib('dispatch_prepaid_subscription');
   // Amount shareable amongst dependencies
   const transaction = await models.Transaction.findOne({
     where: { OrderId: order.id, type: 'CREDIT' },
   });
-  const shareableAmount = transaction.netAmountInCollectiveCurrency;
-  const jsonUrl = order.data.customData.jsonUrl;
-  const collectives = [];
-  let depRecommendations;
 
+  const originalCreditTransaction = await models.Transaction.findOne({
+    where: { OrderId: order.id, type: 'CREDIT' },
+  });
+
+  const shareableAmount = transaction.netAmountInCollectiveCurrency;
+
+  const collectives = [];
+
+  let depRecommendations;
   try {
-    depRecommendations = await fetchDependencies(jsonUrl);
+    if (!order.data.customData.jsonUrl) {
+      throw new Error('Unable to fetch dependencies, no attached jsonUrl.');
+    }
+    depRecommendations = await fetchDependencies(order.data.customData.jsonUrl);
   } catch (err) {
-    debug('Error fetching dependcies', err);
+    debug('Error fetching dependencies', err);
     console.error(err);
-    throw new Error('Unable to fetch dependencies, please ensure the url is correct');
+    throw new Error('Unable to fetch dependencies, check jsonUrl.');
   }
 
   for (const depRecommended of depRecommendations) {
@@ -58,37 +99,24 @@ export async function dispatchFunds(order) {
       debug(`Unable to fetch collective with slug ${depRecommended.opencollective.slug}`);
       continue;
     }
+    if (collective.HostCollectiveId !== originalCreditTransaction.HostCollectiveId) {
+      debug(`${depRecommended.opencollective.slug} is not hosted by the same host (Open Source Collective).`);
+      continue;
+    }
     collective.weight = depRecommended.weight;
     collectives.push(collective);
   }
 
   const sumOfWeights = collectives.reduce((sum, dependency) => dependency.weight + sum, 0);
-  let HostCollectiveId;
-  if (!order.collective) {
-    const collective = await models.Collective.findByPk(order.CollectiveId);
-    HostCollectiveId = await collective.getHostCollectiveId();
-  } else {
-    HostCollectiveId = await order.collective.getHostCollectiveId();
-  }
 
   if (!order.fromCollective) {
     order.fromCollective = await models.Collective.findByPk(order.FromCollectiveId);
   }
-
   if (!order.createdByUser) {
     order.createdByUser = await models.User.findByPk(order.CreatedByUserId);
   }
 
-  const paymentMethod = await models.PaymentMethod.create({
-    initialBalance: shareableAmount,
-    currency: order.currency,
-    CollectiveId: order.FromCollectiveId,
-    customerId: order.fromCollective.slug,
-    service: 'opencollective',
-    type: 'prepaid',
-    uuid: uuidV4(),
-    data: { HostCollectiveId },
-  });
+  const paymentMethod = await createPaymentMethod(originalCreditTransaction);
 
   return map(
     collectives,
---
+++ test/graphql.createOrder.backyourstack.test.js
@@ -0,0 +1,202 @@
+import sinon from 'sinon';
+import nock from 'nock';
+import { expect } from 'chai';
+import { cloneDeep } from 'lodash';
+
+import models from '../server/models';
+
+import * as utils from './utils';
+import * as store from './stores';
+
+import initNock from './nocks/graphql.createOrder.backyourstack.nock';
+
+const baseOrder = Object.freeze({
+  quantity: 1,
+  interval: null,
+  totalAmount: 20000,
+  paymentMethod: {
+    name: '4242',
+    token: 'tok_1B5j8xDjPFcHOcTm3ogdnq0K',
+    data: {
+      expMonth: 10,
+      expYear: 2023,
+      brand: 'Visa',
+      country: 'US',
+      funding: 'credit',
+    },
+  },
+  collective: {
+    id: null,
+  },
+});
+
+const createOrderQuery = `
+  mutation createOrder($order: OrderInputType!) {
+    createOrder(order: $order) {
+      id
+      status
+      description
+      createdByUser {
+        id
+      }
+      paymentMethod {
+        id
+      }
+      totalAmount
+      fromCollective {
+        id
+        slug
+        name
+        website
+      }
+      collective {
+        id
+        slug
+        currency
+      }
+      subscription {
+        id
+        amount
+        interval
+        isActive
+        stripeSubscriptionId
+      }
+    }
+  }
+  `;
+
+const backyourstackDispatchOrder = `
+  mutation backyourstackDispatchOrder($id: Int!) {
+    backyourstackDispatchOrder(id: $id) {
+      id
+      totalAmount
+      collective {
+        name
+        slug
+      }
+    }
+  }
+  `;
+
+const constants = Object.freeze({
+  paymentMethod: {
+    name: 'payment method',
+    service: 'stripe',
+    type: 'creditcard',
+    data: {
+      expMonth: 11,
+      expYear: 2025,
+    },
+  },
+});
+
+describe('createOrder', () => {
+  let sandbox, backyourstackCollective, xdamman, orderCreated;
+
+  before(async () => {
+    await utils.resetTestDB();
+    sandbox = sinon.createSandbox();
+
+    initNock();
+
+    // Given a user
+    xdamman = (await store.newUser('xdamman')).user;
+    // Given a collective (with a host)
+    const newCollectiveWithHost = await store.newCollectiveWithHost('backyourstack', 'USD', 'USD', 5);
+    backyourstackCollective = newCollectiveWithHost.backyourstack;
+    // And the above collective's host has a stripe account
+    await store.stripeConnectedAccount(backyourstackCollective.HostCollectiveId);
+    // And given that the above collective is active
+    await backyourstackCollective.update({ isActive: true });
+
+    // This add a new tier and removes the "sponsors" tier
+    backyourstackCollective.editTiers([
+      {
+        id: 1,
+        slug: 'monthly-plan',
+        name: 'BackYourStack Monthly Plan',
+        amount: 20000,
+      },
+    ]);
+
+    // And given that the endpoint for creating customers on Stripe
+    // is patched
+    utils.stubStripeCreate(sandbox, {
+      charge: {
+        currency: 'usd',
+        status: 'succeeded',
+      },
+      paymentIntent: {
+        charges: { data: [{ id: 'ch_1AzPXHD8MNtzsDcgXpUhv4pm', currency: 'usd', status: 'succeeded' }] },
+        status: 'succeeded',
+      },
+    });
+    // And given the stripe stuff that depends on values in the
+    // order struct is patch. It's here and not on each test because
+    // the `totalAmount' field doesn't change throught the tests.
+    utils.stubStripeBalance(sandbox, baseOrder.totalAmount, 'usd', Math.round(baseOrder.totalAmount * 0.05), 610); // This is the payment processor fee.
+  });
+
+  // alance(sandbox, amount, currency, applicationFee = 0, stripeFee = 0)
+
+  after(() => {
+    sandbox.restore();
+
+    nock.cleanAll();
+  });
+
+  // describe('monthly plan', () => {
+  it('creates a recurring donation as logged in user', async () => {
+    const order = cloneDeep(baseOrder);
+    // And the parameters for the query
+    order.fromCollective = { id: xdamman.CollectiveId };
+    order.paymentMethod = {
+      ...constants.paymentMethod,
+      token: 'tok_1B5j8xDjPFcHOcTm3ogdnq0K',
+    };
+    order.interval = 'month';
+    // order.totalAmount = 1000;
+    order.collective = { id: backyourstackCollective.id };
+    order.customData = { jsonUrl: 'https://backyourstack.com/578a0a70-cef3-11e9-82be-21fd8ed699cb/file/backing.json' };
+    order.tier = { id: 1 };
+
+    // When the order is created
+    const res = await utils.graphqlQuery(createOrderQuery, { order }, xdamman);
+
+    // There should be no errors
+    res.errors && console.error(res.errors);
+    expect(res.errors).to.not.exist;
+
+    // Then the created transaction should match the requested data
+    orderCreated = res.data.createOrder;
+    const { collective, subscription } = orderCreated;
+
+    expect(subscription.interval).to.equal('month');
+    expect(subscription.isActive).to.be.true;
+    expect(subscription.amount).to.equal(order.totalAmount);
+
+    const transaction = await models.Transaction.findOne({
+      where: {
+        CollectiveId: collective.id,
+        FromCollectiveId: xdamman.CollectiveId,
+        amount: order.totalAmount,
+      },
+    });
+
+    // make sure the transaction has been recorded
+    expect(transaction.FromCollectiveId).to.equal(xdamman.CollectiveId);
+    expect(transaction.CollectiveId).to.equal(collective.id);
+    expect(transaction.currency).to.equal(collective.currency);
+    const createdOrder = await models.Order.findByPk(res.data.createOrder.id);
+    expect(createdOrder.status).to.equal('ACTIVE');
+  });
+
+  it('it dispatch the donation accross dependencies', async () => {
+    // When the order is created
+    const res = await utils.graphqlQuery(backyourstackDispatchOrder, { id: orderCreated.id }, xdamman);
+
+    // There should be no errors
+    res.errors && console.error(res.errors);
+    expect(res.errors).to.not.exist;
+  });
+});
