55ae76d19cf93ddd654bbfcdc9a07cf7b0e6041d
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-09-19 19:13:19 +0200

    chore(NCP/Budget): remove submit expense button

--- components/collective-page/sections/Budget.js
+++ components/collective-page/sections/Budget.js
@@ -99,27 +99,15 @@ const SectionBudget = ({ collective, stats }) => {
           mx={[null, null, 3]}
           minWidth={300}
         >
-          <Box flex="1" py={16} px={24}>
+          <Box flex="1" py={16} px={4}>
             <P fontSize="Tiny" textTransform="uppercase" color="black.700">
               <FormattedMessage id="CollectivePage.SectionBudget.Balance" defaultMessage="Today’s balance" />
             </P>
-            <P fontSize="H5" mt={1} mb={3}>
+            <P fontSize="H5" mt={1}>
               {formatCurrency(stats.balance, collective.currency)} <Span color="black.400">{collective.currency}</Span>
             </P>
-            {collective.isArchived ? (
-              <StyledButton buttonSize="small" fontWeight="bold" py={2} px={3} disabled>
-                <FormattedMessage id="CollectivePage.SectionBudget.SubmitExpense" defaultMessage="Submit Expenses" /> →
-              </StyledButton>
-            ) : (
-              <Link route="createExpense" params={{ collectiveSlug: collective.slug }}>
-                <StyledButton buttonSize="small" fontWeight="bold" py={2} px={3}>
-                  <FormattedMessage id="CollectivePage.SectionBudget.SubmitExpense" defaultMessage="Submit Expenses" />{' '}
-                  →
-                </StyledButton>
-              </Link>
-            )}
           </Box>
-          <Container flex="1" background="#F5F7FA" py={16} px={24}>
+          <Container flex="1" background="#F5F7FA" py={16} px={4}>
             <DefinedTerm term={Terms.ESTIMATED_BUDGET} fontSize="Tiny" textTransform="uppercase" color="black.700" />
             <P fontSize="H5" mt={2}>
               <Span fontWeight="bold">~ {formatCurrency(stats.yearlyBudget, collective.currency)}</Span>{' '}
