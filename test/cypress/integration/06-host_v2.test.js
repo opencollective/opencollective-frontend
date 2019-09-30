3233479037dc784e8b6e305b34572328f5031409
Kate Beard <asouthernbellein@gmail.com>
2019-09-30 11:14:21 +0100

    fix(NCP): rewrite Contributions tests to follow best practices better

--- test/cypress/integration/06-host_v2.test.js
+++ test/cypress/integration/06-host_v2.test.js
@@ -8,14 +8,19 @@ describe('New host page', () => {
 
   describe('Contributions section', () => {
     // The rest of the contributions section is already tested in `05-user_v2.test.js`
-    it.skip('Show fiscally hosted collectives', () => {
-      // TODO
+    it('Show fiscally hosted collectives', () => {
+      cy.contains('[data-cy=filter-button]', 'Fiscal Host').click();
+      cy.contains('[data-cy=Contributions]', 'Open Source Collective');
+      cy.contains('[data-cy=Contributions]', 'APEX');
+      cy.contains('[data-cy=Contributions]', 'tipbox');
+      cy.contains('[data-cy=Contributions]', 'Test Collective');
     });
   });
 
   describe('Contributors section', () => {
-    it.skip('Only shows core contributors without any filter', () => {
-      // TODO
+    it('Only shows core contributors without any filter', () => {
+      cy.get('[data-cy=Contributors] button').should('not.exist');
+      cy.contains('[data-cy=contributors-grid]', /(pia mancini|Xavier Damma|Open Source Host User)/);
     });
   });
 
