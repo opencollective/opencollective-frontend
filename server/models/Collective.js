29fd02ff05996bcbfd64f4a07af00de2a1f339f9
jeffin sam <jeffinsam@karunya.edu.in>
2019-10-08 23:11:58 +0530

    fix : Unpredictable behavior when hosts change their Stripe account

--- server/models/Collective.js
+++ server/models/Collective.js
@@ -2097,6 +2097,7 @@ export default function(Sequelize, DataTypes) {
           id &&
           models.ConnectedAccount.findOne({
             where: { service: 'stripe', CollectiveId: id },
+            order: [['createdAt', 'DESC']],
           })
         );
       })
