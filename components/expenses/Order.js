a77c565a32e9304004467bc548309285fdba6dd0
Oluwaseun Omoyajowo <olu@opencollective.com>
2019-10-16 14:26:45 +0100

    HostDashboard: Add cancel pending order button (#2820)
    * enhancement(hostDashboard): add CancelPendingOrderBtn component
    
    * enhancement(hostDashboard): update graphql schema
    
    * enhancement(hostDashboard): update localization files
    
    * enhancement(hostDashboard): remove CancelPendingOrderBtn component
    
    * enhancement(hostDashboard): use Yes or No for modal action buttons for cancel pending order confirmation
    
    * enhancement(hostDashboard): update localization files

--- components/expenses/Order.js
+++ components/expenses/Order.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { defineMessages, injectIntl } from 'react-intl';
+import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
+import { Flex } from '@rebass/grid';
 
 import colors from '../../lib/constants/colors';
 import { capitalize } from '../../lib/utils';
@@ -8,6 +9,7 @@ import { capitalize } from '../../lib/utils';
 import Avatar from '..//Avatar';
 import Link from '../Link';
 import LinkCollective from '../LinkCollective';
+import StyledButton from '../StyledButton';
 import Moment from '../Moment';
 import AmountCurrency from './AmountCurrency';
 import MarkOrderAsPaidBtn from './MarkOrderAsPaidBtn';
@@ -22,6 +24,7 @@ class Order extends React.Component {
     includeHostedCollectives: PropTypes.bool,
     LoggedInUser: PropTypes.object,
     intl: PropTypes.object.isRequired,
+    onClickCancel: PropTypes.func,
   };
 
   constructor(props) {
@@ -38,6 +41,7 @@ class Order extends React.Component {
       error: { id: 'order.error', defaultMessage: 'error' },
       active: { id: 'order.active', defaultMessage: 'active' },
       cancelled: { id: 'order.cancelled', defaultMessage: 'cancelled' },
+      expired: { id: 'order.expired', defaultMessage: 'expired' },
     });
     this.currencyStyle = {
       style: 'currency',
@@ -222,7 +226,12 @@ class Order extends React.Component {
           </div>
           <OrderDetails order={order} mode={mode} />
           {order.status === 'PENDING' && canMarkOrderAsPaid && (
-            <MarkOrderAsPaidBtn order={order} collective={order.collective} />
+            <Flex>
+              <MarkOrderAsPaidBtn order={order} collective={order.collective} />
+              <StyledButton bg="red.500" color="#fff" onClick={() => this.props.onClickCancel(order.id)}>
+                <FormattedMessage id="order.pending.cancel" defaultMessage="Cancel" />
+              </StyledButton>
+            </Flex>
           )}
         </div>
       </div>
