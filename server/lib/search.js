5cca5c21a54687959ca836612396b2ed991989c6
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2019-10-16 16:35:14 +0200

    chore(search): Only return active collectives by default

--- server/lib/search.js
+++ server/lib/search.js
@@ -103,6 +103,7 @@ export const searchCollectivesInDB = async (term, offset = 0, limit = 100, types
     FROM "Collectives" c
     WHERE "deletedAt" IS NULL 
     AND "deactivatedAt" IS NULL 
+    AND "isActive" = true
     AND type IN (:types) ${dynamicConditions}
     ORDER BY __rank__ DESC
     OFFSET :offset
