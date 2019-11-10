34b3d50fc1587b4bbf73eb5c4c50c6b62d8cd2da
NotJohs <notjohs@NotJohs.local>
2019-11-10 15:39:13 +0100

    add StyleDragDrop component && styleguide. update Contribute Tier && Event Panels to use StyleDrapDrop component

--- components/contribute-cards/ContributeEventsPanel.js
+++ components/contribute-cards/ContributeEventsPanel.js
@@ -4,13 +4,39 @@ import { FormattedMessage } from 'react-intl';
 import { Box } from '@rebass/grid';
 import dynamic from 'next/dynamic';
 
+import { Pencil } from 'styled-icons/boxicons-solid/Pencil';
+import { Move } from 'styled-icons/boxicons-regular/Move';
+import { Close } from 'styled-icons/material/Close';
+import styled from 'styled-components';
+import memoizeOne from 'memoize-one';
+
 import CreateNew from './CreateNew';
 import ContributeEvent from './ContributeEvent';
 import ContributeCollective from './ContributeCollective';
 import { CONTRIBUTE_CARD_WIDTH } from './Contribute';
 import LoadingPlaceholder from '../LoadingPlaceholder';
+import colors from '../../lib/constants/colors';
 
-const ContributeEventPanel = ({
+const Handle = styled.div`
+  background-color: ${({ bgColor }) => bgColor || colors.white};
+  box-shadow: 1px 1px 1px ${colors.darkgray};
+  opacity: ${({ hide }) => (hide ? 0 : 1)};
+  transition: all 0.3s ease-out;
+  justify-content: center;
+  color: ${colors.white};
+  align-items: center;
+  margin-bottom: 5px;
+  border-radius: 50%;
+  outline: none;
+  display: flex;
+  height: 30px;
+  width: 30px;
+  > * {
+    color: ${({ color }) => color || colors.darkgray};
+  }
+`;
+
+const ContributeEventsPanel = ({
   isAdmin,
   collective,
   joinedEvents,
@@ -18,12 +44,49 @@ const ContributeEventPanel = ({
   CONTRIBUTE_CARD_PADDING_X,
   hasNoContributorForEvents,
 }) => {
-  //   const StyledDragDropPlaceHolder = () => <LoadingPlaceholder width={CONTRIBUTE_CARD_WIDTH} />;
-  //   const dynamicOptions = { loading: StyledDragDropPlaceHolder, ssr: false };
-  //   const StyledDragDrop = dynamic(
-  //     () => import(/* webpackChunkName: 'StyledDragDrop' */ '../StyledDropPanel'),
-  //     dynamicOptions,
-  //   );
+  const CONTRIBUTE_CARD_HEIGHT = 341;
+
+  const StyledDragDropPlaceHolder = () => (
+    <LoadingPlaceholder width={CONTRIBUTE_CARD_WIDTH} heigth={CONTRIBUTE_CARD_HEIGHT} />
+  );
+  const dynamicOptions = { loading: StyledDragDropPlaceHolder, ssr: false };
+  const StyledDragDrop = dynamic(
+    () => import(/* webpackChunkName: 'StyledDragDrop' */ '../StyledDragDrop'),
+    dynamicOptions,
+  );
+
+  const getOtherWaysToContributeOrder = memoizeOne(({ collective }) => {
+    const { settings = {} } = collective;
+    const { collectivePage = {} } = settings;
+    const { otherWaysToContributeOrder } = collectivePage;
+
+    if (!otherWaysToContributeOrder) return [];
+    if (!otherWaysToContributeOrder[0]) return [];
+    return otherWaysToContributeOrder.filter(i => i !== 'custom');
+  });
+
+  const handleShuffle = otherWaysToContributeOrder => {
+    const { settings = {} } = collective;
+    const { collectivePage = {} } = settings;
+
+    handleSettingsUpdate({
+      ...settings,
+      collectivePage: {
+        ...collectivePage,
+        otherWaysToContributeOrder,
+      },
+    });
+  };
+
+  const identifier = memoizeOne(item => {
+    if (!item) return;
+    if (typeof item.id === 'string') {
+      if (item.id.toLowerCase().includes('event')) return item.id;
+      if (item.id.toLowerCase().includes('collective')) return item.id;
+      throw Error("Error in identifier function: item.id <event.id || childCollective.id> doesn't event or collective");
+    }
+    return item.__typename.toLowerCase().includes('event') ? `event-${item.id}` : `collective-${item.id}`;
+  });
 
   if (!isAdmin) {
     return (
@@ -49,22 +112,50 @@ const ContributeEventPanel = ({
             <FormattedMessage id="event.create.btn" defaultMessage="Create Event" />
           </CreateNew>
         </Box>
-        {joinedEvents.map(item =>
-          item.__typename.toLowerCase().includes('event') ? (
-            <Box key={item.id} px={CONTRIBUTE_CARD_PADDING_X}>
-              <ContributeEvent collective={collective} event={item} hideContributors={hasNoContributorForEvents} />
-            </Box>
-          ) : (
-            <Box key={item.id} px={CONTRIBUTE_CARD_PADDING_X}>
-              <ContributeCollective collective={item} />
-            </Box>
-          ),
-        )}
+        <StyledDragDrop
+          handle={true}
+          id={collective.id}
+          items={joinedEvents}
+          direction="horizontal"
+          onShuffle={handleShuffle}
+          identifier={identifier}
+          itemsOrder={getOtherWaysToContributeOrder({ collective })}
+        >
+          {({ item, cssHelper, handleProps: { wrapper, dragProps, hideDuringDrag } }) => (
+            <Fragment>
+              {wrapper(
+                <Handle key={'move'} bgColor={colors.black} color={colors.white} {...dragProps}>
+                  <Move size={15} />
+                </Handle>,
+                <Handle key={'pencil'} hide={hideDuringDrag}>
+                  <Pencil size={15} />
+                </Handle>,
+                <Handle key={'close'} hide={hideDuringDrag}>
+                  <Close size={15} />
+                </Handle>,
+              )}
+              {item.__typename.toLowerCase().includes('event') ? (
+                <Box key={item.id} px={CONTRIBUTE_CARD_PADDING_X}>
+                  <ContributeEvent
+                    css={cssHelper}
+                    collective={collective}
+                    event={item}
+                    hideContributors={hasNoContributorForEvents}
+                  />
+                </Box>
+              ) : (
+                <Box key={item.id} px={CONTRIBUTE_CARD_PADDING_X}>
+                  <ContributeCollective css={cssHelper} collective={item} />
+                </Box>
+              )}
+            </Fragment>
+          )}
+        </StyledDragDrop>
       </Fragment>
     );
 };
 
-ContributeEventPanel.propTypes = {
+ContributeEventsPanel.propTypes = {
   isAdmin: PropTypes.bool.isRequired,
   collective: PropTypes.object.isRequired,
   joinedEvents: PropTypes.array.isRequired,
@@ -73,4 +164,4 @@ ContributeEventPanel.propTypes = {
   hasNoContributorForEvents: PropTypes.bool.isRequired,
 };
 
-export default ContributeEventPanel;
+export default React.memo(ContributeEventsPanel);
--- styleguide/examples/StyledDragDrop.md
+++ styleguide/examples/StyledDragDrop.md
@@ -1,8 +1,104 @@
-This component has no default opinions about padding or layout internally.
+```jsx noeditor
+// See https://github.com/styleguidist/react-styleguidist/issues/1278
+import { Box } from '@rebass/grid';
+import Move from 'styled-icons/boxicons-regular/Move';
+import { Close } from 'styled-icons/material/Close';
+import styled from 'styled-components';
+
+import { webpackCollective } from '../mocks/collectives';
+import StyledCard from 'components/StyledCard';
+```
+
+### default
 
 ```js
-<StyledDropPanel>
-  <h3>Styled Drag and Drop</h3>
-  <p>List of sortable drag cards</p>
-</StyledDropPanel>
+import { webpackCollective } from '../mocks/collectives';
+import StyledCard from 'components/StyledCard';
+initialState = {
+  items: webpackCollective,
+  itemsOrder: [],
+};
+
+<StyledDragDrop
+  id={state.items.id}
+  items={state.items.tiers}
+  itemsOrder={state.itemsOrder}
+  onShuffle={newItemsOrder => {}}
+>
+  {({ item, index }) => (
+    <StyledCard display={'flex'} justifyContent="space-evenly" px={1} py={1} m={1} textAlign="center">
+      <h2>ID: {item.id}</h2>
+      <h2>Title: {item.name}</h2>
+      <h2>Slug: {item.slug}</h2>
+      <h3>Index: {index}</h3>
+    </StyledCard>
+  )}
+</StyledDragDrop>;
+```
+
+### with options
+
+```js
+import { Move } from 'styled-icons/boxicons-regular/Move';
+import { Close } from 'styled-icons/material/Close';
+import styled from 'styled-components';
+
+import { webpackCollective } from '../mocks/collectives';
+import StyledCard from 'components/StyledCard';
+
+initialState = {
+  id: webpackCollective.id,
+  items: webpackCollective.tiers,
+  itemsOrder: [266, 267],
+};
+
+const Handle = styled.div`
+  opacity: ${({ hide }) => (hide ? 0 : 1)};
+  background-color: #484b4c;
+  border-radius: 50%;
+  margin-bottom: 5px;
+  color: #ffffff;
+  padding: 5px;
+`;
+
+<StyledDragDrop
+  handle={true}
+  id={state.id}
+  items={state.items}
+  itemsOrder={state.itemsOrder}
+  onShuffle={newItemsOrder => setState({ itemsOrder: newItemsOrder })}
+  direction="horizontal"
+>
+  {({ item, index, cssHelper, handleProps: { wrapper, dragProps, hideDuringDrag }, isDragging }) => (
+    <React.Fragment>
+      {wrapper(
+        <Handle {...dragProps}>
+          <Move size={15} />
+        </Handle>,
+        <Handle
+          onClick={() => {
+            const items = Array.from(state.items).filter(i => i.id !== item.id);
+            setState({ items });
+          }}
+          hide={hideDuringDrag}
+        >
+          <Close size={15} />
+        </Handle>,
+      )}
+      <StyledCard
+        px={4}
+        py={3}
+        mx={1}
+        maxWidth={500}
+        css={cssHelper}
+        style={{ backgroundColor: !isDragging ? '#FFFFFF' : '#75CC1F' }}
+      >
+        <h2>ID: {item.id}</h2>
+        <h2>Title: {item.name}</h2>
+        <h2>Slug: {item.slug}</h2>
+        <h3>Index: {index}</h3>
+      </StyledCard>
+    </React.Fragment>
+  )}
+</StyledDragDrop>;
 ```
