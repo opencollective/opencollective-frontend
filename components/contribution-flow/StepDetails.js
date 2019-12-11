3b2be65a2d14f0bb4a44bea326919be9edb2d06e
Namuli Joyce <jollankent@gmail.com>
2019-12-11 17:28:07 +0300

    style(ContributionFlow): Outline "other" amount active state in details
    #3026

--- components/contribution-flow/StepDetails.js
+++ components/contribution-flow/StepDetails.js
@@ -170,7 +170,7 @@ const StepDetails = ({
                 width={1}
                 onChange={({ target }) => dispatchChange({ amount: Math.round(parseFloat(target.value) * 100) })}
                 containerProps={{ borderRadius: hasOptions ? '0 4px 4px 0' : 3, ml: '-1px' }}
-                prependProps={{ pl: 2, pr: 0, bg: 'white.full', color: 'black.800' }}
+                prependProps={{ pl: 2, pr: 0, bg: 'white.full' }}
                 px="2px"
               />
             )}
