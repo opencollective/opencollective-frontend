95ebefc0e798fb4e4c23def53460e8816358552f
François Hodierne <francois@hodierne.net>
2020-01-09 12:59:26 +0100

    wip(plans): add plans table

--- components/edit-collective/EditHostSettings.js
+++ components/edit-collective/EditHostSettings.js
@@ -3,8 +3,39 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
 
+import Button from '../Button';
+
 const Capitalized = styled.span`
   text-transform: capitalize;
+`;
+
+const Table = styled.table`
+  width: 100%;
+`;
+
+const Plan = styled.td`
+  font-size: 1.2rem;
+  text-align: center;
+  width: 20%;
+`;
+
+const PlanFeatures = styled.p`
+  font-size: 1.1rem;
+  text-align: center;
+  min-height: 13rem;
+  padding-top: 1rem;
+`;
+
+const PlanName = styled.h4`
+  font-size: 1.4rem;
+  text-align: center;
+  font-weight: bold;
+`;
+
+const PlanPrice = styled.p`
+  font-size: 1.4rem;
+  text-align: center;
+  font-weight: bold;
 `;
 
 class EditHostSettings extends React.Component {
@@ -19,6 +50,8 @@ class EditHostSettings extends React.Component {
   render() {
     const { collective } = this.props;
 
+    const redirectUrl = `${process.env.WEBSITE_URL}/${collective.slug}/edit/hostSettings`;
+
     return (
       <div className="ExportData">
         <h2>
@@ -26,20 +59,129 @@ class EditHostSettings extends React.Component {
         </h2>
 
         <h3>
-          <FormattedMessage id="collective.hostSettings.plan.title" defaultMessage="Plan" />
+          <FormattedMessage id="collective.hostSettings.changePlan.title" defaultMessage="Plans" />
+        </h3>
+
+        <Table>
+          <tr>
+            <Plan>
+              <PlanName>Single Host Plan</PlanName>
+              <PlanFeatures>
+                <ul>
+                  <li>1 hosted collective</li>
+                  <li>Unlimited added funds</li>
+                </ul>
+              </PlanFeatures>
+              <PlanPrice>$10 / month</PlanPrice>
+
+              {/* Local
+              <Button href={`/opencollective/contribute/single-host-plan-2808/checkout?redirect=${redirectUrl}`}>
+                Upgrade
+              </Button> */}
+
+              {/* Staging */}
+              <Button href={`/opencollective/contribute/single-host-plan-14966/checkout?redirect=${redirectUrl}`}>
+                Upgrade
+              </Button>
+            </Plan>
+
+            <Plan>
+              <PlanName>Small Host Plan</PlanName>
+              <PlanFeatures>
+                <ul>
+                  <li>Up to 5 hosted collectives</li>
+                  <li>Unlimited added funds</li>
+                  <li>Manual bank transfers</li>
+                  <li>Host dashboard</li>
+                </ul>
+              </PlanFeatures>
+              <PlanPrice>$25 / month</PlanPrice>
+
+              {/* Local
+              <Button href={`/opencollective/contribute/small-host-plan-2809/checkout?redirect=${redirectUrl}`}>
+                Upgrade
+              </Button>*/}
+
+              {/* Staging */}
+              <Button href={`/opencollective/contribute/small-host-plan-14968/checkout?redirect=${redirectUrl}`}>
+                Upgrade
+              </Button>
+            </Plan>
+
+            <Plan>
+              <PlanName>Medium Host Plan</PlanName>
+              <PlanFeatures>
+                <ul>
+                  <li>Up to 10 hosted collectives</li>
+                  <li>Unlimited added funds</li>
+                  <li>Manual bank transfers</li>
+                  <li>Host dashboard</li>
+                </ul>
+              </PlanFeatures>
+              <PlanPrice>$50 / month</PlanPrice>
+
+              {/* Staging */}
+              <Button href={`/opencollective/contribute/medium-host-plan-14969/checkout?redirect=${redirectUrl}`}>
+                Upgrade
+              </Button>
+            </Plan>
+
+            <Plan>
+              <PlanName>Large Host Plan</PlanName>
+              <PlanFeatures>
+                <ul>
+                  <li>Up to 25 hosted collectives</li>
+                  <li>Unlimited added funds</li>
+                  <li>Manual bank transfers</li>
+                  <li>Host dashboard</li>
+                </ul>
+              </PlanFeatures>
+              <PlanPrice>$100 / month</PlanPrice>
+
+              {/* Staging */}
+              <Button href={`/opencollective/contribute/large-host-plan-14967/checkout?redirect=${redirectUrl}`}>
+                Upgrade
+              </Button>
+            </Plan>
+
+            <Plan>
+              <PlanName>Network Host Plan</PlanName>
+              <PlanFeatures>
+                <ul>
+                  <li>Unlimited hosted collectives</li>
+                  <li>Unlimited added funds</li>
+                  <li>Manual bank transfers</li>
+                  <li>Host dashboard</li>
+                </ul>
+              </PlanFeatures>
+              <PlanPrice>$100+ / month</PlanPrice>
+              <Button disabled>Contact Us</Button>
+            </Plan>
+          </tr>
+        </Table>
+
+        <h3>
+          <FormattedMessage id="collective.hostSettings.currentPlan.title" defaultMessage="Current Plan Usage" />
         </h3>
 
         <ul>
           <li>
-            <strong>Name</strong>: <Capitalized>{collective.plan.name}</Capitalized>
+            <strong>Current Plan</strong>: <Capitalized>{collective.plan.name}</Capitalized>
           </li>
           <li>
             <strong>Collective Limit</strong>: {collective.plan.hostedCollectives} of{' '}
             {collective.plan.hostedCollectivesLimit}
           </li>
           <li>
-            <strong>Add Funds Limit</strong>: ${collective.plan.addedFunds / 100} of $
-            {collective.plan.addedFundsLimit / 100}
+            <strong>Added Funds Limit</strong>:&nbsp;
+            {collective.plan.addedFundsLimit && (
+              <span>
+                ${collective.plan.addedFunds / 100} of ${collective.plan.addedFundsLimit / 100}
+              </span>
+            )}
+            {!collective.plan.addedFundsLimit && (
+              <FormattedMessage id="collective.hostSettings.addedFunds.unlimited" defaultMessage="Unlimited" />
+            )}
           </li>
           <li>
             <strong>Host Dashboard</strong>: {collective.plan.hostDashboard ? 'Yes' : 'No'}
