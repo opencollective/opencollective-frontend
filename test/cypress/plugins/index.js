6ef7f21369850fc1a2cd045b928393f08907fbaa
François Hodierne <francois@hodierne.net>
2020-01-20 14:45:36 +0100

    e2e: setup coverage

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 // eslint-disable-next-line
 module.exports = (on, config) => {
+  // eslint-disable-next-line node/no-unpublished-require
+  on('task', require('@cypress/code-coverage/task'));
   config.baseUrl = process.env.WEBSITE_URL || 'http://localhost:3000';
   config.env = config.env || {};
   return config;
