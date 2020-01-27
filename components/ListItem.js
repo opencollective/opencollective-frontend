2d29cbce4bd4add6ffd751006c9136e5382704d1
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2020-01-27 18:32:26 +0100

    enhancement(ProfileMenu): Include link to accounts settings

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { space, textAlign, width } from 'styled-system';
+import { space, textAlign, width, display, flexbox } from 'styled-system';
 import { listStyle } from '../lib/styled_system_custom';
 
 const ListItem = styled.li`
@@ -9,6 +9,8 @@ const ListItem = styled.li`
   ${space}
   ${textAlign}
   ${width}
+  ${display}
+  ${flexbox}
 `;
 
 export default ListItem;
