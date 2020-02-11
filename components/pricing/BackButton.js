21f3a53efbf1abf3ed9d3c32ce47e759c67f28ba
flickz <omoyajowo2015@gmail.com>
2020-02-11 10:42:18 +0100

    feat(pricing): fix back button style, title alignment, fix texture stretching

--- components/pricing/BackButton.js
+++ components/pricing/BackButton.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { FormattedMessage } from 'react-intl';
 import { ArrowBack } from '@styled-icons/boxicons-regular/ArrowBack';
 
 import StyledButton from '../StyledButton';
 import { Span } from '../Text';
 
+const Button = styled(StyledButton)`
+  padding: 5px 12px;
+  width: 69px;
+  white-space: nowrap;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
 const BackButton = ({ onClick }) => (
-  <StyledButton buttonStyle="secondary" onClick={onClick} py="5px" width="69px" px="12px">
+  <Button buttonStyle="secondary" onClick={onClick}>
     <ArrowBack size="13" />{' '}
     <Span fontSize="12px" lineHeight="14px">
       <FormattedMessage id="pricing.back.btn" defaultMessage="Back" />
     </Span>
-  </StyledButton>
+  </Button>
 );
 
 BackButton.propTypes = {
--- components/pricing/PricingTabs.js
+++ components/pricing/PricingTabs.js
@@ -16,12 +16,20 @@ const Wrapper = styled(Container)`
     background-size: 100% 100%;
     min-height: 108px;
     margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+
+  @media screen and (min-width: 88em) {
+    width: 1216px;
   }
 `;
 
 const TabBox = styled(Box)`
   width: 330px;
-  height: 104px;
+  height: 96px;
   padding: 16px 32px;
   outline: none;
   border: none;
@@ -36,10 +44,30 @@ const TabBox = styled(Box)`
   ${({ active }) =>
     active &&
     `
+    height: 104px;
     border-bottom: none;
     background: #fff;
     border-top-color: #297EFF;
   `};
+`;
+
+const Title = styled(H1)`
+  @media screen and (min-width: 64em) {
+    position: relative;
+    top: 40px;
+    margin-bottom: 32px;
+  }
+`;
+
+const TabEndLines = styled(Span)`
+  border-bottom: 1px solid #e8e9eb;
+
+  @media screen and (min-width: 64em) {
+    width: 25px;
+  }
+  @media screen and (min-width: 88em) {
+    width: 105px;
+  }
 `;
 
 const messages = defineMessages({
@@ -74,14 +102,19 @@ const tabs = ['singleCollectiveWithAccount', 'singleCollectiveWithoutAccount', '
 const PricingTabs = ({ onChange, activeTab }) => {
   const intl = useIntl();
   return (
-    <Container display={[activeTab ? 'none' : 'block', null, 'block']}>
+    <Container display={[activeTab ? 'none' : 'block', null, 'flex']} flexDirection="column" alignItems="center">
       <Wrapper>
         <Box width={1} display={['flex', null, 'none']}>
           <img width="100%" src="/static/images/pricing-star-bg-sm.png" alt="Pricing" />
         </Box>
-        <H1 fontSize={['H3', null, 'H1']} lineHeight={['40px', null, 'H1']} letterSpacing="-0.4px" textAlign="center">
+        <Title
+          fontSize={['H3', null, 'H1']}
+          lineHeight={['40px', null, 'H1']}
+          letterSpacing="-0.4px"
+          textAlign="center"
+        >
           <FormattedMessage id="pricing.title" defaultMessage="Our pricing" />
-        </H1>
+        </Title>
       </Wrapper>
       <Container>
         <Box mx={3} my={2}>
@@ -136,9 +169,12 @@ const PricingTabs = ({ onChange, activeTab }) => {
           my={4}
           display={['none', null, 'flex']}
           justifyContent="center"
+          alignItems="flex-end"
           borderBottom="1px solid"
           borderColor="#E8E9EB"
+          width={1}
         >
+          <TabEndLines />
           {tabs.map(tab => {
             return (
               <TabBox as="button" key={tab} active={tab === activeTab} onClick={() => onChange(tab)}>
@@ -147,6 +183,7 @@ const PricingTabs = ({ onChange, activeTab }) => {
               </TabBox>
             );
           })}
+          <TabEndLines />
         </Container>
       </Container>
     </Container>
