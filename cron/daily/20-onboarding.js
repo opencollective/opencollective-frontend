65f602bfa923967a4a21b47a17492155eac1c927
François Hodierne <francois@hodierne.net>
2020-02-11 09:36:16 +0100

    notifications: remove day35 for noExpenses

--- cron/daily/20-onboarding.js
+++ cron/daily/20-onboarding.js
@@ -53,7 +53,6 @@ Promise.all([
   processOnBoardingTemplate('onboarding.day7-b', XDaysAgo(7)),
   processOnBoardingTemplate('onboarding.day21.noTwitter', XDaysAgo(21), onlyCollectivesWithoutTwitterActivated),
   processOnBoardingTemplate('onboarding.noExpenses', XDaysAgo(14), onlyCollectivesWithoutExpenses),
-  processOnBoardingTemplate('onboarding.noExpenses', XDaysAgo(35), onlyCollectivesWithoutExpenses), // another reminder after 35 days if no expenses filed yet
   processOnBoardingTemplate('onboarding.noUpdates', XDaysAgo(21), onlyCollectivesWithoutUpdates),
   processOnBoardingTemplate('onboarding.day7', XDaysAgo(7)),
   processOnBoardingTemplate('onboarding.day2', XDaysAgo(2)),
