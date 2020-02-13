e5783f5d908385a1f8cd42d0bd763627ef8860f6
Leonardo Kewitz <leokewitz@gmail.com>
2020-02-13 12:17:20 -0300

    fix: allow defined form defined PayoutMethods

--- pages/createExpense.js
+++ pages/createExpense.js
@@ -34,7 +34,9 @@ class CreateExpensePage extends React.Component {
   };
 
   getPayoutMethod = expenseData => {
-    if (expenseData.payoutMethod === 'paypal') {
+    if (expenseData.PayoutMethod) {
+      return expenseData.PayoutMethod;
+    } else if (expenseData.payoutMethod === 'paypal') {
       return { type: 'PAYPAL', data: { email: expenseData.paypalEmail } };
     } else {
       return null;
