e79db4954eb10196c0791af98a25ccb92c100d41
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2020-02-20 12:55:50 +0100

    feat(SPAM): Prevent spammy collectives from being created

--- server/lib/spam.ts
+++ server/lib/spam.ts
@@ -2,10 +2,35 @@ import { clamp } from 'lodash';
 import config from 'config';
 import slackLib from '../lib/slack';
 
-// Watched content. Please keep everything in there lowercase.
+/** Return type when running a spam analysis */
+export type SpamAnalysisReport = {
+  /** When did the report occur */
+  date: string;
+  /** What's the context of the report */
+  context: string;
+  /** Data of the entity that was analyzed */
+  data: object;
+  /** A score between 0 and 1. 0=NotSpam, 1=IsSpamForSure */
+  score: number;
+  /** Detected spam keywords */
+  keywords: string[];
+  /** Detected blacklisted domains */
+  domains: string[];
+};
+
+// Watched content
 const ANALYZED_FIELDS: string[] = ['name', 'website', 'description', 'longDescription'];
-const SPAM_KEYWORDS: string[] = ['keto', 'porn', 'pills', 'male enhancement'];
-const BLACKLISTED_DOMAINS = [
+
+// A map of spam keywords<>score. Please keep everything in there lowercase.
+const SPAM_KEYWORDS: { [keyword: string]: number } = {
+  keto: 0.3,
+  porn: 0.2,
+  pills: 0.1,
+  'male enhancement': 0.3,
+};
+
+// Any domain from there gives you a SPAM scrore of 1
+const SPAMMERS_DOMAINS = [
   'supplementslove.com',
   'supplementspeak.com',
   'fitcareketo.com',
@@ -31,43 +56,38 @@ const BLACKLISTED_DOMAINS = [
   'totaldiet4you.com',
   'trypurenutrition.com',
   'reviewsbox.org',
+  'dasilex.co.uk',
+  'thehealthwind.com',
+  'wellnessketoz.com',
 ];
-
-/** Return type when running a spam analysis */
-type SpamAnalysisResult = {
-  /** A score between 0 and 1. 0=NotSpam, 1=IsSpamForSure */
-  score: number;
-  /** Detected spam keywords */
-  keywords: string[];
-  /** Detected blacklisted domains */
-  domains: string[];
-};
 
 /**
  * Returns suspicious keywords found in content
  */
-const getSuspiciousKeywords = (content: string): string[] => {
+export const getSuspiciousKeywords = (content: string): string[] => {
   if (!content) {
     return [];
   }
 
-  return SPAM_KEYWORDS.reduce((result, keyword) => {
+  return Object.keys(SPAM_KEYWORDS).reduce((result, keyword) => {
     if (content.toLowerCase().includes(keyword)) {
       result.push(keyword);
     }
+
     return result;
   }, []);
 };
+
 /**
  *
  * Returns blacklisted domains found in content
  */
-const getBlacklistedDomains = (content: string): string[] => {
+const getSpamDomains = (content: string): string[] => {
   if (!content) {
     return [];
   }
 
-  return BLACKLISTED_DOMAINS.reduce((result, domain) => {
+  return SPAMMERS_DOMAINS.reduce((result, domain) => {
     if (content.toLowerCase().includes(domain)) {
       result.push(domain);
     }
@@ -78,20 +98,19 @@ const getBlacklistedDomains = (content: string): string[] => {
 /**
  * Checks the values for this collective to try to determinate if it's a spammy profile.
  */
-export const collectiveSpamCheck = (collective: object): SpamAnalysisResult => {
+export const collectiveSpamCheck = (collective: any, context: string): SpamAnalysisReport => {
   const result = { score: 0, keywords: new Set<string>(), domains: new Set<string>() };
-  const scroreByField = 1.0 / ANALYZED_FIELDS.length;
 
   ANALYZED_FIELDS.forEach(field => {
     // Check each field for SPAM keywords
     const suspiciousKeywords = getSuspiciousKeywords(collective[field] || '');
-    if (suspiciousKeywords.length) {
-      suspiciousKeywords.forEach(keyword => result.keywords.add(keyword));
-      result.score += scroreByField;
-    }
+    suspiciousKeywords.forEach(keyword => {
+      result.keywords.add(keyword);
+      result.score += SPAM_KEYWORDS[keyword];
+    });
 
     // Check for blacklisted domains
-    const blacklitedDomains = getBlacklistedDomains(collective[field] || '');
+    const blacklitedDomains = getSpamDomains(collective[field] || '');
     if (blacklitedDomains.length) {
       blacklitedDomains.forEach(domain => result.domains.add(domain));
       result.score = 1;
@@ -99,33 +118,48 @@ export const collectiveSpamCheck = (collective: object): SpamAnalysisResult => {
   });
 
   return {
+    date: new Date().toISOString(),
     score: clamp(result.score, 0, 1),
     keywords: Array.from(result.keywords),
     domains: Array.from(result.domains),
+    data: collective.info || collective,
+    context,
   };
-};
-
-const buildSlackAlertMessage = (collective: object, spamAnalysisResult: SpamAnalysisResult): string => {
-  const { score, keywords, domains } = spamAnalysisResult;
-  let message = `*Suspicious collective data was submitted for collective:* https://opencollective.com/${collective['slug']}`;
-  const addLine = (line: string): string => (line ? `${message}\n${line}` : message);
-
-  message = addLine(`Score: ${score}`);
-  message = addLine(keywords.length > 0 && `Keywords: \`${keywords.toString()}\``);
-  message = addLine(domains.length > 0 && `Domains: \`${domains.toString()}\``);
-  return message;
 };
 
 /**
  * Post a message on Slack if the collective is suspicious
  */
-export const notifyTeamAboutSuspiciousCollective = async (collective: object): Promise<void> => {
-  const spamAnalysisResult = collectiveSpamCheck(collective);
+export const notifyTeamAboutSuspiciousCollective = async (report: SpamAnalysisReport): Promise<void> => {
+  const { score, keywords, domains, data } = report;
+  let message = `*Suspicious collective data was submitted for collective:* https://opencollective.com/${data['slug']}`;
+  const addLine = (line: string): string => (line ? `${message}\n${line}` : message);
+  message = addLine(`Score: ${score}`);
+  message = addLine(keywords.length > 0 && `Keywords: \`${keywords.toString()}\``);
+  message = addLine(domains.length > 0 && `Domains: \`${domains.toString()}\``);
+  return slackLib.postMessage(message, config.slack.webhookUrl, {
+    channel: config.slack.abuseChannel,
+  });
+};
 
-  if (spamAnalysisResult.score > 0) {
-    const message = buildSlackAlertMessage(collective, spamAnalysisResult);
-    return slackLib.postMessage(message, config.slack.webhookUrl, {
-      channel: config.slack.abuseChannel,
-    });
+/**
+ * Post a message on Slack if the collective is suspicious
+ */
+export const notifyTeamAboutPreventedCollectiveCreate = async (
+  report: SpamAnalysisReport,
+  user: any | null,
+): Promise<void> => {
+  const { keywords, domains } = report;
+  let message = `A collective creation was prevented and the user has been put in limited mode.`;
+  const addLine = (line: string): string => (line ? `${message}\n${line}` : message);
+  if (user) {
+    message = addLine(`UserId: ${user.id}`);
   }
+  message = addLine(keywords.length > 0 && `Keywords: \`${keywords.toString()}\``);
+  message = addLine(domains.length > 0 && `Domains: \`${domains.toString()}\``);
+  message = addLine(`Collective data:`);
+  message = addLine(`> ${JSON.stringify(report.data)}`);
+  return slackLib.postMessage(message, config.slack.webhookUrl, {
+    channel: config.slack.abuseChannel,
+  });
 };
--- test/server/lib/spam.test.js
+++ test/server/lib/spam.test.js
@@ -1,37 +1,65 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import slackLib from '../../../server/lib/slack';
-import { collectiveSpamCheck, notifyTeamAboutSuspiciousCollective } from '../../../server/lib/spam';
+import {
+  collectiveSpamCheck,
+  notifyTeamAboutSuspiciousCollective,
+  notifyTeamAboutPreventedCollectiveCreate,
+} from '../../../server/lib/spam';
+import { fakeCollective } from '../../test-helpers/fake-data';
 
 describe('server/lib/spam', () => {
+  let clock;
+
+  before(() => {
+    clock = sinon.useFakeTimers(new Date('2020-01-01'));
+  });
+
+  after(() => {
+    clock.restore();
+  });
+
   describe('collectiveSpamCheck', () => {
     it('detects bad keywords', async () => {
       // Description
-      expect(collectiveSpamCheck({ description: 'Some keto stuff' })).to.deep.eq({
-        score: 0.25,
+      const collectiveWithBadDescription = await fakeCollective({ description: 'Some keto stuff' });
+      expect(collectiveSpamCheck(collectiveWithBadDescription, 'test')).to.deep.eq({
+        score: 0.3,
         keywords: ['keto'],
         domains: [],
+        context: 'test',
+        data: collectiveWithBadDescription.info,
+        date: '2020-01-01T00:00:00.000Z',
       });
 
       // Long description
       expect(collectiveSpamCheck({ longDescription: 'Some PORN stuff' })).to.deep.eq({
-        score: 0.25,
+        score: 0.2,
         keywords: ['porn'],
         domains: [],
+        context: undefined,
+        date: '2020-01-01T00:00:00.000Z',
+        data: { longDescription: 'Some PORN stuff' },
       });
 
       // Website
       expect(collectiveSpamCheck({ website: 'https://maxketo.com' })).to.deep.eq({
-        score: 0.25,
+        score: 0.3,
         keywords: ['keto'],
         domains: [],
+        context: undefined,
+        date: '2020-01-01T00:00:00.000Z',
+        data: { website: 'https://maxketo.com' },
       });
 
       // Name
       expect(collectiveSpamCheck({ name: 'BEST KeTo!!!' })).to.deep.eq({
-        score: 0.25,
+        score: 0.3,
         keywords: ['keto'],
         domains: [],
+        context: undefined,
+        date: '2020-01-01T00:00:00.000Z',
+        data: { name: 'BEST KeTo!!!' },
       });
     });
 
@@ -41,6 +69,9 @@ describe('server/lib/spam', () => {
         score: 1,
         keywords: [],
         domains: ['supplementslove.com'],
+        context: undefined,
+        date: '2020-01-01T00:00:00.000Z',
+        data: { website: 'https://supplementslove.com/promotion' },
       });
     });
   });
@@ -57,12 +88,37 @@ describe('server/lib/spam', () => {
     });
 
     it('notifies Slack with the report info', async () => {
-      await notifyTeamAboutSuspiciousCollective({ name: 'Keto stuff', slug: 'ketoooo' });
+      const report = collectiveSpamCheck({ name: 'Keto stuff', slug: 'ketoooo' });
+      await notifyTeamAboutSuspiciousCollective(report);
       expect(slackPostMessageStub.calledOnce).to.be.true;
 
       const args = slackPostMessageStub.getCall(0).args;
       expect(args[0]).to.eq(
-        '*Suspicious collective data was submitted for collective:* https://opencollective.com/ketoooo\nScore: 0.25\nKeywords: `keto`',
+        '*Suspicious collective data was submitted for collective:* https://opencollective.com/ketoooo\nScore: 0.3\nKeywords: `keto`',
+      );
+      expect(args[2].channel).to.eq('#abuse');
+    });
+  });
+
+  describe('notifyTeamAboutPreventedCollectiveCreate', () => {
+    let slackPostMessageStub = null;
+
+    before(() => {
+      slackPostMessageStub = sinon.stub(slackLib, 'postMessage');
+    });
+
+    after(() => {
+      slackPostMessageStub.restore();
+    });
+
+    it('notifies Slack with the report info', async () => {
+      const report = collectiveSpamCheck({ name: 'Keto stuff', slug: 'ketoooo' });
+      await notifyTeamAboutPreventedCollectiveCreate(report);
+      expect(slackPostMessageStub.calledOnce).to.be.true;
+
+      const args = slackPostMessageStub.getCall(0).args;
+      expect(args[0]).to.eq(
+        'A collective creation was prevented and the user has been put in limited mode.\nKeywords: `keto`\nCollective data:\n> {"name":"Keto stuff","slug":"ketoooo"}',
       );
       expect(args[2].channel).to.eq('#abuse');
     });
