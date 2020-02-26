37f74ec0d64856dbeb7e25722ef50299acfc88eb
Kate Beard <asouthernbellein@gmail.com>
2020-02-26 13:50:26 +0000

    Remove unused fragment

--- components/create-collective/sections/CollectiveCategoryPicker.js
+++ components/create-collective/sections/CollectiveCategoryPicker.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Flex, Box } from '@rebass/grid';
 import styled from 'styled-components';
