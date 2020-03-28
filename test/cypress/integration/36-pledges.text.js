4af4934bb2580a8e2c51eb20e610ee30eb38fb0f
samcaspus <sandeepguptan1998@gmail.com>
2020-03-28 03:18:54 -0700

    added test for creation of pledge and joing a pledge with verification of the amount symbol
    verified display of elements in FAQ

--- test/cypress/integration/36-pledges.text.js
+++ test/cypress/integration/36-pledges.text.js
@@ -3,17 +3,62 @@ describe('Pledges', () => {
     cy.login({ redirect: `/pledges/new` });
   });
 
-  it('Create the pledge', () => {
-    cy.get('#name', { timeout: 200000 }).clear().type('samcaspus');
-    cy.get('#slug', { timeout: 200000 }).clear().type('opencollective');
-    cy.get('#githubHandle', { timeout: 200000 }).clear().type('opencollective/opencollective-frontend');
-    cy.get('#publicMessage', { timeout: 200000 }).clear().type('publicMessage');
-    cy.get('[type="submit"]', { timeout: 200000 }).click();
+  describe('creating an individual pledge', () => {
+    it('Create the pledge', () => {
+      cy.get('[id=name]', { timeout: 200000 }).clear().type('samcaspus');
+      cy.get('[id=slug]', { timeout: 200000 }).clear().type('opencollective');
+      cy.get('[id=githubHandle]', { timeout: 200000 }).clear().type('opencollective/opencollective-frontend');
+      cy.get('[id=publicMessage]', { timeout: 200000 }).clear().type('publicMessage');
+      cy.get('[type="submit"]', { timeout: 200000 }).click();
+      cy.get('span', { timeout: 200000 })
+        .should('have.class', 'Text__P-sc-3suny7-0-span')
+        .and('have.class', 'kTfuAH')
+        .contains('$');
+    });
 
-    cy.get('.Text__P-sc-3suny7-0-span.sfJZk', { timeout: 200000 }).eq(1).contains('$');
-    cy.get('.src__Box-sc-1sbtrzs-0 .gpbLUB', { timeout: 200000 }).contains(
-      'https://github.com/opencollective/opencollective-frontend',
-    );
-    cy.get('.Text__P-sc-3suny7-0-span .sfJZk').should('be.visible').contains('$');
+    it('join an existing pledge individual pledge', () => {
+      cy.get('[id=name]', { timeout: 200000 }).clear().type('samcaspus2');
+      cy.get('[id=slug]', { timeout: 200000 }).clear().type('opencollective');
+      cy.get('[id=githubHandle]', { timeout: 200000 }).clear().type('opencollective/opencollective-frontend');
+      cy.get('[id=publicMessage]', { timeout: 200000 }).clear().type('publicMessage');
+      cy.get('[type="submit"]', { timeout: 200000 }).click();
+      cy.get('span', { timeout: 200000 })
+        .should('have.class', 'Text__P-sc-3suny7-0-span')
+        .and('have.class', 'kTfuAH')
+        .contains('$');
+    });
+  });
+
+  describe('check FAQ context in each pledge is valid or not', () => {
+    it('verift what is pledge ?', () => {
+      cy.get('.createPledge__Details-f3btza-0', { timeout: 200000 }).eq(0).click();
+      cy.get('.createPledge__Details-f3btza-0', { timeout: 200000 })
+        .eq(0)
+        .contains(
+          'A pledge allows supporters (companies and individuals) to pledge funds towards a collective that hasn’t been created yet. If you can’t find a collective you want to support, pledge to it!',
+        );
+    });
+    it('verift what happends after i pledge ?', () => {
+      cy.get('.createPledge__Details-f3btza-0', { timeout: 200000 }).eq(1).click();
+      cy.get('.createPledge__Details-f3btza-0', { timeout: 200000 })
+        .eq(1)
+        .contains(
+          'Once someone makes a pledge to a collective, we automatically create a pledged collective. We don’t spam folks, so please help us reach out to the community via twitter / github or, if you can, via email.',
+        );
+    });
+    it('verift when do i pay?', () => {
+      cy.get('.createPledge__Details-f3btza-0', { timeout: 200000 }).eq(2).click();
+      cy.get('.createPledge__Details-f3btza-0', { timeout: 200000 })
+        .eq(2)
+        .contains('Once that pledged collective is claimed, we will email you to fulfill your pledge.');
+    });
+    it('verift what is pledge ?', () => {
+      cy.get('.createPledge__Details-f3btza-0', { timeout: 200000 }).eq(3).click();
+      cy.get('.createPledge__Details-f3btza-0', { timeout: 200000 })
+        .eq(3)
+        .contains(
+          'You’ll need to authenticate with the github profile that owns / admins that project. Just click on the Claim Collective button in the pledged collective. We will be rolling out other forms of authentication in the future.',
+        );
+    });
   });
 });
