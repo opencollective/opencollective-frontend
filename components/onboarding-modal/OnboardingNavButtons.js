65495fc28f83a0e093c00c59ca03bce182d07482
Kate Beard <asouthernbellein@gmail.com>
2020-03-31 19:02:22 +0100

    fix(Create Collective Flow): Fix onboarding modal Formik submit bug

--- components/onboarding-modal/OnboardingNavButtons.js
+++ components/onboarding-modal/OnboardingNavButtons.js
@@ -48,6 +48,7 @@ class OnboardingNavButtons extends React.Component {
           <Fragment>
             {viewport === VIEWPORTS.MOBILE ? (
               <StyledButton
+                type="button"
                 mx={1}
                 buttonStyle="primary"
                 disabled={this.getStepParams(step, 'disabled')}
@@ -63,6 +64,7 @@ class OnboardingNavButtons extends React.Component {
               </StyledButton>
             ) : (
               <StyledRoundButton
+                type="button"
                 mx={1}
                 size={48}
                 disabled={this.getStepParams(step, 'disabled')}
@@ -78,7 +80,7 @@ class OnboardingNavButtons extends React.Component {
               </StyledRoundButton>
             )}
 
-            <StyledButton buttonStyle="primary" onClick={handleSubmit} loading={loading}>
+            <StyledButton buttonStyle="primary" onClick={() => handleSubmit} loading={loading}>
               <FormattedMessage id="Finish" defaultMessage="Finish" />
             </StyledButton>
           </Fragment>
@@ -86,6 +88,7 @@ class OnboardingNavButtons extends React.Component {
           <Fragment>
             {viewport === VIEWPORTS.MOBILE ? (
               <StyledButton
+                type="button"
                 mx={1}
                 buttonStyle="primary"
                 disabled={this.getStepParams(step, 'disabled')}
@@ -101,6 +104,7 @@ class OnboardingNavButtons extends React.Component {
               </StyledButton>
             ) : (
               <StyledRoundButton
+                type="button"
                 mx={1}
                 size={48}
                 disabled={this.getStepParams(step, 'disabled')}
@@ -117,6 +121,7 @@ class OnboardingNavButtons extends React.Component {
             )}
             {viewport === VIEWPORTS.MOBILE ? (
               <StyledButton
+                type="button"
                 mx={1}
                 buttonStyle="primary"
                 onClick={() => {
@@ -131,6 +136,7 @@ class OnboardingNavButtons extends React.Component {
               </StyledButton>
             ) : (
               <StyledRoundButton
+                type="button"
                 mx={1}
                 size={48}
                 onClick={() => {
