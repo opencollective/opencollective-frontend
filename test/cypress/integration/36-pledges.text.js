26e75a91ad13d51645ff598378741694f708ac7a
samcaspus <sandeepguptan1998@gmail.com>
2020-03-31 02:33:36 -0700

    reduced timeout to 1200

--- test/cypress/integration/36-pledges.text.js
+++ test/cypress/integration/36-pledges.text.js
@@ -10,21 +10,21 @@ describe('Pledges', () => {
       cy.get('[data-cy=githubHandleInput]').clear().type('opencollective/opencollective-frontend');
       cy.get('[id=publicMessage]').clear().type('publicMessage');
       cy.get('[data-cy="submit"]').click();
-      cy.get('[data-cy="currencyAmount"]', { timeout: 600000 }).should('contain', '$');
+      cy.get('[data-cy="currencyAmount"]', { timeout: 12000 }).should('contain', '$');
     });
 
     it('join an existing pledge individual pledge', () => {
       cy.visit('/opencollective');
       let value1 = '';
-      cy.get('[data-cy="currencyAmount"]', { timeout: 600000 }).then($val => {
+      cy.get('[data-cy="currencyAmount"]', { timeout: 12000 }).then($val => {
         value1 = $val.text();
       });
       cy.get('[data-cy=makeAPledgeButton]').click();
       cy.get('[data-cy=publicMessage]').clear().type('publicMessage');
       cy.get('[data-cy="submit"]').click();
-      cy.get('[data-cy="currencyAmount"]', { timeout: 600000 }).should('contain', '$');
+      cy.get('[data-cy="currencyAmount"]', { timeout: 12000 }).should('contain', '$');
       cy.reload();
-      cy.get('[data-cy="currencyAmount"]', { timeout: 600000 }).then($val => {
+      cy.get('[data-cy="currencyAmount"]', { timeout: 12000 }).then($val => {
         const value2 = $val.text();
         expect(value1).not.to.eq(value2);
       });
@@ -35,9 +35,9 @@ describe('Pledges', () => {
       cy.get('[data-cy=githubHandleInput]').clear().type('opencollective/opencollective-frontend');
       cy.get('[id=publicMessage]').clear().type('publicMessage');
       cy.get('[data-cy="submit"]').click();
-      cy.get('[data-cy="pledgeStats"]', { timeout: 600000 }).contains('$');
+      cy.get('[data-cy="pledgeStats"]', { timeout: 12000 }).contains('$');
       let amountOutside = '';
-      cy.get('[data-cy="currencyAmount"]', { timeout: 600000 })
+      cy.get('[data-cy="currencyAmount"]', { timeout: 12000 })
         .should('contain', '$')
         .then($val => {
           amountOutside = $val.text();
@@ -46,7 +46,7 @@ describe('Pledges', () => {
       cy.get('[data-cy="user-menu-trigger"]').click();
       cy.get('[data-cy="logout"]').click();
       cy.visit('/opencollective/pledges/new');
-      cy.get('[data-cy="amountPledgedTotal"] > span', { timeout: 600000 }).should('contain', amountOutside);
+      cy.get('[data-cy="amountPledgedTotal"] > span', { timeout: 12000 }).should('contain', amountOutside);
     });
   });
   it('creating a pledge unable to verify the organization', () => {
@@ -57,7 +57,7 @@ describe('Pledges', () => {
     cy.get('[data-cy="submit"]').click();
     cy.url().should('contain', '/pledges/new');
 
-    cy.get('[data-cy="errorMessage"]', { timeout: 600000 }).should(
+    cy.get('[data-cy="errorMessage"]', { timeout: 12000 }).should(
       'contain',
       'Error:  We could not verify the GitHub organization exists',
     );
@@ -70,7 +70,7 @@ describe('Pledges', () => {
     cy.get('[data-cy=publicMessage]').clear().type('publicMessage');
     cy.get('[data-cy="submit"]').click();
     cy.url().should('contain', '/pledges/new');
-    cy.get('[data-cy="errorMessage"]', { timeout: 600000 }).should(
+    cy.get('[data-cy="errorMessage"]', { timeout: 12000 }).should(
       'contain',
       'Error:  We could not verify the GitHub repository exists',
     );
@@ -82,7 +82,7 @@ describe('Pledges', () => {
     cy.get('[data-cy=slugInput]').clear().type('demoslug');
     cy.get('[data-cy=githubHandleInput]').clear().type('demo/dummy');
     cy.get('[data-cy=publicMessage]').clear().type('publicMessage');
-    cy.get('[data-cy="errorMessage"]', { timeout: 600000 }).contains(
+    cy.get('[data-cy="errorMessage"]', { timeout: 12000 }).contains(
       'Error: No collective id/website/githubHandle provided',
     );
 
@@ -93,7 +93,7 @@ describe('Pledges', () => {
 describe('check FAQ context in each pledge is valid or not', () => {
   it('verift what is pledge ?', () => {
     cy.get('[data-cy="whatIsAPledge"]').click();
-    cy.get('[data-cy="whatIsAPledge"]', { timeout: 600000 }).should(
+    cy.get('[data-cy="whatIsAPledge"]', { timeout: 12000 }).should(
       'contain',
       'towards a collective that hasn’t been created yet. If you',
     );
@@ -101,7 +101,7 @@ describe('check FAQ context in each pledge is valid or not', () => {
 
   it('verift how do i claim a pledge ?', () => {
     cy.get('[data-cy="howDoIClaimPledge"]').click();
-    cy.get('[data-cy="howDoIClaimPledge"]', { timeout: 600000 }).should(
+    cy.get('[data-cy="howDoIClaimPledge"]', { timeout: 12000 }).should(
       'contain',
       'authenticate with the github profile that owns',
     );
