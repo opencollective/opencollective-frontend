a0a67bd71783af4e33909391db9097c17dbcbe7f
sandeepguptan <sandeep.guptan@hackerearth.com>
2020-04-01 17:12:29 +0530

    removed be.visible as per review

--- test/cypress/integration/36-pledges.text.js
+++ test/cypress/integration/36-pledges.text.js
@@ -10,7 +10,7 @@ describe('Pledges', () => {
       cy.get('[data-cy=githubHandleInput]').clear().type('opencollective/opencollective-frontend');
       cy.get('[id=publicMessage]').clear().type('publicMessage');
       cy.get('[data-cy="submit"]').click();
-      cy.get('[data-cy="currencyAmount"]').should('be.visible').should('contain', '$');
+      cy.get('[data-cy="currencyAmount"]').should('contain', '$');
     });
 
     it('join an existing pledge individual pledge', () => {
@@ -22,14 +22,12 @@ describe('Pledges', () => {
       cy.get('[data-cy=makeAPledgeButton]').click();
       cy.get('[data-cy=publicMessage]').clear().type('publicMessage');
       cy.get('[data-cy="submit"]').click();
-      cy.get('[data-cy="currencyAmount"]').should('be.visible').should('contain', '$');
+      cy.get('[data-cy="currencyAmount"]').should('contain', '$');
       cy.reload();
-      cy.get('[data-cy="currencyAmount"]')
-        .should('be.visible')
-        .then($val => {
-          const value2 = $val.text();
-          expect(value1).not.to.eq(value2);
-        });
+      cy.get('[data-cy="currencyAmount"]').then($val => {
+        const value2 = $val.text();
+        expect(value1).not.to.eq(value2);
+      });
     });
 
     it('creating a pledge and ensuring the amount is visible loggedout', () => {
@@ -38,10 +36,9 @@ describe('Pledges', () => {
       cy.get('[data-cy=githubHandleInput]').clear().type('opencollective/opencollective-frontend');
       cy.get('[id=publicMessage]').clear().type('publicMessage');
       cy.get('[data-cy="submit"]').click();
-      cy.get('[data-cy="pledgeStats"]').should('be.visible').contains('$');
+      cy.get('[data-cy="pledgeStats"]').contains('$');
       let amountOutside = '';
       cy.get('[data-cy="currencyAmount"]')
-        .should('be.visible')
         .should('contain', '$')
         .then($val => {
           amountOutside = $val.text();
@@ -50,7 +47,7 @@ describe('Pledges', () => {
       cy.get('[data-cy="user-menu-trigger"]').click();
       cy.get('[data-cy="logout"]').click();
       cy.visit('/opencollective/pledges/new');
-      cy.get('[data-cy="amountPledgedTotal"] > span').should('be.visible').should('contain', amountOutside);
+      cy.get('[data-cy="amountPledgedTotal"] > span').should('contain', amountOutside);
     });
   });
   it('creating a pledge unable to verify the organization', () => {
@@ -61,9 +58,7 @@ describe('Pledges', () => {
     cy.get('[data-cy="submit"]').click();
     cy.url().should('contain', '/pledges/new');
 
-    cy.get('[data-cy="errorMessage"]')
-      .should('be.visible')
-      .should('contain', 'Error:  We could not verify the GitHub organization exists');
+    cy.get('[data-cy="errorMessage"]').should('contain', 'Error:  We could not verify the GitHub organization exists');
   });
 
   it('creating a pledge unable to verify the repository', () => {
@@ -73,9 +68,7 @@ describe('Pledges', () => {
     cy.get('[data-cy=publicMessage]').clear().type('publicMessage');
     cy.get('[data-cy="submit"]').click();
     cy.url().should('contain', '/pledges/new');
-    cy.get('[data-cy="errorMessage"]')
-      .should('be.visible')
-      .should('contain', 'Error:  We could not verify the GitHub repository exists');
+    cy.get('[data-cy="errorMessage"]').should('contain', 'Error:  We could not verify the GitHub repository exists');
   });
 
   it('creating a pledge unable to verify the repository', () => {
@@ -84,9 +77,7 @@ describe('Pledges', () => {
     cy.get('[data-cy=slugInput]').clear().type('demoslug');
     cy.get('[data-cy=githubHandleInput]').clear().type('demo/dummy');
     cy.get('[data-cy=publicMessage]').clear().type('publicMessage');
-    cy.get('[data-cy="errorMessage"]')
-      .should('be.visible')
-      .contains('Error: No collective id/website/githubHandle provided');
+    cy.get('[data-cy="errorMessage"]').contains('Error: No collective id/website/githubHandle provided');
 
     cy.url().should('contain', '/pledges/new');
   });
@@ -100,9 +91,7 @@ describe('check FAQ context in each pledge is valid or not', () => {
 
   it('verift how do i claim a pledge ?', () => {
     cy.get('[data-cy="howDoIClaimPledge"]').click();
-    cy.get('[data-cy="howDoIClaimPledge"]')
-      .should('be.visible')
-      .should('contain', 'authenticate with the github profile that owns');
+    cy.get('[data-cy="howDoIClaimPledge"]').should('contain', 'authenticate with the github profile that owns');
   });
 });
 
@@ -114,11 +103,9 @@ describe('Pledges Logged out', () => {
   it('verify if loggedout pledged amount is same as logged in', () => {
     cy.visit('/opencollective/pledges/new');
     let amountOutside = '';
-    cy.get('[data-cy="amountPledgedTotal"]')
-      .should('be.visible')
-      .then($val => {
-        amountOutside = $val.text();
-      });
+    cy.get('[data-cy="amountPledgedTotal"]').then($val => {
+      amountOutside = $val.text();
+    });
     cy.login();
     cy.visit('/opencollective');
     cy.get('[data-cy="currencyAmount"]').should('contain', amountOutside);
