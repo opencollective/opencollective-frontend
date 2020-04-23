be3ec5dd45ced1a8e7ddca610d23e3341362aff1
Kate Beard <asouthernbellein@gmail.com>
2020-04-23 18:14:12 +0100

    feat(Accept financial contributions): Add organization flow

--- components/accept-financial-contributions/AcceptContributionsMyselfOrOrg.js
+++ components/accept-financial-contributions/AcceptContributionsMyselfOrOrg.js
@@ -1,21 +1,27 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from '@apollo/react-hoc';
+import { PlusCircle } from '@styled-icons/boxicons-regular/PlusCircle';
 import { Field, Form, Formik } from 'formik';
+import { uniqBy } from 'lodash';
 import { withRouter } from 'next/router';
 import { FormattedMessage } from 'react-intl';
+import styled from 'styled-components';
 
 import { getErrorFromGraphqlException } from '../../lib/errors';
 import { API_V2_CONTEXT, gqlV2 } from '../../lib/graphql/helpers';
 import { Router } from '../../server/pages';
 
 import Avatar from '../Avatar';
+import { getCollectivePageQuery } from '../collective-page/graphql/queries';
 import CollectiveNavbar from '../CollectiveNavbar';
 import Container from '../Container';
+import CreateCollectiveMiniForm from '../CreateCollectiveMiniForm';
 import FinancialContributionsFAQ from '../faqs/FinancialContributionsFAQ';
 import { Box, Flex } from '../Grid';
 import MessageBox from '../MessageBox';
 import StyledButton from '../StyledButton';
+import StyledHr from '../StyledHr';
 import StyledInputField from '../StyledInputField';
 import StyledTextarea from '../StyledTextarea';
 import { H1, H2, P } from '../Text';
@@ -23,7 +29,39 @@ import { withUser } from '../UserProvider';
 
 import StripeOrBankAccountPicker from './StripeOrBankAccountPicker';
 
-class AcceptContributionsMyself extends React.Component {
+import acceptOrganizationIllustration from '../../public/static/images/create-collective/acceptContributionsOrganizationHoverIllustration.png';
+
+const CreateNewOrg = styled(Flex)`
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  cursor: pointer;
+`;
+
+const OrgCard = styled(Flex)`
+  cursor: pointer;
+  border-radius: 10px;
+  &:hover {
+    background: rgba(0, 0, 0, 0.1);
+  }
+`;
+
+const Image = styled.img`
+  @media screen and (min-width: 52em) {
+    height: 256px;
+    width: 256px;
+  }
+  @media screen and (max-width: 40em) {
+    height: 192px;
+    width: 192px;
+  }
+  @media screen and (min-width: 40em) and (max-width: 52em) {
+    height: 208px;
+    width: 208px;
+  }
+`;
+
+class AcceptContributionsMyselfOrOrg extends React.Component {
   static propTypes = {
     collective: PropTypes.object,
     router: PropTypes.object,
@@ -38,9 +76,12 @@ class AcceptContributionsMyself extends React.Component {
 
     this.state = {
       loading: null,
+      miniForm: false,
+      organization: null,
     };
   }
 
+  // GraphQL functions
   addHost = async (collective, host) => {
     const collectiveInput = {
       legacyId: collective.id,
@@ -54,6 +95,8 @@ class AcceptContributionsMyself extends React.Component {
           collective: collectiveInput,
           host: hostInput,
         },
+        refetchQueries: [{ query: getCollectivePageQuery, variables: { slug: this.props.collective.slug } }],
+        awaitRefetchQueries: true,
       });
     } catch (err) {
       const errorMsg = getErrorFromGraphqlException(err).message;
@@ -101,8 +144,21 @@ class AcceptContributionsMyself extends React.Component {
 
   render() {
     const { collective, router, LoggedInUser } = this.props;
-    const { error } = this.state;
+    const { error, miniForm, organization, loading } = this.state;
 
+    // Get and filter orgs LoggedInUser is part of
+    const memberships = uniqBy(
+      LoggedInUser.memberOf.filter(m => m.role === 'ADMIN'),
+      m => m.collective.id,
+    );
+
+    const orgs = memberships
+      .filter(m => m.collective.type === 'ORGANIZATION')
+      .sort((a, b) => {
+        return a.collective.slug.localeCompare(b.collective.slug);
+      });
+
+    // Form values and submit
     const initialValues = {
       bankInformation: '',
     };
@@ -126,6 +182,12 @@ class AcceptContributionsMyself extends React.Component {
       }
     };
 
+    // Conditional rendering
+    const noOrganizationPicked = router.query.path === 'organization' && !organization;
+    const organizationPicked = router.query.path === 'organization' && organization;
+    const ableToChoseStripeOrBankAccount =
+      organizationPicked || (!router.query.method && router.query.path === 'myself');
+
     return (
       <Fragment>
         <CollectiveNavbar collective={collective} onlyInfos={true} />
@@ -136,21 +198,92 @@ class AcceptContributionsMyself extends React.Component {
         </Box>
         <Container display="flex" flexDirection="column" alignItems="center">
           <Flex flexDirection="column" alignItems="center" maxWidth={'575px'} my={2} mx={[3, 0]}>
-            <Avatar collective={LoggedInUser.collective} radius={64} mb={2} />
-            <P fontSize="LeadParagraph" lineHeight="LeadCaption" fontWeight="bold" mb={3}>
-              {LoggedInUser.collective.name}
-            </P>
-            <H2 fontSize="H5" fontWeight="bold" color="black.900" textAlign="center">
-              {router.query.method === 'bank' ? (
-                <FormattedMessage id="acceptContributions.addBankAccount" defaultMessage="Add bank account" />
-              ) : (
-                <FormattedMessage
-                  id="acceptContributions.howAreYouAcceptingContributions"
-                  defaultMessage="How are you accepting contributions?"
-                />
-              )}
-            </H2>
+            {noOrganizationPicked ? (
+              <Fragment>
+                <Image src={acceptOrganizationIllustration} alt="" />
+                <H2 fontSize="H5" fontWeight="bold" color="black.900" textAlign="center">
+                  <FormattedMessage id="acceptContributions.organization.subtitle" defaultMessage="Our organization" />
+                </H2>
+              </Fragment>
+            ) : (
+              <Fragment>
+                <Avatar collective={organizationPicked ? organization : LoggedInUser.collective} radius={64} mb={2} />
+                <P fontSize="LeadParagraph" lineHeight="LeadCaption" fontWeight="bold" mb={3}>
+                  {organizationPicked ? organization.name : LoggedInUser.collective.name}
+                </P>
+                <H2 fontSize="H5" fontWeight="bold" color="black.900" textAlign="center">
+                  {router.query.method === 'bank' ? (
+                    <FormattedMessage id="acceptContributions.addBankAccount" defaultMessage="Add bank account" />
+                  ) : (
+                    <FormattedMessage
+                      id="acceptContributions.howAreYouAcceptingContributions"
+                      defaultMessage="How are you accepting contributions?"
+                    />
+                  )}
+                </H2>
+              </Fragment>
+            )}
           </Flex>
+          {noOrganizationPicked && (
+            <Flex flexDirection="column" justifyContent="center" alignItems="center" my={3} minWidth={'450px'}>
+              <Flex px={3} width="100%">
+                <P my={2} fontSize="Caption" textTransform="uppercase" color="black.700">
+                  <FormattedMessage id="acceptContributions.organization.myOrgs" defaultMessage="My organizations" />
+                </P>
+                <Flex flexGrow={1} alignItems="center">
+                  <StyledHr width="100%" ml={2} />
+                </Flex>
+              </Flex>
+              {orgs.length > 0 && (
+                <Flex px={3} width="100%" flexDirection="column">
+                  {orgs.map(org => (
+                    <OrgCard
+                      alignItems="center"
+                      key={org.collective.id}
+                      my={2}
+                      onClick={() => this.setState({ organization: org.collective })}
+                    >
+                      <Avatar radius={56} collective={org.collective} />
+                      <Flex flexDirection="column" ml={3}>
+                        <P color="black.900" mb={1}>
+                          {org.collective.name}
+                        </P>
+                        <P color="black.600">@{org.collective.slug}</P>
+                      </Flex>
+                    </OrgCard>
+                  ))}
+                </Flex>
+              )}
+              <Flex px={3} width="100%">
+                <P my={2} fontSize="Caption" textTransform="uppercase" color="black.700">
+                  <FormattedMessage id="CollectivePicker.CreateNew" defaultMessage="Create new" />
+                </P>
+                <Flex flexGrow={1} alignItems="center">
+                  <StyledHr width="100%" ml={2} />
+                </Flex>
+              </Flex>
+
+              <Flex my={2} px={3} flexDirection="column" width="100%">
+                {miniForm ? (
+                  <CreateCollectiveMiniForm
+                    type="ORGANIZATION"
+                    onCancel={() => this.setState({ miniForm: false })}
+                    onSuccess={data => this.setState({ organization: data })}
+                    LoggedInUser={LoggedInUser}
+                    addLoggedInUserAsAdmin
+                    excludeAdminFields
+                  />
+                ) : (
+                  <CreateNewOrg alignItems="center" onClick={() => this.setState({ miniForm: true })}>
+                    <PlusCircle size="24" color="gray" />
+                    <P fontSize="Caption" color="black.800" ml={2}>
+                      <FormattedMessage id="Organization.CreateNew" defaultMessage="Create new Organization" />
+                    </P>
+                  </CreateNewOrg>
+                )}
+              </Flex>
+            </Flex>
+          )}
           {router.query.method === 'bank' && (
             <Flex flexDirection={['column', 'row']} justifyContent={'space-evenly'} mx={[2, 4]} my={3}>
               <Box width={1 / 5} display={['none', null, 'block']}></Box>
@@ -233,7 +366,7 @@ class AcceptContributionsMyself extends React.Component {
                               ml={2}
                               buttonStyle="dark"
                               type="submit"
-                              loading={this.state.loading}
+                              loading={loading}
                               onSubmit={handleSubmit}
                             >
                               <FormattedMessage id="save" defaultMessage="Save" />
@@ -250,10 +383,10 @@ class AcceptContributionsMyself extends React.Component {
               </Flex>
             </Flex>
           )}
-          {!router.query.method && (
+          {ableToChoseStripeOrBankAccount && (
             <StripeOrBankAccountPicker
               LoggedInUser={LoggedInUser}
-              hostCollectiveSlug={LoggedInUser.collective.slug}
+              hostCollectiveSlug={organization ? organization.slug : LoggedInUser.collective.slug}
               addHost={this.addHost}
               collective={collective}
             />
@@ -296,4 +429,4 @@ const addBankAccountMutation = graphql(bankAccountMutation, {
   options: { context: API_V2_CONTEXT },
 });
 
-export default withUser(withRouter(addBankAccountMutation(addApplyToHostMutation(AcceptContributionsMyself))));
+export default withUser(withRouter(addBankAccountMutation(addApplyToHostMutation(AcceptContributionsMyselfOrOrg))));
