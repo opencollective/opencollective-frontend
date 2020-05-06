54bae8bf3a2106d0744f2ce4aa02c2226581be9b
Leo Kewitz <leokewitz@gmail.com>
2020-05-06 15:10:26 +0000

    refact: improve formatAccountDetails()

--- components/edit-collective/UpdateBankDetailsForm.js
+++ components/edit-collective/UpdateBankDetailsForm.js
@@ -50,23 +50,36 @@ class UpdateBankDetailsForm extends React.Component {
 
   formatAccountDetails(payoutMethodData) {
     const ignoredKeys = ['type', 'isManualBankTransfer', 'currency'];
+    const labels = {
+      abartn: 'Routing Number: ',
+      firstLine: '',
+    };
+
     const formatKey = s => {
-      if (toUpper(s) === s) {
-        return s;
+      if (labels[s] !== undefined) {
+        return labels[s];
       }
-      return startCase(s);
+      if (toUpper(s) === s) {
+        return `${s}: `;
+      }
+      return `${startCase(s)}: `;
     };
 
     const renderObject = (object, prefix = '') =>
-      Object.entries(object).reduce((acc, [key, value]) => {
-        if (ignoredKeys.includes(key)) {
-          return acc;
-        }
-        if (typeof value === 'object') {
-          return [...acc, formatKey(key), ...renderObject(value, '  ')];
-        }
-        return [...acc, `${prefix}${formatKey(key)}: ${value}`];
-      }, []);
+      Object.entries(object)
+        .sort(a => (typeof a[1] == 'object' ? 1 : -1))
+        .reduce((acc, [key, value]) => {
+          if (ignoredKeys.includes(key)) {
+            return acc;
+          }
+          if (typeof value === 'object') {
+            if (key === 'details') {
+              return [...acc, ...renderObject(value, '')];
+            }
+            return [...acc, formatKey(key), ...renderObject(value, '  ')];
+          }
+          return [...acc, `${prefix}${formatKey(key)}${value}`];
+        }, []);
 
     const { accountHolderName, currency, ...data } = payoutMethodData;
     const lines = renderObject({ accountHolderName, currency, ...data });
