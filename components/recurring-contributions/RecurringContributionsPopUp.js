8212c0dc2e3d21d20e1a5e40dcecfb8a0cc91254
Kate Beard <asouthernbellein@gmail.com>
2020-06-03 12:25:13 +0100

    feat(Recurring contributions): Update tiers pop up

--- components/recurring-contributions/RecurringContributionsPopUp.js
+++ components/recurring-contributions/RecurringContributionsPopUp.js
@@ -19,6 +19,7 @@ import StyledHr from '../StyledHr';
 import { P } from '../Text';
 import { withUser } from '../UserProvider';
 
+import UpdateOrderPopUp from './UpdateOrderPopUp';
 import UpdatePaymentMethodPopUp from './UpdatePaymentMethodPopUp';
 
 const messages = defineMessages({
@@ -121,22 +122,22 @@ const RecurringContributionsPopUp = ({ contribution, status, createNotification,
   });
 
   // detect click outside menu to close it - https://medium.com/@pitipatdop/little-neat-trick-to-capture-click-outside-with-react-hook-ba77c37c7e82
-  const popupNode = useRef();
-  const handleClick = e => {
-    if (popupNode.current.contains(e.target)) {
-      // inside click
-      return;
-    }
-    // outside click
-    setShowPopup(false);
-  };
-  useEffect(() => {
-    document.addEventListener('mousedown', handleClick);
+  // const popupNode = useRef();
+  // const handleClick = e => {
+  //   if (popupNode.current.contains(e.target)) {
+  //     // inside click
+  //     return;
+  //   }
+  //   // outside click
+  //   setShowPopup(false);
+  // };
+  // useEffect(() => {
+  //   document.addEventListener('mousedown', handleClick);
 
-    return () => {
-      document.removeEventListener('mousedown', handleClick);
-    };
-  }, []);
+  //   return () => {
+  //     document.removeEventListener('mousedown', handleClick);
+  //   };
+  // }, []);
 
   const { formatMessage } = useIntl();
   const mainMenu = menuState === 'mainMenu' && status === 'ACTIVE';
@@ -146,7 +147,7 @@ const RecurringContributionsPopUp = ({ contribution, status, createNotification,
   const activateMenu = menuState === 'mainMenu' && status === 'CANCELLED';
 
   return (
-    <PopUpMenu minHeight={160} maxHeight={360} width={'100%'} overflowY={'auto'} ref={popupNode}>
+    <PopUpMenu minHeight={160} maxHeight={360} width={'100%'} overflowY={'auto'}>
       {mainMenu && (
         <MenuSection>
           <Flex flexGrow={1 / 4} width={1} alignItems="center" justifyContent="center">
@@ -346,27 +347,12 @@ const RecurringContributionsPopUp = ({ contribution, status, createNotification,
 
       {updateTierMenu && (
         <MenuSection>
-          <Flex flexGrow={1 / 4} width={1} alignItems="center" justifyContent="center">
-            <P my={2} fontSize="Caption" textTransform="uppercase" color="black.700">
-              {formatMessage(messages.updateTier)}
-            </P>
-            <Flex flexGrow={1} alignItems="center">
-              <StyledHr width="100%" ml={2} />
-            </Flex>
-          </Flex>
-          <Flex flexGrow={1 / 4} width={1} alignItems="center">
-            <StyledButton
-              buttonSize="small"
-              onClick={() => {
-                setMenuState('mainMenu');
-              }}
-            >
-              {formatMessage(messages.cancel)}
-            </StyledButton>
-            <StyledButton buttonSize="small" buttonStyle="secondary">
-              {formatMessage(messages.update)}
-            </StyledButton>
-          </Flex>
+          <UpdateOrderPopUp
+            setMenuState={setMenuState}
+            contribution={contribution}
+            createNotification={createNotification}
+            setShowPopup={setShowPopup}
+          />
         </MenuSection>
       )}
     </PopUpMenu>
