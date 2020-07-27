b9992080476c6375cd5889773e3e178c10dc1aae
flickz <omoyajowo2015@gmail.com>
2020-07-27 13:45:11 +0100

    feat(homepage): update homepage makeCoummunity section

--- components/home/SectionTitle.js
+++ components/home/SectionTitle.js
@@ -3,25 +3,31 @@ import { color, display, space, typography } from 'styled-system';
 
 const SectionTitle = styled.h2`
   word-break: break-word;
-  letter-spacing: -0.2px;
-  font-weight: 500;
+  letter-spacing: -0.8px;
+  font-weight: bold;
 
   ${color}
   ${display}
   ${space}
   ${typography}
 
-  @media screen and (min-width: 64em) {
-    font-size: ${props => props.theme.fontSizes.H2}px;
-    line-height: 48px;
+  @media screen and (min-width: 52em) {
+    font-size: 32px;
+    line-height: 40px;
     letter-spacing: -1.2px;
+  }
+  
+  @media screen and (min-width: 88em) {
+    font-size: 40px;
+    line-height: 48px;
+    letter-spacing: -1.6px;
   }
 `;
 
 SectionTitle.defaultProps = {
-  fontSize: 'H4',
-  lineHeight: 'H4',
-  color: 'black.800',
+  fontSize: '24px',
+  lineHeight: '32px',
+  color: 'black.900',
   mb: 3,
 };
 
--- components/home/sections/MakeCommunity.js
+++ components/home/sections/MakeCommunity.js
@@ -8,23 +8,29 @@ import { Box, Flex } from '../../Grid';
 import Link from '../../Link';
 import StyledButton from '../../StyledButton';
 import Modal from '../../StyledModal';
-import { H1, H4, P, Span } from '../../Text';
+import { H1, H2, P, Span } from '../../Text';
 import Illustration from '../HomeIllustration';
 
 const SustainTextWrapper = styled(Span)`
   background-image: url('/static/images/home/sustain-underline-sm.png');
   background-repeat: no-repeat;
   background-size: cover;
-  background-position-y: 7px;
+  background-position-y: 10px;
   background-position-x: -12px;
   padding-right: 12px;
 
   @media screen and (min-width: 64em) {
     background-image: url('/static/images/home/sustain-underline.png');
     background-repeat: no-repeat;
-    background-size: 100% 100%;
+    background-size: cover;
     background-position-y: 16px;
     background-position-x: -12px;
+  }
+`;
+
+const LineBreak = styled.br`
+  @media screen and (min-width: 64em) {
+    display: none;
   }
 `;
 
@@ -35,69 +41,66 @@ const MakeCommunity = () => {
     <Fragment>
       <Flex
         mx={[3, 4]}
-        my={[null, null, 5]}
-        flexDirection={['column-reverse', null, 'column']}
-        alignItems="center"
+        my={[null, 4, 5]}
+        flexDirection={['column-reverse', 'row']}
+        alignItems={['center', 'flex-start', null, null, 'center']}
         justifyContent={[null, 'center']}
       >
-        <Flex flexDirection="column" textAlign="center" alignItems={['center']} width={[null, null, null, null, null]}>
-          <H1
-            my={[3, null, 3]}
-            letterSpacing={['-0.2px', null, '-0.8px']}
-            fontSize={['H4', null, 'H1']}
-            lineHeight={['H4', null, 'H1']}
-            textAlign="center"
-          >
-            <Span>
-              <FormattedMessage
-                id="home.makeCommunitySection.title.makeYourCommunity"
-                defaultMessage="Make your community {sustainable}"
-                values={{
-                  sustainable: (
-                    <SustainTextWrapper>
-                      <FormattedMessage id="home.makeCommunitySection.title.sustain" defaultMessage="sustainable" />
-                    </SustainTextWrapper>
-                  ),
-                }}
-              />
-            </Span>
-          </H1>
-          <H4
-            mb={[2, null, 4]}
-            letterSpacing={['-0.016em', null, '-0.8px']}
-            fontSize={['15px', null, 'H4']}
-            lineHeight={['25px', null, 'H4']}
-            color="black.700"
-            fontWeight="300"
-          >
-            <FormattedMessage
-              id="home.makeCommunitySection.subTitle"
-              defaultMessage="Collect and spend money transparently."
-            />
-          </H4>
-          <Box width={['288px', 1, '576px']}>
+        <Flex flexDirection="column" alignItems={['center', 'flex-start']} mr={[null, null, null, null, 3]}>
+          <Box width={[null, null, '458px', null, '558px']}>
+            <H1
+              my={[3, null, 3]}
+              letterSpacing={['-1.2px', '-1.6px', null, null, '-2px']}
+              fontSize={['32px', '40px', null, null, '52px']}
+              lineHeight={['40px', '48px', null, null, '56px']}
+              textAlign={['center', 'left']}
+            >
+              <Span>
+                <FormattedMessage
+                  id="home.makeCommunitySection.title.makeYourCommunity"
+                  defaultMessage="Make your community {sustainable}"
+                  values={{
+                    sustainable: (
+                      <SustainTextWrapper>
+                        <FormattedMessage id="home.makeCommunitySection.title.sustain" defaultMessage="sustainable" />
+                      </SustainTextWrapper>
+                    ),
+                  }}
+                />
+              </Span>
+            </H1>
+          </Box>
+          <Box width={['288px', '306px', '342px', null, '558px']}>
             <P
-              fontSize={['Caption', null, 'Paragraph']}
+              fontSize={['15px', '16px', null, null, '18px']}
               color="black.800"
               mt={1}
               mb={4}
-              letterSpacing="-0.016em"
-              lineHeight={['19px', null, 'LeadParagraph']}
-              textAlign="center"
+              letterSpacing={['-0.12px', '-0.16px', null, null, '-0.2px']}
+              lineHeight={['23px', '24px', null, null, '27px']}
+              textAlign={['center', 'left']}
+              fontWeight="500"
             >
               <FormattedMessage
                 id="home.makeCommunitySection.description"
                 defaultMessage="Community is about trust and sharing. Open Collective lets you manage your finances so everyone can see where money comes from and where it goes."
               />
+              <LineBreak />{' '}
+              <Span fontWeight="bold" display={['none', 'block']}>
+                <FormattedMessage
+                  id="home.makeCommunitySection.subTitle"
+                  defaultMessage="Collect and spend money transparently."
+                />
+              </Span>
             </P>
           </Box>
-          <Box display="flex" flexDirection={['column', null, 'row']} alignItems={['center', null, null]}>
+          <Box display="flex" flexDirection={['column', 'row']} alignItems={['center', null, null]}>
             <Link route="/create">
-              <StyledButton minWidth={175} my={[2, null, 0]} mr={[0, null, 3]} buttonStyle="dark">
+              <StyledButton minWidth={158} my={[2, null, 0]} mr={[0, 3]} buttonStyle="dark" whiteSpace="nowrap">
                 <FormattedMessage id="home.create" defaultMessage="Create a Collective" />
               </StyledButton>
             </Link>
-            <StyledButton onClick={() => setShowModal(true)} my={[2, null, 0]} minWidth={155}>
+            <StyledButton onClick={() => setShowModal(true)} my={[2, null, 0]} minWidth={158}>
               <Span mr={2}>
                 <FormattedMessage id="home.makeCommunitySection.watchVideo" defaultMessage="Watch Video" />
               </Span>
@@ -107,7 +110,13 @@ const MakeCommunity = () => {
             </StyledButton>
           </Box>
         </Flex>
-        <Flex mt={5} width={[1, null, '672px']} justifyContent="center" alignItems="center">
+        <Flex
+          mt={[5, 3]}
+          width={[1, null, '458px', null, '558px']}
+          justifyContent="center"
+          alignItems="center"
+          ml={[null, null, null, null, 3]}
+        >
           <Illustration
             alt="Make your community sustainable"
             src="/static/images/home/makecommunity-section-illustration.png"
@@ -120,6 +129,43 @@ const MakeCommunity = () => {
           />
         </Flex>
       </Flex>
+      {/* What is great about Open Collective? */}
+      <Container
+        display={['none', 'flex']}
+        justifyContent="center"
+        alignItems={[null, null, 'center']}
+        mx={[null, 4]}
+        my={[null, 5]}
+      >
+        <Flex width={[null, '414px', '548px', null, '696px']} mr={[null, 3, null, null, 4]}>
+          <Illustration
+            alt="Make your community sustainable"
+            src="/static/images/home/whatisgreataboutOC-Illustration.png"
+          />
+        </Flex>
+        <Box width={[null, '224px', '396px']}>
+          <H2
+            mb={[null, 2]}
+            letterSpacing={[null, '-0.8px']}
+            fontSize={[null, '24px']}
+            lineHeight={[null, '32px']}
+            color="black.800"
+          >
+            <FormattedMessage id="home.whatIsGreatAboutOC" defaultMessage="What's great about Open Collective?" />
+          </H2>
+          <P
+            letterSpacing={[null, '-0.16px', '-0.2px']}
+            fontSize={[null, '16px', '18px']}
+            lineHeight={[null, '24px', '27px']}
+            color="black.800"
+          >
+            <FormattedMessage
+              id="home.whatIsGreatAboutOC.description"
+              defaultMessage="Money management made simple, plus great tools for community engagement, budget reporting, and fiscal sponsorship."
+            />
+          </P>
+        </Box>
+      </Container>
       <Modal
         padding="0"
         background="transparent"
