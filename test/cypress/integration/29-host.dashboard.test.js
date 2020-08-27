ba97828260c880fab183c89a960f5847beeead1e
Sudharaka Palamakumbura <sudharakap@mun.ca>
2020-08-26 23:30:48 -0700

    refactor: migrate some more pages to styled components

--- test/cypress/integration/29-host.dashboard.test.js
+++ test/cypress/integration/29-host.dashboard.test.js
@@ -27,7 +27,7 @@ describe('host dashboard', () => {
   it('mark pending order as paid', () => {
     cy.login({ redirect: '/brusselstogetherasbl/dashboard/donations' });
     cy.get('.Orders .item:first .status').contains('pending');
-    cy.get('.MarkOrderAsPaidBtn button').first().click();
+    cy.get('[data-cy="MarkOrderAsPaidBtn"] button').first().click();
     cy.get('.Orders .item:first .status').contains('paid');
     cy.wait(1000);
   });
