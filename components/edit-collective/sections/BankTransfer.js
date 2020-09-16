d7e3af5f184b6b1185c90143a154cce00cf3cba9
Leo Kewitz <leokewitz@gmail.com>
2020-09-16 14:26:11 -0300

    refact: allow unstructured Bank Details

--- components/edit-collective/sections/BankTransfer.js
+++ components/edit-collective/sections/BankTransfer.js
@@ -168,7 +168,7 @@ const BankTransfer = props => {
           initialValues={initialValues}
           onSubmit={async (values, { setSubmitting }) => {
             const { data, instructions } = values;
-            if (data) {
+            if (data?.currency) {
               await createPayoutMethod({
                 variables: {
                   payoutMethod: { data: { ...data, isManualBankTransfer: true }, type: 'BANK_ACCOUNT' },
@@ -235,6 +235,7 @@ const BankTransfer = props => {
                       fixedCurrency={fixedCurrency}
                       ignoreBlockedCurrencies={false}
                       isNew
+                      optional
                     />
                   </Flex>
                 </React.Fragment>
