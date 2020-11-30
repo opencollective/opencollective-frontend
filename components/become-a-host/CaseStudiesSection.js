5aba479457d78f121727d37fc92cf958cca6b013
flickz <omoyajowo2015@gmail.com>
2020-11-30 09:57:01 +0100

    feat(becomeAhost): update the use case section

--- components/become-a-host/CaseStudiesSection.js
+++ components/become-a-host/CaseStudiesSection.js
@@ -2,35 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 
+import Avatar from '../Avatar';
 import Container from '../Container';
 import { Box, Flex } from '../Grid';
-// import Link from '../Link';
 import StyledCarousel from '../StyledCarousel';
+import StyledLink from '../StyledLink';
 import { H2, H3, P } from '../Text';
 
 const messages = defineMessages({
-  'becomeAHost.caseStudies.womenWhoCode': {
-    id: 'becomeAHost.caseStudies.womenWhoCode',
+  'becomeAHost.caseStudies.wwcode': {
+    id: 'becomeAHost.caseStudies.wwcode',
     defaultMessage:
       'Whenever we start a new meetup chapter, we pair it with an Open Collective so they have the means to raise money under our umbrella.',
   },
-  'becomeAHost.caseStudies.socialChangeNest': {
-    id: 'becomeAHost.caseStudies.socialChangeNest',
+  'becomeAHost.caseStudies.socialchangenestcollective': {
+    id: 'becomeAHost.caseStudies.socialchangenestcollective',
     defaultMessage:
       'We have been able to support over 150 Mutual Aid groups to get going quickly by providing them with fiscal hosting back office support.',
   },
-  'becomeAHost.caseStudies.OCF': {
-    id: 'becomeAHost.caseStudies.OCF',
+  'becomeAHost.caseStudies.foundation': {
+    id: 'becomeAHost.caseStudies.foundation',
     defaultMessage:
       'The Open Collective Platform provides the tools to more effectively manage group finances. They can offer their donors tax-deductible status without needing to incorporate a legal entity.',
   },
-  'becomeAHost.caseStudies.OSC': {
-    id: 'becomeAHost.caseStudies.OSC',
+  'becomeAHost.caseStudies.opensource': {
+    id: 'becomeAHost.caseStudies.opensource',
     defaultMessage:
       'We provide financial and legal infrastructure for thousands of open source projects. We’re an API between the world of distributed collaboration and the world of accounting and invoices.',
   },
-  'becomeAHost.caseStudies.climate': {
-    id: 'becomeAHost.caseStudies.climate',
+  'becomeAHost.caseStudies.allforclimate-collective': {
+    id: 'becomeAHost.caseStudies.allforclimate-collective',
     defaultMessage:
       'A fiscal sponsor for your local climate justice group, we act as a shared administrative back office so that local groups can focus on their actions.',
   },
@@ -38,43 +39,48 @@ const messages = defineMessages({
 
 const caseStudies = [
   {
-    id: 'womenWhoCode',
+    id: 'wwcode',
     name: 'Women Who Code',
-    collectivePath: '/wwcodeatl',
-    learnMorePath: '/wwcodeinc',
-    bgImage: '/static/images/home/oc-users-womenwhocode.png',
-  },
-  {
-    id: 'socialChangeNest',
-    name: 'Social Change Nest',
-    collectivePath: '#',
+    collectivePath: '/wwcode',
     learnMorePath: '#',
-    bgImage: '/static/images/home/oc-users-womenwhocode.png',
+    bgImage: '/static/images/become-a-host/wwc.png',
+    logo: '/static/images/become-a-host/wwc-logo.png',
   },
   {
-    id: 'OCF',
+    id: 'socialchangenestcollective',
+    name: 'Social Change Nest',
+    collectivePath: '/socialchangenestcollective',
+    learnMorePath: '#',
+    bgImage: '/static/images/become-a-host/socialchangenest.png',
+    logo: '/static/images/become-a-host/socialchangenest-logo.png',
+  },
+  {
+    id: 'foundation',
     name: 'Open Collective Foundation',
     collectivePath: '/foundation',
     learnMorePath: '#',
-    bgImage: '/static/images/home/oc-users-extinctionrebllion.png',
+    bgImage: '/static/images/become-a-host/ocf.png',
+    logo: '/static/images/become-a-host/ocf-logo.png',
   },
   {
-    id: 'OSC',
+    id: 'opensource',
     name: 'Open Source Collective',
     collectivePath: '/opensource',
     learnMorePath: '#',
-    bgImage: '/static/images/home/oc-users-extinctionrebllion.png',
+    bgImage: '/static/images/become-a-host/osc.png',
+    logo: '/static/images/become-a-host/osc-logo.png',
   },
   {
-    id: 'climate',
+    id: 'allforclimate-collective',
     name: 'All for Climate',
-    collectivePath: '#',
+    collectivePath: '/allforclimate-collective',
     learnMorePath: '#',
-    bgImage: '/static/images/home/oc-users-extinctionrebllion.png',
+    bgImage: '/static/images/become-a-host/allforclimate-collective.png',
+    logo: '/static/images/become-a-host/climate-logo.png',
   },
 ];
 
-const CaseStudy = ({ bgImage, name, id }) => {
+const CaseStudy = ({ bgImage, name, id, logo, learnMorePath }) => {
   const intl = useIntl();
 
   return (
@@ -83,19 +89,24 @@ const CaseStudy = ({ bgImage, name, id }) => {
       flexDirection="column"
       alignItems="center"
       mr={[null, 3, null, null, '36px']}
-      my={[null, 3]}
+      my={[null, 3, null, null, 4]}
     >
-      <Box
+      <Container
         width={[1, '205px', '306px', null, '360px']}
         height={['210px', '218px', null, null, '256px']}
         background={`url("${bgImage}") no-repeat`}
         backgroundSize="contain"
-      ></Box>
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Avatar radius="96px" src={logo} name={name} type="ORGANIZATION" />
+      </Container>
       <Container
         display="flex"
         flexDirection="column"
         alignItems="flex-start"
-        mt={['16px', 0]}
+        mt={['12px', 0, '24px']}
         width={[1, '206px', '306px', null, '360px']}
       >
         <H3
@@ -116,6 +127,11 @@ const CaseStudy = ({ bgImage, name, id }) => {
           &ldquo;{intl.formatMessage(messages[`becomeAHost.caseStudies.${id}`])}&ldquo;
         </P>
       </Container>
+      <Box mt={4} alignSelf={[null, null, null, null, 'end']}>
+        <StyledLink buttonStyle="standard" buttonSize="medium" href={learnMorePath} fontWeight="500">
+          <FormattedMessage id="LearnMore" defaultMessage="Learn more" />
+        </StyledLink>
+      </Box>
     </Container>
   );
 };
