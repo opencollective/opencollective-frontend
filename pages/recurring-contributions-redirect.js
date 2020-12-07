ceea1aae179371fd987cac1fd129c80a7928d766
Benjamin Piouffle <benjamin@opencollective.com>
2020-12-07 10:36:17 +0100

    debt: Remove some legacy components & styled-jsx

--- pages/recurring-contributions-redirect.js
+++ pages/recurring-contributions-redirect.js
@@ -2,12 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Router from 'next/router';
 
-import colors from '../lib/constants/colors';
-
-import Body from '../components/Body';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import { Box } from '../components/Grid';
 import Link from '../components/Link';
+import Page from '../components/Page';
 import { withUser } from '../components/UserProvider';
 
 class RecurringContributionsRedirectPage extends React.Component {
@@ -39,87 +36,23 @@ class RecurringContributionsRedirectPage extends React.Component {
   render() {
     const { LoggedInUser } = this.props;
     return (
-      <div className="SubscriptionsPage">
-        <Header
-          title={'Subscriptions'}
-          description="All the collectives that you are giving money to"
-          LoggedInUser={LoggedInUser}
-        />
-        <style jsx>
-          {`
-            .Subscriptions-container {
-              background-color: ${colors.offwhite};
-              overflow: hidden;
-              min-height: 500px;
-            }
-            .content {
-              align-items: left;
-              color: black;
-              margin: auto;
-              margin-top: 100px;
-              margin-left: 32px;
-              max-width: 1024px;
-            }
-            .small .content {
-              margin-top: 0px;
-            }
-            .Subscriptions-header {
-              text-align: left;
-              overflow: hidden;
-              max-width: 1024px;
-            }
-            .Subscriptions-title {
-              margin: auto;
-              font-size: 40px;
-              font-weight: 700;
-              line-height: 1.08;
-              text-align: left;
-              color: ${colors.black};
-              border-left: 4px solid ${colors.ocblue};
-              padding-left: 32px;
-            }
-            .link {
-              cursor: pointer;
-              color: ${colors.blue};
-            }
-            .redirect-message {
-              padding-top: 25px;
-              font-size: 24px;
-              font-weight: 500;
-              padding: 37px;
-            }
-          `}
-        </style>
-        <Body>
-          <div className="SubscriptionsRedirect-container">
-            <div className="content">
-              <div className="Subscriptions-header">
-                <div className="Subscriptions-title">
-                  {LoggedInUser && (
-                    <div>
-                      This page has moved. Your recurring financial contributions are now at
-                      <Link
-                        route={'recurring-contributions'}
-                        params={{
-                          slug: LoggedInUser.collective.slug,
-                        }}
-                      >
-                        <span className="link">
-                          {' '}
-                          /{LoggedInUser.collective.slug}
-                          /recurring-contributions
-                        </span>
-                      </Link>
-                      . Redirecting...
-                    </div>
-                  )}
-                </div>
-              </div>
+      <Page title="Subscriptions" description="All the collectives that you are giving money to">
+        <Box py={[5, 6, 7]} textAlign="center">
+          <strong>Redirecting...</strong>
+          {LoggedInUser && (
+            <div>
+              This page has moved. Your recurring financial contributions are now at
+              <Link route="recurring-contributions" params={{ slug: LoggedInUser.collective.slug }}>
+                <span>
+                  {' '}
+                  /{LoggedInUser.collective.slug}
+                  /recurring-contributions
+                </span>
+              </Link>
             </div>
-          </div>
-        </Body>
-        <Footer />
-      </div>
+          )}
+        </Box>
+      </Page>
     );
   }
 }
