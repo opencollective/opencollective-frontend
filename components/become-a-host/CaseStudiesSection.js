edea46f5d611df7244befd9563d8f98c036644bd
flickz <omoyajowo2015@gmail.com>
2020-12-08 11:08:19 +0100

    feat(becomeAhost): fix lint

--- components/become-a-host/CaseStudiesSection.js
+++ components/become-a-host/CaseStudiesSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+import styled from 'styled-components';
 
 import Avatar from '../Avatar';
 import Container from '../Container';
