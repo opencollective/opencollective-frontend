84d29125cdff78623266d02eae2df948734a0a9d
flickz <omoyajowo2015@gmail.com>
2020-12-09 09:24:09 +0100

    feat(becomeAhost): remove exisiting become a host page
    feat(becomeAhost): remove become a host from marketingPage static build

--- server/pages.js
+++ server/pages.js
@@ -9,7 +9,7 @@ const routes = require('next-routes');
 const pages = routes()
   .add('home', '/', 'index')
   .add('become-a-sponsor', '/become-a-sponsor', 'become-a-sponsor')
-  .add('become-a-host', '/become-a-host', 'become-a-host')
+  .add('become-a-host', '/(become-a-sponsor|become-a-fiscal-host)', 'become-a-host')
   .add('static', '/:pageSlug(widgets|tos|privacypolicy|support|hiring)', 'staticPage')
   .add('pricing', '/pricing')
   .add('pricing-old', '/pricing-old')
@@ -150,11 +150,7 @@ pages.add('applications', '/applications');
 // Marketing Pages
 // ---------------
 
-pages.add(
-  'marketing',
-  '/:pageSlug(how-it-works|gift-of-giving|gift-cards|pricing|old-pricing|become-a-fiscal-host)',
-  'marketingPage',
-);
+pages.add('marketing', '/:pageSlug(how-it-works|gift-of-giving|gift-cards|pricing|old-pricing)', 'marketingPage');
 
 // Collective
 // ----------
