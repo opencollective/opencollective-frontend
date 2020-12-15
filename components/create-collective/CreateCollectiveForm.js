3da79ce12f79d9ceec5345da6f77480347399b66
Benjamin Piouffle <benjamin@opencollective.com>
2020-12-15 16:43:49 +0100

    style(CreateCollectiveForm): Responsiveness + alignment fixes & design

--- components/create-collective/CreateCollectiveForm.js
+++ components/create-collective/CreateCollectiveForm.js
@@ -9,9 +9,8 @@ import styled from 'styled-components';
 
 import { suggestSlug } from '../../lib/collective.lib';
 
-import CollectiveNavbar from '../collective-navbar';
 import Container from '../Container';
-import { Box, Flex } from '../Grid';
+import { Box, Flex, Grid } from '../Grid';
 import Illustration from '../home/HomeIllustration';
 import MessageBox from '../MessageBox';
 import StyledButton from '../StyledButton';
@@ -28,11 +27,12 @@ const BackButton = styled(StyledButton)`
 `;
 
 const ContainerWithImage = styled(Container)`
+  width: 100%;
   @media screen and (min-width: 40em) {
     background: url('/static/images/create-collective/formIllustration.png');
     background-repeat: no-repeat;
     background-size: 40%;
-    background-position: right bottom;
+    background-position: right 17px bottom;
   }
 `;
 
@@ -79,6 +79,8 @@ const formatGithubRepoName = repoName => {
   // replaces dash and underscore with space, then capitalises the words
   return repoName.replace(/[-_]/g, ' ').replace(/(?:^|\s)\S/g, words => words.toUpperCase());
 };
+
+const LABEL_STYLES = { fontWeight: 500, fontSize: '14px', lineHeight: '17px' };
 
 class CreateCollectiveForm extends React.Component {
   static propTypes = {
@@ -146,160 +148,146 @@ class CreateCollectiveForm extends React.Component {
     };
 
     return (
-      <Flex flexDirection="column" m={[3, 0]}>
-        {host && host.slug !== 'opensource' && (
-          <Container mb={4}>
-            <CollectiveNavbar collective={host} onlyInfos={true} />
-          </Container>
-        )}
-        <Flex flexDirection="column" my={[2, 4]}>
-          <Box textAlign="left" minHeight="32px" marginLeft={['none', '224px']}>
-            <BackButton asLink onClick={() => window && window.history.back()}>
-              ←&nbsp;
-              <FormattedMessage id="Back" defaultMessage="Back" />
-            </BackButton>
-          </Box>
-          <Box mb={[2, 3]}>
-            <H1
-              fontSize={['20px', '32px']}
-              lineHeight={['24px', '36px']}
-              fontWeight="bold"
-              textAlign="center"
-              color="black.900"
-            >
-              <FormattedMessage id="home.create" defaultMessage="Create a Collective" />
-            </H1>
-          </Box>
-          <Box textAlign="center" minHeight="24px">
-            <P fontSize="16px" color="black.600" mb={2}>
+      <Grid gridTemplateColumns={['1fr', null, null, '1fr 1fr 1fr']} pt={42}>
+        <Container
+          display={['none', null, null, 'flex']}
+          minHeight="32px"
+          justifyContent="center"
+          alignItems="flex-start"
+        >
+          <BackButton asLink onClick={() => window && window.history.back()}>
+            ←&nbsp;
+            <FormattedMessage id="Back" defaultMessage="Back" />
+          </BackButton>
+        </Container>
+        <Box>
+          <Flex flexDirection="column" mb={[2, 4]} px={2}>
+            <Box mb={[2, 3]}>
+              <H1
+                fontSize={['20px', '32px']}
+                lineHeight={['24px', '36px']}
+                fontWeight="bold"
+                textAlign="center"
+                color="black.900"
+                mt={3}
+              >
+                <FormattedMessage id="home.create" defaultMessage="Create a Collective" />
+              </H1>
+            </Box>
+            <P fontSize="16px" color="black.600" textAlign="center" mb={2}>
               <FormattedMessage
                 id="createCollective.subtitle.introduce"
                 defaultMessage="Introduce your Collective to the community."
               />
             </P>
-          </Box>
-        </Flex>
-        {error && (
-          <Flex alignItems="center" justifyContent="center">
-            <MessageBox type="error" withIcon mb={[1, 3]} data-cy="ccf-error-message">
-              {error}
-            </MessageBox>
           </Flex>
-        )}
-        <Flex alignItems="center" justifyContent="center">
-          <ContainerWithImage
-            mb={[1, 5]}
-            width={[320, 512, 576]}
-            border={['none', '1px solid #E6E8EB']}
-            borderRadius={['none', '8px']}
-            px={[1, 4]}
-          >
-            <Formik validate={validate} initialValues={initialValues} onSubmit={submit} validateOnChange={true}>
-              {formik => {
-                const { values, handleSubmit, errors, touched, setFieldValue } = formik;
+          {error && (
+            <Flex alignItems="center" justifyContent="center">
+              <MessageBox type="error" withIcon mb={[1, 3]} data-cy="ccf-error-message">
+                {error}
+              </MessageBox>
+            </Flex>
+          )}
+          <Flex alignItems="center" justifyContent="center">
+            {' '}
+            <ContainerWithImage
+              mb={[1, 5]}
+              maxWidth={576}
+              border={[null, '1px solid #E6E8EB']}
+              borderRadius={[0, '8px']}
+              px={[2, 4]}
+              pb={[0, 50]}
+            >
+              <Formik validate={validate} initialValues={initialValues} onSubmit={submit} validateOnChange={true}>
+                {formik => {
+                  const { values, handleSubmit, errors, touched, setFieldValue } = formik;
 
-                const handleSlugChange = e => {
-                  if (!touched.slug) {
-                    setFieldValue('slug', suggestSlug(e.target.value));
-                  }
-                };
+                  const handleSlugChange = e => {
+                    if (!touched.slug) {
+                      setFieldValue('slug', suggestSlug(e.target.value));
+                    }
+                  };
 
-                return (
-                  <Form>
-                    <StyledInputField
-                      name="name"
-                      htmlFor="name"
-                      error={touched.name && errors.name}
-                      label={intl.formatMessage(messages.nameLabel)}
-                      value={values.name}
-                      onChange={handleSlugChange}
-                      required
-                      mt={4}
-                      mb={3}
-                      data-cy="ccf-form-name"
-                    >
-                      {inputProps => <Field as={StyledInput} {...inputProps} placeholder={placeholders.name} />}
-                    </StyledInputField>
-                    <StyledInputField
-                      name="slug"
-                      htmlFor="slug"
-                      error={touched.slug && errors.slug}
-                      label={intl.formatMessage(messages.slugLabel)}
-                      value={values.slug}
-                      required
-                      mt={3}
-                      mb={2}
-                      data-cy="ccf-form-slug"
-                    >
-                      {inputProps => (
-                        <Field
-                          onChange={e => {
-                            setFieldValue('slug', e.target.value);
-                          }}
-                          as={StyledInputGroup}
-                          {...inputProps}
-                          prepend="opencollective.com/"
-                          placeholder={placeholders.slug}
-                        />
-                      )}
-                    </StyledInputField>
-                    {values.name.length > 0 && !touched.slug && (
-                      <P fontSize="10px">{intl.formatMessage(messages.suggestedLabel)}</P>
-                    )}
-                    <StyledInputField
-                      name="description"
-                      htmlFor="description"
-                      error={touched.description && errors.description}
-                      label={intl.formatMessage(messages.descriptionLabel)}
-                      value={values.description}
-                      required
-                      mt={3}
-                      mb={2}
-                      data-cy="ccf-form-description"
-                    >
-                      {inputProps => (
-                        <Field
-                          as={StyledInput}
-                          {...inputProps}
-                          placeholder={intl.formatMessage(messages.descriptionPlaceholder)}
-                        />
-                      )}
-                    </StyledInputField>
-                    <P fontSize="11px">{intl.formatMessage(messages.descriptionHint)}</P>
-
-                    <Flex flexDirection="column" mx={1} my={4}>
-                      <StyledCheckbox
-                        name="tos"
-                        label={
-                          <FormattedMessage
-                            id="createcollective.tos.label"
-                            defaultMessage="I agree with the {toslink} of Open Collective."
-                            values={{
-                              toslink: (
-                                <StyledLink href="/tos" openInNewTab>
-                                  <FormattedMessage id="tos" defaultMessage="terms of service" />
-                                </StyledLink>
-                              ),
-                            }}
-                          />
-                        }
+                  return (
+                    <Form>
+                      <StyledInputField
+                        name="name"
+                        htmlFor="name"
+                        error={touched.name && errors.name}
+                        labelProps={LABEL_STYLES}
+                        label={intl.formatMessage(messages.nameLabel)}
+                        value={values.name}
+                        onChange={handleSlugChange}
                         required
-                        onChange={({ checked }) => {
-                          this.setState({ tos: checked });
-                        }}
-                      />
-                      {!router.query.hostTos && host && host.termsUrl && (
+                        mt={4}
+                        mb={3}
+                        data-cy="ccf-form-name"
+                      >
+                        {inputProps => <Field as={StyledInput} {...inputProps} placeholder={placeholders.name} />}
+                      </StyledInputField>
+                      <StyledInputField
+                        name="slug"
+                        htmlFor="slug"
+                        error={touched.slug && errors.slug}
+                        labelProps={LABEL_STYLES}
+                        label={intl.formatMessage(messages.slugLabel)}
+                        value={values.slug}
+                        required
+                        mt={3}
+                        mb={2}
+                        data-cy="ccf-form-slug"
+                      >
+                        {inputProps => (
+                          <Field
+                            onChange={e => {
+                              setFieldValue('slug', e.target.value);
+                            }}
+                            as={StyledInputGroup}
+                            {...inputProps}
+                            prepend="opencollective.com/"
+                            placeholder={placeholders.slug}
+                          />
+                        )}
+                      </StyledInputField>
+                      {values.name.length > 0 && !touched.slug && (
+                        <P fontSize="10px">{intl.formatMessage(messages.suggestedLabel)}</P>
+                      )}
+                      <StyledInputField
+                        name="description"
+                        htmlFor="description"
+                        error={touched.description && errors.description}
+                        labelProps={LABEL_STYLES}
+                        label={intl.formatMessage(messages.descriptionLabel)}
+                        value={values.description}
+                        required
+                        mt={3}
+                        mb={2}
+                        data-cy="ccf-form-description"
+                      >
+                        {inputProps => (
+                          <Field
+                            as={StyledInput}
+                            {...inputProps}
+                            placeholder={intl.formatMessage(messages.descriptionPlaceholder)}
+                          />
+                        )}
+                      </StyledInputField>
+                      <P fontSize="11px" color="black.600">
+                        {intl.formatMessage(messages.descriptionHint)}
+                      </P>
+
+                      <Flex flexDirection="column" mx={1} my={4}>
                         <StyledCheckbox
-                          alignItems="flex-start"
-                          name="hostTos"
+                          name="tos"
+                          labelProps={LABEL_STYLES}
                           label={
                             <FormattedMessage
-                              id="createcollective.hosttos.label"
-                              defaultMessage="I agree with the the {hosttoslink} of the host that will collect money on behalf of our collective."
+                              id="createcollective.tos.label"
+                              defaultMessage="I agree with the {toslink} of Open Collective."
                               values={{
-                                hosttoslink: (
-                                  <StyledLink href={host.termsUrl} openInNewTab>
-                                    <FormattedMessage id="fiscaltos" defaultMessage="terms of fiscal sponsorship" />
+                                toslink: (
+                                  <StyledLink href="/tos" openInNewTab>
+                                    <FormattedMessage id="tos" defaultMessage="terms of service" />
                                   </StyledLink>
                                 ),
                               }}
@@ -307,41 +295,61 @@ class CreateCollectiveForm extends React.Component {
                           }
                           required
                           onChange={({ checked }) => {
-                            this.setState({ hostTos: checked });
+                            this.setState({ tos: checked });
                           }}
                         />
-                      )}
-                    </Flex>
+                        {!router.query.hostTos && host && host.termsUrl && (
+                          <StyledCheckbox
+                            alignItems="flex-start"
+                            name="hostTos"
+                            labelProps={LABEL_STYLES}
+                            label={
+                              <FormattedMessage
+                                id="createcollective.hosttos.label"
+                                defaultMessage="I agree with the the {hosttoslink} of the host that will collect money on behalf of our collective."
+                                values={{
+                                  hosttoslink: (
+                                    <StyledLink href={host.termsUrl} openInNewTab>
+                                      <FormattedMessage id="fiscaltos" defaultMessage="terms of fiscal sponsorship" />
+                                    </StyledLink>
+                                  ),
+                                }}
+                              />
+                            }
+                            required
+                            onChange={({ checked }) => {
+                              this.setState({ hostTos: checked });
+                            }}
+                          />
+                        )}
+                      </Flex>
 
-                    <Flex justifyContent={['center', 'left']} mb={4}>
-                      <StyledButton
-                        fontSize="13px"
-                        minWidth="148px"
-                        minHeight="36px"
-                        buttonStyle="primary"
-                        type="submit"
-                        loading={loading}
-                        onSubmit={handleSubmit}
-                        data-cy="ccf-form-submit"
-                      >
-                        <FormattedMessage id="collective.create.button" defaultMessage="Create Collective" />
-                      </StyledButton>
-                    </Flex>
-                  </Form>
-                );
-              }}
-            </Formik>
-            <Flex justifyContent="center" mb={4} display={['flex', 'none']}>
-              <Illustration
-                display={['block', 'none']}
-                src="/static/images/create-collective/mobileForm.png"
-                width="320px"
-                height="200px"
-              />
-            </Flex>
-          </ContainerWithImage>
-        </Flex>
-      </Flex>
+                      <Flex justifyContent={['center', 'left']} mb={4}>
+                        <StyledButton
+                          fontSize="13px"
+                          minWidth="148px"
+                          minHeight="36px"
+                          buttonStyle="primary"
+                          type="submit"
+                          loading={loading}
+                          onSubmit={handleSubmit}
+                          data-cy="ccf-form-submit"
+                        >
+                          <FormattedMessage id="collective.create.button" defaultMessage="Create Collective" />
+                        </StyledButton>
+                      </Flex>
+                    </Form>
+                  );
+                }}
+              </Formik>
+              <Container justifyContent="center" mb={4} display={['flex', 'none']}>
+                <Illustration src="/static/images/create-collective/mobileForm.png" width="320px" height="200px" />
+              </Container>
+            </ContainerWithImage>
+          </Flex>
+        </Box>
+        <div />
+      </Grid>
     );
   }
 }
