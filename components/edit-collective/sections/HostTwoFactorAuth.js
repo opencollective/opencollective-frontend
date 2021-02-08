525ebf228012d1a7de15cd6639a568e36fd9d35e
Kate Beard <asouthernbellein@gmail.com>
2021-02-08 12:03:59 +0000

    enhancement(Host 2FA): Show notification to confirm limit updated or error

--- components/edit-collective/sections/HostTwoFactorAuth.js
+++ components/edit-collective/sections/HostTwoFactorAuth.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useMutation } from '@apollo/client';
 import { useFormik } from 'formik';
@@ -16,6 +16,7 @@ import StyledButton from '../../StyledButton';
 import StyledCheckbox from '../../StyledCheckbox';
 import StyledInputAmount from '../../StyledInputAmount';
 import StyledInputField from '../../StyledInputField';
+import TemporaryNotification from '../../TemporaryNotification';
 import { P } from '../../Text';
 import { editCollectiveSettingsMutation } from '../mutations';
 import SettingsTitle from '../SettingsTitle';
@@ -54,6 +55,7 @@ const ScreenshotPreview = styled.div`
 
 const HostTwoFactorAuth = ({ collective }) => {
   const { formatMessage } = useIntl();
+  const [notification, setNotification] = useState(false);
   const [setSettings, { loading, error }] = useMutation(editCollectiveSettingsMutation);
   const doesHostAlreadyHaveTwoFactorAuthEnabled = get(collective, 'settings.payoutsTwoFactorAuth.enabled', false);
   const hostRollingLimitAmount = get(collective, 'settings.payoutsTwoFactorAuth.rollingLimit', 1000000);
@@ -69,100 +71,120 @@ const HostTwoFactorAuth = ({ collective }) => {
       const updatedCollective = cloneDeep(collective);
       set(updatedCollective, 'settings.payoutsTwoFactorAuth.rollingLimit', rollingLimit);
       await setSettings({ variables: pick(updatedCollective, ['id', 'settings']) });
+      setNotification(true);
     },
   });
 
   return (
-    <Flex flexDirection="column">
-      <Container>
-        <SettingsTitle>
-          <FormattedMessage id="TwoFactorAuth.Setup.Title" defaultMessage="Set up two-factor authentication" />
-        </SettingsTitle>
-        <Flex mb={2} flexWrap="wrap" justifyContent="center">
-          <Container mr={3} pr={3} flex="1 1" minWidth={300} maxWidth={700} borderRight={[null, '1px solid #dcdee0']}>
-            <P wordBreak="break-word">
-              <FormattedMessage
-                id="TwoFactorAuth.Setup.FiscalHost.Info"
-                defaultMessage="Two-factor authentication (2FA) adds an extra layer of security for your account when logging in. For fiscal hosts, you can turn on 2FA to be used with high-risk activities like paying expenses. Enabling 2FA for the fiscal host means that every host admin (including yourself) will be required to enable 2FA for logging into their account."
-              />
-            </P>
-          </Container>
-          <Flex flexDirection="column" alignItems="center" justifyContent="center" minWidth={300}>
-            {doesHostAlreadyHaveTwoFactorAuthEnabled ? (
-              <Container>
-                <form onSubmit={formik.handleSubmit}>
-                  <Flex>
-                    <StyledInputField
-                      name="rollingLimit"
-                      htmlFor="rollingLimit"
-                      disabled={loading}
-                      label={formatMessage(messages['rollingLimit.label'])}
-                      labelProps={{ mb: 2, pt: 2, lineHeight: '18px', fontWeight: 'bold' }}
-                    >
-                      {inputProps => (
-                        <StyledInputAmount
-                          {...inputProps}
-                          currency={collective.currency}
-                          type="number"
-                          fontSize="14px"
-                          value={formik.values.rollingLimit}
-                          placeholder={formatMessage(messages['rollingLimit.placeholder'])}
-                          onChange={value => formik.setFieldValue('rollingLimit', value)}
-                          min={100}
-                          precision={2}
-                          px="2px"
-                        />
-                      )}
-                    </StyledInputField>
-                    <Flex alignItems="flex-end">
-                      <StyledButton
-                        maxHeight="45px"
-                        buttonStyle="primary"
-                        mx={2}
-                        buttonSize="small"
-                        type="submit"
-                        onSubmit={formik.handleSubmit}
+    <Fragment>
+      {notification && (
+        <TemporaryNotification type={error ? 'error' : 'default'} position="absolute">
+          {error ? (
+            <FormattedMessage
+              id="TwoFactorAuth.Setup.FiscalHost.Error"
+              defaultMessage="There was an issue updating your rolling payout limit."
+            />
+          ) : (
+            <FormattedMessage
+              id="TwoFactorAuth.Setup.FiscalHost.Updated"
+              defaultMessage="Your rolling payout limit has been updated."
+            />
+          )}
+        </TemporaryNotification>
+      )}
+      <Flex flexDirection="column">
+        <Container>
+          <SettingsTitle>
+            <FormattedMessage id="TwoFactorAuth.Setup.Title" defaultMessage="Set up two-factor authentication" />
+          </SettingsTitle>
+          <Flex mb={2} flexWrap="wrap" justifyContent="center">
+            <Container mr={3} pr={3} flex="1 1" minWidth={300} maxWidth={700} borderRight={[null, '1px solid #dcdee0']}>
+              <P wordBreak="break-word">
+                <FormattedMessage
+                  id="TwoFactorAuth.Setup.FiscalHost.Info"
+                  defaultMessage="Two-factor authentication (2FA) adds an extra layer of security for your account when logging in. For fiscal hosts, you can turn on 2FA to be used with high-risk activities like paying expenses. Enabling 2FA for the fiscal host means that every host admin (including yourself) will be required to enable 2FA for logging into their account."
+                />
+              </P>
+            </Container>
+            <Flex flexDirection="column" alignItems="center" justifyContent="center" minWidth={300}>
+              {doesHostAlreadyHaveTwoFactorAuthEnabled ? (
+                <Container>
+                  <form onSubmit={formik.handleSubmit}>
+                    <Flex>
+                      <StyledInputField
+                        name="rollingLimit"
+                        htmlFor="rollingLimit"
+                        disabled={loading}
+                        label={formatMessage(messages['rollingLimit.label'])}
+                        labelProps={{ mb: 2, pt: 2, lineHeight: '18px', fontWeight: 'bold' }}
                       >
-                        <FormattedMessage id="save" defaultMessage="Save" />
-                      </StyledButton>
+                        {inputProps => (
+                          <StyledInputAmount
+                            {...inputProps}
+                            currency={collective.currency}
+                            type="number"
+                            fontSize="14px"
+                            value={formik.values.rollingLimit}
+                            placeholder={formatMessage(messages['rollingLimit.placeholder'])}
+                            onChange={value => formik.setFieldValue('rollingLimit', value)}
+                            min={100}
+                            precision={2}
+                            px="2px"
+                          />
+                        )}
+                      </StyledInputField>
+                      <Flex alignItems="flex-end">
+                        <StyledButton
+                          maxHeight="45px"
+                          buttonStyle="primary"
+                          mx={2}
+                          buttonSize="small"
+                          type="submit"
+                          onSubmit={formik.handleSubmit}
+                          loading={loading}
+                          minWidth={80}
+                        >
+                          <FormattedMessage id="save" defaultMessage="Save" />
+                        </StyledButton>
+                      </Flex>
                     </Flex>
-                  </Flex>
-                </form>
-              </Container>
-            ) : (
-              <StyledCheckbox
-                name="enable-rolling-limit"
-                label={formatMessage(messages['rollingLimit.enable'])}
-                defaultChecked={defaultIsChecked}
-                width="auto"
-                isLoading={loading}
-                onChange={({ target }) => {
-                  const updatedCollective = cloneDeep(collective);
-                  set(updatedCollective, 'settings.payoutsTwoFactorAuth.enabled', target.value);
-                  return setSettings({ variables: pick(updatedCollective, ['id', 'settings']) });
-                }}
-              />
-            )}
+                  </form>
+                </Container>
+              ) : (
+                <StyledCheckbox
+                  name="enable-rolling-limit"
+                  label={formatMessage(messages['rollingLimit.enable'])}
+                  defaultChecked={defaultIsChecked}
+                  width="auto"
+                  isLoading={loading}
+                  onChange={({ target }) => {
+                    const updatedCollective = cloneDeep(collective);
+                    set(updatedCollective, 'settings.payoutsTwoFactorAuth.enabled', target.value);
+                    return setSettings({ variables: pick(updatedCollective, ['id', 'settings']) });
+                  }}
+                />
+              )}
+            </Flex>
           </Flex>
-        </Flex>
-        {error && (
-          <MessageBox type="error" fontSize="14px" withIcon mb={3}>
-            {getErrorFromGraphqlException(error).message}
-          </MessageBox>
-        )}
-        <Flex flexWrap="wrap" justifyContent="space-between" width="100%">
-          <ScreenshotPreview>
-            <img src={imgPreviewModal} alt="Preview host payouts rolling limit" />
-          </ScreenshotPreview>
-          <ScreenshotPreview>
-            <img src={imgPreviewPrompt} alt="Preview host payouts rolling limit" />
-          </ScreenshotPreview>
+          {error && (
+            <MessageBox type="error" fontSize="14px" withIcon mb={3}>
+              {getErrorFromGraphqlException(error).message}
+            </MessageBox>
+          )}
+          <Flex flexWrap="wrap" justifyContent="space-between" width="100%">
+            <ScreenshotPreview>
+              <img src={imgPreviewModal} alt="Preview host payouts rolling limit" />
+            </ScreenshotPreview>
+            <ScreenshotPreview>
+              <img src={imgPreviewPrompt} alt="Preview host payouts rolling limit" />
+            </ScreenshotPreview>
+            <hr />
+          </Flex>
           <hr />
-        </Flex>
-        <hr />
-        <HostPayouts2FARollingLimitFAQ defaultOpen currency={collective.currency} />
-      </Container>
-    </Flex>
+          <HostPayouts2FARollingLimitFAQ defaultOpen currency={collective.currency} />
+        </Container>
+      </Flex>
+    </Fragment>
   );
 };
 
