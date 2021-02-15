c8df44d821565cad0d99311f8a7b4cc0bd8bd4f1
Sudharaka Palamakumbura <sudharakap@mun.ca>
2021-02-15 07:27:23 -0800

    minor link and prettier fixes after merge

--- components/contribution-flow/ContributionFlowSuccess.js
+++ components/contribution-flow/ContributionFlowSuccess.js
@@ -25,9 +25,9 @@ import StyledLink from '../../components/StyledLink';
 import { H3, P } from '../../components/Text';
 import { withUser } from '../../components/UserProvider';
 
-import { orderSuccessFragment } from './graphql/fragments';
 import Link from '../Link';
 
+import { orderSuccessFragment } from './graphql/fragments';
 import PublicMessageForm from './ContributionFlowPublicMessage';
 import ContributorCardWithTier from './ContributorCardWithTier';
 import successIllustrationUrl from './success-illustration.jpg';
@@ -255,7 +255,7 @@ class ContributionFlowSuccess extends React.Component {
                 <ContributorCardWithTier width={250} height={380} contribution={order} my={2} useLink={!isEmbed} />
                 {!isEmbed && (
                   <Box my={4}>
-                  <Link href={{ pathname: 'discover', query: { show: getMainTag(order.toAccount) } }}>
+                    <Link href={{ pathname: 'discover', query: { show: getMainTag(order.toAccount) } }}>
                       <P color="black.800" fontWeight={500}>
                         <FormattedMessage
                           id="NewContributionFlow.Success.DiscoverMore"
--- components/transactions/TransactionsFilters.js
+++ components/transactions/TransactionsFilters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { isEmpty,isNil, omitBy } from 'lodash';
+import { isEmpty, isNil, omitBy } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
 
