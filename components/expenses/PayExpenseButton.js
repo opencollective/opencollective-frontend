28f76b61f0a1905a3377759e3fd37f760eb58ca3
François Hodierne <francois@hodierne.net>
2021-03-03 15:27:37 +0100

    fix balanceWithBlockedFunds crash

--- components/expenses/PayExpenseButton.js
+++ components/expenses/PayExpenseButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Paypal as PaypalIcon } from '@styled-icons/fa-brands/Paypal';
 import { University as OtherIcon } from '@styled-icons/fa-solid/University';
-import { includes } from 'lodash';
+import { get, includes } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 
 import { PayoutMethodType } from '../../lib/constants/payout-method';
@@ -16,7 +16,12 @@ import { Span } from '../Text';
 import PayExpenseModal from './PayExpenseModal';
 
 const getDisabledMessage = (expense, collective, host, payoutMethod) => {
-  const balance = collective.stats.balanceWithBlockedFunds.valueInCents;
+  // Collective can be v1 or v2 there ...
+  const balance = get(
+    collective,
+    'stats.balanceWithBlockedFunds.valueInCents',
+    get(collective, 'stats.balanceWithBlockedFunds', 0),
+  );
   if (!host) {
     return (
       <FormattedMessage id="expense.pay.error.noHost" defaultMessage="Expenses cannot be paid without a Fiscal Host" />
@@ -146,10 +151,13 @@ PayExpenseButton.propTypes = {
       plan: PropTypes.object,
     }),
     stats: PropTypes.shape({
-      balanceWithBlockedFunds: PropTypes.shape({
-        valueInCents: PropTypes.number.isRequired,
-        currency: PropTypes.string.isRequired,
-      }),
+      balanceWithBlockedFunds: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.shape({
+          valueInCents: PropTypes.number.isRequired,
+          currency: PropTypes.string.isRequired,
+        }),
+      ]),
     }),
   }).isRequired,
   host: PropTypes.shape({
