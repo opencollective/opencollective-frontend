8286cd0ada503c909da91d4ae297b158b664f575
Benjamin Piouffle <benjamin@opencollective.com>
2021-03-11 18:39:54 +0100

    enhancenemt(Updates): Use RichTextEditor and update styles

--- pages/createUpdate.js
+++ pages/createUpdate.js
@@ -107,7 +107,7 @@ class CreateUpdatePage extends React.Component {
                 </Container>
               </Link>
             </BackButtonWrapper>
-            <Container width={1}>
+            <Container width={1} maxWidth={650}>
               {!isAdmin && (
                 <div className="login">
                   <p>
