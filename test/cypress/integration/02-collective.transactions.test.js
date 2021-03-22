fb8c271683fa9d2a9b1f6e327a9a9c2cc3f3b982
Benjamin Piouffle <benjamin@opencollective.com>
2021-03-22 10:51:19 +0100

    fix(Header): Meta property

--- test/cypress/integration/02-collective.transactions.test.js
+++ test/cypress/integration/02-collective.transactions.test.js
@@ -2,13 +2,13 @@ describe('collective.transactions', () => {
   it('Should have no-index meta for user profiles', () => {
     cy.visit('/xdamman/transactions');
     cy.getByDataCy('transactions-page-content'); // Wait for page to be loaded
-    cy.get('meta[property="robots"]').should('have.attr', 'content', 'none');
+    cy.get('meta[name="robots"]').should('have.attr', 'content', 'none');
   });
 
   it('Should NOT have no-index meta for collectives', () => {
     cy.visit('/apex/transactions');
     cy.getByDataCy('transactions-page-content'); // Wait for page to be loaded
-    cy.get('meta[property="robots"]').should('not.exist');
+    cy.get('meta[name="robots"]').should('not.exist');
   });
 
   it("shows the 'Download CSV' button", () => {
