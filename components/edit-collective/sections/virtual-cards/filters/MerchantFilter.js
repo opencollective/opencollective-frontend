10eeb6344c8a832b8ebaddec770bf122a1be0e6d
Sudharaka <sudharakap@mun.ca>
2021-04-23 16:18:31 -0700

    fix: add custom option to trigger all merchants

--- components/edit-collective/sections/virtual-cards/filters/MerchantFilter.js
+++ components/edit-collective/sections/virtual-cards/filters/MerchantFilter.js
@@ -3,21 +3,28 @@ import PropTypes from 'prop-types';
 
 import { CollectiveType } from '../../../../../lib/constants/collectives';
 
+import { CUSTOM_OPTIONS_POSITION } from '../../../../CollectivePicker';
 import CollectivePickerAsync from '../../../../CollectivePickerAsync';
 
-function MerchantFilter({ onChange }) {
+function MerchantFilter({ inputId, onChange }) {
   return (
     <CollectivePickerAsync
-      inputId="virtual-card-merchant-filter"
+      id={inputId}
+      inputId={inputId}
       types={[CollectiveType.COLLECTIVE]}
+      maxMenuHeight={200}
       placeholder="Search for Merchants"
       preload={true}
+      limit={10}
       onChange={({ value }) => onChange(value.slug)}
+      emptyCustomOptions={[{ value: 'ALL', label: 'All Merchants' }]}
+      customOptionsPosition={CUSTOM_OPTIONS_POSITION.TOP}
     />
   );
 }
 
 MerchantFilter.propTypes = {
+  inputId: PropTypes.string,
   onChange: PropTypes.func,
 };
 
