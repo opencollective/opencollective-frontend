424c3b8455537d23d4cfac7c8c1ee1a6c43f29d4
Sudharaka <sudharakap@mun.ca>
2021-05-28 09:39:02 -0700

    fix: expense attachments icons are not respecting their container size

--- components/expenses/ExpenseSummary.js
+++ components/expenses/ExpenseSummary.js
@@ -191,8 +191,7 @@ const ExpenseSummary = ({
                       url={attachment.url}
                       isLoading={isLoading || isLoadingLoggedInUser}
                       isPrivate={!attachment.url && !isLoading}
-                      size={[640, 48]}
-                      maxHeight={48}
+                      size={48}
                     />
                   </Box>
                 )}
