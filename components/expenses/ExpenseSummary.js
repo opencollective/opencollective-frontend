70a16ab3ef55958c218dccdbeac68ab9707552ff
Sudharaka <sudharakap@mun.ca>
2021-05-31 08:25:05 -0700

    fix: add 100% width to attachment preview on smaller screens

--- components/expenses/ExpenseSummary.js
+++ components/expenses/ExpenseSummary.js
@@ -191,7 +191,8 @@ const ExpenseSummary = ({
                       url={attachment.url}
                       isLoading={isLoading || isLoadingLoggedInUser}
                       isPrivate={!attachment.url && !isLoading}
-                      size={48}
+                      size={['100%', 48]}
+                      maxHeight={48}
                     />
                   </Box>
                 )}
