d1bb5c385e9876f8f57be995881eee11b9874389
Sudharaka <sudharakap@mun.ca>
2021-05-31 15:01:51 -0700

    feat: upgrade jest to v27

--- test/setup.js
+++ test/setup.js
@@ -6,14 +6,10 @@ import Enzyme from 'enzyme';
 import jsdom from 'jsdom';
 
 const { JSDOM } = jsdom;
-const { document } = new JSDOM('<!doctype html><html><body></body></html>').window;
+const url = 'http://localhost';
+const { document } = new JSDOM('<!doctype html><html><body></body></html>', { url }).window;
 global.document = document;
 global.window = document.defaultView;
-global.window.localStorage = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  removeItem: jest.fn(),
-};
 global.navigator = {
   userAgent: 'node.js',
 };
