d9e953155a14e972c45dda1e29c7f8da8a13596a
Benjamin Piouffle <benjamin@opencollective.com>
2021-06-03 17:40:39 +0200

    style(TopBar): Accessibility + design

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,18 +1,36 @@
 import { ThemeProvider } from 'styled-components';
 import { IntlProvider } from 'react-intl';
 import theme from '../lib/theme';
+import { ApolloProvider } from '@apollo/client';
+import * as nextImage from 'next/image';
+
+import UserProvider from '../components/UserProvider';
+import { initClient } from '../lib/apollo-client';
 
 import 'nprogress/nprogress.css';
 import 'react-datetime/css/react-datetime.css';
 import 'trix/dist/trix.css';
 import '../public/static/styles/app.css';
 
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: props => <img {...props} />,
+});
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
 export const decorators = [
   Story => (
-    <ThemeProvider theme={theme}>
-      <IntlProvider locale="en">
-        <Story />
-      </IntlProvider>
-    </ThemeProvider>
+    <ApolloProvider client={initClient()}>
+      <ThemeProvider theme={theme}>
+        <IntlProvider locale="en">
+          <UserProvider>
+            <Story />
+          </UserProvider>
+        </IntlProvider>
+      </ThemeProvider>
+    </ApolloProvider>
   ),
 ];
