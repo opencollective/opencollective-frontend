0ab66b4f66a540e328b20be2beeebf8c33035574
Sudharaka <sudharakap@mun.ca>
2021-06-08 07:29:09 -0700

    fix: correct lang files and fix a lint error

--- pages/createUpdate.js
+++ pages/createUpdate.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from '@apollo/client/react/hoc';
 import { ArrowBack } from '@styled-icons/boxicons-regular';
-import { mapValues } from 'lodash';
 import { withRouter } from 'next/router';
 import { defineMessages, FormattedMessage, injectIntl } from 'react-intl';
 import styled from 'styled-components';
