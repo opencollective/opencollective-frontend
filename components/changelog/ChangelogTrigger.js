c70aeda7c11c6a64b70aaa2173aafe64229904df
Sudharaka <sudharakap@mun.ca>
2021-06-21 09:43:44 -0700

    refactor: remove Avatar.js from the ChangelogTrigger.js

--- components/changelog/ChangelogTrigger.js
+++ components/changelog/ChangelogTrigger.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from '@apollo/client/react/hoc';
+import themeGet from '@styled-system/theme-get';
+import styled from 'styled-components';
 
 import { API_V2_CONTEXT, gqlV2 } from '../../lib/graphql/helpers';
 import { parseToBoolean } from '../../lib/utils';
 
-import Avatar from '../Avatar';
 import Container from '../Container';
 import { Flex } from '../Grid';
 import { withNewsAndUpdates } from '../NewsAndUpdatesProvider';
@@ -15,6 +16,17 @@ import { useUser } from '../UserProvider';
 import ChangelogNotificationDropdown from './ChangelogNotificationDropdown';
 
 const CHANGE_LOG_UPDATES_ENABLED = parseToBoolean(process.env.CHANGE_LOG_UPDATES_ENABLED);
+
+const FlameIcon = styled(Flex)`
+  border-radius: 50%;
+  background-color: ${props => themeGet(props.backgroundColor)};
+  background-image: ${props => `url(${props.url})`};
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 30px;
+  width: 30px;
+  margin-left: 2px;
+`;
 
 const ChangelogTrigger = props => {
   const { setShowNewsAndUpdates, setChangelogViewDate } = props;
@@ -37,27 +49,13 @@ const ChangelogTrigger = props => {
   return (
     <React.Fragment>
       {LoggedInUser && CHANGE_LOG_UPDATES_ENABLED && (
-        <Flex>
-          {hasSeenNewUpdates && (
-            <Avatar
-              onClick={handleShowNewUpdates}
-              src="/static/images/flame-default.svg"
-              radius="30px"
-              backgroundSize={10}
-              ml={2}
-            />
-          )}
-          {!hasSeenNewUpdates && (
+        <Flex onClick={handleShowNewUpdates}>
+          {hasSeenNewUpdates ? (
+            <FlameIcon backgroundColor="black.100" url="/static/images/flame-default.svg" />
+          ) : (
             <Dropdown>
               <React.Fragment>
-                <Avatar
-                  onClick={handleShowNewUpdates}
-                  src="/static/images/flame-red.svg"
-                  radius="30px"
-                  backgroundSize={10}
-                  backgroundColor="yellow.100"
-                  ml={2}
-                />
+                <FlameIcon backgroundColor="yellow.100" url="/static/images/flame-red.svg" />
                 {showChangelogDropdown && (
                   <Container>
                     <ChangelogNotificationDropdown onClose={() => setShowChangelogDropdown(false)} />
