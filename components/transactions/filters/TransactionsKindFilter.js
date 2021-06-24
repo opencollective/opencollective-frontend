1d9f3bef6f3bf1809405a48c8737dbd530f68892
Benjamin Piouffle <benjamin@opencollective.com>
2021-06-24 08:40:06 +0200

    fix(TransactionsKindFilter): Text truncation/responsiveness (#6530)

--- components/transactions/filters/TransactionsKindFilter.js
+++ components/transactions/filters/TransactionsKindFilter.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { omit, size } from 'lodash';
 import { useIntl } from 'react-intl';
+import { components as ReactSelectComponents } from 'react-select';
 import styled from 'styled-components';
+import { maxWidth } from 'styled-system';
 
 import { TransactionKind } from '../../../lib/constants/transactions';
 import { i18nTransactionKind } from '../../../lib/i18n/transaction';
 
 import { StyledSelectFilter } from '../../StyledSelectFilter';
+import { Span } from '../../Text';
 
 const DISPLAYED_TRANSACTION_KINDS = omit(TransactionKind, [
   'PLATFORM_FEE',
@@ -41,25 +44,37 @@ export const parseTransactionKinds = str => {
   }
 };
 
-const MultiKindSelect = styled(StyledSelectFilter)`
-  div[class$='ValueContainer'] {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    max-width: 70%;
-    display: block;
-  }
+const TruncatedItemsList = styled(Span).attrs({
+  truncateOverflow: true,
+  pl: 2,
+  maxWidth: ['calc(100vw - 135px)', '75px', '175px', '200px'],
+})`
+  display: inline-block;
+  max-width: 75px;
+  ${maxWidth}
 `;
 
+const TruncatedValueContainer = props => {
+  const { selectProps, children } = props;
+  const itemsList = (selectProps.value || []).map(({ label }) => label);
+  const itemsListStr = itemsList.join(', ');
+
+  return (
+    <ReactSelectComponents.SelectContainer {...props}>
+      <TruncatedItemsList title={itemsListStr}>{itemsListStr}</TruncatedItemsList>
+      {children}
+    </ReactSelectComponents.SelectContainer>
+  );
+};
+
+TruncatedValueContainer.propTypes = {
+  selectProps: PropTypes.object,
+  children: PropTypes.node,
+};
+
 const REACT_SELECT_COMPONENT_OVERRIDE = {
-  MultiValue: ({ data, selectProps }) => {
-    const totalItems = selectProps.value.length;
-    if (data.idx === totalItems - 1) {
-      return data.label;
-    } else {
-      return `${data.label}, `;
-    }
-  },
+  ValueContainer: TruncatedValueContainer,
+  MultiValue: () => null, // Items will be displayed as a truncated string in `TruncatedValueContainer `
 };
 
 const TransactionsKindFilter = ({ onChange, value, ...props }) => {
@@ -71,7 +86,7 @@ const TransactionsKindFilter = ({ onChange, value, ...props }) => {
     [value],
   );
   return (
-    <MultiKindSelect
+    <StyledSelectFilter
       isSearchable={false}
       onChange={options => onChange(optionsToQueryString(options))}
       value={selectedOptions}
