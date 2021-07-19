961eed62e96113d8bac63fdc2e0c4217110c7e41
Sudharaka Palamakumbura <sudharakap@mun.ca>
2021-07-19 13:28:25 -0700

    Sync Payment Methods between Host Dashboard and Expenses Page (#6631)
    * fix: sync payment methods between host dashboard and expenses page
    
    * fix: remove unnecessary host fields

--- components/collective-page/sections/Budget.js
+++ components/collective-page/sections/Budget.js
@@ -24,9 +24,10 @@ import TransactionItem from '../../transactions/TransactionItem';
 import { withUser } from '../../UserProvider';
 import BudgetStats from '../BudgetStats';
 import ContainerSectionContent from '../ContainerSectionContent';
+import { budgetSectionQuery } from '../graphql/queries';
 
-export const budgetSectionQuery = gqlV2/* GraphQL */ `
-  query BudgetSection($slug: String!, $hostSlug: String!, $limit: Int!, $kind: [TransactionKind]) {
+export const budgetSectionWithHostQuery = gqlV2/* GraphQL */ `
+  query BudgetSectionWithHost($slug: String!, $hostSlug: String!, $limit: Int!, $kind: [TransactionKind]) {
     host(slug: $hostSlug) {
       ...ExpenseHostFields
     }
@@ -127,7 +128,8 @@ ViewAllLink.propTypes = {
  */
 const SectionBudget = ({ collective, stats, LoggedInUser }) => {
   const [filter, setFilter] = React.useState('all');
-  const budgetQueryResult = useQuery(budgetSectionQuery, {
+  const budgetQuery = collective.host ? budgetSectionWithHostQuery : budgetSectionQuery;
+  const budgetQueryResult = useQuery(budgetQuery, {
     variables: getBudgetSectionQueryVariables(collective.slug, collective.host?.slug),
     context: API_V2_CONTEXT,
   });
