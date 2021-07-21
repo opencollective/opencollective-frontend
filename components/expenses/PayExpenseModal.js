6ba06d6f8a6b86269a634de6b0ce56b99d410384
Sudharaka Palamakumbura <sudharakap@mun.ca>
2021-07-21 05:50:43 -0700

    feat: remove console log
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>

--- components/expenses/PayExpenseModal.js
+++ components/expenses/PayExpenseModal.js
@@ -120,7 +120,6 @@ const PayExpenseModal = ({ onClose, onSubmit, expense, collective, host, error }
   const payoutMethodLabel = getPayoutLabel(intl, payoutMethodType);
   const hasBankInfoWithoutWise = payoutMethodType === PayoutMethodType.BANK_ACCOUNT && host.transferwise === null;
   const isScheduling = formik.values.action === 'SCHEDULE_FOR_PAYMENT';
-  console.log(host);
 
   return (
     <StyledModal
