b1d7a5e97dd9030958f9859cc52b64d44e2294e2
Sudharaka Palamakumbura <sudharakap@mun.ca>
2021-07-21 10:29:32 -0700

    Hide download receipt button for internal transfers (#6633)
    * feat: hide download receipt button for internal transfers
    
    * fix: remove deprecated fields and correct as per code review

--- components/transactions/graphql/fragments.js
+++ components/transactions/graphql/fragments.js
@@ -72,6 +72,16 @@ export const transactionsQueryCollectionFragment = gqlV2/* GraphQL */ `
         type
         imageUrl
         isIncognito
+        ... on Event {
+          parent {
+            id
+          }
+        }
+        ... on Project {
+          parent {
+            id
+          }
+        }
         ... on Individual {
           isGuest
         }
