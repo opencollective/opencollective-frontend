01afd86ff12653af323575e99038d5861dd903c3
Sudharaka <sudharakap@mun.ca>
2021-07-28 07:53:49 -0700

    fix: make sure service is a list of PaymentMethods

--- components/SendMoneyToCollectiveBtn.js
+++ components/SendMoneyToCollectiveBtn.js
@@ -108,7 +108,7 @@ const paymentMethodsQuery = gqlV2/* GraphQL */ `
   query SendMoneyToCollectivePaymentMethods($slug: String) {
     account(slug: $slug) {
       id
-      paymentMethods(service: OPENCOLLECTIVE, type: ["collective"]) {
+      paymentMethods(service: [OPENCOLLECTIVE], type: ["collective"]) {
         id
         service
         name
