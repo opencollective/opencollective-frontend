6594a7a4633aca7787714002179ecefe910199bb
Sudharaka <sudharakap@mun.ca>
2021-08-17 13:41:22 -0700

    feat: add support to add funds when event is over

--- components/AddFundsBtn.js
+++ components/AddFundsBtn.js
@@ -5,7 +5,7 @@ import { FormattedMessage } from 'react-intl';
 import AddFundsModal from './host-dashboard/AddFundsModal';
 import StyledButton from './StyledButton';
 
-const AddFundsBtn = ({ children, collective, host }) => {
+const AddFundsBtn = ({ children, collective, host, isFromCollective }) => {
   const [showModal, setShowModal] = React.useState(false);
   return (
     <Fragment>
@@ -16,6 +16,7 @@ const AddFundsBtn = ({ children, collective, host }) => {
         show={showModal}
         setShow={setShowModal}
         onClose={() => setShowModal(null)}
+        isFromCollective={isFromCollective}
       />
     </Fragment>
   );
@@ -25,6 +26,7 @@ AddFundsBtn.propTypes = {
   children: PropTypes.func.isRequired,
   collective: PropTypes.object.isRequired,
   host: PropTypes.object.isRequired,
+  isFromCollective: PropTypes.bool,
 };
 
 const DefaultAddFundsButton = props => (
