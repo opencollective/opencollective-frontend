6e294d79a7d04b857f468751d6107f16e987bb5d
Benjamin Piouffle <benjamin@opencollective.com>
2021-08-20 09:39:09 +0200

    enhancement(RecurringContribution): Remove redirect warning

--- pages/recurring-contributions-redirect.js
+++ pages/recurring-contributions-redirect.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Router from 'next/router';
 
 import { Box } from '../components/Grid';
-import Link from '../components/Link';
 import Page from '../components/Page';
 import { withUser } from '../components/UserProvider';
 
@@ -34,23 +33,10 @@ class RecurringContributionsRedirectPage extends React.Component {
   }
 
   render() {
-    const { LoggedInUser } = this.props;
     return (
       <Page title="Subscriptions" description="All the collectives that you are giving money to">
         <Box py={[5, 6, 7]} textAlign="center">
           <strong>Redirecting...</strong>
-          {LoggedInUser && (
-            <div>
-              This page has moved. Your recurring financial contributions are now at
-              <Link href={`/${LoggedInUser.collective.slug}/recurring-contributions`}>
-                <span>
-                  {' '}
-                  /{LoggedInUser.collective.slug}
-                  /recurring-contributions
-                </span>
-              </Link>
-            </div>
-          )}
         </Box>
       </Page>
     );
