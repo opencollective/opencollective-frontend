5e72efa7fdec8e444c48b9a0593116cb1144d0ca
Sudharaka <sudharakap@mun.ca>
2021-09-16 15:38:02 -0600

    feat: add support for collective and period filters in the transactions view

--- components/host-dashboard/reports-section/TransactionsOverviewSection.js
+++ components/host-dashboard/reports-section/TransactionsOverviewSection.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useQuery } from '@apollo/client';
 import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
 
 import { formatCurrency } from '../../../lib/currency-utils';
+import { API_V2_CONTEXT, gqlV2 } from '../../../lib/graphql/helpers';
 
 import PeriodFilter from '../../budget/filters/PeriodFilter';
 import CollectivePickerAsync from '../../CollectivePickerAsync';
 import Container from '../../Container';
 import { Flex } from '../../Grid';
+import Loading from '../../Loading';
 import { P, Span } from '../../Text';
 
 const FilterLabel = styled.label`
@@ -51,24 +54,100 @@ const Square = styled(Container)`
   background-color: ${props => props.color};
 `;
 
-const TransactionsOverviewSection = ({ currency, filters, onChange }) => {
-  const numberOfContributions = 8459;
-  const numberOfExpenses = 956;
-  const dailyAvgContributions = 1959.59;
-  const dailyAvgExpenses = 1959.59;
-  const numOneTime = 56;
-  const numRecurring = 33;
-  const numInvoices = 33;
-  const numReimbursed = 33;
-  const numGrants = 33;
+const transactionsOverviewQuery = gqlV2/* GraphQL */ `
+  query TransactionsOverviewQuery(
+    $hostSlug: String!
+    $accounts: [AccountReferenceInput!]
+    $dateFrom: String
+    $dateTo: String
+  ) {
+    host(slug: $hostSlug) {
+      id
+      contributionStats(accounts: $accounts, from: $dateFrom, to: $dateTo) {
+        numContributions
+        numOneTime
+        numRecurring
+        dailyAvgIncome
+      }
+      expenseStats(accounts: $accounts, from: $dateFrom, to: $dateTo) {
+        numExpenses
+        dailyAverage
+        numInvoices
+        numReimbursements
+        numGrants
+      }
+    }
+  }
+`;
 
-  const getFilterProps = name => ({
-    inputId: `transactions-filter-${name}`,
-    value: filters?.[name],
-    onChange: value => {
-      onChange({ ...filters, [name]: value === 'ALL' ? null : value });
-    },
+const TransactionsOverviewSection = ({ hostSlug, currency }) => {
+  const [dateFrom, setDateFrom] = useState(null);
+  const [dateTo, setDateTo] = useState(null);
+  const [collectives, setCollectives] = useState(null);
+
+  const { data, loading, refetch } = useQuery(transactionsOverviewQuery, {
+    variables: { hostSlug, dateFrom, dateTo, accounts: collectives },
+    context: API_V2_CONTEXT,
   });
+  const contributionStats = data?.host.contributionStats;
+  const expenseStats = data?.host.expenseStats;
+
+  useEffect(() => {
+    refetch();
+  }, [dateFrom, dateTo, collectives]);
+
+  const { numContributions, numRecurring, numOneTime, dailyAvgIncome } = contributionStats;
+  const { numExpenses, numInvoices, numReimbursements, numGrants, dailyAverage } = expenseStats;
+
+  const setDate = dateRange => {
+    if (!dateRange) {
+      setDateFrom(null);
+      setDateTo(null);
+      refetch();
+      return;
+    }
+    const dates = dateRange.split('→');
+    const dateFrom = dates[0];
+    const dateTo = dates[1];
+    if (dateFrom === 'all') {
+      setDateFrom(null);
+    } else {
+      setDateFrom(dateFrom);
+    }
+    if (dateTo === 'all') {
+      setDateTo(null);
+    } else {
+      setDateTo(dateTo);
+    }
+  };
+
+  const getDateString = () => {
+    let dateString;
+    if (!dateFrom && !dateTo) {
+      return;
+    }
+    if (dateFrom) {
+      dateString = `${dateFrom}→`;
+    } else {
+      dateString = 'all→';
+    }
+
+    if (dateTo) {
+      dateString += dateTo;
+    } else {
+      dateString += 'all';
+    }
+    return dateString;
+  };
+
+  const setCollectiveFilter = collectives => {
+    if (collectives.length === 0) {
+      setCollectives(null);
+    } else {
+      const collectiveIds = collectives.map(collective => ({ legacyId: collective.value.id }));
+      setCollectives(collectiveIds);
+    }
+  };
 
   return (
     <React.Fragment>
@@ -77,7 +156,7 @@ const TransactionsOverviewSection = ({ currency, filters, onChange }) => {
           <FilterLabel htmlFor="transactions-period-filter">
             <FormattedMessage id="TransactionsOverviewSection.PeriodFilter" defaultMessage="Filter by Date" />
           </FilterLabel>
-          <PeriodFilter {...getFilterProps('period')} />
+          <PeriodFilter onChange={value => setDate(value)} value={getDateString()} />
         </Container>
         <Container width={[1, 1, 1 / 2]}>
           <FilterLabel htmlFor="transactions-collective-filter">
@@ -88,74 +167,92 @@ const TransactionsOverviewSection = ({ currency, filters, onChange }) => {
             data-cy="transactions-collective-filter"
             types={['COLLECTIVE']}
             isMulti
-            {...getFilterProps('collective')}
+            onChange={value => setCollectiveFilter(value)}
           />
         </Container>
       </Flex>
-      <Flex flexWrap="wrap" mt={18} mb={12}>
-        <FundAmounts>
-          <TotalFundsLabel minWidth="280px">
-            <P>
-              <Span fontWeight="500">
-                {numberOfContributions} <FormattedMessage id="Contributions" defaultMessage="Contributions" />
-              </Span>{' '}
-              | <FormattedMessage id="DailyAverage" defaultMessage="Daily avg" />
-              {': '}
-              <Span fontWeight="700">{formatCurrency(dailyAvgContributions, currency)}</Span>
-            </P>
-          </TotalFundsLabel>
-          <TotalFundsLabel
-            minWidth="250px"
-            position="relative"
-            left={['-280px', '-280px', '-280px', '300px']}
-            top={['85px', '85px', '85px', '0px']}
-          >
-            <P>
-              <Span fontWeight="500">
-                {numberOfExpenses} <FormattedMessage id="section.expenses.title" defaultMessage="Expenses" />
-              </Span>{' '}
-              | <FormattedMessage id="DailyAverage" defaultMessage="Daily avg" />
-              {': '}
-              <Span fontWeight="700">{formatCurrency(dailyAvgExpenses, currency)}</Span>
-            </P>
-          </TotalFundsLabel>
-        </FundAmounts>
-        <Container mt={2} flexWrap>
-          <Span mr={3}>
-            <Square color="#51E094" />
-            {` ${numOneTime} `}
-            <FormattedMessage id="Frequency.OneTime" defaultMessage="One time" />
-          </Span>
-          <Span mr={['20px', '20px', '20px', '380px']}>
-            <Square color="#BEFADA" />
-            {` ${numRecurring} `}
-            <FormattedMessage id="TransactionsOverviewSection.Recurring" defaultMessage="Recurring" />
-          </Span>
-          <Span mr={3}>
-            <Square color="#CC2955" />
-            {` ${numInvoices} `}
-            <FormattedMessage id="TransactionsOverviewSection.Invoices" defaultMessage="Invoices" />
-          </Span>
-          <Span mr={3}>
-            <Square color="#F55882" />
-            {` ${numReimbursed} `}
-            <FormattedMessage id="TransactionsOverviewSection.Reimbursements" defaultMessage="Reimbursements" />
-          </Span>
-          <Span mr={3}>
-            <Square color="#FFC2D2" />
-            {` ${numGrants} `}
-            <FormattedMessage id="TransactionsOverviewSection.Grants" defaultMessage="Grants" />
-          </Span>
-        </Container>
-      </Flex>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Flex flexWrap="wrap" mt={18} mb={12}>
+          <FundAmounts>
+            <TotalFundsLabel minWidth="280px">
+              <P>
+                <Span fontWeight="500">
+                  {numContributions} <FormattedMessage id="Contributions" defaultMessage="Contributions" />
+                </Span>{' '}
+                | <FormattedMessage id="DailyAverage" defaultMessage="Daily avg" />
+                {': '}
+                <Span fontWeight="700">{formatCurrency(dailyAvgIncome, currency)}</Span>
+              </P>
+            </TotalFundsLabel>
+            <TotalFundsLabel
+              minWidth="250px"
+              position="relative"
+              left={['-280px', '-280px', '-280px', '300px']}
+              top={['85px', '85px', '85px', '0px']}
+            >
+              <P>
+                <Span fontWeight="500">
+                  {numExpenses} <FormattedMessage id="section.expenses.title" defaultMessage="Expenses" />
+                </Span>{' '}
+                | <FormattedMessage id="DailyAverage" defaultMessage="Daily avg" />
+                {': '}
+                <Span fontWeight="700">{formatCurrency(dailyAverage, currency)}</Span>
+              </P>
+            </TotalFundsLabel>
+          </FundAmounts>
+          <Container mt={2}>
+            <Span mr={3}>
+              <Square color="#51E094" />
+              {` ${numOneTime} `}
+              <FormattedMessage id="Frequency.OneTime" defaultMessage="One time" />
+            </Span>
+            <Span mr={['20px', '20px', '20px', '380px']}>
+              <Square color="#BEFADA" />
+              {` ${numRecurring} `}
+              <FormattedMessage id="TransactionsOverviewSection.Recurring" defaultMessage="Recurring" />
+            </Span>
+            <Span mr={3}>
+              <Square color="#CC2955" />
+              {` ${numInvoices} `}
+              <FormattedMessage id="TransactionsOverviewSection.Invoices" defaultMessage="Invoices" />
+            </Span>
+            <Span mr={3}>
+              <Square color="#F55882" />
+              {` ${numReimbursements} `}
+              <FormattedMessage id="TransactionsOverviewSection.Reimbursements" defaultMessage="Reimbursements" />
+            </Span>
+            <Span mr={3}>
+              <Square color="#FFC2D2" />
+              {` ${numGrants} `}
+              <FormattedMessage id="TransactionsOverviewSection.Grants" defaultMessage="Grants" />
+            </Span>
+          </Container>
+        </Flex>
+      )}
     </React.Fragment>
   );
 };
 
 TransactionsOverviewSection.propTypes = {
+  hostSlug: PropTypes.string,
   onChange: PropTypes.func,
   filters: PropTypes.object,
   currency: PropTypes.string,
+  contributionStats: PropTypes.shape({
+    numContributions: PropTypes.number,
+    numOneTime: PropTypes.number,
+    numRecurring: PropTypes.number,
+    dailyAvgIncome: PropTypes.number,
+  }),
+  expenseStats: PropTypes.shape({
+    numExpenses: PropTypes.number,
+    numInvoices: PropTypes.number,
+    numReimbursements: PropTypes.number,
+    numGrants: PropTypes.number,
+    dailyAverage: PropTypes.number,
+  }),
 };
 
 export default TransactionsOverviewSection;
