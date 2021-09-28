89923a87f01f1d0f8e4e8685b210d90842d23e24
Sudharaka <sudharakap@mun.ca>
2021-09-28 10:39:28 -0600

    feat: add the remaining supported crypto currencies by the giving block

--- components/contribution-flow/constants.js
+++ components/contribution-flow/constants.js
@@ -284,4 +284,194 @@ export const CRYPTO_CURRENCIES = [
     labelWithoutImage: 'YFI (Yearn Finance)',
     value: 'YFI',
   },
+  {
+    label: (
+      <Flex>
+        <Image alt="ALCX (Alchemix)" width={18} height={18} src="/static/images/crypto-logos/ALCX.svg" />
+        <Span ml={2}>ALCX (Alchemix)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'ALCX (Alchemix)',
+    value: 'ALCX',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="SAND (Sandbox)" width={18} height={18} src="/static/images/crypto-logos/SAND.svg" />
+        <Span ml={2}>SAND (Sandbox)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'SAND (Sandbox)',
+    value: 'SAND',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="INJ (Injective Protocol)" width={18} height={18} src="/static/images/crypto-logos/INJ.svg" />
+        <Span ml={2}>INJ (Injective Protocol)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'INJ (Injective Protocol)',
+    value: 'INJ',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="PAXG (PAX Gold)" width={18} height={18} src="/static/images/crypto-logos/PAXG.svg" />
+        <Span ml={2}>PAXG (PAX Gold)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'PAXG (PAX Gold)',
+    value: 'PAXG',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="ENJ (Enjin Coin)" width={18} height={18} src="/static/images/crypto-logos/ENJ.svg" />
+        <Span ml={2}>ENJ (Enjin Coin)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'ENJ (Enjin Coin)',
+    value: 'ENJ',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="FIL (Filecoin)" width={18} height={18} src="/static/images/crypto-logos/FIL.svg" />
+        <Span ml={2}>FIL (Filecoin)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'FIL (Filecoin)',
+    value: 'FIL',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="ANKR (Ankr)" width={18} height={18} src="/static/images/crypto-logos/ANKR.svg" />
+        <Span ml={2}>ANKR (Ankr)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'ANKR (Ankr)',
+    value: 'ANKR',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="XTZ (Tezos)" width={18} height={18} src="/static/images/crypto-logos/XTZ.svg" />
+        <Span ml={2}>XTZ (Tezos)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'XTZ (Tezos)',
+    value: 'XTZ',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="MKR (Maker)" width={18} height={18} src="/static/images/crypto-logos/MKR.svg" />
+        <Span ml={2}>MKR (Maker)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'MKR (Maker)',
+    value: 'MKR',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="LRC (Loopring)" width={18} height={18} src="/static/images/crypto-logos/LRC.svg" />
+        <Span ml={2}>LRC (Loopring)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'LRC (Loopring)',
+    value: 'LRC',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="BOND (BarnBridge)" width={18} height={18} src="/static/images/crypto-logos/BOND.svg" />
+        <Span ml={2}>BOND (BarnBridge)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'BOND (BarnBridge)',
+    value: 'BOND',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="SKL (Skale)" width={18} height={18} src="/static/images/crypto-logos/SKL.svg" />
+        <Span ml={2}>SKL (Skale)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'SKL (Skale)',
+    value: 'SKL',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="MIR (Mirror Protocol)" width={18} height={18} src="/static/images/crypto-logos/MIR.svg" />
+        <Span ml={2}>MIR (Mirror Protocol)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'MIR (Mirror Protocol)',
+    value: 'MIR',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="LPT (Livepeer)" width={18} height={18} src="/static/images/crypto-logos/LPT.svg" />
+        <Span ml={2}>LPT (Livepeer)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'LPT (Livepeer)',
+    value: 'LPT',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="MANA (Mana)" width={18} height={18} src="/static/images/crypto-logos/MANA.svg" />
+        <Span ml={2}>MANA (Mana)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'MANA (Mana)',
+    value: 'MANA',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="FTM (Fantom)" width={18} height={18} src="/static/images/crypto-logos/FTM.svg" />
+        <Span ml={2}>FTM (Fantom)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'FTM (Fantom)',
+    value: 'FTM',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="KNC (Kyber)" width={18} height={18} src="/static/images/crypto-logos/KNC.svg" />
+        <Span ml={2}>KNC (Kyber)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'KNC (Kyber)',
+    value: 'KNC',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="CUBE (Cube)" width={18} height={18} src="/static/images/crypto-logos/CUBE.svg" />
+        <Span ml={2}>CUBE (Cube)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'CUBE (Cube)',
+    value: 'CUBE',
+  },
+  {
+    label: (
+      <Flex>
+        <Image alt="REN (Ren)" width={18} height={18} src="/static/images/crypto-logos/REN.svg" />
+        <Span ml={2}>REN (Ren)</Span>
+      </Flex>
+    ),
+    labelWithoutImage: 'REN (Ren)',
+    value: 'REN',
+  },
 ];
