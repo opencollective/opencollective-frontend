5e444e088502d8e018018324e6ba9d152adfeb1f
Sudharaka Palamakumbura <sudharakap@mun.ca>
2021-09-30 00:00:15 -0700

    feat: add view profile button to dropdown profile menu (#6947)

--- components/TopBarProfileMenu.js
+++ components/TopBarProfileMenu.js
@@ -6,7 +6,7 @@ import { ChevronDown } from '@styled-icons/boxicons-regular/ChevronDown';
 import { ChevronRight } from '@styled-icons/boxicons-regular/ChevronRight';
 import { get } from 'lodash';
 import { FormattedMessage } from 'react-intl';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 import { API_V2_CONTEXT, gqlV2 } from '../lib/graphql/helpers';
 import { getFromLocalStorage, LOCAL_STORAGE_KEYS } from '../lib/local-storage';
@@ -40,6 +40,14 @@ const HideGlobalScroll = createGlobalStyle`
     body {
       overflow: hidden;
     }
+  }
+`;
+
+const ViewProfileLink = styled(StyledLink)`
+  &:hover {
+    background-color: white;
+    border-color: black;
+    color: black;
   }
 `;
 
@@ -237,12 +245,26 @@ class TopBarProfileMenu extends React.Component {
             <React.Fragment>
               <Box order={[2, 1]} flex="10 1 50%" width={[1, 1, 1 / 2]} p={3} display={['none', 'flex']} bg="#F7F8FA">
                 <Hide xs>
-                  <Avatar collective={LoggedInUser.collective} radius={56} mr={2} />
-                  <P mt={2} color="black.800" fontWeight="500" fontSize="14px" lineHeight="20px">
-                    {LoggedInUser.collective.name}
-                  </P>
-                  <P mt="2px" mb={5} wordBreak="break-all" color="black.700" fontSize="13px">
+                  <Link href={`/${LoggedInUser.collective.slug}`}>
+                    <Avatar collective={LoggedInUser.collective} radius={56} mr={2} />
+                    <P mt={2} color="black.800" fontWeight="500" fontSize="14px" lineHeight="20px">
+                      {LoggedInUser.collective.name}
+                    </P>
+                  </Link>
+                  <P mt="2px" wordBreak="break-all" color="black.700" fontSize="13px">
                     {LoggedInUser.email}
+                  </P>
+                  <P mb={4} mt={3} color="black.800">
+                    <ViewProfileLink
+                      as={Link}
+                      buttonSize="tiny"
+                      buttonStyle="standard"
+                      href={`/${LoggedInUser.collective.slug}`}
+                    >
+                      <Span css={{ verticalAlign: 'middle' }}>
+                        <FormattedMessage defaultMessage="View Profile" />
+                      </Span>
+                    </ViewProfileLink>
                   </P>
                   <P color="black.900" fontSize="12px" fontWeight="700" letterSpacing="1px" textTransform="uppercase">
                     <FormattedMessage id="menu.myAccount" defaultMessage="My account" />
