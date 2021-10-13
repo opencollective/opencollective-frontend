bd34dbdf3f39432c9cf4d18419511dcb5edf121d
François Hodierne <francois@hodierne.net>
2021-10-13 11:45:31 +0200

    fix empty transactions in Budget section (#6998)
    when no transaction without Expense

--- components/collective-page/sections/Budget.js
+++ components/collective-page/sections/Budget.js
@@ -34,7 +34,6 @@ export const budgetSectionWithHostQuery = gqlV2/* GraphQL */ `
     transactions(
       account: { slug: $slug }
       limit: $limit
-      hasExpense: false
       kind: $kind
       includeIncognitoTransactions: true
       includeGiftCardTransactions: true
@@ -90,7 +89,11 @@ const getBudgetItems = (transactions, expenses, filter) => {
   } else if (filter === 'transactions') {
     return transactions;
   } else {
-    return orderBy([...transactions, ...expenses], 'createdAt', 'desc').slice(0, 3);
+    const expenseIds = expenses.map(expense => expense.id);
+    const transactionsWithoutMatchingExpense = transactions.filter(
+      transaction => !expenseIds.includes(transaction.expense?.id),
+    );
+    return orderBy([...transactionsWithoutMatchingExpense, ...expenses], 'createdAt', 'desc').slice(0, 3);
   }
 };
 
