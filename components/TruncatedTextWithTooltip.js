0e4168f4f8f6291fc7e7f8eb8d2e9391c66589c4
Benjamin Piouffle <benjamin@opencollective.com>
2021-10-20 11:13:37 +0200

    CollectivePage/Hero: Duplicate Part of/fiscal host (#7023)
    * fix(CollectivePage/Hero): Duplicate Part of/fiscal host
    
    * chore(Hero): Remove unecessary data-cy
    
    Co-authored-by: Sudharaka Palamakumbura <sudharakap@mun.ca>
    
    Co-authored-by: Sudharaka Palamakumbura <sudharakap@mun.ca>

--- components/TruncatedTextWithTooltip.js
+++ components/TruncatedTextWithTooltip.js
@@ -9,13 +9,13 @@ import { Span } from './Text';
  * A tooltip that truncates a value if it's longer than the
  * provided length.
  */
-const TruncatedTextWithTooltip = ({ value, length = 30 }) => {
+const TruncatedTextWithTooltip = ({ value, cursor, length = 30 }) => {
   if (value.length <= length) {
     return value;
   } else {
     return (
       <React.Fragment>
-        <StyledTooltip content={() => <Span color="black.100">{value}</Span>}>
+        <StyledTooltip containerCursor={cursor} content={() => <Span color="black.100">{value}</Span>}>
           {truncate(value, { length })}
         </StyledTooltip>
       </React.Fragment>
@@ -26,6 +26,7 @@ const TruncatedTextWithTooltip = ({ value, length = 30 }) => {
 TruncatedTextWithTooltip.propTypes = {
   value: PropTypes.string.isRequired,
   length: PropTypes.number,
+  cursor: PropTypes.string,
 };
 
 export default TruncatedTextWithTooltip;
