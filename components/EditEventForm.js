33267965ff4ae21d87743cbb6e82d026d7f0f652
Benjamin Piouffle <benjamin@opencollective.com>
2021-10-21 11:43:29 +0200

    enhancement(EditTiers): Enable tier types for projects (#7032)

--- components/EditEventForm.js
+++ components/EditEventForm.js
@@ -222,12 +222,10 @@ class EditEventForm extends React.Component {
           {['e2e', 'ci'].includes(process.env.OC_ENV) && (
             <Tickets
               title="Tickets"
-              types={['TICKET']}
               tiers={this.state.tiers}
               collective={{ ...event, type: 'EVENT' }}
               currency={event.parentCollective.currency}
               onChange={tiers => this.setState({ tiers })}
-              defaultType="TICKET"
             />
           )}
         </Container>
