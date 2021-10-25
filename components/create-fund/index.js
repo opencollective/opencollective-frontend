e44aad6260bc9ef03388ccd210b8854002a474a6
Benjamin Piouffle <benjamin@opencollective.com>
2021-10-25 13:20:13 +0200

    fix(createFund): form + error handling

--- components/create-fund/index.js
+++ components/create-fund/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from '@apollo/client/react/hoc';
-import { omit } from 'lodash';
+import { omit, pick } from 'lodash';
 import { withRouter } from 'next/router';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl } from 'react-intl';
 
-import { getErrorFromGraphqlException } from '../../lib/errors';
+import { i18nGraphqlException } from '../../lib/errors';
 import { API_V2_CONTEXT, gqlV2 } from '../../lib/graphql/helpers';
 
 import { Box, Flex } from '../Grid';
@@ -19,6 +19,7 @@ import Form from './Form';
 class CreateFund extends Component {
   static propTypes = {
     host: PropTypes.object,
+    intl: PropTypes.object,
     LoggedInUser: PropTypes.object, // from withUser
     refetchLoggedInUser: PropTypes.func.isRequired, // from withUser
     router: PropTypes.object.isRequired, // from withRouter
@@ -65,7 +66,7 @@ class CreateFund extends Component {
     // try mutation
     try {
       const res = await this.props.createFund({
-        variables: { fund: omit(fund, ['tos', 'hostTos']), host: omit(host, ['termsUrl', 'name']) },
+        variables: { fund: omit(fund, ['tos', 'hostTos']), host: pick(host, ['slug']) },
       });
       await this.props.refetchLoggedInUser();
       this.props.router
@@ -77,7 +78,7 @@ class CreateFund extends Component {
         })
         .then(() => window.scrollTo(0, 0));
     } catch (err) {
-      const errorMsg = getErrorFromGraphqlException(err).message;
+      const errorMsg = i18nGraphqlException(this.props.intl, err);
       this.setState({ error: errorMsg, creating: false });
     }
   }
@@ -135,4 +136,4 @@ const addCreateFundMutation = graphql(createFundMutation, {
   options: { context: API_V2_CONTEXT },
 });
 
-export default withRouter(withUser(addCreateFundMutation(CreateFund)));
+export default withRouter(withUser(addCreateFundMutation(injectIntl(CreateFund))));
