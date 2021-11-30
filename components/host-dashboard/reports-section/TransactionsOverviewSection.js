dcd8f67a3800d6cfc4bf59355156ac0e743de6df
Sudharaka <sudharakap@mun.ca>
2021-11-30 11:09:50 -0800

    feat: make sure only the dates filtered are shown in the transactions graph

--- components/host-dashboard/reports-section/TransactionsOverviewSection.js
+++ components/host-dashboard/reports-section/TransactionsOverviewSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 import dynamic from 'next/dynamic';
 import { FormattedMessage, useIntl } from 'react-intl';
 
@@ -11,7 +12,7 @@ import ProportionalAreaChart from '../../ProportionalAreaChart';
 import { P, Span } from '../../Text';
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
-const getChartOptions = (intl, category) => {
+const getChartOptions = (intl, category, dateFrom, dateTo) => {
   return {
     chart: {
       id: 'chart-transactions-overview',
@@ -32,9 +33,12 @@ const getChartOptions = (intl, category) => {
     dataLabels: {
       enabled: false,
     },
+    markers: {
+      size: 4,
+    },
 
     xaxis: {
-      categories: getCategories(intl, category),
+      categories: getCategories(intl, category, dateFrom, dateTo),
     },
     yaxis: {
       labels: {
@@ -46,40 +50,46 @@ const getChartOptions = (intl, category) => {
   };
 };
 
-const getCategories = (intl, category) => {
+const getCategories = (intl, category, dateFrom, dateTo) => {
   if (category === 'WEEK') {
     return [...new Array(7)].map((_, idx) => intl.formatDate(new Date(0, 0, idx), { weekday: 'long' }).toUpperCase());
   } else if (category === 'MONTH') {
-    const currentMonth = new Date().getUTCMonth();
-    return [...new Array(12)].map((_, idx) =>
-      intl.formatDate(new Date(0, idx + currentMonth + 1), { month: 'short' }).toUpperCase(),
+    const currentMonth = dateTo.month();
+    const numberOfMonths = Math.round(dateTo.diff(dateFrom, 'month', true));
+    return [...new Array(numberOfMonths)].map((_, idx) =>
+      intl.formatDate(new Date(0, idx + currentMonth - numberOfMonths + 1), { month: 'short' }).toUpperCase(),
     );
   } else if (category === 'YEAR') {
-    return [...new Array(6)].map((_, idx) =>
-      intl.formatDate(new Date(new Date().getUTCFullYear() - 5 + idx, 0), { year: 'numeric' }).toUpperCase(),
+    const numberOfYears = Math.round(dateTo.diff(dateFrom, 'year', true));
+    return [...new Array(numberOfYears)].map((_, idx) =>
+      intl
+        .formatDate(new Date(new Date().getUTCFullYear() - numberOfYears + 1 + idx, 0), { year: 'numeric' })
+        .toUpperCase(),
     );
   }
 };
 
-const constructChartDataPoints = (category, dataPoints) => {
+const constructChartDataPoints = (category, dataPoints, dateFrom, dateTo) => {
   let chartDataPoints;
 
-  // Show data for the past 5 years from the current year.
+  // Show data aggregated yearly
   if (category === 'YEAR') {
-    chartDataPoints = new Array(6).fill(0);
+    const numberOfYears = Math.round(dateTo.diff(dateFrom, 'year', true));
+    chartDataPoints = new Array(numberOfYears).fill(0);
     const currentYear = new Date().getUTCFullYear();
     dataPoints.forEach(dataPoint => {
       const year = new Date(dataPoint.date).getUTCFullYear();
-      if (year > currentYear - 6) {
-        chartDataPoints[5 - (currentYear - year)] = Math.abs(dataPoint.amount.value);
+      if (year > currentYear - numberOfYears) {
+        chartDataPoints[numberOfYears - 1 - (currentYear - year)] = Math.abs(dataPoint.amount.value);
       }
     });
-    // Show data for the past 12 months
+    // Show data aggregated monthly
   } else if (category === 'MONTH') {
-    chartDataPoints = new Array(12).fill(0);
+    const numberOfMonths = Math.round(dateTo?.diff(dateFrom, 'month', true));
+    chartDataPoints = new Array(numberOfMonths).fill(0);
     dataPoints.forEach(dataPoint => {
-      const date = new Date(dataPoint.date);
-      chartDataPoints[(date.getUTCMonth() + 1) % 12] = Math.abs(dataPoint.amount.value);
+      const month = new Date(dataPoint.date).getUTCMonth();
+      chartDataPoints[(month + 1) % numberOfMonths] = Math.abs(dataPoint.amount.value);
     });
     // Show data for the past 7 days
   } else if (category === 'WEEK') {
@@ -227,9 +237,11 @@ const getTransactionsBreakdownChartData = host => {
   return areas;
 };
 
-const TransactionsOverviewSection = ({ host, isLoading }) => {
+const TransactionsOverviewSection = ({ host, isLoading, dateInterval }) => {
   const intl = useIntl();
   const { locale } = intl;
+  const dateFrom = dateInterval.from ? dayjs(dateInterval.from) : dayjs(host?.createdAt);
+  const dateTo = dateInterval.to ? dayjs(dateInterval.to) : dayjs();
 
   const contributionStats = host?.contributionStats;
   const expenseStats = host?.expenseStats;
@@ -242,12 +254,14 @@ const TransactionsOverviewSection = ({ host, isLoading }) => {
     {
       name: 'Contributions',
       data: contributionAmountOverTime
-        ? constructChartDataPoints(categoryType, contributionAmountOverTime.nodes)
+        ? constructChartDataPoints(categoryType, contributionAmountOverTime.nodes, dateFrom, dateTo)
         : null,
     },
     {
       name: 'Expenses',
-      data: expenseAmountOverTime ? constructChartDataPoints(categoryType, expenseAmountOverTime.nodes) : null,
+      data: expenseAmountOverTime
+        ? constructChartDataPoints(categoryType, expenseAmountOverTime.nodes, dateFrom, dateTo)
+        : null,
     },
   ];
 
@@ -273,7 +287,7 @@ const TransactionsOverviewSection = ({ host, isLoading }) => {
             type="area"
             width="100%"
             height="250px"
-            options={getChartOptions(intl, categoryType)}
+            options={getChartOptions(intl, categoryType, dateFrom, dateTo)}
             series={series}
           />
         )}
