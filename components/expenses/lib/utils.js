37506d6f357be5ac30b8cea4575101c5190f0345
Sudharaka <sudharakap@mun.ca>
2021-11-30 16:41:59 -0800

    fix: simplify the require address condition

--- components/expenses/lib/utils.js
+++ components/expenses/lib/utils.js
@@ -2,17 +2,12 @@ import { CollectiveType } from '../../../lib/collective-sections';
 import expenseTypes from '../../../lib/constants/expenseTypes';
 
 export const checkRequiresAddress = values => {
-  const collectiveTypesNotRequiringAddress = [
-    CollectiveType.COLLECTIVE,
-    CollectiveType.EVENT,
-    CollectiveType.PROJECT,
-    CollectiveType.FUND,
-  ];
+  const collectiveTypesRequiringAddress = [CollectiveType.INDIVIDUAL, CollectiveType.USER, CollectiveType.ORGANIZATION];
   const expenseTypesRequiringAddress = [expenseTypes.INVOICE, expenseTypes.FUNDING_REQUEST, expenseTypes.GRANT];
 
   return (
     values.payee &&
-    (!collectiveTypesNotRequiringAddress.includes(values.payee.type) || values.payee.isHost) &&
+    (collectiveTypesRequiringAddress.includes(values.payee.type) || values.payee.isHost) &&
     !values.payee.isInvite &&
     expenseTypesRequiringAddress.includes(values.type)
   );
