788fdc8d721ef8e62fef2bb815d8f72b3f7feb6a
flickz <omoyajowo2015@gmail.com>
2021-12-09 01:28:28 +0100

    add form mutation api

--- components/help-and-support/ContactSection.js
+++ components/help-and-support/ContactSection.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useMutation } from '@apollo/client';
+import { ArrowLeft2 } from '@styled-icons/icomoon/ArrowLeft2';
 import { ArrowRight2 } from '@styled-icons/icomoon/ArrowRight2';
 import { useFormik } from 'formik';
+import { useRouter } from 'next/router';
 import { FormattedMessage, useIntl } from 'react-intl';
 
-import { createError, ERROR } from '../../lib/errors';
+import { createError, ERROR, i18nGraphqlException } from '../../lib/errors';
 import { formatFormErrorMessage } from '../../lib/form-utils';
+import { API_V2_CONTEXT, gqlV2 } from '../../lib/graphql/helpers';
 import { isValidEmail } from '../../lib/utils';
 
 import Container from '../Container';
 import { Box, Flex } from '../Grid';
+import Link from '../Link';
+import MessageBox from '../MessageBox';
 import RichTextEditor from '../RichTextEditor';
 import StyledButton from '../StyledButton';
 import StyledCard from '../StyledCard';
 import StyledInput from '../StyledInput';
 import StyledInputField from '../StyledInputField';
 import { P, Span } from '../Text';
+import { useUser } from '../UserProvider';
+
+const supportMessageMutation = gqlV2/* GraphQL */ `
+  mutation sendMessage($name: String!, $email: String!, $topic: String!, $message: String!) {
+    sendMessage(name: $name, email: $email, topic: $topic, message: $message) {
+      sent
+    }
+  }
+`;
 
 const validate = values => {
   const errors = {};
@@ -43,7 +58,11 @@ const validate = values => {
 
 const ContactForm = () => {
   const intl = useIntl();
-  const { getFieldProps, handleSubmit, errors, touched } = useFormik({
+  const router = useRouter();
+  const { LoggedInUser } = useUser();
+  const [sendMessage, { error: submitError }] = useMutation(supportMessageMutation, { context: API_V2_CONTEXT });
+
+  const { getFieldProps, handleSubmit, errors, touched, setFieldValue } = useFormik({
     initialValues: {
       name: '',
       email: '',
@@ -52,63 +71,88 @@ const ContactForm = () => {
     },
     validate,
     onSubmit: async values => {
-      console.log(values);
+      const response = await sendMessage({ variables: values });
+      if (response.data.sendMessage) {
+        await router.push('/contact/success');
+      }
     },
   });
 
+  useEffect(() => {
+    if (LoggedInUser) {
+      setFieldValue('name', LoggedInUser.collective.name);
+      setFieldValue('email', LoggedInUser.email);
+    }
+  }, [LoggedInUser]);
+
   return (
     <Flex flexDirection="column" alignItems="center" justifyContent="center" px="16px" mt="48px" mb="120px">
-      <Container
-        width={[null, '597px', '901px']}
-        backgroundImage={[null, "url('/static/images/help-and-support/contactForm-illustrations.png')"]}
-        backgroundRepeat="no-repeat"
-        backgroundSize={[null, 'contain', 'cover']}
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-      >
+      <Container display="flex" justifyContent="center" alignItems="center" position="relative">
+        <Container
+          height="825px"
+          width="825px"
+          position="absolute"
+          backgroundImage={[null, "url('/static/images/help-and-support/contactForm-illustrations.png')"]}
+          backgroundRepeat="no-repeat"
+          backgroundSize={[null, 'contain', 'contain']}
+          display={['none', 'block']}
+          top="-200px"
+          left="-75px"
+        />
         <StyledCard
           padding={[null, '32px']}
           boxShadow={[null, '0px 0px 15px 10px rgba(13, 67, 97, 0.05)']}
           borderRadius={[null, '8px']}
           borderWidth="0"
           width={['288px', '510px']}
+          zIndex="999"
         >
+          {submitError && (
+            <Flex alignItems="center" justifyContent="center">
+              <MessageBox type="error" withIcon mb={[1, 3]}>
+                {i18nGraphqlException(intl, submitError)}
+              </MessageBox>
+            </Flex>
+          )}
           <form onSubmit={handleSubmit}>
-            <Box mb="28px">
-              <StyledInputField
-                label={<FormattedMessage defaultMessage="Your name" />}
-                labelFontWeight="700"
-                labelProps={{
-                  lineHeight: '24px',
-                  fontSize: '16px',
-                }}
-                {...getFieldProps('name')}
-                error={touched.name && formatFormErrorMessage(intl, errors.name)}
-              >
-                {inputProps => <StyledInput {...inputProps} placeholder="Enter your first name" width="100%" />}
-              </StyledInputField>
-            </Box>
-            <Box mb="28px">
-              <StyledInputField
-                label={<FormattedMessage defaultMessage="Your email" />}
-                labelFontWeight="700"
-                labelProps={{
-                  lineHeight: '24px',
-                  fontSize: '16px',
-                }}
-                {...getFieldProps('email')}
-                error={touched.email && formatFormErrorMessage(intl, errors.email)}
-                hint={
-                  <FormattedMessage
-                    id="helpAndSupport.email.description"
-                    defaultMessage="Enter the email ID used for the concerned issue"
-                  />
-                }
-              >
-                {inputProps => <StyledInput {...inputProps} placeholder="e.g. johndoe@gmail.com" width="100%" />}
-              </StyledInputField>
-            </Box>
+            {!LoggedInUser && (
+              <React.Fragment>
+                <Box mb="28px">
+                  <StyledInputField
+                    label={<FormattedMessage defaultMessage="Your name" />}
+                    labelFontWeight="700"
+                    labelProps={{
+                      lineHeight: '24px',
+                      fontSize: '16px',
+                    }}
+                    {...getFieldProps('name')}
+                    error={touched.name && formatFormErrorMessage(intl, errors.name)}
+                  >
+                    {inputProps => <StyledInput {...inputProps} placeholder="Enter your first name" width="100%" />}
+                  </StyledInputField>
+                </Box>
+                <Box mb="28px">
+                  <StyledInputField
+                    label={<FormattedMessage defaultMessage="Your email" />}
+                    labelFontWeight="700"
+                    labelProps={{
+                      lineHeight: '24px',
+                      fontSize: '16px',
+                    }}
+                    {...getFieldProps('email')}
+                    error={touched.email && formatFormErrorMessage(intl, errors.email)}
+                    hint={
+                      <FormattedMessage
+                        id="helpAndSupport.email.description"
+                        defaultMessage="Enter the email ID used for the concerned issue"
+                      />
+                    }
+                  >
+                    {inputProps => <StyledInput {...inputProps} placeholder="e.g. johndoe@gmail.com" width="100%" />}
+                  </StyledInputField>
+                </Box>
+              </React.Fragment>
+            )}
             <Box mb="28px">
               <StyledInputField
                 label={
@@ -141,7 +185,7 @@ const ContactForm = () => {
               <RichTextEditor
                 error={touched.message && formatFormErrorMessage(intl, errors.message)}
                 inputName="message"
-                {...getFieldProps('message')}
+                onChange={e => setFieldValue('message', e.target.value)}
                 withBorders
               />
               <P mt="6px" fontSize="13px" lineHeight="20px" color="black.700">
@@ -151,13 +195,33 @@ const ContactForm = () => {
                 />
               </P>
             </Box>
-            <Box display="flex" justifyContent={[null, 'flex-end']}>
-              <StyledButton width={['288px', '151px']} buttonStyle="dark">
+            <Box
+              display="flex"
+              flexDirection={['column', 'row-reverse']}
+              justifyContent={[null, 'space-between']}
+              alignItems="center"
+            >
+              <StyledButton width={['288px', '151px']} buttonSize="medium" buttonStyle="dark" mb={['24px', 0]}>
                 <FormattedMessage defaultMessage="Submit Issue" />
                 <Span ml={['10px', '5px']}>
                   <ArrowRight2 size="14px" />
                 </Span>
               </StyledButton>
+
+              <Link href="/help">
+                <StyledButton
+                  width={['288px', '151px']}
+                  buttonSize="medium"
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <ArrowLeft2 size="14px" />
+                  <Span ml={['10px', '5px']}>
+                    <FormattedMessage defaultMessage="Back to help" />
+                  </Span>
+                </StyledButton>
+              </Link>
             </Box>
           </form>
         </StyledCard>
