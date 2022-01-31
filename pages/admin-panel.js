6a2813c34e4b08b5966855609789045ea7639361
Benjamin Piouffle <benjamin@opencollective.com>
2022-01-31 10:31:29 +0100

    fix(AdminPanel): Fetch account avatar

--- pages/admin-panel.js
+++ pages/admin-panel.js
@@ -31,6 +31,7 @@ export const adminPanelQuery = gqlV2/* GraphQL */ `
       settings
       isArchived
       isIncognito
+      imageUrl(height: 256)
       features {
         ...NavbarFields
         VIRTUAL_CARDS
