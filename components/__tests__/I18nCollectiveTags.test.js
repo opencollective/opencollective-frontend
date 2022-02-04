e58385f55808f49a740ea7a0a3c0971b3091fda7
François Hodierne <francois@hodierne.net>
2022-02-04 10:23:06 +0100

    remove deprecated snapshotI18n

--- components/__tests__/I18nCollectiveTags.test.js
+++ components/__tests__/I18nCollectiveTags.test.js
@@ -2,27 +2,27 @@ import 'jest-styled-components';
 
 import React from 'react';
 
-import { snapshotI18n } from '../../test/snapshot-helpers';
+import { snapshot } from '../../test/snapshot-helpers';
 
 import I18nCollectiveTags from '../I18nCollectiveTags';
 
 describe('I18nCollectiveTags', () => {
   it('renders a single tag', () => {
-    snapshotI18n(<I18nCollectiveTags tags="open source" />);
-    snapshotI18n(<I18nCollectiveTags tags="not translated" />);
+    snapshot(<I18nCollectiveTags tags="open source" />);
+    snapshot(<I18nCollectiveTags tags="not translated" />);
   });
 
   it('renders a list of tag', () => {
-    snapshotI18n(<I18nCollectiveTags tags={['open source']} />);
-    snapshotI18n(<I18nCollectiveTags tags={['not translated', 'open source', 'Meetup']} />);
+    snapshot(<I18nCollectiveTags tags={['open source']} />);
+    snapshot(<I18nCollectiveTags tags={['not translated', 'open source', 'Meetup']} />);
   });
 
   it('can ignore untranslated', () => {
-    snapshotI18n(<I18nCollectiveTags tags={['not translated', 'open source', 'Meetup']} ignoreUntranslated />);
+    snapshot(<I18nCollectiveTags tags={['not translated', 'open source', 'Meetup']} ignoreUntranslated />);
   });
 
   it('can provide a custom renderer', () => {
-    snapshotI18n(
+    snapshot(
       <I18nCollectiveTags tags={['HelloWorld', 'Meetup']}>
         {tags =>
           tags.map(({ value, isTranslated }) => (
