opencollective/opencollective-frontend

commit 4d66c13c231a471e2646322fa03926f5667d926b

feat: Add Parent Collective Parts to Contribution Flow Related Urls (#7395)
* feat: add contribution flow related urls to support even/project url redirection

* fix: correct some issues identified in code review

* feat: correct some mistakes and refactor code as pointed out in code review

* fix: add eventSlug handling to the addParentToURLIfMissing function

* fix: add missing parent collective slug to the success page url

* graphql changes after merge

* graphql changes after merge

rewrites.js
@@ -207,11 +207,11 @@ exports.REWRITES = [
   },
   // New Routes -> New flow
   {
-    source: `/:collectiveSlug/:verb(donate)/:paymentFlow(crypto)?/:step(${contributionFlowSteps})?`,
+    source: `/:parentCollectiveSlug?/:collectiveType(events|projects)?/:collectiveSlug/:verb(donate)/:paymentFlow(crypto)?/:step(${contributionFlowSteps})?`,
     destination: createOrderPage,
   },
   {
-    source: `/:collectiveSlug/:verb(contribute)/:tierSlug?-:tierId([0-9]+)/checkout/:step(${contributionFlowSteps})?`,
+    source: `/:parentCollectiveSlug?/:collectiveType(events|projects)?/:collectiveSlug/:verb(contribute)/:tierSlug?-:tierId([0-9]+)/checkout/:step(${contributionFlowSteps})?`,
     destination: createOrderPage,
   },
   // Generic Route
@@ -227,11 +227,11 @@ exports.REWRITES = [
   },
   // Embed
   {
-    source: `/embed/:collectiveSlug/:verb(donate)/:paymentFlow(crypto)?/:step(${contributionFlowSteps})?`,
+    source: `/embed/:parentCollectiveSlug?/:collectiveType(events|projects)?/:collectiveSlug/:verb(donate)/:paymentFlow(crypto)?/:step(${contributionFlowSteps})?`,
     destination: '/embed/contribution-flow',
   },
   {
-    source: `/embed/:collectiveSlug/contribute/:tierSlug?-:tierId([0-9]+)/:step(${contributionFlowSteps})?`,
+    source: `/embed/:parentCollectiveSlug?/:collectiveType(events|projects)?/:collectiveSlug/contribute/:tierSlug?-:tierId([0-9]+)/:step(${contributionFlowSteps})?`,
     destination: '/embed/contribution-flow',
   },
   // Pledges

components/collective-navbar/index.js
@@ -332,7 +332,7 @@ const getMainAction = (collective, callsToAction, LoggedInUser) => {
     return {
       type: NAVBAR_ACTION_TYPE.REQUEST_GRANT,
       component: (
-        <Link href={`/${collective.slug}/expenses/new`}>
+        <Link href={`${getCollectivePageRoute(collective)}/expenses/new`}>
           <ActionButton tabIndex="-1">
             <MoneyCheckAlt size="1em" />
             <Span ml={2}>
@@ -346,7 +346,7 @@ const getMainAction = (collective, callsToAction, LoggedInUser) => {
     return {
       type: NAVBAR_ACTION_TYPE.SUBMIT_EXPENSE,
       component: (
-        <Link href={`/${collective.slug}/expenses/new`}>
+        <Link href={`${getCollectivePageRoute(collective)}/expenses/new`}>
           <ActionButton tabIndex="-1">
             <Receipt size="1em" />
             <Span ml={2}>
@@ -360,7 +360,7 @@ const getMainAction = (collective, callsToAction, LoggedInUser) => {
     return {
       type: NAVBAR_ACTION_TYPE.MANAGE_SUBSCRIPTIONS,
       component: (
-        <Link href={`/${collective.slug}/recurring-contributions`}>
+        <Link href={`${getCollectivePageRoute(collective)}/recurring-contributions`}>
           <ActionButton tabIndex="-1">
             <Stack size="1em" />
             <Span ml={2}>

components/contribute-cards/ContributeCollective.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { truncate } from 'lodash';
 
 import { ContributionTypes } from '../../lib/constants/contribution-types';
+import { getCollectivePageRoute } from '../../lib/url-helpers';
 
 import Contribute from './Contribute';
 
@@ -13,7 +14,7 @@ const ContributeCollective = ({ collective, ...props }) => {
   const description = truncate(collective.description, { length: 100 });
   return (
     <Contribute
-      route={`/${collective.slug}`}
+      route={getCollectivePageRoute(collective)}
       type={ContributionTypes.CHILD_COLLECTIVE}
       title={collective.name}
       contributors={collective.contributors}

components/contribute-cards/ContributeCrypto.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { defineMessages, injectIntl } from 'react-intl';
 
 import { ContributionTypes } from '../../lib/constants/contribution-types';
+import { getCollectivePageRoute } from '../../lib/url-helpers';
 
 import Contribute from './Contribute';
 
@@ -20,7 +21,7 @@ const messages = defineMessages({
 const ContributeCrypto = ({ intl, collective, contributors, stats, ...props }) => {
   return (
     <Contribute
-      route={`/${collective.slug}/donate/crypto`}
+      route={`${getCollectivePageRoute(collective)}/donate/crypto`}
       type={ContributionTypes.FINANCIAL_CRYPTO}
       title={intl.formatMessage(messages.title)}
       contributors={contributors}

components/contribute-cards/ContributeCustom.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { defineMessages, injectIntl } from 'react-intl';
 
 import { ContributionTypes } from '../../lib/constants/contribution-types';
+import { getCollectivePageRoute } from '../../lib/url-helpers';
 
 import Contribute from './Contribute';
 
@@ -20,7 +21,7 @@ const messages = defineMessages({
 const ContributeCustom = ({ intl, collective, contributors, stats, ...props }) => {
   return (
     <Contribute
-      route={`/${collective.slug}/donate`}
+      route={`${getCollectivePageRoute(collective)}/donate`}
       type={ContributionTypes.FINANCIAL_CUSTOM}
       title={intl.formatMessage(messages.title)}
       contributors={contributors}

components/contribute-cards/ContributeEvent.js
@@ -8,6 +8,7 @@ import { FormattedDate, FormattedMessage } from 'react-intl';
 import { ContributionTypes } from '../../lib/constants/contribution-types';
 import DayJs from '../../lib/dayjs';
 import { isPastEvent } from '../../lib/events';
+import { getCollectivePageRoute } from '../../lib/url-helpers';
 
 import Container from '../Container';
 import { Box } from '../Grid';
@@ -27,7 +28,7 @@ const ContributeEvent = ({ collective, event, ...props }) => {
 
   return (
     <Contribute
-      route={`/${collective.slug}/events/${event.slug}`}
+      route={`${getCollectivePageRoute(collective)}/events/${event.slug}`}
       type={isPassed ? ContributionTypes.EVENT_PASSED : ContributionTypes.EVENT_PARTICIPATE}
       contributors={event.contributors}
       stats={event.stats.backers}

components/contribute-cards/ContributeProject.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { truncate } from 'lodash';
 
 import { ContributionTypes } from '../../lib/constants/contribution-types';
+import { getCollectivePageRoute } from '../../lib/url-helpers';
 
 import Link from '../Link';
 import StyledLink from '../StyledLink';
@@ -16,7 +17,7 @@ const ContributeProject = ({ collective, project, ...props }) => {
   const description = truncate(project.description, { length: 100 });
   return (
     <Contribute
-      route={`/${collective.slug}/projects/${project.slug}`}
+      route={`${getCollectivePageRoute(collective)}/projects/${project.slug}`}
       type={project.isArchived ? ContributionTypes.ARCHIVED_PROJECT : ContributionTypes.PROJECT}
       contributors={project.contributors}
       stats={project.stats.backers}

components/contribute-cards/ContributeTier.js
@@ -9,6 +9,7 @@ import { TierTypes } from '../../lib/constants/tiers-types';
 import { formatCurrency, getPrecisionFromAmount } from '../../lib/currency-utils';
 import { isPastEvent } from '../../lib/events';
 import { isTierExpired } from '../../lib/tier-utils';
+import { getCollectivePageRoute } from '../../lib/url-helpers';
 import { capitalize } from '../../lib/utils';
 
 import CollapsableText from '../CollapsableText';
@@ -112,9 +113,9 @@ const ContributeTier = ({ intl, collective, tier, ...props }) => {
 
   let route;
   if (tierType === ContributionTypes.TICKET) {
-    route = `/${collective.parentCollective?.slug || 'collective'}/events/${collective.slug}/order/${tier.id}`;
+    route = `${getCollectivePageRoute(collective)}/order/${tier.id}`;
   } else {
-    route = `/${collective.slug}/contribute/${tier.slug}-${tier.id}/checkout`;
+    route = `${getCollectivePageRoute(collective)}/contribute/${tier.slug}-${tier.id}/checkout`;
   }
 
   return (
@@ -150,7 +151,7 @@ const ContributeTier = ({ intl, collective, tier, ...props }) => {
                 <StyledLink
                   as={Link}
                   whiteSpace="nowrap"
-                  href={`/${collective.slug}/contribute/${tier.slug}-${tier.id}`}
+                  href={`${getCollectivePageRoute(collective)}/contribute/${tier.slug}-${tier.id}`}
                 >
                   <FormattedMessage id="ContributeCard.ReadMore" defaultMessage="Read more" />
                 </StyledLink>

commit 72c07a5a3e0e4d1e6cf50a934e037f91a2c370cd

fix embed contribution flow
broken in https://github.com/opencollective/opencollective-frontend/pull/7395

rewrites.js
@@ -193,6 +193,15 @@ exports.REWRITES = [
     source: '/:collectiveSlug/conversations/:slug?-:id([a-z0-9]+)',
     destination: '/conversation',
   },
+  // Embed
+  {
+    source: `/embed/:parentCollectiveSlug?/:collectiveType(events|projects)?/:collectiveSlug/:verb(donate)/:paymentFlow(crypto)?/:step(${contributionFlowSteps})?`,
+    destination: '/embed/contribution-flow',
+  },
+  {
+    source: `/embed/:parentCollectiveSlug?/:collectiveType(events|projects)?/:collectiveSlug/contribute/:tierSlug?-:tierId([0-9]+)/:step(${contributionFlowSteps})?`,
+    destination: '/embed/contribution-flow',
+  },
   // Contribute Flow
   // ---------------
   // Legacy create order route. Deprectated on 2019-02-12
@@ -224,15 +233,6 @@ exports.REWRITES = [
   {
     source: `/:collectiveSlug/:verb(events|projects)/:eventSlug/order/:tierId/:step(${contributionFlowSteps})?`,
     destination: createOrderPage,
-  },
-  // Embed
-  {
-    source: `/embed/:parentCollectiveSlug?/:collectiveType(events|projects)?/:collectiveSlug/:verb(donate)/:paymentFlow(crypto)?/:step(${contributionFlowSteps})?`,
-    destination: '/embed/contribution-flow',
-  },
-  {
-    source: `/embed/:parentCollectiveSlug?/:collectiveType(events|projects)?/:collectiveSlug/contribute/:tierSlug?-:tierId([0-9]+)/:step(${contributionFlowSteps})?`,
-    destination: '/embed/contribution-flow',
   },
   // Pledges
   {