5c88042b5a14660e67b18b64af292acf39532ec9
Sudharaka <sudharakap@mun.ca>
2022-02-22 12:23:52 -0800

    feat: add funds to the collective picker in host dashboards

--- components/CollectivePickerAsync.js
+++ components/CollectivePickerAsync.js
@@ -57,6 +57,10 @@ const Messages = defineMessages({
     id: 'SearchFor3',
     defaultMessage: 'Search for {entity1}, {entity2} or {entity3}',
   },
+  searchForType_4: {
+    id: 'SearchFor4',
+    defaultMessage: 'Search for {entity1}, {entity2}, {entity3} or {entity4}',
+  },
   search: {
     id: 'Search',
     defaultMessage: 'Search',
@@ -69,7 +73,7 @@ const Messages = defineMessages({
  */
 const getPlaceholder = (intl, types) => {
   const nbTypes = types ? types.length : 0;
-  if (nbTypes === 0 || nbTypes > 3) {
+  if (nbTypes === 0 || nbTypes > 4) {
     return intl.formatMessage(Messages.search);
   } else if (nbTypes === 1) {
     return intl.formatMessage(Messages.searchForType, { entity: formatCollectiveType(intl, types[0], 100) });
