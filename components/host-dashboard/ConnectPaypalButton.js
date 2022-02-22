a7f5e9f0d9193f1b8c8042f907c2bb6af873ddae
Benjamin Piouffle <benjamin@opencollective.com>
2022-02-22 16:15:22 +0100

    enhancement(PaypalButton): Error message display for preapproval

--- components/host-dashboard/ConnectPaypalButton.js
+++ components/host-dashboard/ConnectPaypalButton.js
@@ -5,7 +5,7 @@ import { FormattedMessage } from 'react-intl';
 import { connectAccount } from '../../lib/api';
 import { useAsyncCall } from '../../lib/hooks/useAsyncCall';
 
-import Container from '../Container';
+import MessageBox from '../MessageBox';
 import StyledButton from '../StyledButton';
 
 /**
@@ -23,9 +23,9 @@ const ConnectPaypalButton = ({ host, paymentMethod }) => {
   return (
     <React.Fragment>
       {error && (
-        <Container color="red.500" fontSize="10px">
+        <MessageBox withIcon type="error" fontSize="10px" mb={2}>
           {error.message || 'Oops, something went wrong. Please try again.'}
-        </Container>
+        </MessageBox>
       )}
       <StyledButton
         buttonStyle="secondary"
