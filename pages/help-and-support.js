84220f4146e5bd3915912c4be4336ebfad5b2d5f
Benjamin Piouffle <benjamin@opencollective.com>
2022-04-07 14:10:59 +0200

    refact: Fix typo in filename ContactUsSucess -> ContactUsSuccess

--- pages/help-and-support.js
+++ pages/help-and-support.js
@@ -5,7 +5,7 @@ import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 import { Flex } from '../components/Grid';
 import BrowseTopics from '../components/help-and-support/BrowseTopicSection';
 import ContactForm from '../components/help-and-support/ContactSection';
-import ContactUsSuccess from '../components/help-and-support/ContactUsSucess';
+import ContactUsSuccess from '../components/help-and-support/ContactUsSuccess';
 import HowCanWeHelp from '../components/help-and-support/HowCanWeHelpSection';
 import HowOCWorks from '../components/help-and-support/HowOCWorksSection';
 import NeedHelp from '../components/help-and-support/NeedHelpSection';
