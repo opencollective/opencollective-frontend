787f1675cead7c6a8427122be16bfe4813340af9
Benjamin Piouffle <benjamin@opencollective.com>
2022-05-25 09:55:18 +0200

    enhancement(AuthorizedApps): redirect pagination if invalid page

--- components/admin-panel/sections/AuthorizedApps.js
+++ components/admin-panel/sections/AuthorizedApps.js
@@ -23,6 +23,18 @@ const AuthorizedAppsSection = () => {
   const variables = { limit: 10, offset: query.offset ? parseInt(query.offset) : 0 };
   const { data, loading, error, refetch } = useQuery(authorizedAppsQuery, { variables, context: API_V2_CONTEXT });
   const { LoggedInUser } = useUser();
+  const authorizations = data?.loggedInAccount?.oAuthAuthorizations;
+
+  // Redirect to previous page when removing the last item of a page
+  React.useEffect(() => {
+    if (variables.offset && variables.offset >= authorizations?.totalCount) {
+      const pathname = router.asPath.split('?')[0];
+      const offset = Math.max(0, variables.offset - variables.limit);
+      router.push({ pathname, query: { offset, limit: variables.limit } });
+      refetch();
+    }
+  }, [authorizations?.totalCount, variables.offset]);
+
   return loading ? (
     <LoadingPlaceholder height={300} />
   ) : error ? (
@@ -44,23 +56,23 @@ const AuthorizedAppsSection = () => {
     </div>
   ) : (
     <Box mt={3}>
-      {data.loggedInAccount.oAuthAuthorizations.nodes.map((authorization, index) => (
+      {authorizations.nodes.map((authorization, index) => (
         <React.Fragment key={authorization.id}>
           <AuthorizedApp authorization={authorization} onRevoke={refetch} />
-          {index !== data.loggedInAccount.oAuthAuthorizations.nodes.length - 1 && (
-            <StyledHr my={4} borderColor="black.300" />
-          )}
+          {index !== authorizations.nodes.length - 1 && <StyledHr my={4} borderColor="black.300" />}
         </React.Fragment>
       ))}
-      <Flex mt={5} justifyContent="center">
-        <Pagination
-          total={data.loggedInAccount.oAuthAuthorizations?.totalCount}
-          limit={variables.limit}
-          offset={variables.offset}
-          ignoredQueryParams={['slug', 'section']}
-          scrollToTopOnChange
-        />
-      </Flex>
+      {authorizations.totalCount > variables.limit && (
+        <Flex mt={5} justifyContent="center">
+          <Pagination
+            total={authorizations.totalCount}
+            limit={variables.limit}
+            offset={variables.offset}
+            ignoredQueryParams={['slug', 'section']}
+            scrollToTopOnChange
+          />
+        </Flex>
+      )}
     </Box>
   );
 };
