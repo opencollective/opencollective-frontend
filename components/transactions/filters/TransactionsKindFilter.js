aa10649c1af46bc885f6d3ca4507c2b8660a3086
KT <ka14406@protonmail.com>
2022-06-29 18:02:47 -0400

    feat: Add host fee to default transaction filters (#5549)

--- components/transactions/filters/TransactionsKindFilter.js
+++ components/transactions/filters/TransactionsKindFilter.js
@@ -20,6 +20,7 @@ export const getDefaultKinds = () => {
     TransactionKind.CONTRIBUTION,
     TransactionKind.EXPENSE,
     TransactionKind.PLATFORM_TIP,
+    TransactionKind.HOST_FEE,
   ];
 };
 
