2708d92bc8d619f126458ebfd5ed8c124e2163b9
Nitin Kumar <snitin315@gmail.com>
2022-07-09 15:47:29 +0530

    refactor: apply suggestion

--- components/collective-page/sections/Location.js
+++ components/collective-page/sections/Location.js
@@ -48,18 +48,9 @@ const Location = ({ collective: event, refetch }) => {
             <P fontWeight="bold" fontSize="18px">
               <FormattedMessage id="event.privateInstructions.label" defaultMessage="Private instructions" />
             </P>
-            <pre
-              style={{
-                whiteSpace: 'pre-wrap',
-                fontSize: '14px',
-                marginTop: 3,
-                background: 'transparent',
-                border: 'none',
-                fontFamily: "'Inter', sans-serif",
-              }}
-            >
+            <P mt={3} fontSize="14px" whiteSpace="pre-wrap">
               {event.privateInstructions}
-            </pre>
+            </P>
           </Container>
         )}
       </ContainerSectionContent>
