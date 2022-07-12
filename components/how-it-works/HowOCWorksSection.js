80571e24233e0eff61a524abab2619b58c063b07
flickz <omoyajowo2015@gmail.com>
2022-07-12 23:08:02 +0200

    chore: update how-it-works page

--- components/how-it-works/HowOCWorksSection.js
+++ components/how-it-works/HowOCWorksSection.js
@@ -5,12 +5,23 @@ import NextIllustration from '../collectives/HomeNextIllustration';
 import Container from '../Container';
 import { Box, Flex } from '../Grid';
 import Link from '../Link';
+import { MainTitle } from '../marketing/Text';
 import StyledButton from '../StyledButton';
-import { H1, P } from '../Text';
+import { P } from '../Text';
 
 const HowOCWorks = () => {
   return (
-    <Flex flexDirection={['column', 'row-reverse']} justifyContent="center" alignItems="center" px="16px" mt={4}>
+    <Flex flexDirection={['column', null, 'row-reverse']} justifyContent="center" alignItems="center" px="16px" mt={4}>
+      <Box
+        mt={[3, 0]}
+        mb={[3, null, null, null, '24px']}
+        width={['288px', '676px', '413px', null, '555px']}
+        display={[null, null, 'none']}
+      >
+        <MainTitle textAlign={['center', null, 'left']} whiteSpace={[null, null, 'pre-line']}>
+          <FormattedMessage id="OC.howItWorks" defaultMessage="How Open Collective works" />
+        </MainTitle>
+      </Box>
       <Box ml={[null, '6px', '40px', null, '84px']} width={['288px', '306px', '458px', null, '558px']}>
         <NextIllustration
           alt="How Open Collective works"
@@ -19,20 +30,18 @@ const HowOCWorks = () => {
           height={462}
         />
       </Box>
-      <Container display="flex" flexDirection="column" alignItems={['center', 'flex-start']}>
-        <Box mt={[3, 0]} mb={[3, null, null, null, '24px']} width={['288px', '344px', '413px', null, '555px']}>
-          <H1
-            letterSpacing="-0.04em"
-            fontSize={['32px', '40px', null, null, '52px']}
-            lineHeight={['40px', '48px', null, null, '56px']}
-            textAlign={['center', 'left']}
-            color="black.900"
-            whiteSpace={[null, null, 'pre-line']}
-          >
+      <Container display="flex" flexDirection="column" alignItems={['center', null, 'flex-start']}>
+        <Box
+          mt={[3, 0]}
+          mb={[3, null, null, null, '24px']}
+          width={['288px', '344px', '413px', null, '555px']}
+          display={['none', null, 'block']}
+        >
+          <MainTitle textAlign={['center', null, 'left']} whiteSpace={[null, null, 'pre-line']}>
             <FormattedMessage id="OC.howItWorks" defaultMessage="How Open Collective works" />
-          </H1>
+          </MainTitle>
         </Box>
-        <Box width={['288px', '344px', '458px', null, '558px']}>
+        <Box width={['288px', '676px', '458px', null, '558px']}>
           <P
             fontSize={['15px', '18px']}
             lineHeight={['22px', '26px']}
@@ -47,7 +56,7 @@ const HowOCWorks = () => {
           </P>
         </Box>
         <Link href="/create">
-          <StyledButton minWidth={158} mt={['56px', '24px']} buttonStyle="dark" whiteSpace="nowrap">
+          <StyledButton minWidth={158} mt={['56px', '24px']} buttonStyle="marketing" whiteSpace="nowrap">
             <FormattedMessage id="home.create" defaultMessage="Create a Collective" />
           </StyledButton>
         </Link>
