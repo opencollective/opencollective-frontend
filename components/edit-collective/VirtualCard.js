60a816eb5067a97fa2a3ef07af7dba50278e9b7d
Benjamin Piouffle <benjamin@opencollective.com>
2022-07-25 09:31:20 +0200

    debt(VirtualCards): remove expireDate (#8043)

--- components/edit-collective/VirtualCard.js
+++ components/edit-collective/VirtualCard.js
@@ -320,19 +320,9 @@ const VirtualCard = props => {
             <Flex>
               <Box mt="19px" mr={4}>
                 <P fontSize="18px" fontWeight="700" lineHeight="26px">
-                  {
-                    // expireDate should be removed once https://github.com/opencollective/opencollective-api/pull/7307 is deployed to production
-                    props.privateData.expireDate || props.privateData.expiryDate
-                  }
+                  {props.privateData.expiryDate}
 
-                  <Action
-                    color="black"
-                    ml={2}
-                    onClick={
-                      // expireDate should be removed once https://github.com/opencollective/opencollective-api/pull/7307 is deployed to production
-                      handleCopy(props.privateData.expireDate || props.privateData.expiryDate)
-                    }
-                  >
+                  <Action color="black" ml={2} onClick={handleCopy(props.privateData.expiryDate)}>
                     <Copy size="18px" />
                   </Action>
                 </P>
