060536eb663200710b3b049702db966a73da18f5
Benjamin Piouffle <benjamin@opencollective.com>
2022-08-08 17:33:46 +0200

    Revert "fix: adjust spacings in navbar (#4683) (#8020)" (#8079)
    This reverts commit 1ab0a4524cad5f0e7142ffda7c248da7108a6ae7.

--- components/collective-navbar/index.js
+++ components/collective-navbar/index.js
@@ -147,13 +147,6 @@ const CollectiveName = styled(LinkCollective).attrs({
   }
 `;
 
-const RightContainer = styled(Container)`
-  flex-shrink: 0;
-  @media screen and (min-width: 64em) {
-    width: 100%;
-  }
-`;
-
 const CategoriesContainer = styled(Container)`
   background-color: #ffffff;
   max-height: calc(100vh - 70px);
@@ -460,7 +453,7 @@ const CollectiveNavbar = ({
         minHeight={NAVBAR_HEIGHT}
       >
         {/** Collective info */}
-        <InfosContainer px={[3, 0]} py={[2, 1]} flexGrow="1">
+        <InfosContainer px={[3, 0]} py={[2, 1]}>
           <Flex alignItems="center" maxWidth={['90%', '100%']} flex="1 1">
             <BackButtonAndAvatar data-hide-on-desktop={isInHero}>
               {showBackButton && (
@@ -524,9 +517,10 @@ const CollectiveNavbar = ({
         {/** Main navbar items */}
 
         {!onlyInfos && (
-          <RightContainer
+          <Container
             overflowY="auto"
             display={['block', 'flex']}
+            width="100%"
             justifyContent="space-between"
             flexDirection={['column', 'row']}
           >
@@ -560,8 +554,8 @@ const CollectiveNavbar = ({
               display={isExpanded ? 'flex' : ['none', 'flex']}
               flexDirection={['column', 'row']}
               flexBasis="fit-content"
+              marginLeft={[0, 'auto']}
               backgroundColor="#fff"
-              flexShrink="0"
               zIndex={1}
             >
               {mainAction && (
@@ -593,7 +587,7 @@ const CollectiveNavbar = ({
                 </Container>
               )}
             </Container>
-          </RightContainer>
+          </Container>
         )}
       </NavbarContentContainer>
     </NavBarContainer>
