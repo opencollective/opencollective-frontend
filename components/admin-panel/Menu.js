6439cd3f324985d645af8631bef9f87236e95b20
Henrique <hdiniz@users.noreply.github.com>
2022-09-13 07:20:40 -0300

    Feat: add tier revamped section to org menu

--- components/admin-panel/Menu.js
+++ components/admin-panel/Menu.js
@@ -44,6 +44,11 @@ const OrganizationSettingsMenuLinks = ({ collective, isAccountantOnly }) => {
       {!isAccountantOnly && (
         <React.Fragment>
           <MenuLink collective={collective} section={ORG_BUDGET_SECTIONS.TIERS} />
+          <MenuLink
+            collective={collective}
+            section={COLLECTIVE_SECTIONS.TIERS_REVAMP}
+            if={['development', 'staging'].includes(process.env.OC_ENV)}
+          />
           <MenuLink collective={collective} section={ORG_BUDGET_SECTIONS.GIFT_CARDS} />
           <MenuLink collective={collective} section={ALL_SECTIONS.WEBHOOKS} />
           <MenuLink
