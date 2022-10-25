0ed0d5e8b38a752d59d8eea13b928bc4703d231f
Henrique <hdiniz@users.noreply.github.com>
2022-10-25 12:57:39 -0300

    Add typings to StyledHr

--- components/StyledHr.tsx
+++ components/StyledHr.tsx
@@ -1,12 +1,27 @@
 import propTypes from '@styled-system/prop-types';
 import { themeGet } from '@styled-system/theme-get';
 import styled from 'styled-components';
-import { border, display, flex, layout, shadow, space } from 'styled-system';
+import {
+  border,
+  BorderProps,
+  display,
+  DisplayProps,
+  flex,
+  FlexboxProps,
+  layout,
+  LayoutProps,
+  shadow,
+  ShadowProps,
+  space,
+  SpaceProps,
+} from 'styled-system';
+
+type StyledHrProps = SpaceProps & FlexboxProps & LayoutProps & ShadowProps & BorderProps & DisplayProps;
 
 /**
  * An horizontal line. Control the color and size using border properties.
  */
-const StyledHr = styled.hr`
+const StyledHr = styled.hr<StyledHrProps>`
   border: 0;
   border-top: 1px solid ${themeGet('colors.black.400')};
   margin: 0;
