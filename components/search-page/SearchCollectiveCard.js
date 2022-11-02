802feffc14c23a20e3f78d4bd7b9b726c44ff13f
Sudharaka <sudharakap@mun.ca>
2022-11-02 11:30:21 -0700

    fix: change money spent to contributed

--- components/search-page/SearchCollectiveCard.js
+++ components/search-page/SearchCollectiveCard.js
@@ -102,7 +102,7 @@ const SearchCollectiveCard = ({ collective, ...props }) => {
                     </Span>
                     {` `}
                     <Span fontSize="12px" fontWeight={400} color="black.700">
-                      <FormattedMessage defaultMessage="Money Spent" />
+                      <FormattedMessage defaultMessage="Contributed" />
                     </Span>
                   </Box>
                 )}
