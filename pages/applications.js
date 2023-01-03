72521aa5269fba4b68050466c915c12cbfea0cc3
flickz <omoyajowo2015@gmail.com>
2023-01-03 16:10:18 +0100

    chore: update application page with link to personal tokens

--- pages/applications.js
+++ pages/applications.js
@@ -1,57 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Mutation, Query } from '@apollo/client/react/components';
-import { cloneDeep, get, update } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 
-import { gqlV1 } from '../lib/graphql/helpers';
-
 import AuthenticatedPage from '../components/AuthenticatedPage';
-import Container from '../components/Container';
-import { Box, Flex } from '../components/Grid';
-import Loading from '../components/Loading';
+import { Flex } from '../components/Grid';
 import MessageBox from '../components/MessageBox';
 import StyledButton from '../components/StyledButton';
-import StyledCard from '../components/StyledCard';
-import StyledHr from '../components/StyledHr';
 import StyledLink from '../components/StyledLink';
-
-const loggedInUserApplicationsQuery = gqlV1/* GraphQL */ `
-  query LoggedInUserApplications {
-    LoggedInUser {
-      id
-      collective {
-        id
-        ... on User {
-          id
-          applications {
-            id
-            type
-            apiKey
-          }
-        }
-      }
-    }
-  }
-`;
-
-const createApplicationMutation = gqlV1/* GraphQL */ `
-  mutation CreateApplication($application: ApplicationInput!) {
-    createApplication(application: $application) {
-      id
-      type
-      apiKey
-    }
-  }
-`;
-
-const deleteApplicationMutation = gqlV1/* GraphQL */ `
-  mutation DeleteApplication($id: Int!) {
-    deleteApplication(id: $id) {
-      id
-    }
-  }
-`;
 
 class Apps extends React.Component {
   static propTypes = {
@@ -62,125 +17,17 @@ class Apps extends React.Component {
   render() {
     return (
       <AuthenticatedPage title="Applications">
-        {() => (
-          <Query query={loggedInUserApplicationsQuery}>
-            {({ data, loading }) => {
-              if (loading) {
-                return (
-                  <Flex justifyContent="center" my={6}>
-                    <Loading />
-                  </Flex>
-                );
-              }
-
-              const applications = get(data, 'LoggedInUser.collective.applications', []);
-              const apiKeys = applications.filter(app => app.type === 'API_KEY');
-
-              return (
-                <Container maxWidth="80%" m="40px auto">
-                  <h3>
-                    <FormattedMessage id="applications.ApiKeys" defaultMessage="API Keys" />
-                  </h3>
-
-                  <p>
-                    <FormattedMessage
-                      id="applications.ApiKeys.description"
-                      defaultMessage="Use API Keys to interact with the Open Collective GraphQL API with your own account."
-                    />
-                  </p>
-
-                  {(!apiKeys || apiKeys.length === 0) && (
-                    <MessageBox type="info" withIcon>
-                      <FormattedMessage id="applications.ApiKeys.none" defaultMessage="No API Key registered." />
-                    </MessageBox>
-                  )}
-
-                  {apiKeys && apiKeys.length > 0 && (
-                    <Mutation
-                      mutation={deleteApplicationMutation}
-                      update={(cache, { data: { deleteApplication } }) => {
-                        const { LoggedInUser } = cache.readQuery({ query: loggedInUserApplicationsQuery });
-                        const updatedUser = cloneDeep(LoggedInUser);
-
-                        update(updatedUser, 'collective.applications', applications => {
-                          return applications ? applications.filter(a => a.id !== deleteApplication.id) : [];
-                        });
-
-                        cache.writeQuery({
-                          query: loggedInUserApplicationsQuery,
-                          data: { LoggedInUser: updatedUser },
-                        });
-                      }}
-                    >
-                      {(deleteApplication, { loading, error }) => (
-                        <Box>
-                          {error && (
-                            <MessageBox type="error" withIcon mb={3}>
-                              {error.message}
-                            </MessageBox>
-                          )}
-                          {apiKeys.map(application => (
-                            <StyledCard m="20px 0" p={10} key={application.id} data-cy="api-key">
-                              <div className="keys">
-                                <FormattedMessage
-                                  id="applications.ApiKeys.code"
-                                  defaultMessage="API Key: {code}"
-                                  values={{ code: <code>{application.apiKey}</code> }}
-                                />
-                                &nbsp; - &nbsp;
-                                <StyledLink
-                                  disabled={loading}
-                                  onClick={() => deleteApplication({ variables: { id: application.id } })}
-                                >
-                                  <FormattedMessage id="actions.delete" defaultMessage="Delete" />
-                                </StyledLink>
-                              </div>
-                            </StyledCard>
-                          ))}
-                        </Box>
-                      )}
-                    </Mutation>
-                  )}
-
-                  <StyledHr my={3} />
-                  <Mutation
-                    mutation={createApplicationMutation}
-                    update={(cache, { data: { createApplication } }) => {
-                      const { LoggedInUser } = cache.readQuery({ query: loggedInUserApplicationsQuery });
-                      const updatedUser = cloneDeep(LoggedInUser);
-
-                      update(updatedUser, 'collective.applications', applications => {
-                        return applications ? [...applications, createApplication] : [createApplication];
-                      }),
-                        cache.writeQuery({
-                          query: loggedInUserApplicationsQuery,
-                          data: {
-                            LoggedInUser: updatedUser,
-                          },
-                        });
-                    }}
-                  >
-                    {(createApplication, { loading, error }) => (
-                      <Box>
-                        {error && (
-                          <MessageBox type="error" withIcon mb={3}>
-                            {error.message}
-                          </MessageBox>
-                        )}
-                        <StyledButton
-                          buttonStyle="primary"
-                          loading={loading}
-                          onClick={() => createApplication({ variables: { application: { type: 'API_KEY' } } })}
-                        >
-                          <FormattedMessage id="applications.ApiKeys.new" defaultMessage="New API Key" />
-                        </StyledButton>
-                      </Box>
-                    )}
-                  </Mutation>
-                </Container>
-              );
-            }}
-          </Query>
+        {LoggedInUser => (
+          <Flex flexDirection="column" alignItems="center" justifyContent="center" width={1} my={5}>
+            <MessageBox type="info" withIcon>
+              <FormattedMessage defaultMessage="Application has been deprecated in favour of personal token" />
+            </MessageBox>
+            <StyledLink href={`/${LoggedInUser.collective.slug}/admin/for-developers`}>
+              <StyledButton buttonStyle="primary" buttonSize="medium" mt={3}>
+                <FormattedMessage defaultMessage="View personal tokens" />
+              </StyledButton>
+            </StyledLink>
+          </Flex>
         )}
       </AuthenticatedPage>
     );
