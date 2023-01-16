eb6813d840889df39a92d2127951eff7c52ef9f6
François Hodierne <francois@hodierne.net>
2023-01-16 10:38:52 +0100

    update storybook to webpack5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
 module.exports = {
   stories: ['../stories/index.stories.mdx', '../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-docs', '@storybook/addon-essentials', 'storybook-addon-designs'],
@@ -8,7 +10,10 @@ module.exports = {
       include: /node_modules/,
       type: 'javascript/auto',
     });
-
+    config.plugins.push(new NodePolyfillPlugin());
     return config;
+  },
+  core: {
+    builder: 'webpack5',
   },
 };
