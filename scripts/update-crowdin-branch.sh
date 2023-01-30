1b0110d586ed7bad5cd217fdbda81dbbd01c76af
opencollective-robot <56958728+opencollective-robot@users.noreply.github.com>
2023-01-30 11:42:06 +0100

    New Crowdin updates (#8492)
    * i18n: Update translations
    * i18n: Update translation progress
    * i18n: Improve crowdin script
    
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>

--- scripts/update-crowdin-branch.sh
+++ scripts/update-crowdin-branch.sh
@@ -4,9 +4,10 @@
 CROWDIN_BRANCH=i18n/crowdin
 
 git branch -D $CROWDIN_BRANCH
-git pull \
-  && git checkout $CROWDIN_BRANCH \
-  && git merge -Xours main \
-  && npm run build:langs \
-  && git status \
-  && echo "Eventually commit the changes above, and if everything looks ok, run 'git push origin $CROWDIN_BRANCH'"
+git pull &&
+  git checkout $CROWDIN_BRANCH &&
+  git merge -Xours main &&
+  npm run build:langs &&
+  langs:update-progress &&
+  git status &&
+  echo "Eventually commit the changes above, and if everything looks ok, run 'git push origin $CROWDIN_BRANCH'"
