5608495bad1d5a99acc1e3b25b4a89213a3cb05e
François Hodierne <francois@hodierne.net>
2023-02-02 15:49:43 +0100

    wip: finalized reset password

--- pages/reset-password.js
+++ pages/reset-password.js
@@ -6,9 +6,10 @@ import dynamic from 'next/dynamic';
 import { withRouter } from 'next/router';
 import { FormattedMessage, injectIntl } from 'react-intl';
 
-import { API_V2_CONTEXT } from '../lib/graphql/helpers';
 import { i18nGraphqlException } from '../lib/errors';
+import { API_V2_CONTEXT } from '../lib/graphql/helpers';
 
+import Avatar from '../components/Avatar';
 import Body from '../components/Body';
 import Container from '../components/Container';
 import { Box, Flex } from '../components/Grid';
@@ -18,6 +19,7 @@ import MessageBox from '../components/MessageBox';
 import StyledButton from '../components/StyledButton';
 import StyledInput from '../components/StyledInput';
 import StyledInputField from '../components/StyledInputField';
+import { P } from '../components/Text';
 
 // Dynamic imports
 const PasswordStrengthBar = dynamic(() => import('react-password-strength-bar'));
@@ -34,8 +36,11 @@ class ResetPasswordPage extends React.Component {
     router: PropTypes.object.isRequired,
     /* From injectIntl */
     intl: PropTypes.object.isRequired,
-    /* From withRouter */
+    /* From addResetPasswordMutation */
     resetPassword: PropTypes.func,
+
+    /* From addResetPasswordAccountQuery */
+    data: PropTypes.object,
   };
 
   constructor(props) {
@@ -109,85 +114,116 @@ class ResetPasswordPage extends React.Component {
                   <FormattedMessage defaultMessage="Enter your new password" />
                 </Flex>
 
-                <Container
-                  as="form"
-                  method="POST"
-                  noValidate
-                  data-cy="resetPassword-form"
-                  onSubmit={event => {
-                    event.preventDefault();
-                    this.submitResetPassword();
-                  }}
-                >
-                  {showError && passwordError && (
-                    <MessageBox type="error" withIcon my={2} data-cy="password-error">
-                      {passwordError}
-                    </MessageBox>
-                  )}
+                {!this.props.data?.loggedInAccount && (
+                  <MessageBox type="error" withIcon my={2} data-cy="password-error">
+                    <FormattedMessage defaultMessage="There was an error while trying to process your request to reset password. Try again or contact support if the problem persists." />
+                  </MessageBox>
+                )}
 
-                  <StyledInputField
-                    labelFontWeight={600}
-                    labelFontSize="13px"
-                    alignItems="left"
-                    width="100%"
-                    label={<FormattedMessage defaultMessage="New Password" />}
-                    htmlFor="new-password"
-                    my={2}
+                {this.props.data?.loggedInAccount && (
+                  <Container
+                    as="form"
+                    method="POST"
+                    noValidate
+                    data-cy="resetPassword-form"
+                    onSubmit={event => {
+                      event.preventDefault();
+                      this.submitResetPassword();
+                    }}
                   >
+                    <Flex mb={'4'}>
+                      <Avatar collective={this.props.data.loggedInAccount} radius={40} mr={2} />
+                      <Box>
+                        <P color="black.800" fontWeight="500" fontSize="14px" lineHeight="20px">
+                          {this.props.data.loggedInAccount.name}
+                        </P>
+                        <P mt="2px" wordBreak="break-all" color="black.700" fontSize="13px">
+                          {this.props.data.loggedInAccount.email}
+                        </P>
+                      </Box>
+                    </Flex>
+
+                    {/* We're adding a hidden email field to helper password managers remember the credentials */}
                     <StyledInput
-                      fontSize="14px"
-                      id="new-password"
-                      autoComplete="new-password"
-                      type="password"
-                      width={1}
-                      autoFocus={true}
-                      required={true}
-                      onChange={({ target }) => {
-                        this.setState({
-                          password: target.value,
-                          passwordError: target.validationMessage,
-                          showError: false,
-                        });
-                      }}
-                      onKeyDown={e => {
-                        // See https://github.com/facebook/react/issues/6368
-                        if (e.key === ' ') {
-                          e.preventDefault();
-                        } else if (e.key === 'Enter') {
-                          this.setState({ passwordError: e.target.validationMessage, showError: true });
-                        }
-                      }}
-                      onBlur={() => this.setState({ showError: true })}
-                      onInvalid={event => {
-                        event.preventDefault();
-                        this.setState({ passwordError: event.target.validationMessage });
+                      style={{ display: 'none' }}
+                      id="email"
+                      readOnly
+                      autoComplete="email"
+                      name="email"
+                      value={this.props.data.loggedInAccount.email}
+                      type="email"
+                    />
+
+                    {showError && passwordError && (
+                      <MessageBox type="error" withIcon my={2}>
+                        {passwordError}
+                      </MessageBox>
+                    )}
+
+                    <StyledInputField
+                      labelFontWeight={600}
+                      labelFontSize="13px"
+                      alignItems="left"
+                      width="100%"
+                      label={<FormattedMessage defaultMessage="New Password" />}
+                      htmlFor="new-password"
+                      my={2}
+                    >
+                      <StyledInput
+                        fontSize="14px"
+                        id="new-password"
+                        autoComplete="new-password"
+                        type="password"
+                        width={1}
+                        autoFocus={true}
+                        required={true}
+                        onChange={({ target }) => {
+                          this.setState({
+                            password: target.value,
+                            passwordError: target.validationMessage,
+                            showError: false,
+                          });
+                        }}
+                        onKeyDown={e => {
+                          // See https://github.com/facebook/react/issues/6368
+                          if (e.key === ' ') {
+                            e.preventDefault();
+                          } else if (e.key === 'Enter') {
+                            this.setState({ passwordError: e.target.validationMessage, showError: true });
+                          }
+                        }}
+                        onBlur={() => this.setState({ showError: true })}
+                        onInvalid={event => {
+                          event.preventDefault();
+                          this.setState({ passwordError: event.target.validationMessage });
+                        }}
+                      />
+                    </StyledInputField>
+
+                    <PasswordStrengthBar
+                      style={{ visibility: password ? 'visible' : 'hidden' }}
+                      password={password}
+                      onChangeScore={passwordScore => {
+                        this.setState({ passwordScore });
                       }}
                     />
-                  </StyledInputField>
 
-                  <PasswordStrengthBar
-                    style={{ visibility: password ? 'visible' : 'hidden' }}
-                    password={password}
-                    onChangeScore={passwordScore => {
-                      this.setState({ passwordScore });
-                    }}
-                  />
-
-                  <Flex justifyContent="center" mb="24px" mt="26px">
-                    <StyledButton
-                      data-cy="signin-btn"
-                      buttonStyle="primary"
-                      fontWeight="500"
-                      disabled={!password}
-                      loading={passwordLoading}
-                      minWidth={157}
-                      type="submit"
-                      whiteSpace="nowrap"
-                    >
-                      <FormattedMessage id="actions.continue" defaultMessage="Continue" />
-                    </StyledButton>
-                  </Flex>
-                </Container>
+                    <Flex justifyContent="center" mb="24px" mt="26px">
+                      <StyledButton
+                        data-cy="signin-btn"
+                        buttonStyle="primary"
+                        fontWeight="500"
+                        disabled={!password}
+                        loading={passwordLoading}
+                        minWidth={157}
+                        type="submit"
+                        whiteSpace="nowrap"
+                      >
+                        <FormattedMessage id="actions.continue" defaultMessage="Continue" />
+                      </StyledButton>
+                    </Flex>
+                  </Container>
+                )}
               </Box>
             </Fragment>
           </Flex>
@@ -205,14 +241,39 @@ const resetPasswordMutation = gql`
   }
 `;
 
+const resetPasswordAccountQuery = gql`
+  query ResetPasswordAccount {
+    loggedInAccount {
+      id
+      slug
+      name
+      email
+      imageUrl
+    }
+  }
+`;
+
 const addResetPasswordMutation = graphql(resetPasswordMutation, {
   name: 'resetPassword',
-  options: props => ({
-    context: {
-      ...API_V2_CONTEXT,
-      headers: { authorization: `Bearer ${props.token}` },
-    },
-  }),
+  options: props => {
+    return {
+      context: {
+        ...API_V2_CONTEXT,
+        headers: { authorization: `Bearer ${props.token}` },
+      },
+    };
+  },
 });
 
-export default withRouter(injectIntl(addResetPasswordMutation(ResetPasswordPage)));
+const addResetPasswordAccountQuery = graphql(resetPasswordAccountQuery, {
+  options: props => {
+    return {
+      context: {
+        ...API_V2_CONTEXT,
+        headers: { authorization: `Bearer ${props.token}` },
+      },
+    };
+  },
+});
+
+export default withRouter(injectIntl(addResetPasswordAccountQuery(addResetPasswordMutation(ResetPasswordPage))));
