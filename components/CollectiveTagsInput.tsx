038405ad69e6a8e58fe03ec40ff941354c45281a
Gustav Larsson <gustav.larsson@gmail.com>
2023-02-10 14:44:24 +0100

    Use dnd-kit for drag and drop instead of react-sortable-hoc

--- components/CollectiveTagsInput.tsx
+++ components/CollectiveTagsInput.tsx
@@ -1,6 +1,9 @@
 import React, { Fragment, MouseEventHandler, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { gql, useLazyQuery } from '@apollo/client';
+import { DndContext, DragOverlay } from '@dnd-kit/core';
+import { arrayMove, SortableContext, useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import { debounce } from 'lodash';
 import AnimateHeight from 'react-animate-height';
 import { FormattedMessage, useIntl } from 'react-intl';
@@ -8,43 +11,19 @@ import {
   components as ReactSelectComponents,
   ContainerProps,
   InputProps,
-  MultiValueGenericProps,
   MultiValueProps,
   OnChangeValue,
   OptionProps,
-  Props,
 } from 'react-select';
 import CreatableSelect from 'react-select/creatable';
-import {
-  SortableContainer,
-  SortableContainerProps,
-  SortableElement,
-  SortableHandle,
-  SortEndHandler,
-} from 'react-sortable-hoc';
-import styled from 'styled-components';
 
 import { IGNORED_TAGS } from '../lib/constants/collectives';
 import { API_V2_CONTEXT } from '../lib/graphql/helpers';
 import colors from '../lib/theme/colors';
 
 import { Flex } from './Grid';
+import StyledTag from './StyledTag';
 import { Span } from './Text';
-
-const StyledTagButton = styled.button<{ isSelected: boolean }>`
-  border-radius: 2px 12px 12px 2px;
-  padding: 3px 10px 3px 6px;
-  color: black.700;
-  background: #f0f2f5;
-  font-size: 12px;
-  border: 0;
-  cursor: pointer;
-  opacity: ${props => (props.isSelected ? '25%' : '100%')};
-  transition: opacity 0.1s;
-  &:focus {
-    outline: none;
-  }
-`;
 
 export const searchTagsQuery = gql`
   query SearchTags($term: String) {
@@ -62,29 +41,39 @@ type TagOption = {
   value: string;
 };
 
-// Sorting logic from https://react-select.com/advanced#sortable-multiselect
-function arrayMove<T>(array: readonly T[], from: number, to: number) {
-  const slicedArray = array.slice();
-  slicedArray.splice(to < 0 ? array.length + to : to, 0, slicedArray.splice(from, 1)[0]);
-  return slicedArray;
-}
-
-const SortableSelect = SortableContainer(CreatableSelect) as React.ComponentClass<Props & SortableContainerProps>;
-
-const SortableMultiValue = SortableElement((props: MultiValueProps<TagOption>) => {
+const MultiValue = (props: MultiValueProps<TagOption>) => {
   // This prevents the menu from being opened/closed when the user clicks
   // on a value to begin dragging it
   const onMouseDown: MouseEventHandler<HTMLDivElement> = e => {
     e.preventDefault();
     e.stopPropagation();
   };
-  const innerProps = { ...props.innerProps, onMouseDown };
-  return <ReactSelectComponents.MultiValue {...props} innerProps={innerProps} />;
-});
 
-const SortableMultiValueLabel = SortableHandle((props: MultiValueGenericProps) => (
-  <ReactSelectComponents.MultiValueLabel {...props} />
-));
+  const { attributes, isDragging, listeners, setNodeRef, transform, transition } = useSortable({
+    id: props.data.value,
+  });
+
+  const style = {
+    transform: CSS.Translate.toString(transform),
+    transition,
+    opacity: isDragging ? '0.4' : undefined,
+  };
+
+  return (
+    <div style={style} ref={setNodeRef} {...attributes} {...listeners}>
+      <StyledTag
+        m="4px"
+        variant="rounded-right"
+        maxHeight="none"
+        style={{ cursor: 'grab' }}
+        onMouseDown={onMouseDown}
+        closeButtonProps={{ ...props.removeProps, isFocused: props.isFocused, onPointerDown: e => e.stopPropagation() }}
+      >
+        {props.children}
+      </StyledTag>
+    </div>
+  );
+};
 
 const Input = (props: InputProps) => {
   return <ReactSelectComponents.Input {...props} data-cy={'tags-select-input'} />;
@@ -114,6 +103,7 @@ const SelectContainer = ({ innerProps, ...props }: ContainerProps) => (
 const debouncedSearch = debounce((searchFunc, variables) => {
   return searchFunc({ variables });
 }, 500);
+
 function CollectiveTagsInput({ defaultValue = [], onChange, suggestedTags = [] }) {
   const intl = useIntl();
   const [searchTags, { loading: fetching, data }] = useLazyQuery(searchTagsQuery, {
@@ -126,6 +116,10 @@ function CollectiveTagsInput({ defaultValue = [], onChange, suggestedTags = [] }
   const [selected, setSelected] = useState<readonly TagOption[]>(
     defaultValue?.map(tag => ({ label: tag, value: tag })) || [],
   );
+  const [draggingTag, setDraggingTag] = useState<string | null>(null);
+
+  // Fix for infinity loop bug in dnd-kit with variable width items: https://github.com/clauderic/dnd-kit/issues/842#issuecomment-1192622612
+  const [overItemsForDelta, setOverItemsForDelta] = useState({});
 
   useEffect(() => {
     onChange(selected);
@@ -157,76 +151,82 @@ function CollectiveTagsInput({ defaultValue = [], onChange, suggestedTags = [] }
     }
   }, [fetching, data]);
 
-  const onSortEnd: SortEndHandler = ({ oldIndex, newIndex }) => {
-    const newValue = arrayMove(selected, oldIndex, newIndex);
-    setSelected(newValue);
-  };
+  function handleDragOver(event) {
+    const { active, over, delta } = event;
+
+    const jsonDelta = JSON.stringify(delta);
+    if (over && active.id !== over.id && !overItemsForDelta[jsonDelta]) {
+      setSelected(selected => {
+        const oldIndex = selected.findIndex(item => item.value === active.id);
+        const newIndex = selected.findIndex(item => item.value === over.id);
+        return arrayMove(selected, oldIndex, newIndex);
+      });
+      setOverItemsForDelta({ ...overItemsForDelta, [jsonDelta]: over.id });
+    }
+  }
+
+  function handleDragStart(event) {
+    setDraggingTag(event.active.id);
+  }
+
+  function handleDragEnd() {
+    setDraggingTag(null);
+    setOverItemsForDelta({});
+  }
 
   return (
     <Fragment>
-      <SortableSelect
-        useDragHandle
-        // cacheOptions
-        // react-sortable-hoc props:
-        axis="xy"
-        onSortEnd={onSortEnd}
-        distance={0}
-        // small fix for https://github.com/clauderic/react-sortable-hoc/pull/352:
-        getHelperDimensions={({ node }) => node.getBoundingClientRect()}
-        // react-select props:
-        openMenuOnFocus
-        placeholder={intl.formatMessage({ id: 'collective.tags.input.placeholder', defaultMessage: '+ Add tags' })}
-        isMulti
-        value={selected}
-        menuPortalTarget={document.body}
-        components={{
-          // @ts-ignore We're failing to provide a required index prop to SortableElement
-          MultiValue: SortableMultiValue,
-          // @ts-ignore We're failing to provide a required index prop to SortableElement
-          MultiValueLabel: SortableMultiValueLabel,
-          SelectContainer,
-          Input,
-          Option,
-        }}
-        onInputChange={value => setInput(value)}
-        options={options}
-        isLoading={loading}
-        onChange={(selectedOptions: OnChangeValue<TagOption, true>) => setSelected(selectedOptions)}
-        styles={{
-          multiValue: baseStyles => ({
-            ...baseStyles,
-            borderRadius: '2px 12px 12px 2px',
-            background: colors.black[100],
-            overflow: 'hidden',
-            zIndex: 9999,
-          }),
-          multiValueLabel: baseStyles => ({
-            ...baseStyles,
-            cursor: 'grab',
-          }),
-          multiValueRemove: (baseStyles, state) => ({
-            ...baseStyles,
-            color: state.isFocused ? colors.black[900] : colors.black[600],
-            backgroundColor: state.isFocused ? colors.black[400] : 'transparent',
-            borderRadius: 0,
-            paddingLeft: '3px',
-            cursor: 'pointer',
-            '&:hover': {
-              backgroundColor: colors.black[400],
-              color: colors.black[900],
-            },
-          }),
-          menuPortal: styles => ({ ...styles, zIndex: 9999 }),
-          control: (baseStyles, state) => ({
-            ...baseStyles,
-            boxShadow: `inset 0px 2px 2px ${colors.primary[50]}`,
-            borderColor: state.isFocused ? colors.primary[500] : colors.black[300],
-            '&:hover': {
-              borderColor: state.isFocused ? colors.primary[500] : colors.primary[300],
-            },
-          }),
-        }}
-      />
+      <DndContext
+        onDragStart={handleDragStart}
+        onDragOver={handleDragOver}
+        onDragEnd={handleDragEnd}
+        onDragCancel={handleDragEnd}
+      >
+        <SortableContext items={selected.map(o => o.value)}>
+          <CreatableSelect
+            openMenuOnFocus
+            placeholder={intl.formatMessage({ id: 'collective.tags.input.placeholder', defaultMessage: '+ Add tags' })}
+            isMulti
+            value={selected}
+            menuPortalTarget={document.body}
+            components={{
+              MultiValue,
+              SelectContainer,
+              Input,
+              Option,
+            }}
+            onInputChange={value => setInput(value)}
+            options={options}
+            isLoading={loading}
+            onChange={(selectedOptions: OnChangeValue<TagOption, true>) => setSelected(selectedOptions)}
+            styles={{
+              menuPortal: styles => ({ ...styles, zIndex: 9999 }),
+              control: (baseStyles, state) => ({
+                ...baseStyles,
+                boxShadow: `inset 0px 2px 2px ${colors.primary[50]}`,
+                borderColor: state.isFocused ? colors.primary[500] : colors.black[300],
+                '&:hover': {
+                  borderColor: state.isFocused ? colors.primary[500] : colors.primary[300],
+                },
+              }),
+            }}
+          />
+        </SortableContext>
+        <DragOverlay>
+          {draggingTag ? (
+            <StyledTag
+              m="4px"
+              variant="rounded-right"
+              style={{ cursor: 'grabbing', color: 'black' }}
+              maxHeight="none"
+              backgroundColor="black.200"
+              closeButtonProps={true}
+            >
+              {draggingTag}
+            </StyledTag>
+          ) : null}
+        </DragOverlay>
+      </DndContext>
       <AnimateHeight height={suggestedTags?.length > 0 ? 'auto' : 0}>
         <Flex mt={2} gap={'6px'} flexWrap="wrap" alignItems={'center'}>
           {suggestedTags && (
@@ -238,11 +238,13 @@ function CollectiveTagsInput({ defaultValue = [], onChange, suggestedTags = [] }
           {suggestedTags?.map(tag => {
             const isSelected = selected.some(({ value }) => value === tag);
             return (
-              <StyledTagButton
+              <StyledTag
+                variant="rounded-right"
                 type="button"
                 tabIndex={-1}
                 key={tag}
-                isSelected={isSelected}
+                closeButtonProps={false}
+                style={{ opacity: isSelected ? 0.5 : 1, cursor: 'pointer' }}
                 onClick={() =>
                   isSelected
                     ? setSelected(selected.filter(({ value }) => value !== tag))
@@ -250,7 +252,7 @@ function CollectiveTagsInput({ defaultValue = [], onChange, suggestedTags = [] }
                 }
               >
                 {tag}
-              </StyledTagButton>
+              </StyledTag>
             );
           })}
         </Flex>
