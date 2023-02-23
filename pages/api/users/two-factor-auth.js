fbb4af473aa80a6dc017a75f23a5eb01a35797b7
François Hodierne <francois@hodierne.net>
2023-02-23 20:24:41 +0100

    refactor local proxy handling

--- pages/api/users/two-factor-auth.js
+++ pages/api/users/two-factor-auth.js
@@ -1,15 +1,16 @@
+import { pick } from 'lodash';
+
 export default async function handle(req, res) {
   const apiUrl = `${process.env.API_URL}/users/two-factor-auth?api_key=${process.env.API_KEY}`;
 
   const result = await fetch(apiUrl, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    method: req.method,
+    headers: pick(req.headers, ['accept', 'content-type', 'authorization', 'user-agent', 'accept-language']),
     body: JSON.stringify(req.body),
   });
 
   const json = await result.json();
-  const statusCode = result.status;
 
   res.setHeader('Content-Type', 'application/json');
-  res.status(statusCode).json(json);
+  res.status(result.status).json(json);
 }
