53cd7f9c26e414c3f703d70d3703d18742197f15
Leo Kewitz <leo@opencollective.com>
2023-03-08 14:57:46 +0100

    refact: deprecate Download CSV V1

--- components/transactions/TransactionsDownloadCSV.js
+++ components/transactions/TransactionsDownloadCSV.js
@@ -7,77 +7,15 @@ import { FormattedMessage, useIntl } from 'react-intl';
 
 import { fetchCSVFileFromRESTService } from '../../lib/api';
 import { parseDateInterval } from '../../lib/date-utils';
-import { formatErrorMessage, i18nGraphqlException } from '../../lib/errors';
-import { exportFile } from '../../lib/export_file';
-import { transactionsQuery } from '../../lib/graphql/queries';
+import { formatErrorMessage } from '../../lib/errors';
 import { getFromLocalStorage, LOCAL_STORAGE_KEYS } from '../../lib/local-storage';
 
-import { Box, Flex } from '../Grid';
-import Link from '../Link';
-import PopupMenu from '../PopupMenu';
 import StyledButton from '../StyledButton';
+import StyledTooltip from '../StyledTooltip';
 import { TOAST_TYPE, useToasts } from '../ToastProvider';
 import { getDefaultKinds } from '../transactions/filters/TransactionsKindFilter';
 
-const transformResultInCSV = json => {
-  const q = value => `"${value}"`; /* Quote value */
-  const f = value => (value / 100).toFixed(2); /* Add cents */
-  const d = value => dayjs(new Date(value)).format('YYYY-MM-DD HH:mm:ss');
-
-  // Sanity check. It will return an empty CSV for the user
-  if (json.length === 0) {
-    return '';
-  }
-
-  // All the json lines contain the same values for these two
-  // variables. It's save to get them from any index.
-  const hostCurrency = json[0].host.currency;
-  const collectiveCurrency = json[0].currency;
-
-  const header = [
-    'Transaction Description',
-    'User Name',
-    'User Profile',
-    'Transaction Date',
-    'Collective Currency',
-    'Host Currency',
-    'Transaction Amount',
-    `Host Fee (${hostCurrency})`,
-    `Open Collective Fee (${hostCurrency})`,
-    `Payment Processor Fee (${hostCurrency})`,
-    `Net Amount (${collectiveCurrency})`,
-    'Subscription Interval',
-    'Order Date',
-    'Tags',
-  ].join(',');
-
-  const lines = json.map(i => {
-    const profile = `http://opencollective.com/${i.fromCollective.slug}`;
-    const subscriptionInterval = i.subscription ? i.subscription.interval : 'one time';
-    const expenseTags = i.expense?.tags ? i.expense.tags.join(', ') : '';
-
-    return [
-      q(i.description) /* Transaction Description */,
-      q(i.fromCollective.name) /* User Name  */,
-      q(profile) /* User Profile  */,
-      d(i.createdAt) /* Transaction Date  */,
-      q(i.currency) /* Currency */,
-      q(hostCurrency) /* Host Currency */,
-      f(i.amount) /* Transaction Amount */,
-      f(i.hostFeeInHostCurrency) /* Host Fee */,
-      f(i.platformFeeInHostCurrency) /* Platform Fee */,
-      f(i.paymentProcessorFeeInHostCurrency) /* Payment Processor Fee */,
-      f(i.netAmountInCollectiveCurrency) /* Net Amount */,
-      q(subscriptionInterval) /* Interval of subscription */,
-      q(new Date(i.createdAt).toISOString()) /* Order Date */,
-      q(expenseTags) /* Tags */,
-    ].join(',');
-  });
-
-  return [header].concat(lines).join('\n');
-};
-
-const TransactionsDownloadCSV = ({ collective, client, query }) => {
+const TransactionsDownloadCSV = ({ collective, query }) => {
   const intl = useIntl();
   const [loading, setLoading] = React.useState(null);
   const { addToast } = useToasts();
@@ -85,43 +23,6 @@ const TransactionsDownloadCSV = ({ collective, client, query }) => {
   if (query.period) {
     ({ from: dateFrom, to: dateTo } = parseDateInterval(query.period));
   }
-
-  const type = query.type;
-
-  const kinds = query.kind ? query.kind.split(',') : getDefaultKinds();
-
-  const download = async () => {
-    try {
-      setLoading('v1');
-      const result = await client.query({
-        query: transactionsQuery,
-        variables: {
-          dateFrom: dateFrom,
-          dateTo: dateTo,
-          CollectiveId: collective.legacyId,
-          type: type,
-          kinds: kinds,
-        },
-      });
-      const csv = transformResultInCSV(result.data.allTransactions);
-
-      // Don't prompt the file download unless there's data coming
-      if (csv === '') {
-        return;
-      }
-
-      // Helper to prepare date values to be part of the file name
-      const format = d => dayjs(d).format('YYYY-MM-DD');
-      let fileName = `${collective.slug}-from-`;
-      fileName += `${format(dateFrom)}-to-`;
-      fileName += `${format(dateTo)}.csv`;
-      return exportFile('text/plain;charset=utf-8', fileName, csv);
-    } catch (error) {
-      addToast({ type: TOAST_TYPE.ERROR, message: i18nGraphqlException(intl, error) });
-    } finally {
-      setLoading(null);
-    }
-  };
 
   const downloadV2 = async event => {
     const accessToken = getFromLocalStorage(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
@@ -206,58 +107,30 @@ const TransactionsDownloadCSV = ({ collective, client, query }) => {
   };
 
   return (
-    <Flex flexWrap="wrap" my={['8px', 0]}>
-      <PopupMenu
-        placement="bottom-end"
-        Button={({ onClick }) => (
-          <StyledButton
-            data-cy="download-csv"
-            onClick={onClick}
-            buttonSize="small"
-            minWidth={140}
-            isBorderless
-            flexGrow={1}
-          >
-            <FormattedMessage id="transactions.downloadcsvbutton" defaultMessage="Download CSV" />
-            <IconDownload size="13px" style={{ marginLeft: '8px' }} />
-          </StyledButton>
-        )}
+    <StyledTooltip
+      content={
+        <FormattedMessage
+          id="Transactions.DownloadCSV.Description"
+          defaultMessage="Use the filters to define the transactions you would like to download."
+        />
+      }
+    >
+      <StyledButton
+        data-cy="download-csv"
+        onClick={downloadV2}
+        buttonSize="small"
+        minWidth={140}
+        height={38}
+        mb="8px"
+        p="6px 10px"
+        isBorderless
+        loading={loading === 'v2'}
+        flexGrow={1}
       >
-        <Box mx="8px" my="16px" width="190px">
-          <small>
-            <FormattedMessage
-              id="Transactions.DownloadCSV.Description"
-              defaultMessage="Use the filters to define the transactions you would like to download."
-            />
-          </small>
-          <br />
-          <Link onClick={downloadV2} href={downloadUrl()} openInNewTab={true}>
-            <StyledButton
-              data-cy="download-csv-download-v2"
-              buttonSize="tiny"
-              buttonStyle="primary"
-              mt="12px"
-              loading={loading === 'v2'}
-              disabled={Boolean(loading) && loading !== 'v2'}
-              minWidth={115}
-            >
-              <FormattedMessage id="Download" defaultMessage="Download" /> (v2)
-            </StyledButton>
-          </Link>
-          <StyledButton
-            data-cy="download-csv-download"
-            buttonSize="tiny"
-            onClick={download}
-            mt="12px"
-            loading={loading === 'v1'}
-            disabled={Boolean(loading) && loading !== 'v1'}
-            minWidth={115}
-          >
-            <FormattedMessage id="Download" defaultMessage="Download" /> (v1)
-          </StyledButton>
-        </Box>
-      </PopupMenu>
-    </Flex>
+        <FormattedMessage id="transactions.downloadcsvbutton" defaultMessage="Download CSV" />
+        <IconDownload size="13px" style={{ marginLeft: '8px' }} />
+      </StyledButton>
+    </StyledTooltip>
   );
 };
 
