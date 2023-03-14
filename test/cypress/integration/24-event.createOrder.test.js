859cadefae7488f6017fa252fea38c9b0f3b4904
Henrique <hdiniz@users.noreply.github.com>
2023-03-13 22:32:06 -0300

    Use cypress label for tickets button teest

--- test/cypress/integration/24-event.createOrder.test.js
+++ test/cypress/integration/24-event.createOrder.test.js
@@ -31,7 +31,7 @@ describe('event.createOrder page', () => {
 
     // Create Ticket
     cy.contains('a', 'Create Ticket').click();
-    cy.get('[data-cy="admin-panel-container"] [data-cy="create-contribute-tier"]').click();
+    cy.get('[data-cy="admin-panel-container"] [data-cy="create-ticket"]').click();
     cy.get('[data-cy=name]').type('Free ticket');
     cy.get('input[data-cy=amount]').type('0');
     cy.getByDataCy('confirm-btn').click();
@@ -56,7 +56,7 @@ describe('event.createOrder page', () => {
 
     // Create Ticket
     cy.contains('a', 'Create Ticket').click();
-    cy.get('[data-cy="admin-panel-container"] [data-cy="create-contribute-tier"]').click();
+    cy.get('[data-cy="admin-panel-container"] [data-cy="create-ticket"]').click();
     cy.get('[data-cy=name]').type('Paying Ticket');
     cy.get('input[data-cy=amount]').type('10');
     cy.getByDataCy('confirm-btn').click();
@@ -86,7 +86,7 @@ describe('event.createOrder page', () => {
 
     // Create Ticket
     cy.contains('a', 'Create Ticket').click();
-    cy.get('[data-cy="admin-panel-container"] [data-cy="create-contribute-tier"]').click();
+    cy.get('[data-cy="admin-panel-container"] [data-cy="create-ticket"]').click();
     cy.get('[data-cy=name]').type('Flexible Paying Ticket');
     cy.get('[data-cy=amountType]').click();
     cy.contains('[data-cy=select-option]', 'Flexible').click();
@@ -128,7 +128,7 @@ describe('event.createOrder page', () => {
     cy.contains('a', 'Create Ticket').click();
 
     // Create tickets
-    cy.get('[data-cy="admin-panel-container"] [data-cy="create-contribute-tier"]').click();
+    cy.get('[data-cy="admin-panel-container"] [data-cy="create-ticket"]').click();
     cy.get('[data-cy=name]').type('Ticket with VAT');
     cy.get('input[data-cy=amount]').type('10');
     cy.getByDataCy('confirm-btn').click();
