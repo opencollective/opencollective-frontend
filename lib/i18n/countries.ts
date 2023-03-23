5b2f629bc223e67d5df29dcb218786fc758881a4
Henrique <hdiniz@users.noreply.github.com>
2023-03-23 16:23:57 -0300

    Add country select

--- lib/i18n/countries.ts
+++ lib/i18n/countries.ts
@@ -1,14 +1,13 @@
+import { countryData, getEmojiByCountryCode } from 'country-currency-emoji-flags';
 import { IntlShape } from 'react-intl';
 
 import { getIntlDisplayNames } from '.';
 
 export function getFlagEmoji(countryCode: string) {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split('')
-    .map(char => 127397 + char.charCodeAt(0));
-  return String.fromCodePoint(...codePoints);
+  return getEmojiByCountryCode(countryCode);
 }
+
+export const isoCountryList = Object.keys(countryData);
 
 export function getCountryDisplayName(intl: IntlShape, countryCode: string) {
   const regionNames = getIntlDisplayNames(intl, 'region');
