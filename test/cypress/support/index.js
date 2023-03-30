efee33a63a82f919622a8a85892df2bd4b4555f7
Henrique <hdiniz@users.noreply.github.com>
2023-03-30 15:44:45 -0300

    wip

--- test/cypress/support/index.js
+++ test/cypress/support/index.js
@@ -12,10 +12,10 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-
 import '@cypress/code-coverage/support';
 // Import commands.js using ES2015 syntax:
 import './commands';
+import './typed-commands';
 
 // See https://github.com/opencollective/opencollective/issues/2676
 Cypress.on('uncaught:exception', err => {
