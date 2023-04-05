103c757c8268c5e1e1e96325497c4444150c2b50
François Hodierne <francois@hodierne.net>
2023-04-05 10:40:57 +0200

    cleanup

--- lib/withData.tsx
+++ lib/withData.tsx
@@ -6,8 +6,6 @@ import PropTypes from 'prop-types';
 import { ApolloClient } from '@apollo/client';
 import { getDataFromTree } from '@apollo/client/react/ssr';
 
-// import { getSession } from 'next-auth/react';
-// import { authOptions } from '../pages/api/auth/[...nextauth]';
 import { withTwoFactorAuthentication } from './two-factor-authentication/TwoFactorAuthenticationContext';
 import { initClient } from './apollo-client';
 import { compose } from './utils';
@@ -26,8 +24,6 @@ const withData = ComposedComponent => {
   return class WithData extends React.Component<WithDataProps> {
     static async getInitialProps(context) {
       const { Component } = context;
-
-      // const session = await getSession(context.req, context.res, authOptions);
 
       const client = initClient();
 
