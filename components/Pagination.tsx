b74787cfdffa08e8dbc8830993e8536a83cdc1fa
Henrique <hdiniz@users.noreply.github.com>
2023-04-14 17:03:05 -0300

    Refactor list pagination component as variant

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,55 +1,159 @@
+import qs from 'querystring';
+
 import React from 'react';
-import PropTypes from 'prop-types';
+import { ChevronLeft } from '@styled-icons/boxicons-regular/ChevronLeft';
+import { ChevronRight } from '@styled-icons/boxicons-regular/ChevronRight';
 import { omit } from 'lodash';
 import { useRouter } from 'next/router';
 import { FormattedMessage } from 'react-intl';
+import styled from 'styled-components';
+
+import { paginationElements } from '../lib/pagination';
 
 import Container from './Container';
-import { Flex } from './Grid';
+import { Box, Flex } from './Grid';
 import Link from './Link';
 import StyledButton from './StyledButton';
 import StyledInput from './StyledInput';
 
-const Pagination = ({ route, limit, offset, total, scrollToTopOnChange, isDisabled, ignoredQueryParams }) => {
+type PaginationProps = BasePaginationProps &
+  (
+    | ({
+        variant?: 'input';
+      } & InputPaginationProps)
+    | ({
+        variant: 'list';
+      } & ListPaginationProps)
+  );
+
+type BasePaginationProps = {
+  offset: number;
+  limit: number;
+  total: number;
+  isDisabled?: boolean;
+} & (
+  | {
+      onPageChange: (page: number) => void;
+    }
+  | {
+      route?: string;
+      ignoredQueryParams?: string[];
+    }
+);
+
+export default function Pagination(props: PaginationProps) {
   const router = useRouter();
+  const useCallback = 'onPageChange' in props;
 
-  if (!router) {
-    return null;
-  }
+  const totalPages = Math.ceil(props.total / props.limit);
+  const currentPage = Math.ceil(props.offset / props.limit) + 1;
 
-  const totalPages = Math.ceil(total / limit);
-  const currentPage = offset / limit + 1;
-  const queryParams = ignoredQueryParams ? omit(router.query, ignoredQueryParams) : router.query;
-  route = route || router.asPath.split('?')[0];
-  isDisabled = isDisabled || totalPages <= 1;
-
-  const changePage = async ({ target, key }) => {
-    if (key && key !== 'Enter') {
-      return;
+  const pageUrl = React.useMemo(() => {
+    if (useCallback) {
+      return null;
     }
 
-    const { value } = target;
-    if (!value || !parseInt(value) || parseInt(value) === currentPage) {
-      return;
+    if ('route' in props) {
+      return (page: number) => `${props.route}?limit=${props.limit}&offset=${props.limit * (page - 1)}`;
     }
-    await router.push({ pathname: route, query: { ...queryParams, offset: (value - 1) * limit } });
 
-    if (scrollToTopOnChange) {
-      window.scrollTo(0, 0);
+    const baseUrl = router.asPath.split('?')[0];
+    return (page: number) => {
+      const query = new URLSearchParams(qs.encode(omit(router.query, props.ignoredQueryParams)));
+      query.set('offset', `${props.limit * (page - 1)}`);
+      return `${baseUrl}?${query.toString()}`;
+    };
+  }, [props.limit, !useCallback && props.route, !useCallback && props.ignoredQueryParams, !useCallback && router]);
+
+  const onPageChange = React.useCallback(
+    (page: number) => {
+      if (page <= 0 || page > totalPages) {
+        return;
+      }
+
+      if (!useCallback) {
+        router.push(pageUrl(page));
+      } else {
+        props.onPageChange(page);
+      }
+    },
+    [router, useCallback, pageUrl, totalPages],
+  );
+
+  const PageComponent = React.useMemo(() => {
+    if (useCallback) {
+      return function PageComponent(props: PageComponentProps) {
+        return <Box onClick={() => onPageChange(props.page)}>{props.children}</Box>;
+      };
     }
+
+    return function PageComponent(props: PageComponentProps) {
+      return <Link href={pageUrl(props.page)}>{props.children}</Link>;
+    };
+  }, [useCallback, onPageChange]);
+
+  const isDisabled = props.isDisabled || totalPages <= 1;
+
+  const variantProps: CommonVariantProps = {
+    isDisabled,
+    pageUrl,
+    currentPage,
+    totalPages,
+    onPageChange,
+    useCallback,
+    PageComponent,
   };
+
+  switch (props.variant) {
+    case 'list': {
+      return <ListPagination {...props} {...variantProps} />;
+    }
+    default: {
+      return <InputPagination {...props} {...variantProps} />;
+    }
+  }
+}
+
+// variants
+
+type CommonVariantProps = {
+  currentPage: number;
+  totalPages: number;
+  isDisabled: boolean;
+  onPageChange?: (page: number) => void;
+  pageUrl?: (page: number) => string;
+  useCallback: boolean;
+  PageComponent: React.FC<PageComponentProps>;
+};
+type PageComponentProps = React.PropsWithChildren<{ page: number }>;
+
+type InputPaginationProps = {};
+
+function InputPagination(props: InputPaginationProps & CommonVariantProps) {
+  const changePage = React.useCallback(
+    ({ target, key }) => {
+      if (key && key !== 'Enter') {
+        return;
+      }
+
+      const { value } = target;
+      if (!value || !parseInt(value) || parseInt(value) === props.currentPage) {
+        return;
+      }
+
+      props.onPageChange(value);
+    },
+    [props.currentPage, props.onPageChange],
+  );
 
   return (
     <Flex alignItems="center">
-      {currentPage > 1 && (
-        <Link
-          href={{ pathname: route, query: { ...queryParams, offset: offset - limit } }}
-          scroll={scrollToTopOnChange}
-        >
-          <StyledButton buttonSize="small" disabled={isDisabled}>
+      {props.currentPage > 1 && (
+        <props.PageComponent page={props.currentPage - 1}>
+          <StyledButton buttonSize="small" disabled={props.isDisabled}>
             <FormattedMessage id="Pagination.Prev" defaultMessage="Previous" />
           </StyledButton>
-        </Link>
+        </props.PageComponent>
       )}
       <Container display="inline-block" mx={2}>
         <FormattedMessage
@@ -58,8 +162,8 @@ const Pagination = ({ route, limit, offset, total, scrollToTopOnChange, isDisabl
           values={{
             current: (
               <StyledInput
-                key={offset}
-                defaultValue={currentPage}
+                key={props.currentPage}
+                defaultValue={props.currentPage}
                 onBlur={changePage}
                 onKeyPress={changePage}
                 textAlign="center"
@@ -67,45 +171,91 @@ const Pagination = ({ route, limit, offset, total, scrollToTopOnChange, isDisabl
                 px={1}
                 py={1}
                 width={30}
-                disabled={isDisabled}
+                disabled={props.isDisabled}
                 type="text"
                 pattern="[0-9]+"
                 inputMode="numeric"
                 data-cy="pagination-current"
               />
             ),
-            total: <span data-cy="pagination-total">{totalPages || 1}</span>,
+            total: <span data-cy="pagination-total">{props.totalPages || 1}</span>,
           }}
         />
       </Container>
-      {currentPage < totalPages && (
-        <Link
-          href={{ pathname: route, query: { ...queryParams, offset: offset + limit } }}
-          scroll={scrollToTopOnChange}
-        >
-          <StyledButton buttonSize="small" disabled={isDisabled}>
+      {props.currentPage < props.totalPages && (
+        <props.PageComponent page={props.currentPage + 1}>
+          <StyledButton buttonSize="small" disabled={props.isDisabled}>
             <FormattedMessage id="Pagination.Next" defaultMessage="Next" />
           </StyledButton>
-        </Link>
+        </props.PageComponent>
       )}
     </Flex>
   );
+}
+
+type ListPaginationProps = {
+  neighbooringPages?: number;
 };
 
-Pagination.propTypes = {
-  limit: PropTypes.number,
-  offset: PropTypes.number,
-  total: PropTypes.number,
-  isDisabled: PropTypes.bool,
-  route: PropTypes.string,
-  /** By default, all query params will be transferred in the paginated link. This prop is useful to filter query params that are part of the URL. */
-  ignoredQueryParams: PropTypes.arrayOf(PropTypes.string),
-  /** Use this to scroll back on top when pagination changes */
-  scrollToTopOnChange: PropTypes.bool,
-};
+const ListPaginationPage = styled(Box)<{ selected?: boolean }>`
+  border-style: ${({ selected }) => (selected ? 'solid' : 'none')};
+  border-width: 1px;
+  border-radius: 5px;
+  font-weight: 400;
+  color: ${({ selected }) => (selected ? '#1153D6' : '#4D4F51')};
+  padding: ${({ selected }) => (selected ? '2px' : '3px')};
+  line-height: 14px;
+  font-size: 14px;
+  height: 22px;
+  min-width: 22px;
+  text-align: center;
+  cursor: pointer;
+`;
 
-Pagination.defaultProps = {
-  scrollToTopOnChange: true,
-};
+const ListPaginationArrow = styled(Box)<{ enabled: boolean }>`
+  cursor: ${({ enabled }) => (enabled ? 'pointer' : undefined)};
+  height: 22px;
+  width: 22px;
+`;
 
-export default Pagination;
+function ListPagination(props: ListPaginationProps & CommonVariantProps) {
+  const elements = React.useMemo(
+    () =>
+      paginationElements({
+        currentPage: props.currentPage,
+        totalPages: props.totalPages,
+      }),
+    [props.currentPage, props.totalPages],
+  );
+
+  return (
+    <Flex gap="5px">
+      <props.PageComponent page={props.currentPage - 1}>
+        <ListPaginationArrow mr="5px" enabled={props.currentPage > 1}>
+          <ChevronLeft />
+        </ListPaginationArrow>
+      </props.PageComponent>
+
+      <React.Fragment>
+        {elements.map(el => {
+          const isElipsis = el === 'left_elipsis' || el === 'right_elipsis';
+
+          if (isElipsis) {
+            return <ListPaginationPage key={el}>{'...'}</ListPaginationPage>;
+          }
+
+          return (
+            <props.PageComponent key={el} page={el}>
+              <ListPaginationPage selected={el === props.currentPage}>{el}</ListPaginationPage>
+            </props.PageComponent>
+          );
+        })}
+      </React.Fragment>
+      <props.PageComponent page={props.currentPage + 1}>
+        <ListPaginationArrow ml="5px" enabled={props.currentPage < props.totalPages}>
+          <ChevronRight />
+        </ListPaginationArrow>
+      </props.PageComponent>
+    </Flex>
+  );
+}
