6cd49a4b907e080f15d0524ca12fd600b8949523
François Hodierne <francois@hodierne.net>
2023-04-20 21:12:44 +0200

    fixes

--- components/InputSwitch.js
+++ components/InputSwitch.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { withStyles } from '@mui/material/styles';
 import Switch from '@mui/material/Switch';
+import { withStyles } from '@mui/styles';
 
 export default withStyles({
   switchBase: {
