f8463837fe837cd5934627f1991aaeb40dfe4f9e
Benjamin Piouffle <benjamin@opencollective.com>
2023-05-09 15:38:46 +0200

    i18n: Update files & script

--- scripts/update-crowdin-branch.sh
+++ scripts/update-crowdin-branch.sh
@@ -6,7 +6,7 @@ CROWDIN_BRANCH=i18n/crowdin
 git branch -D $CROWDIN_BRANCH
 git pull &&
   git checkout $CROWDIN_BRANCH &&
-  git merge -Xours main &&
+  git merge -m "Merge branch 'main' into i18n/crowdin" -Xours main &&
   npm run build:langs &&
   npm run langs:update-progress &&
   git status &&
