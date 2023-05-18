1588c22cfe553a6988f6d0162d03a3997ceed2e8
Leo Kewitz <leo@opencollective.com>
2023-05-18 11:25:27 +0200

    Expense "On Hold" logic (#8896)
    * feat: add HOLD/RELEASE expense actions
    
    * refact: add ON HOLD pseudo status and filter
    
    * feat: add HOLD/RELEASE activities
    
    * feat: smart private comment default state
    
    * chore: update langs and schemas

--- components/expenses/graphql/fragments.ts
+++ components/expenses/graphql/fragments.ts
@@ -150,6 +150,7 @@ export const expensePageExpenseFieldsFragment = gql`
     currency
     type
     status
+    onHold
     privateMessage
     tags
     amount
@@ -390,6 +391,8 @@ export const expensePageExpenseFieldsFragment = gql`
       canUnschedulePayment
       canVerifyDraftExpense
       canUsePrivateNote
+      canHold
+      canRelease
       approve {
         allowed
         reason
@@ -540,6 +543,8 @@ export const expensesListFieldsFragment = gql`
       canSeeInvoiceInfo
       canEditTags
       canUnschedulePayment
+      canHold
+      canRelease
       approve {
         allowed
         reason
@@ -587,6 +592,7 @@ export const expensesListFieldsFragment = gql`
 export const expensesListAdminFieldsFragment = gql`
   fragment ExpensesListAdminFieldsFragment on Expense {
     id
+    onHold
     payoutMethod {
       id
       type
