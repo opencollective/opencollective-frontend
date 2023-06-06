b9fd4de27b8673ab87d0ef36c553ac0e0ad5a57d
Benjamin Piouffle <benjamin@opencollective.com>
2023-06-06 10:39:17 +0200

    Disable CSS animations for lost-pixel (#8940)

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -1,10 +1,20 @@
 // eslint-disable-next-line node/no-unpublished-import
 import { CustomProjectConfig } from 'lost-pixel';
 
-export const config: CustomProjectConfig = {
+export const config: Partial<CustomProjectConfig> = {
   storybookShots: {
     storybookUrl: './storybook-static',
   },
   lostPixelProjectId: process.env.LOST_PIXEL_PROJECT_ID,
   apiKey: process.env.LOST_PIXEL_API_KEY,
+  beforeScreenshot: async page => {
+    await page.addStyleTag({
+      content: `
+        * {
+          animation-play-state: paused !important;
+          animation: none !important;
+        }
+      `,
+    });
+  },
 };
