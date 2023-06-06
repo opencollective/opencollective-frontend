691f1f37ebf374dd922f5051a098df96614b264c
Benjamin Piouffle <benjamin@opencollective.com>
2023-06-06 09:52:15 +0200

    fix: Disable CSS animations for lost-pixel

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, createGlobalStyle, css } from 'styled-components';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
 import { IntlProvider } from 'react-intl';
 import theme from '../lib/theme';
@@ -9,10 +9,25 @@ import UserProvider from '../components/UserProvider';
 import { initClient } from '../lib/apollo-client';
 import { withDesign } from 'storybook-addon-designs';
 
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'nprogress/nprogress.css';
 import 'trix/dist/trix.css';
 import '../public/static/styles/app.css';
 import ToastProvider from '../components/ToastProvider';
+import { parseToBoolean } from '../lib/utils';
+
+const MUST_DISABLE_ANIMATIONS = parseToBoolean(process.env.STORYBOOK_DISABLE_CSS_ANIMATIONS);
+
+const ControlAnimations = createGlobalStyle`
+  ${({ disable }) =>
+    disable &&
+    css`
+      * {
+        animation-play-state: paused !important;
+      }
+    `}
+`;
 
 Object.defineProperty(nextImage, 'default', {
   configurable: true,
@@ -33,6 +48,7 @@ export const decorators = [
         <IntlProvider locale="en">
           <UserProvider>
             <ToastProvider>
+              <ControlAnimations disable={MUST_DISABLE_ANIMATIONS} />
               <Story />
             </ToastProvider>
           </UserProvider>
