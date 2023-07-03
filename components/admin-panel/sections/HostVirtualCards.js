2efd124f3642b8c4c130f6621423bb85858b33e2
Henrique <hdiniz@users.noreply.github.com>
2023-07-03 17:46:01 -0300

    move create/assign buttons

--- components/admin-panel/sections/HostVirtualCards.js
+++ components/admin-panel/sections/HostVirtualCards.js
@@ -4,7 +4,6 @@ import { gql, useQuery } from '@apollo/client';
 import { isNil, omit, toLower, toUpper } from 'lodash';
 import { useRouter } from 'next/router';
 import { FormattedMessage } from 'react-intl';
-import styled from 'styled-components';
 
 import { API_V2_CONTEXT } from '../../../lib/graphql/helpers';
 import useQueryFilter, { AmountRangeFilter, BooleanFilter, DateRangeFilter } from '../../../lib/hooks/useQueryFilter';
@@ -16,7 +15,6 @@ import { Box, Flex, Grid } from '../../Grid';
 import { getI18nLink } from '../../I18nFormatters';
 import Loading from '../../Loading';
 import Pagination from '../../Pagination';
-import StyledButton from '../../StyledButton';
 import { P } from '../../Text';
 import { TOAST_TYPE, useToasts } from '../../ToastProvider';
 import VirtualCardFilters from '../../VirtualCardFilters';
@@ -112,11 +110,6 @@ const hostVirtualCardsQuery = gql`
       }
     }
   }
-`;
-
-const AddCardPlaceholder = styled(Flex)`
-  border-radius: 20px;
-  ${props => `border: 1px dashed ${props.theme.colors.primary[500]};`}
 `;
 
 const VIRTUAL_CARDS_PER_PAGE = 6;
@@ -227,6 +220,8 @@ const HostVirtualCards = props => {
             onTotalSpentChange={queryFilter.setTotalSpent}
             searchTerm={queryFilter.values.searchTerm}
             onSearchTermChange={queryFilter.setSearchTerm}
+            onCreateCardClick={() => setCreateVirtualCardModalDisplay(true)}
+            onAssignCardClick={() => setAssignCardModalDisplay(true)}
           />
         </Flex>
       </Box>
@@ -235,50 +230,6 @@ const HostVirtualCards = props => {
       ) : (
         <React.Fragment>
           <Grid mt={4} gridTemplateColumns={['100%', '366px 366px']} gridGap="32px 24px">
-            <AddCardPlaceholder
-              width="366px"
-              height="248px"
-              justifyContent="center"
-              alignItems="center"
-              flexDirection="column"
-            >
-              <StyledButton
-                my={1}
-                px={14}
-                py={10}
-                buttonStyle="primary"
-                buttonSize="medium"
-                data-cy="confirmation-modal-continue"
-                onClick={() => setCreateVirtualCardModalDisplay(true)}
-              >
-                +
-              </StyledButton>
-              <Box mt="10px">
-                <FormattedMessage defaultMessage="Create virtual card" />
-              </Box>
-            </AddCardPlaceholder>
-            <AddCardPlaceholder
-              width="366px"
-              height="248px"
-              justifyContent="center"
-              alignItems="center"
-              flexDirection="column"
-            >
-              <StyledButton
-                my={1}
-                px={14}
-                py={10}
-                buttonStyle="primary"
-                buttonSize="medium"
-                data-cy="confirmation-modal-continue"
-                onClick={() => setAssignCardModalDisplay(true)}
-              >
-                +
-              </StyledButton>
-              <Box mt="10px">
-                <FormattedMessage id="Host.VirtualCards.AssignCard" defaultMessage="Assign Card" />
-              </Box>
-            </AddCardPlaceholder>
             {data.host.hostedVirtualCards.nodes.map(vc => (
               <VirtualCard
                 key={vc.id}
