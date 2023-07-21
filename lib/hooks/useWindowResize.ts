727da4a6fe58c218d30bc9559d174385684a4e88
Leo Kewitz <leo@opencollective.com>
2023-07-21 16:24:14 +0200

    refact: add viewport return type to useWindowResize hook

--- lib/hooks/useWindowResize.ts
+++ lib/hooks/useWindowResize.ts
@@ -1,15 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { throttle } from 'lodash';
 
-import { BREAKPOINTS } from '../withViewport';
+import { BREAKPOINTS, getViewportFromWidth, VIEWPORTS } from '../withViewport';
 
-export { BREAKPOINTS };
+export { BREAKPOINTS, VIEWPORTS };
 
-export const useWindowResize = onResizeCallback => {
+export const useWindowResize = (onResizeCallback?) => {
+  const [viewport, setViewport] = useState<keyof typeof VIEWPORTS>(VIEWPORTS.UNKNOWN);
+  const callback = (...args) => {
+    const newViewport = getViewportFromWidth(window.innerWidth);
+    if (newViewport !== viewport) {
+      setViewport(newViewport);
+    }
+    onResizeCallback?.(...args);
+  };
+
   useEffect(() => {
-    const debouncedCallback = throttle(onResizeCallback, 100);
+    const debouncedCallback = throttle(callback, 100);
     debouncedCallback();
     window.addEventListener('resize', debouncedCallback);
     return () => window.removeEventListener('resize', debouncedCallback);
   }, []);
+
+  return { viewport };
 };
