ffbed3f4fbc1707485a724b8e4a59d005116d6fc
Henrique <hdiniz@users.noreply.github.com>
2023-08-01 19:24:50 -0300

    Add two factor authentication settings

---
+++ components/two-factor-authentication/TwoFactorAuthenticationSettings.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import { Individual, UserTwoFactorMethod } from '../../lib/graphql/types/v2/graphql';
+
+import { Box } from '../Grid';
+import { P } from '../Text';
+
+import { AuthenticatorSettings } from './AuthenticatorSettings';
+import { DevicesSettings } from './DevicesSettings';
+import { SaveRecoveryCodesModal } from './SaveRecoveryCodesModal';
+
+type TwoFactorAuthenticationSettingsProps = {
+  userTwoFactorAuthenticationMethods: UserTwoFactorMethod[];
+  individual: Pick<Individual, 'id' | 'email'>;
+};
+
+export function TwoFactorAuthenticationSettings(props: TwoFactorAuthenticationSettingsProps) {
+  const [recoveryCodes, setRecoveryCodes] = React.useState(null);
+
+  return (
+    <React.Fragment>
+      <Box>
+        <P mb={3}>
+          <FormattedMessage
+            id="TwoFactorAuth.Setup.Info"
+            defaultMessage="Two-factor authentication adds an extra layer of security for your account when logging in or performing admin actions."
+          />
+        </P>
+
+        <Box>
+          <AuthenticatorSettings
+            onRecoveryCodes={codes => setRecoveryCodes(codes)}
+            individual={props.individual}
+            userTwoFactorAuthenticationMethods={props.userTwoFactorAuthenticationMethods}
+          />
+        </Box>
+
+        <Box mt={3}>
+          <DevicesSettings
+            onRecoveryCodes={codes => setRecoveryCodes(codes)}
+            individual={props.individual}
+            userTwoFactorAuthenticationMethods={props.userTwoFactorAuthenticationMethods}
+          />
+        </Box>
+      </Box>
+      {recoveryCodes && <SaveRecoveryCodesModal recoveryCodes={recoveryCodes} onClose={() => setRecoveryCodes(null)} />}
+    </React.Fragment>
+  );
+}
