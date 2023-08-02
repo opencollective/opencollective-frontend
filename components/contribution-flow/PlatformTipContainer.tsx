658896d2474182793cc1eb03f0e75d982c9b4d4d
Henrique <hdiniz@users.noreply.github.com>
2023-08-02 12:11:50 -0300

    Add other input to platform tip container

--- components/contribution-flow/PlatformTipContainer.tsx
+++ components/contribution-flow/PlatformTipContainer.tsx
@@ -1,16 +1,131 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
 
 import theme from '../../lib/theme';
 
 import { Box, Flex } from '../Grid';
 import Image from '../Image';
+import StyledButtonSet from '../StyledButtonSet';
+import StyledInputAmount from '../StyledInputAmount';
 import StyledLinkButton from '../StyledLinkButton';
 import { P } from '../Text';
 
 import { WhyPlatformTipModal } from './WhyPlatformTipModal';
 
-export function PlatformTipContainer() {
+const I18nMessages = defineMessages({
+  NO_THANKS: {
+    defaultMessage: 'No thanks',
+  },
+  OTHER: {
+    defaultMessage: 'Other',
+  },
+});
+
+export const enum PlatformTipOption {
+  NONE = 'NONE',
+  TEN_PERCENT = 'TEN_PERCENT',
+  FIFTEEN_PERCENT = 'FIFTEEN_PERCENT',
+  TWENTY_PERCENT = 'TWENTY_PERCENT',
+  OTHER = 'OTHER',
+}
+
+type PlatformTipInputProps = {
+  value: number;
+  selectedOption: PlatformTipOption;
+  amount: number;
+  currency: string;
+  onChange: (selectedOption: PlatformTipOption, value?: number) => void;
+  disabled: boolean;
+};
+
+function PlatformTipInput(props: PlatformTipInputProps) {
+  const intl = useIntl();
+
+  const options = React.useMemo(
+    () => ({
+      [PlatformTipOption.NONE]: {
+        label: intl.formatMessage(I18nMessages.NO_THANKS),
+        percent: 0,
+      },
+      [PlatformTipOption.TEN_PERCENT]: {
+        label: '10%',
+        percent: 0.1,
+      },
+      [PlatformTipOption.FIFTEEN_PERCENT]: {
+        label: '15%',
+        percent: 0.15,
+      },
+      [PlatformTipOption.TWENTY_PERCENT]: {
+        label: '20%',
+        percent: 0.2,
+      },
+      [PlatformTipOption.OTHER]: {
+        label: intl.formatMessage(I18nMessages.OTHER),
+        percent: 0.15,
+      },
+    }),
+    [intl],
+  );
+
+  const onOptionChange = React.useCallback(
+    (value: PlatformTipOption) => {
+      if (props.selectedOption === value) {
+        return;
+      }
+
+      props.onChange(value, options[value].percent * props.amount);
+    },
+    [props.onChange, props.selectedOption, props.amount, options],
+  );
+
+  const onOtherChange = React.useCallback(
+    value => {
+      props.onChange(props.selectedOption, value);
+    },
+    [props.onChange],
+  );
+
+  return (
+    <Box>
+      <StyledButtonSet
+        disabled={props.disabled}
+        items={[
+          PlatformTipOption.NONE,
+          PlatformTipOption.TEN_PERCENT,
+          PlatformTipOption.FIFTEEN_PERCENT,
+          PlatformTipOption.TWENTY_PERCENT,
+          PlatformTipOption.OTHER,
+        ]}
+        selected={props.selectedOption}
+        onChange={onOptionChange}
+      >
+        {({ item }) => options[item as number].label}
+      </StyledButtonSet>
+      {props.selectedOption === PlatformTipOption.OTHER && (
+        <Flex mt={3} justifyContent="flex-end">
+          <StyledInputAmount
+            id="feesOnTop"
+            name="platformTip"
+            disabled={props.disabled}
+            currency={props.currency}
+            onChange={onOtherChange}
+            value={props.value}
+          />
+        </Flex>
+      )}
+    </Box>
+  );
+}
+
+type PlatformTipContainerProps = {
+  value: number;
+  selectedOption: PlatformTipOption;
+  amount: number;
+  currency: string;
+  onChange: (selectedOption: PlatformTipOption, value?: number) => void;
+};
+
+export function PlatformTipContainer(props: PlatformTipContainerProps) {
   const [isWhyPlatformTipModalOpen, setIsWhyPlatformTipModalOpen] = React.useState(false);
   return (
     <React.Fragment>
@@ -40,7 +155,16 @@ export function PlatformTipContainer() {
             </StyledLinkButton>
           </P>
         </Flex>
-        <Box mt="12px">input</Box>
+        <Box mt="12px">
+          <PlatformTipInput
+            disabled={!props.amount}
+            amount={props.amount}
+            currency={props.currency}
+            selectedOption={props.selectedOption}
+            value={props.value}
+            onChange={props.onChange}
+          />
+        </Box>
         <P mt="12px" fontWeight="400" fontSize="16px">
           <FormattedMessage defaultMessage="Thanks for your help." />
         </P>
--- stories/contribution-flow/PlatformTipContainer.stories.tsx
+++ stories/contribution-flow/PlatformTipContainer.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { PlatformTipContainer } from '../../components/contribution-flow/PlatformTipContainer';
+import { PlatformTipContainer, PlatformTipOption } from '../../components/contribution-flow/PlatformTipContainer';
+import { Box } from '../../components/Grid';
 
 const meta = {
   component: PlatformTipContainer,
@@ -8,6 +9,36 @@ const meta = {
 
 export default meta;
 
+function DefaultStory() {
+  const amount = 10000;
+  const [value, setValue] = React.useState(10000 * 0.15);
+  const [selectedOption, setSelectedOption] = React.useState(PlatformTipOption.FIFTEEN_PERCENT);
+  return (
+    <Box>
+      <PlatformTipContainer
+        value={value}
+        selectedOption={selectedOption}
+        amount={amount}
+        currency="USD"
+        onChange={(option, value) => {
+          setValue(value);
+          setSelectedOption(option);
+        }}
+      />
+      <Box mt={4}>
+        Amount: {amount}
+        <br />
+        Platform tip: {value}
+        <br />
+        Platform tip option: {selectedOption}
+        <br />
+      </Box>
+    </Box>
+  );
+}
+
 export const Default = {
-  render: () => <PlatformTipContainer />,
+  render: () => {
+    return <DefaultStory />;
+  },
 };
