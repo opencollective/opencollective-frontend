c59eea1041d9d0ce92fdb8f4f489602e72e03e6a
Henrique <hdiniz@users.noreply.github.com>
2023-09-01 13:36:11 -0300

    Lazy load intl locale messages

--- lib/i18n/request.ts
+++ lib/i18n/request.ts
@@ -1,3 +1,4 @@
+import Sentry from '@sentry/nextjs';
 import accepts from 'accepts';
 import cookie from 'cookie';
 import { pick } from 'lodash';
@@ -5,7 +6,7 @@ import { NextPageContext } from 'next';
 
 const supportedLanguages = ['en'];
 
-const languages = require.context('../../lang', false, /\.json$/i);
+const languages = require.context('../../lang', false, /\.json$/i, 'lazy');
 
 languages.keys().forEach(element => {
   const match = element.match(/\.?\/?([^.]+)\.json$/);
@@ -17,50 +18,54 @@ languages.keys().forEach(element => {
 type IntlProps = {
   language?: string;
   locale: string;
-  messages: Record<string, string>;
 };
 
 export function getIntlProps(ctx: NextPageContext): IntlProps {
   if (!ctx?.req) {
-    return pick(window.__NEXT_DATA__.props, 'language', 'locale', 'messages');
+    return pick(window.__NEXT_DATA__.props, 'language', 'locale');
   }
 
   const { language, locale } = getRequestIntl(ctx.req);
-  const messages = getLocaleMessages(locale);
   return {
     language,
     locale,
-    messages,
   };
 }
 
-export function getRequestIntl(req: NextPageContext['req']): Pick<IntlProps, 'language' | 'locale'> {
-  let language: string, locale: string;
+export function getRequestIntl(req: NextPageContext['req']): IntlProps {
+  try {
+    let language: string, locale: string;
 
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const queryLanguage = url.searchParams.get('language');
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const queryLanguage = url.searchParams.get('language');
 
-  if (queryLanguage && supportedLanguages.includes(queryLanguage)) {
-    language = queryLanguage;
-  } else {
-    const cookies = cookie.parse(req?.headers?.['cookie'] ?? '');
-    const cookieLanguage = cookies?.['language'];
+    if (queryLanguage && supportedLanguages.includes(queryLanguage)) {
+      language = queryLanguage;
+    } else {
+      const cookies = cookie.parse(req?.headers?.['cookie'] ?? '');
+      const cookieLanguage = cookies?.['language'];
 
-    if (cookieLanguage && supportedLanguages.includes(cookieLanguage)) {
-      language = cookieLanguage;
+      if (cookieLanguage && supportedLanguages.includes(cookieLanguage)) {
+        language = cookieLanguage;
+      }
     }
-  }
 
-  if (['test', 'e2e', 'ci'].includes(process.env.OC_ENV)) {
-    locale = language || 'en';
-  } else {
-    locale = language || accepts(req).language(supportedLanguages) || 'en';
-  }
+    if (['test', 'e2e', 'ci'].includes(process.env.OC_ENV)) {
+      locale = language || 'en';
+    } else {
+      locale = language || accepts(req).language(supportedLanguages) || 'en';
+    }
 
-  return {
-    language,
-    locale,
-  };
+    return {
+      language,
+      locale,
+    };
+  } catch (e) {
+    Sentry.captureException(e);
+    return {
+      locale: 'en',
+    };
+  }
 }
 
 export function getLocaleMessages(locale: string) {
