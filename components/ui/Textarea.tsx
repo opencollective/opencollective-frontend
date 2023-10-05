72e93bf2dcfa2a51fe4a8afcb308a2715485ba30
Gustav Larsson <gustav.larsson@gmail.com>
2023-10-05 14:28:33 +0200

    Add showCount prop to ui/Textarea

--- components/ui/Textarea.tsx
+++ components/ui/Textarea.tsx
@@ -2,20 +2,44 @@ import * as React from 'react';
 
 import { cn } from '../../lib/utils';
 
-export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+import { Badge } from './Badge';
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ className, ...props }, ref) => {
-  return (
-    <textarea
-      className={cn(
-        'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  showCount?: boolean;
+}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, showCount = false, ...props }, ref) => {
+    const value = (props.value || props.defaultValue || '') as string;
+
+    const input = (
+      <textarea
+        className={cn(
+          'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+
+    if (!showCount) {
+      return input;
+    }
+    return (
+      <div className="relative">
+        {input}
+        <div className="absolute bottom-1.5 right-1.5">
+          <Badge size="sm">
+            <span>{value.length}</span>
+            {props.maxLength && <span className="text-muted-foreground">/{props.maxLength}</span>}
+          </Badge>
+        </div>
+      </div>
+    );
+  },
+);
+
 Textarea.displayName = 'Textarea';
 
 export { Textarea };
