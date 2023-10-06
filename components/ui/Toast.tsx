8b75ec5c8b4a0d8aa8dd8d410c366cf1273ebc0d
Gustav Larsson <gustav.larsson@gmail.com>
2023-10-06 11:48:20 +0200

    Add data-cy to dismiss toast button

--- components/ui/Toast.tsx
+++ components/ui/Toast.tsx
@@ -97,6 +97,7 @@ const ToastClose = React.forwardRef<
       'absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600',
       className,
     )}
+    data-cy="dismiss-toast-btn"
     toast-close=""
     {...props}
   >
