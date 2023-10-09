096e1b2bdaa37183d528d2ad30e2455966164c94
Leo Kewitz <leo@opencollective.com>
2023-10-09 08:57:24 +0200

    refact: accept className prop on components created with elementFromClass

--- lib/react-utils.ts
+++ lib/react-utils.ts
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { cn } from './utils';
+
 /**
  * Merge react refs.
  * Adapted from https://github.com/smooth-code/react-merge-refs
@@ -25,12 +27,19 @@ export function elementFromClass<T>(type: T, className: string): T;
 // eslint-disable-next-line no-redeclare
 export function elementFromClass(type, className) {
   const render = (
-    { children, ...props }: Parameters<typeof React.createElement>[1] & { children: React.ReactNode },
+    {
+      children,
+      className: propsClassName,
+      ...props
+    }: Parameters<typeof React.createElement>[1] & { children: React.ReactNode; className?: string },
     ref,
   ) => {
     children = Array.isArray(children) ? children : [children];
-    // TODO: Merge classNames using Gustav's helper function
-    return React.createElement(type, { ...props, className, ref } as any, ...(children as any));
+    return React.createElement(
+      type,
+      { ...props, className: cn(className, propsClassName), ref } as any,
+      ...(children as any),
+    );
   };
   render.displayName = typeof type === 'string' ? type : type.name;
   return React.forwardRef(render);
