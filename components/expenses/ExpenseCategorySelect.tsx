8e88ccd0ed5a81e1147d6b533bf6b7caef1f1322
Benjamin Piouffle <benjamin@opencollective.com>
2023-11-09 12:18:38 +0100

    feat(Expense): show selection info for accounting category

--- components/expenses/ExpenseCategorySelect.tsx
+++ components/expenses/ExpenseCategorySelect.tsx
@@ -13,6 +13,8 @@ import { Separator } from '../ui/Separator';
 type ExpenseCategorySelectProps = {
   host: Host;
   selectedCategory: AccountingCategory | undefined | null;
+  submitterCategory?: AccountingCategory;
+  accountAdminCategory?: AccountingCategory;
   onChange: (category: AccountingCategory) => void;
   allowNone?: boolean;
   id?: string;
@@ -36,32 +38,103 @@ const getSearchTextFromCategory = (category: AccountingCategory) => {
     .toLocaleLowerCase();
 };
 
-const getCategoryLabel = (intl: IntlShape, category: AccountingCategory) => {
-  if (category === null) {
-    return intl.formatMessage({ defaultMessage: "I don't know" });
-  } else if (category) {
-    return category.friendlyName || category.name;
+/**
+ * Returns true if two categories are equal, which means:
+ * - They are the same object (or both are null)
+ * - Or they have the same id
+ * - Or they have the same code
+ */
+const isSameCategory = (category1: AccountingCategory | null, category2: AccountingCategory | null): boolean => {
+  return category1 === category2 || category1?.id === category2?.id || category1?.code === category2?.code;
+};
+
+/**
+ * Returns a message to display next to the category label to show which role selected which category.
+ */
+const getSelectionInfoForLabel = (
+  category: AccountingCategory | null,
+  submitterCategory: AccountingCategory | undefined | null,
+  accountAdminCategory: AccountingCategory | undefined | null,
+): React.ReactNode => {
+  if (!submitterCategory && !accountAdminCategory) {
+    return null;
+  }
+
+  const isSelectedBySubmitter = isSameCategory(category, submitterCategory);
+  const isSelectedByAccountAdmin = isSameCategory(category, accountAdminCategory);
+  if (isSelectedBySubmitter || isSelectedByAccountAdmin) {
+    return (
+      <span className="ml-1 text-xs font-normal italic text-gray-700">
+        {'• '}
+        <FormattedMessage
+          id="accountingCategory.selectedBy"
+          defaultMessage="Selected by {isSelectedBySubmitter, select, true {submitter} other {}}{hasBoth, select, true { and } other {}}{isSelectedByAccountAdmin, select, true {collective admin} other {}}"
+          values={{
+            isSelectedBySubmitter,
+            isSelectedByAccountAdmin,
+            hasBoth: isSelectedBySubmitter && isSelectedByAccountAdmin,
+          }}
+        />
+      </span>
+    );
   }
 };
 
-const getOptions = (intl: IntlShape, host: Host, allowNone: boolean) => {
+const getCategoryLabel = (
+  intl: IntlShape,
+  category: AccountingCategory,
+  submitterCategory: AccountingCategory | null,
+  accountAdminCategory: AccountingCategory | null,
+) => {
+  // Get category label
+  let result;
+  if (category === null) {
+    result = intl.formatMessage({ defaultMessage: "I don't know" });
+  } else if (category) {
+    result =
+      category.friendlyName ||
+      category.name ||
+      intl.formatMessage({ id: 'accountingCategory.doNotKnow', defaultMessage: 'Unknown category' });
+  }
+
+  // Add selection info
+  const selectionInfo = getSelectionInfoForLabel(category, submitterCategory, accountAdminCategory);
+  if (selectionInfo) {
+    result = (
+      <React.Fragment>
+        {result}
+        {selectionInfo}
+      </React.Fragment>
+    );
+  }
+
+  return result;
+};
+
+const getOptions = (
+  intl: IntlShape,
+  host: Host,
+  allowNone: boolean,
+  submitterCategory: AccountingCategory | null,
+  accountAdminCategory: AccountingCategory | null,
+): OptionsMap => {
   const hostCategories = get(host, 'accountingCategories.nodes', []);
   const categoriesById: OptionsMap = {};
 
   hostCategories.forEach(category => {
     categoriesById[category.id] = {
       value: category,
-      label: getCategoryLabel(intl, category),
+      label: getCategoryLabel(intl, category, submitterCategory, accountAdminCategory),
       searchText: getSearchTextFromCategory(category),
     };
   });
 
   if (allowNone) {
-    const label = getCategoryLabel(intl, null);
+    const label = getCategoryLabel(intl, null, submitterCategory, accountAdminCategory);
     categoriesById[VALUE_NONE] = {
       value: null,
       label,
-      searchText: label.toLocaleLowerCase(),
+      searchText: intl.formatMessage({ defaultMessage: "I don't know" }).toLocaleLowerCase(),
     };
   }
 
@@ -71,6 +144,8 @@ const getOptions = (intl: IntlShape, host: Host, allowNone: boolean) => {
 const ExpenseCategorySelect = ({
   host,
   selectedCategory,
+  submitterCategory,
+  accountAdminCategory,
   onChange,
   id,
   error,
@@ -78,8 +153,12 @@ const ExpenseCategorySelect = ({
   children,
 }: ExpenseCategorySelectProps) => {
   const intl = useIntl();
-  const options = React.useMemo(() => getOptions(intl, host, allowNone), [host, allowNone]);
   const [isOpen, setOpen] = React.useState(false);
+  const options = React.useMemo(
+    () => getOptions(intl, host, allowNone, submitterCategory, accountAdminCategory),
+    [host, allowNone, submitterCategory, accountAdminCategory],
+  );
+
   return (
     <div>
       <Popover open={isOpen} onOpenChange={setOpen}>
@@ -93,7 +172,8 @@ const ExpenseCategorySelect = ({
               })}
             >
               <span className={cn('mr-3 max-w-[328px]  truncate', { 'text-gray-400': isUndefined(selectedCategory) })}>
-                {getCategoryLabel(intl, selectedCategory) || intl.formatMessage({ defaultMessage: 'Select category' })}
+                {getCategoryLabel(intl, selectedCategory, submitterCategory, accountAdminCategory) ||
+                  intl.formatMessage({ defaultMessage: 'Select category' })}
               </span>
               <ChevronDown size="1em" />
             </button>
@@ -101,7 +181,7 @@ const ExpenseCategorySelect = ({
         </PopoverTrigger>
         <PopoverContent className="z-[5000] w-[320px] p-0">
           <Command filter={(categoryId, search) => (options[categoryId].searchText.includes(search) ? 1 : 0)}>
-            {size(options) > 10 && <CommandInput placeholder="Filter by name" />}
+            {size(options) > 6 && <CommandInput placeholder="Filter by name" />}
             <CommandEmpty>
               <FormattedMessage defaultMessage="No category found" />
             </CommandEmpty>
@@ -110,7 +190,7 @@ const ExpenseCategorySelect = ({
                 <React.Fragment key={categoryId}>
                   <CommandItem
                     value={categoryId}
-                    className={cn('p-3 text-sm', { 'font-semibold': selectedCategory?.id === categoryId })}
+                    className={cn('block p-3 text-xs', { 'font-semibold': selectedCategory?.id === categoryId })}
                     onSelect={categoryId => {
                       onChange(options[categoryId].value);
                       setOpen(false);
