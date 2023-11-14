fbe0d918a299be6573a24046c3fd037c85e82f8f
François Hodierne <francois@hodierne.net>
2023-11-14 17:24:37 +0100

    follow up

--- pages/api/legacy/contribute.js
+++ pages/api/legacy/contribute.js
@@ -2,8 +2,8 @@ import path from 'path';
 
 export default function handler(req, res) {
   const color = req.query.color === 'blue' ? 'blue' : 'white';
-  const verb = req.query.verb ? req.query.verb : 'contribute';
-  const size = req.query.size ? req.query.size : '';
+  const verb = ['donate', 'contribute'].includes(req.query.verb) ? req.query.verb : 'contribute';
+  const size = req.query.size === '2x' ? req.query.size : '';
   res.set('Cache-Control', `public, max-age=86400`);
   res.sendFile(path.join(process.cwd(), `public/static/images/buttons/${verb}-button-${color}${size}.png`));
 }
