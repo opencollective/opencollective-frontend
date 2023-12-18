1f8cbd8dd83cf33e1707c665750fd4559d0d02d7
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2023-12-18 09:01:40 -0300

    feat: move preview features modal to a global context
    Co-authored-by: Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>

---
+++ components/PreviewFeaturesProvider.js
@@ -0,0 +1,38 @@
+import React, { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const PreviewFeaturesContext = React.createContext({
+  setShowPreviewFeatures: () => {},
+});
+
+const PreviewFeaturesProvider = ({ children }) => {
+  const [showPreviewFeatures, setShowPreviewFeatures] = useState(false);
+
+  const context = {
+    showPreviewFeatures,
+    setShowPreviewFeatures,
+  };
+
+  return <PreviewFeaturesContext.Provider value={context}>{children}</PreviewFeaturesContext.Provider>;
+};
+
+PreviewFeaturesProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export const usePreviewFeatures = () => {
+  return useContext(PreviewFeaturesContext);
+};
+
+export const withPreviewFeatures = WrappedComponent => {
+  const WithPreviewFeatures = props => (
+    <PreviewFeaturesContext.Consumer>
+      {context => <WrappedComponent {...context} {...props} />}
+    </PreviewFeaturesContext.Consumer>
+  );
+
+  WithPreviewFeatures.getInitialProps = WrappedComponent.getInitialProps;
+  return WithPreviewFeatures;
+};
+
+export default PreviewFeaturesProvider;
