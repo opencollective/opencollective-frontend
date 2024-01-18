c44420f23ee495f5bd182962efeeb6b340df0264
Benjamin Piouffle <benjamin@opencollective.com>
2024-01-18 11:49:29 +0100

    enhancement(ExpenseForm): limit item currency to RECEIPTs only (#9922)

--- components/expenses/ExpenseForm.js
+++ components/expenses/ExpenseForm.js
@@ -24,7 +24,12 @@ import { userMustSetAccountingCategory } from './lib/accounting-categories';
 import { expenseTypeSupportsAttachments } from './lib/attachments';
 import { addNewExpenseItem, newExpenseItem } from './lib/items';
 import { checkExpenseSupportsOCR, updateExpenseFormWithUploadResult } from './lib/ocr';
-import { checkRequiresAddress, getSupportedCurrencies, validateExpenseTaxes } from './lib/utils';
+import {
+  checkRequiresAddress,
+  expenseTypeSupportsItemCurrency,
+  getSupportedCurrencies,
+  validateExpenseTaxes,
+} from './lib/utils';
 
 import ConfirmationModal from '../ConfirmationModal';
 import { Box, Flex } from '../Grid';
@@ -416,6 +421,14 @@ const ExpenseFormBody = ({
     // Reset the accounting category (if not supported by the new expense type)
     if (values.accountingCategory && !isSupportedExpenseCategory(values.type, values.accountingCategory, isHostAdmin)) {
       formik.setFieldValue('accountingCategory', undefined);
+    }
+
+    // If the new type does not support setting items currency, reset it
+    if (!expenseTypeSupportsItemCurrency(values.type)) {
+      const itemHasExpenseCurrency = item => !item.amountV2?.currency || item.amountV2?.currency === values.currency;
+      const resetItemAmount = item => ({ ...item, amount: null, amountV2: null });
+      const updatedItems = values.items.map(item => (itemHasExpenseCurrency(item) ? item : resetItemAmount(item)));
+      formik.setFieldValue('items', updatedItems);
     }
   }, [values.type]);
 
