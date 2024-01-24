eb6dff8266aef56f96b985ae9538f2b2b60fd56e
Henrique <hdiniz@users.noreply.github.com>
2024-01-23 21:59:10 -0300

    Refactor accounting category page (#9923)
    * Refactor accounting category page
    
    * update graphql
    
    * fix deep scan issues
    
    * fix deep scan issue
    
    * Adapt order by filter to other fields
    
    * Add order by code
    
    * update langs
    
    * update lang
    
    * Add more filters
    
    * Add combo select filter helper
    
    * update types

---
+++ components/admin-panel/sections/accounting/AccountingCategoryDrawer.tsx
@@ -0,0 +1,194 @@
+import React from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+
+import { AccountingCategory, AccountingCategoryKind } from '../../../../lib/graphql/types/v2/graphql';
+import { i18nExpenseType } from '../../../../lib/i18n/expense';
+
+import { Drawer, DrawerActions, DrawerHeader } from '../../../Drawer';
+import StyledButton from '../../../StyledButton';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../../../ui/DropdownMenu';
+
+import {
+  AccountingCategoryForm,
+  AccountingCategoryKindI18n,
+  useAccoutingCategoryFormik,
+} from './AccountingCategoryForm';
+
+type AccountingCategoryDrawerProps = {
+  open: boolean;
+  onClose: () => void;
+  onEdit: (
+    category: Pick<AccountingCategory, 'id' | 'kind' | 'name' | 'friendlyName' | 'code' | 'expensesTypes'>,
+  ) => void;
+  onDelete: (category: Pick<AccountingCategory, 'id'>) => void;
+  accountingCategory?: Pick<AccountingCategory, 'id' | 'kind' | 'name' | 'friendlyName' | 'code' | 'expensesTypes'>;
+};
+
+export function AccountingCategoryDrawer(props: AccountingCategoryDrawerProps) {
+  const [isEditing, setIsEditing] = React.useState(false);
+
+  React.useEffect(() => {
+    if (!props.open) {
+      setIsEditing(false);
+    }
+  }, [props.open]);
+
+  return (
+    <Drawer maxWidth="512px" open={props.open} onClose={props.onClose} showActionsContainer>
+      <DrawerHeader title={props.accountingCategory?.name} onClose={props.onClose} />
+
+      {!isEditing && (
+        <AccountingCategoryDrawerView
+          onEditClick={() => setIsEditing(true)}
+          onDeleteClick={() => props.onDelete(props.accountingCategory)}
+          accountingCategory={props.accountingCategory}
+        />
+      )}
+      {isEditing && (
+        <AccountingCategoryEditingDrawerView
+          accountingCategory={props.accountingCategory}
+          onEdit={props.onEdit}
+          onExitEdit={() => setIsEditing(false)}
+        />
+      )}
+    </Drawer>
+  );
+}
+
+type AccountingCategoryDrawerViewProps = {
+  accountingCategory?: Pick<AccountingCategory, 'id' | 'kind' | 'name' | 'friendlyName' | 'code' | 'expensesTypes'>;
+  onEditClick: () => void;
+  onDeleteClick: () => void;
+};
+
+function AccountingCategoryDrawerView(props: AccountingCategoryDrawerViewProps) {
+  const intl = useIntl();
+  return (
+    <React.Fragment>
+      <div>
+        <label className="mb-1 text-base">
+          <FormattedMessage defaultMessage="Applies to" />
+        </label>
+        <p>
+          {props.accountingCategory?.kind && (
+            <FormattedMessage {...AccountingCategoryKindI18n[props.accountingCategory?.kind]} />
+          )}
+        </p>
+        <label className="mb-1 mt-4 text-base">
+          <FormattedMessage defaultMessage="Category name" />
+        </label>
+        <p>{props.accountingCategory?.name}</p>
+
+        {props.accountingCategory?.friendlyName && (
+          <React.Fragment>
+            <label className="mb-1 mt-4 text-base">
+              <FormattedMessage id="AccountingCategory.friendlyName" defaultMessage="Friendly name" />
+            </label>
+            <p className="italic">{props.accountingCategory?.friendlyName}</p>
+          </React.Fragment>
+        )}
+
+        <label className="mb-1 mt-4 text-base">
+          <FormattedMessage defaultMessage="Accounting code" />
+        </label>
+
+        <p>
+          <span className="inline-block rounded-xl bg-slate-50 px-2 py-1 font-bold text-slate-800">
+            {props.accountingCategory?.code}
+          </span>
+        </p>
+
+        <label className="mb-1 mt-4 text-base">
+          <FormattedMessage defaultMessage="Expense types" />
+        </label>
+        <p>
+          {props.accountingCategory?.expensesTypes ? (
+            props.accountingCategory.expensesTypes.map(value => i18nExpenseType(intl, value)).join(', ')
+          ) : (
+            <FormattedMessage id="AllExpenses" defaultMessage="All expenses" />
+          )}
+        </p>
+      </div>
+      <DrawerActions>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <StyledButton>
+              <FormattedMessage defaultMessage="More actions" />
+            </StyledButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <React.Fragment>
+              <DropdownMenuItem className="cursor-pointer text-red-500" onClick={props.onDeleteClick}>
+                <FormattedMessage id="actions.delete" defaultMessage="Delete" />
+              </DropdownMenuItem>
+            </React.Fragment>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <StyledButton buttonStyle="secondary" onClick={props.onEditClick}>
+          <FormattedMessage defaultMessage="Edit category" />
+        </StyledButton>
+      </DrawerActions>
+    </React.Fragment>
+  );
+}
+
+type AccountingCategoryEditingDrawerViewProps = {
+  accountingCategory?: Pick<AccountingCategory, 'id' | 'kind' | 'name' | 'friendlyName' | 'code' | 'expensesTypes'>;
+  onEdit: (
+    category: Pick<AccountingCategory, 'id' | 'kind' | 'name' | 'friendlyName' | 'code' | 'expensesTypes'>,
+  ) => void;
+  onExitEdit: () => void;
+};
+
+function AccountingCategoryEditingDrawerView(props: AccountingCategoryEditingDrawerViewProps) {
+  const intl = useIntl();
+
+  const initialValues = React.useMemo(() => {
+    return {
+      name: props.accountingCategory?.name,
+      friendlyName: props.accountingCategory?.friendlyName,
+      code: props.accountingCategory?.code,
+      kind: {
+        value: props.accountingCategory?.kind ?? AccountingCategoryKind.EXPENSE,
+        label: intl.formatMessage(
+          AccountingCategoryKindI18n[props.accountingCategory?.kind || AccountingCategoryKind.EXPENSE],
+        ),
+      },
+      expensesTypes: props.accountingCategory?.expensesTypes
+        ? props.accountingCategory.expensesTypes.map(t => ({ value: t, label: i18nExpenseType(intl, t) }))
+        : null,
+    };
+  }, [props.accountingCategory, intl]);
+
+  const formik = useAccoutingCategoryFormik({
+    initialValues,
+    async onSubmit(values) {
+      try {
+        await props.onEdit({
+          ...values,
+          id: props.accountingCategory?.id,
+          kind: values.kind ? values.kind.value : null,
+          expensesTypes:
+            values.expensesTypes && values.expensesTypes.length > 0 ? values.expensesTypes.map(t => t.value) : null,
+        });
+        props.onExitEdit();
+      } catch (e) {
+        // toast handled by onEdit
+      }
+    },
+  });
+  return (
+    <React.Fragment>
+      <AccountingCategoryForm formik={formik} />
+      <DrawerActions>
+        <StyledButton onClick={props.onExitEdit}>
+          <FormattedMessage id="actions.cancel" defaultMessage="Cancel" />
+        </StyledButton>
+        <StyledButton buttonStyle="secondary" onClick={formik.submitForm}>
+          <FormattedMessage id="save" defaultMessage="Save" />
+        </StyledButton>
+      </DrawerActions>
+    </React.Fragment>
+  );
+}
--- components/admin-panel/sections/accounting/index.tsx
+++ components/admin-panel/sections/accounting/index.tsx
@@ -1,20 +1,276 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { gql, useMutation, useQuery } from '@apollo/client';
+import { pick } from 'lodash';
+import { PlusIcon } from 'lucide-react';
+import { defineMessage, FormattedMessage, useIntl } from 'react-intl';
+import { z } from 'zod';
+
+import { i18nGraphqlException } from '../../../../lib/errors';
+import { API_V2_CONTEXT } from '../../../../lib/graphql/helpers';
+import {
+  AccountingCategory,
+  AccountingCategoryKind,
+  AdminAccountingCategoriesQuery,
+  AdminAccountingCategoriesQueryVariables,
+} from '../../../../lib/graphql/types/v2/graphql';
+import useLoggedInUser from '../../../../lib/hooks/useLoggedInUser';
+import useQueryFilter from '../../../../lib/hooks/useQueryFilter';
 
 import DashboardHeader from '../../../dashboard/DashboardHeader';
+import { buildComboSelectFilter } from '../../../dashboard/filters/ComboSelectFilter';
+import { Filterbar } from '../../../dashboard/filters/Filterbar';
+import { buildOrderByFilter } from '../../../dashboard/filters/OrderFilter';
+import { searchFilter } from '../../../dashboard/filters/SearchFilter';
 import { DashboardSectionProps } from '../../../dashboard/types';
+import MessageBoxGraphqlError from '../../../MessageBoxGraphqlError';
+import { Button } from '../../../ui/Button';
+import { useToast } from '../../../ui/useToast';
 
 import { AccountingCategoriesTable } from './AccountingCategoriesTable';
+import { AccountingCategoryKindI18n } from './AccountingCategoryForm';
+import { CreateAccountingCategoryModal } from './CreateAccountingCategoryModal';
+
+const accountingCategoriesQuery = gql`
+  query AdminAccountingCategories($hostSlug: String!) {
+    host(slug: $hostSlug) {
+      id
+      slug
+      accountingCategories {
+        totalCount
+        nodes {
+          id
+          kind
+          code
+          name
+          friendlyName
+          expensesTypes
+          createdAt
+        }
+      }
+    }
+  }
+`;
+
+// TODO adapt for host types other than organization
+const editAccountingCategoryMutation = gql`
+  mutation EditAccountingCategories($hostSlug: String!, $categories: [AccountingCategoryInput!]!) {
+    editAccountingCategories(account: { slug: $hostSlug }, categories: $categories) {
+      id
+      ... on Organization {
+        host {
+          id
+          slug
+          accountingCategories {
+            totalCount
+            nodes {
+              id
+              kind
+              code
+              name
+              friendlyName
+              expensesTypes
+              createdAt
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+function categoryToEditableFields(category: AccountingCategory) {
+  const editableFields = ['kind', 'code', 'name', 'friendlyName', 'expensesTypes'];
+  return pick(category, ['id', ...editableFields]);
+}
+
+const orderByCodeFilter = buildOrderByFilter(
+  z.enum(['CODE,DESC', 'CODE,ASC', 'NAME,DESC', 'NAME,ASC']).default('CODE,ASC'),
+  {
+    'CODE,DESC': defineMessage({ defaultMessage: 'Code descending' }),
+    'CODE,ASC': defineMessage({ defaultMessage: 'Code ascending' }),
+    'NAME,DESC': defineMessage({ defaultMessage: 'Name descending' }),
+    'NAME,ASC': defineMessage({ defaultMessage: 'Name ascending' }),
+  },
+);
+
+const appliesToFilter = buildComboSelectFilter(
+  z
+    .array(
+      z.enum([AccountingCategoryKind.ADDED_FUNDS, AccountingCategoryKind.CONTRIBUTION, AccountingCategoryKind.EXPENSE]),
+    )
+    .optional(),
+  defineMessage({ defaultMessage: 'Applies to' }),
+  AccountingCategoryKindI18n,
+);
 
 /**
  * The accounting sections lets host admins customize their chart of accounts.
  */
 export const HostAdminAccountingSection = ({ accountSlug }: DashboardSectionProps) => {
-  return (
-    <div className="flex max-w-screen-lg flex-col gap-4">
-      <DashboardHeader title={<FormattedMessage defaultMessage="Chart of Accounts" />} />
+  const { LoggedInUser } = useLoggedInUser();
+  const intl = useIntl();
+  const { toast } = useToast();
 
-      <AccountingCategoriesTable hostSlug={accountSlug} />
-    </div>
+  const [isCreateCategoryModalOpen, setIsCreateCategoryModalOpen] = React.useState(false);
+  const queryFilter = useQueryFilter({
+    schema: React.useMemo(
+      () =>
+        z.object({
+          searchTerm: searchFilter.schema,
+          orderBy: orderByCodeFilter.schema,
+          appliesTo: appliesToFilter.schema,
+        }),
+      [],
+    ),
+    filters: {
+      searchTerm: searchFilter.filter,
+      orderBy: orderByCodeFilter.filter,
+      appliesTo: appliesToFilter.filter,
+    },
+    toVariables: {
+      searchTerm: searchFilter.toVariables,
+      orderBy: orderByCodeFilter.toVariables,
+      appliesTo: appliesToFilter.toVariables,
+    },
+  });
+
+  const query = useQuery<AdminAccountingCategoriesQuery, AdminAccountingCategoriesQueryVariables>(
+    accountingCategoriesQuery,
+    {
+      context: API_V2_CONTEXT,
+      variables: {
+        hostSlug: accountSlug,
+      },
+    },
+  );
+
+  const categories = React.useMemo(
+    () => query.data?.host?.accountingCategories?.nodes || [],
+    [query.data?.host?.accountingCategories?.nodes],
+  );
+
+  const filterFn: (c: (typeof categories)[number]) => boolean = React.useMemo(() => {
+    if (!queryFilter.values.searchTerm && !queryFilter.values.appliesTo) {
+      return null;
+    }
+
+    const termRegExp = queryFilter.values.searchTerm && new RegExp(queryFilter.values.searchTerm, 'i');
+    return c => {
+      return (
+        (!termRegExp || termRegExp.test(c.code) || termRegExp.test(c.name) || termRegExp.test(c.friendlyName)) &&
+        (!queryFilter.values.appliesTo || queryFilter.values.appliesTo.includes(c.kind))
+      );
+    };
+  }, [queryFilter.values.searchTerm, queryFilter.values.appliesTo]);
+
+  const sortFn: (a: (typeof categories)[number], b: (typeof categories)[number]) => number = React.useMemo(() => {
+    return (a, b) => {
+      if (queryFilter.values.orderBy === 'CODE,ASC') {
+        return a.code.localeCompare(b.code);
+      } else if (queryFilter.values.orderBy === 'CODE,DESC') {
+        return b.code.localeCompare(a.code);
+      } else if (queryFilter.values.orderBy === 'NAME,ASC') {
+        return a.name.localeCompare(b.name);
+      } else if (queryFilter.values.orderBy === 'NAME,DESC') {
+        return b.name.localeCompare(a.name);
+      }
+    };
+  }, [queryFilter.values.orderBy]);
+
+  const filteredCategories = React.useMemo(() => {
+    let result = categories;
+
+    if (filterFn) {
+      result = result.filter(filterFn);
+    }
+
+    result = result.toSorted(sortFn);
+
+    return result;
+  }, [categories, sortFn, filterFn]);
+
+  const isAdmin = Boolean(LoggedInUser?.isAdminOfCollective(query.data?.host)); // Accountants can't edit accounting categories
+
+  const [editAccountingCategories] = useMutation(editAccountingCategoryMutation, {
+    context: API_V2_CONTEXT,
+    variables: {
+      hostSlug: accountSlug,
+    },
+  });
+
+  const updateCategories = React.useCallback(
+    async (newCategories, onSuccess = null) => {
+      try {
+        const cleanCategories = newCategories.map(categoryToEditableFields);
+        await editAccountingCategories({ variables: { categories: cleanCategories } });
+        toast({ variant: 'success', message: intl.formatMessage({ id: 'saved', defaultMessage: 'Saved' }) });
+        onSuccess && onSuccess();
+      } catch (e) {
+        toast({ variant: 'error', message: i18nGraphqlException(intl, e) });
+        throw e;
+      }
+    },
+    [intl, toast, editAccountingCategories],
+  );
+
+  const onDelete = React.useCallback(
+    async deleted => {
+      await updateCategories(categories.filter(c => c.id !== deleted.id));
+    },
+    [updateCategories, categories],
+  );
+
+  const onEdit = React.useCallback(
+    async (edited: Pick<AccountingCategory, 'id' | 'kind' | 'name' | 'friendlyName' | 'code' | 'expensesTypes'>) => {
+      await updateCategories([...categories.filter(c => c.id !== edited.id), edited]);
+    },
+    [updateCategories, categories],
+  );
+
+  const onCreate = React.useCallback(
+    async (
+      created: Pick<AccountingCategory, 'kind' | 'name' | 'friendlyName' | 'code' | 'expensesTypes'> & { id: never },
+    ) => {
+      await updateCategories([...categories, created], () => setIsCreateCategoryModalOpen(false));
+    },
+    [updateCategories, categories],
+  );
+
+  return (
+    <React.Fragment>
+      <div className="flex max-w-screen-lg flex-col gap-4">
+        <DashboardHeader
+          title={<FormattedMessage defaultMessage="Chart of Accounts" />}
+          description={
+            <FormattedMessage defaultMessage="Manage your accounting categories, and use these categories to keep your Collectives’ expenses organized." />
+          }
+          actions={
+            <Button size="sm" className="gap-1" onClick={() => setIsCreateCategoryModalOpen(true)}>
+              <span>
+                <FormattedMessage defaultMessage="Create category" />
+              </span>
+              <PlusIcon size={20} />
+            </Button>
+          }
+        />
+
+        <Filterbar {...queryFilter} />
+
+        {query.error && <MessageBoxGraphqlError error={query.error} />}
+
+        <AccountingCategoriesTable
+          hostSlug={accountSlug}
+          accountingCategories={filteredCategories}
+          isAdmin={isAdmin}
+          loading={query.loading}
+          isFiltered={!!queryFilter.values.searchTerm}
+          onDelete={onDelete}
+          onEdit={onEdit}
+        />
+      </div>
+      {isCreateCategoryModalOpen && (
+        <CreateAccountingCategoryModal onClose={() => setIsCreateCategoryModalOpen(false)} onCreate={onCreate} />
+      )}
+    </React.Fragment>
   );
 };
