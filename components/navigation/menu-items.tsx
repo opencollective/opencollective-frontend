fc12aef60721eed7d905472d765c313c27080ce8
serge <sergewilfried@outlook.fr>
2024-01-31 17:47:18 +0000

    chore(menu-items.tsx): update href values to '#' to disable links temporarily
    The href values for the Blog and About links in the landingPageItems array and the Use cases link in the regularFooterItems array have been updated to '#' to disable the links temporarily. This is done to prevent users from navigating to external pages that are not yet available or under construction.

--- components/navigation/menu-items.tsx
+++ components/navigation/menu-items.tsx
@@ -38,7 +38,7 @@ export const landingPageItems: Item[] = [
     items: [
       {
         label: <FormattedMessage id="company.blog" defaultMessage="Blog" />,
-        href: 'https://blog.opencollective.com/',
+        href: '#',
       },
       {
         label: <FormattedMessage id="OC.e2c" defaultMessage="Exit to Community" />,
@@ -46,7 +46,7 @@ export const landingPageItems: Item[] = [
       },
       {
         label: <FormattedMessage id="collective.about.title" defaultMessage="About" />,
-        href: 'https://docs.opencollective.com/help/about/introduction',
+        href: '#',
       },
     ],
   },
@@ -86,7 +86,7 @@ export const regularFooterItems: Item[] = [
       },
       {
         label: <FormattedMessage id="platform.useCases" defaultMessage="Use cases" />,
-        href: 'https://blog.opencollective.com/tag/case-studies/',
+        href: '#tag/case-studies/',
       },
       {
         label: <FormattedMessage id="platform.signup" defaultMessage="Sign up" />,
@@ -145,11 +145,11 @@ export const regularFooterItems: Item[] = [
     items: [
       {
         label: <FormattedMessage id="collective.about.title" defaultMessage="About" />,
-        href: 'https://docs.opencollective.com/help/about/introduction',
+        href: '#',
       },
       {
         label: <FormattedMessage id="company.blog" defaultMessage="Blog" />,
-        href: 'https://blog.opencollective.com/',
+        href: '#',
       },
       {
         label: <FormattedMessage id="company.hiring" defaultMessage="Hiring" />,
@@ -169,7 +169,7 @@ export const regularFooterItems: Item[] = [
       },
       {
         label: <FormattedMessage id="company.securityPolicy" defaultMessage="Security Policy" />,
-        href: 'https://docs.opencollective.com/help/product/security',
+        href: '#',
       },
       {
         label: <FormattedMessage id="contactUs" defaultMessage="Contact us" />,
