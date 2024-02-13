52e031e5601da54c6ee6a90a3042623122faeb48
François Hodierne <francois@hodierne.net>
2024-02-13 21:31:13 +0100

    update to @storybook/addon-designs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
+import { withDesign } from '@storybook/addon-designs';
 import { IntlProvider } from 'react-intl';
-import { withDesign } from 'storybook-addon-designs';
 import { ThemeProvider } from 'styled-components';
 
 import { initClient } from '../lib/apollo-client';
