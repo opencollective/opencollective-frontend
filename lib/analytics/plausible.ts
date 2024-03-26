9b78016f492befd7f5fc7b89307cc2d02a95d1d4
Henrique <hdiniz@users.noreply.github.com>
2024-03-26 15:06:05 -0300

    Normalize dashboard urls before submiting to plausible (#10230)

--- lib/analytics/plausible.ts
+++ lib/analytics/plausible.ts
@@ -6,22 +6,63 @@ declare global {
     plausible: (
       event: string,
       options: {
-        props: Record<string, string | boolean | number>;
+        u?: string;
+        props?: Record<string, string | boolean | number>;
       },
     ) => void;
   }
 }
 
 type TrackOptions = {
-  props: Record<AnalyticsProperty, string | number | boolean>;
+  props?: Record<AnalyticsProperty, string | number | boolean>;
 };
 
-export function track(event: AnalyticsEvent, options?: TrackOptions) {
+export function track(event: AnalyticsEvent | 'pageview', options: TrackOptions = {}) {
+  const location = normalizeLocation(window.location.href);
+
   if (process.env.NODE_ENV === 'development') {
     // eslint-disable-next-line no-console
-    console.log('plausible event', event, JSON.stringify(options));
+    console.log('plausible event', event, location, JSON.stringify(options));
   }
   if (window.plausible) {
-    window.plausible(event, options);
+    window.plausible(event, Object.assign(options, { u: location }));
   }
 }
+
+export function normalizeLocation(href: string): string {
+  const url = new URL(href);
+  const pathname = url.pathname;
+
+  if (pathname.startsWith('/dashboard/')) {
+    url.pathname = pathname.replace(/(\/dashboard\/)([^/]+)(.*)/, '$1[slug]$3');
+  }
+
+  return url.href;
+}
+
+// Adapted from https://github.com/plausible/analytics/blob/master/tracker/src/plausible.js to use with manual mode
+// See https://plausible.io/docs/custom-locations
+(function initPlausible() {
+  let lastLocationPathName;
+
+  function trackPageView() {
+    if (lastLocationPathName === location.pathname) {
+      return;
+    }
+    lastLocationPathName = location.pathname;
+    track('pageview');
+  }
+
+  if (typeof window !== 'undefined' && window.history.pushState) {
+    const originalPushState = window.history['pushState'];
+    window.history.pushState = function () {
+      originalPushState.apply(this, arguments);
+      trackPageView();
+    };
+    window.addEventListener('popstate', trackPageView);
+  }
+
+  if (typeof window !== 'undefined') {
+    trackPageView();
+  }
+})();
--- pages/_document.js
+++ pages/_document.js
@@ -62,8 +62,8 @@ export default class IntlDocument extends Document {
       domain: process.env.CLIENT_ANALYTICS_DOMAIN,
       scriptSrc:
         'development' === process.env.OC_ENV
-          ? 'https://plausible.io/js/script.tagged-events.exclusions.local.js'
-          : 'https://plausible.io/js/script.tagged-events.exclusions.js',
+          ? 'https://plausible.io/js/script.manual.tagged-events.exclusions.local.js'
+          : 'https://plausible.io/js/script.manual.tagged-events.exclusions.js',
       exclusions: process.env.CLIENT_ANALYTICS_EXCLUSIONS,
     };
 
