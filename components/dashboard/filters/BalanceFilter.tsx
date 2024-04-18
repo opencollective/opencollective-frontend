6772e988e8aabe08656eebd7414e154c744e7bf9
Leo Kewitz <leo@opencollective.com>
2024-04-18 09:14:28 +0200

    fix: remove unused filter

--- components/dashboard/filters/BalanceFilter.tsx
+++ components/dashboard/filters/BalanceFilter.tsx
@@ -6,22 +6,8 @@ import { FilterConfig } from '../../../lib/filters/filter-types';
 import { amountFilterSchema } from './AmountFilter/schema';
 import { amountFilter } from './AmountFilter';
 
-export const balanceFilter: FilterConfig<z.infer<typeof amountFilterSchema>> = {
-  schema: amountFilterSchema,
-  toVariables: (value: z.infer<typeof amountFilterSchema>) => ({
-    balance: {
-      gte: 'gte' in value ? { valueInCents: value.gte } : undefined,
-      lte: 'lte' in value ? { valueInCents: value.lte } : undefined,
-    },
-  }),
-  filter: {
-    ...amountFilter.filter,
-    labelMsg: defineMessage({ id: 'Balance', defaultMessage: 'Balance' }),
-  },
-};
-
 export const consolidatedBalanceFilter: FilterConfig<z.infer<typeof amountFilterSchema>> = {
-  ...balanceFilter,
+  schema: amountFilterSchema,
   toVariables: (value: z.infer<typeof amountFilterSchema>) => ({
     consolidatedBalance: {
       gte: 'gte' in value ? { valueInCents: value.gte } : undefined,
@@ -29,7 +15,7 @@ export const consolidatedBalanceFilter: FilterConfig<z.infer<typeof amountFilter
     },
   }),
   filter: {
-    ...balanceFilter.filter,
+    ...amountFilter.filter,
     labelMsg: defineMessage({ id: 'Balance', defaultMessage: 'Balance' }),
   },
 };
