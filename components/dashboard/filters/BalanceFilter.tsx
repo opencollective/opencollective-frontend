668962edef59c63a1ded106e56aad1fb5ca7a9c2
Leo Kewitz <leo@opencollective.com>
2024-04-19 16:38:28 +0200

    refact: remove useless IS_EQUAL_TO balance filter option

--- components/dashboard/filters/BalanceFilter.tsx
+++ components/dashboard/filters/BalanceFilter.tsx
@@ -1,10 +1,53 @@
-import { defineMessage } from 'react-intl';
+import React from 'react';
+import { pick } from 'lodash';
+import { defineMessage, useIntl } from 'react-intl';
 import { z } from 'zod';
 
 import { FilterConfig } from '../../../lib/filters/filter-types';
+import { i18nAmountFilterLabel } from '../../../lib/i18n/amount-filter';
 
-import { amountFilterSchema } from './AmountFilter/schema';
-import { amountFilter } from './AmountFilter';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../../ui/Select';
+
+import { AmountFilterValue } from './AmountFilter/AmountFilterValue';
+import { amountFilterSchema, AmountFilterType, AmountFilterValueType } from './AmountFilter/schema';
+import { renderOptions } from './AmountFilter';
+
+const BalanceFilterType = pick(AmountFilterType, ['IS_GREATER_THAN', 'IS_LESS_THAN', 'IS_BETWEEN']);
+
+// eslint-disable-next-line prefer-arrow-callback
+const BalanceFilter = React.memo(function BalanceFilter({
+  value,
+  onChange,
+}: {
+  value: AmountFilterValueType;
+  onChange: (value: AmountFilterValueType) => void;
+}) {
+  const intl = useIntl();
+  value = value ?? { type: AmountFilterType.IS_GREATER_THAN };
+
+  return (
+    <div className="flex flex-col gap-2 p-3">
+      <Select defaultValue={value.type} onValueChange={(type: AmountFilterType) => onChange({ type })}>
+        <SelectTrigger className="w-full">
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent
+          onCloseAutoFocus={e => {
+            e.preventDefault();
+          }}
+        >
+          {Object.values(BalanceFilterType).map(type => (
+            <SelectItem key={type} value={type}>
+              {i18nAmountFilterLabel(intl, type)}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      {renderOptions(value, onChange)}
+    </div>
+  );
+});
 
 export const consolidatedBalanceFilter: FilterConfig<z.infer<typeof amountFilterSchema>> = {
   schema: amountFilterSchema,
@@ -15,7 +58,8 @@ export const consolidatedBalanceFilter: FilterConfig<z.infer<typeof amountFilter
     },
   }),
   filter: {
-    ...amountFilter.filter,
+    Component: BalanceFilter,
     labelMsg: defineMessage({ id: 'Balance', defaultMessage: 'Balance' }),
+    valueRenderer: ({ value, meta }) => <AmountFilterValue value={value} currency={meta.currency} />,
   },
 };
