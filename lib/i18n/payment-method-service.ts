099925a3f0402dea4d9b11f2391d2dec9efab9c7
Benjamin Piouffle <benjamin@opencollective.com>
2024-04-24 09:19:53 +0200

    i18n: Misc fixes (#10342)

--- lib/i18n/payment-method-service.ts
+++ lib/i18n/payment-method-service.ts
@@ -1,3 +1,4 @@
+import { startCase } from 'lodash';
 import { defineMessages } from 'react-intl';
 
 import { WebsiteName } from '../../components/I18nFormatters';
@@ -5,29 +6,29 @@ import { WebsiteName } from '../../components/I18nFormatters';
 import { PAYMENT_METHOD_SERVICE } from '../constants/payment-methods';
 
 const i18nPaymentMethodServiceLabels = defineMessages({
-  [PAYMENT_METHOD_SERVICE.STRIPE]: {
-    defaultMessage: 'Stripe',
-    id: 'PaymentMethod.Stripe',
-  },
-  [PAYMENT_METHOD_SERVICE.WISE]: {
-    defaultMessage: 'Wise',
-    id: 'PaymentMethod.Wise',
-  },
-  [PAYMENT_METHOD_SERVICE.THEGIVINGBLOCK]: {
-    defaultMessage: 'The Giving Block',
-    id: 'PaymentMethod.TheGivingBlock',
-  },
-  [PAYMENT_METHOD_SERVICE.PAYPAL]: {
-    defaultMessage: 'PayPal',
-    id: 'PayoutMethod.Type.Paypal',
+  [PAYMENT_METHOD_SERVICE.PREPAID]: {
+    id: 'Prepaid',
+    defaultMessage: 'Prepaid Card',
   },
 });
 
+/**
+ * Service names are not meant to be translated (Stripe in Spanish is still Stripe). This helper
+ * simply returns the service display name with the right capitalization.
+ */
 export const i18nPaymentMethodService = (intl, service) => {
-  if (service === PAYMENT_METHOD_SERVICE.OPENCOLLECTIVE) {
-    return WebsiteName;
-  } else {
-    const i18nMsg = i18nPaymentMethodServiceLabels[service];
-    return i18nMsg ? intl.formatMessage(i18nMsg) : service;
+  if (i18nPaymentMethodServiceLabels[service]) {
+    return intl.formatMessage(i18nPaymentMethodServiceLabels[service]);
+  }
+
+  switch (service) {
+    case PAYMENT_METHOD_SERVICE.PAYPAL:
+      return 'PayPal';
+    case PAYMENT_METHOD_SERVICE.THEGIVINGBLOCK:
+      return 'The Giving Block';
+    case PAYMENT_METHOD_SERVICE.OPENCOLLECTIVE:
+      return WebsiteName;
+    default:
+      return startCase(service.toLowerCase());
   }
 };
