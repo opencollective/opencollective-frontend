965bcf7d2479b39ceb3b9a251fa7d475f3962300
Benjamin Piouffle <benjamin@opencollective.com>
2024-04-24 11:16:47 +0200

    enhancement(TaxForms): misc fixes

--- components/dashboard/sections/legal-documents/LegalDocumentServiceBadge.tsx
+++ components/dashboard/sections/legal-documents/LegalDocumentServiceBadge.tsx
@@ -3,12 +3,13 @@ import { startCase } from 'lodash';
 
 import { LegalDocumentService } from '../../../../lib/graphql/types/v2/graphql';
 
+import { WebsiteName } from '../../../I18nFormatters';
 import { Badge } from '../../../ui/Badge';
 
 export const LegalDocumentServiceBadge = ({ service }: { service: LegalDocumentService }) => {
   return (
     <Badge className="whitespace-nowrap text-nowrap">
-      {service === 'OPENCOLLECTIVE' ? 'Open Collective' : startCase(service)}
+      {service === 'OPENCOLLECTIVE' ? WebsiteName : startCase(service.toLowerCase())}
     </Badge>
   );
 };
