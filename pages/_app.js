9706f676ff67239f3f94a5f31dc2ba3e7c9232f3
Benjamin Piouffle <benjamin@opencollective.com>
2024-08-30 08:42:33 +0200

    debug: Vercel getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -71,6 +71,7 @@ class OpenCollectiveFrontendApp extends App {
   }
 
   static async getInitialProps({ AppTree, Component, ctx }) {
+    console.log('getInitialProps');
     const apolloClient = initClient({
       accessToken: getTokenFromCookie(ctx.req),
     });
@@ -176,6 +177,7 @@ class OpenCollectiveFrontendApp extends App {
       console.log('pageProps apollo cache', pageProps?.[APOLLO_STATE_PROP_NAME]);
     }
 
+    console.log('props', this.props);
     return (
       <Fragment>
         <ApolloProvider
