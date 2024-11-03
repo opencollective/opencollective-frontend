7c80dc3c4420202532953065ffe423b8eac637c9
Henrique <hdiniz@users.noreply.github.com>
2024-11-03 16:11:53 -0300

    Refactor form parent components and form hook

---
+++ components/submit-expense/SubmitExpenseFlowSteps.tsx
@@ -0,0 +1,203 @@
+import React from 'react';
+import { cva } from 'class-variance-authority';
+import type { Path } from 'dot-path-value';
+import { useFormikContext } from 'formik';
+import { get, isEmpty } from 'lodash';
+import type { IntlShape, MessageDescriptor } from 'react-intl';
+import { defineMessage, FormattedMessage } from 'react-intl';
+
+import { cn } from '../../lib/utils';
+
+import type { ExpenseForm, ExpenseFormValues } from './useExpenseForm';
+
+type SubmitExpenseFlowStepsProps = {
+  className?: string;
+  completedSteps?: Step[];
+  activeStep: Step;
+};
+
+const stepListItemVariants = cva('', {
+  variants: {
+    itemType: {
+      header:
+        '[--step-bullet-border-color:#94A3B8] [--step-bullet-border-width:2px] [--step-bullet-border:2px] [--step-bullet-color:white] [--step-bullet-size:24px] [--step-bullet-text-color:#48566A] last:after:hidden',
+      activeHeader:
+        '[--step-bullet-border-color:#1547B8] [--step-bullet-border-width:2px] [--step-bullet-color:white] [--step-bullet-size:24px]',
+      completedHeader:
+        "[--step-bullet-border-color:#1547B8] [--step-bullet-border-width:2px] [--step-bullet-color:#1547B8] [--step-bullet-size:24px] [--step-bullet-text-color:white] before:content-['✓']",
+      activeItem:
+        '[--step-bullet-box-shadow:0_0_0_4px_hsla(216,_100%,_58%,_0.3)] [--step-bullet-color:#1547B8] [--step-text-color:#1547B8]',
+      completedItem: '[--step-bullet-color:#1547B8]',
+      item: '',
+    },
+  },
+  defaultVariants: {
+    itemType: 'item',
+  },
+});
+
+export enum Step {
+  WHO_IS_PAYING = 'WHO_IS_PAYING',
+  WHO_IS_GETTING_PAID = 'WHO_IS_GETTING_PAID',
+  PAYOUT_METHOD = 'PAYOUT_METHOD',
+  TYPE_OF_EXPENSE = 'TYPE_OF_EXPENSE',
+  EXPENSE_CATEGORY = 'EXPENSE_CATEGORY',
+  EXPENSE_ITEMS = 'EXPENSE_ITEMS',
+  EXPENSE_TITLE = 'EXPENSE_TITLE',
+  SUMMARY = 'SUMMARY',
+}
+
+const StepValues: Record<Step, Path<ExpenseFormValues>[]> = {
+  [Step.WHO_IS_PAYING]: ['accountSlug'],
+  [Step.WHO_IS_GETTING_PAID]: ['payeeSlug', 'inviteeNewIndividual', 'inviteeNewOrganization'],
+  [Step.PAYOUT_METHOD]: ['payoutMethodId', 'newPayoutMethod', 'payoutMethodNameDiscrepancyReason'],
+  [Step.TYPE_OF_EXPENSE]: [
+    'expenseTypeOption',
+    'acknowledgedCollectiveInvoiceExpensePolicy',
+    'acknowledgedCollectiveReceiptExpensePolicy',
+    'acknowledgedHostInvoiceExpensePolicy',
+    'acknowledgedHostReceiptExpensePolicy',
+    'invoiceFile',
+    'invoiceNumber',
+  ],
+  [Step.EXPENSE_CATEGORY]: ['accountingCategoryId'],
+  [Step.EXPENSE_ITEMS]: ['expenseItems'],
+  [Step.EXPENSE_TITLE]: ['title', 'acknowledgedCollectiveTitleExpensePolicy', 'acknowledgedHostTitleExpensePolicy'],
+  [Step.SUMMARY]: [],
+};
+
+export const StepTitles: Record<Step, MessageDescriptor> = {
+  [Step.WHO_IS_PAYING]: defineMessage({
+    defaultMessage: 'Who is paying',
+    id: 'NpMPF+',
+  }),
+  [Step.WHO_IS_GETTING_PAID]: defineMessage({
+    defaultMessage: 'Who is getting paid?',
+    id: 'W5Z+Fm',
+  }),
+  [Step.PAYOUT_METHOD]: defineMessage({
+    defaultMessage: 'Payout Method',
+    id: 'p03D8i',
+  }),
+  [Step.TYPE_OF_EXPENSE]: defineMessage({
+    defaultMessage: 'Type of expense',
+    id: 'qftRxm',
+  }),
+  [Step.EXPENSE_CATEGORY]: defineMessage({
+    defaultMessage: 'Expense Category',
+    id: '38dzz9',
+  }),
+  [Step.EXPENSE_ITEMS]: defineMessage({
+    defaultMessage: 'Expense items',
+    id: '3ldWIL',
+  }),
+  [Step.EXPENSE_TITLE]: defineMessage({
+    defaultMessage: 'Expense Title',
+    id: 'E0WDTk',
+  }),
+  [Step.SUMMARY]: defineMessage({
+    defaultMessage: 'Summary',
+    id: 'RrCui3',
+  }),
+};
+
+export const StepSubtitles: Partial<Record<Step, MessageDescriptor>> = {
+  [Step.PAYOUT_METHOD]: defineMessage({
+    defaultMessage: 'Where do you want to receive the money',
+    id: 'CNCPij',
+  }),
+  [Step.WHO_IS_GETTING_PAID]: defineMessage({
+    defaultMessage: 'Select the profile of the recipient who needs to be paid',
+    id: 'hVAL2P',
+  }),
+  [Step.WHO_IS_PAYING]: defineMessage({
+    defaultMessage: 'Select the profile whom you are requesting money from',
+    id: 'nt19l4',
+  }),
+};
+
+function isExpenseFormStepCompleted(form: ExpenseForm, step: Step): boolean {
+  const valueKeys = StepValues[step];
+  if (isEmpty(valueKeys)) {
+    return true;
+  }
+  return valueKeys.map(valueKey => isEmpty(get(form.errors, valueKey))).every(Boolean);
+}
+
+export function expenseFormStepError(intl: IntlShape, form: ExpenseForm, step: Step): string {
+  if (expenseFormStepHasError(form, step) && isExpenseFormStepTouched(form, step) && form.submitCount > 0) {
+    return intl.formatMessage({ defaultMessage: 'Required', id: 'Seanpx' });
+  }
+
+  return null;
+}
+
+export function expenseFormStepHasError(form: ExpenseForm, step: Step): boolean {
+  return !isExpenseFormStepCompleted(form, step);
+}
+
+function isExpenseFormStepTouched(form: ExpenseForm, step: Step): boolean {
+  const valueKeys = StepValues[step];
+  if (isEmpty(valueKeys)) {
+    return true;
+  }
+  return valueKeys.map(valueKey => get(form.touched, valueKey)).some(Boolean);
+}
+
+export function SubmitExpenseFlowSteps(props: SubmitExpenseFlowStepsProps) {
+  const form = useFormikContext() as ExpenseForm;
+  const hasErrors = Object.values(Step).some(step => expenseFormStepHasError(form, step));
+
+  const stepOrder = [
+    Step.WHO_IS_PAYING,
+    Step.WHO_IS_GETTING_PAID,
+    Step.PAYOUT_METHOD,
+    Step.TYPE_OF_EXPENSE,
+    Step.EXPENSE_CATEGORY,
+    Step.EXPENSE_ITEMS,
+    Step.EXPENSE_TITLE,
+  ];
+
+  return (
+    <div className={cn(props.className)}>
+      <ol className="pl-[12px] [--step-bullet-border-color:#CBD5E1] [--step-bullet-border-width:0px] [--step-bullet-color:#CBD5E1] [--step-bullet-size:8px] [--step-line-color:#E1E7EF] [--step-text-color:#344256] *:relative *:flex *:items-center *:gap-2 *:pb-8 *:pl-7 *:text-base *:font-bold *:text-[var(--step-text-color)] *:before:absolute *:before:left-0 *:before:inline-block *:before:h-[var(--step-bullet-size)] *:before:w-[var(--step-bullet-size)] *:before:-translate-x-[calc(var(--step-bullet-size)/2)] *:before:rounded-full *:before:border-[var(--step-bullet-border-color)] *:before:bg-[--step-bullet-color] *:before:text-center *:before:text-sm *:before:font-medium *:before:text-[var(--step-bullet-text-color,var(--step-bullet-border-color))] *:before:[border-width:var(--step-bullet-border-width)] *:before:[box-shadow:var(--step-bullet-box-shadow,initial)] *:after:absolute *:after:left-0 *:after:top-2 *:after:-z-10 *:after:h-full *:after:-translate-x-[1px] *:after:border-l-2 *:after:border-[--step-line-color] *:after:[border-style:var(--step-line-style,solid)]">
+        <li
+          className={cn(
+            "before:content-['1']",
+            stepListItemVariants({
+              itemType: props.activeStep === Step.SUMMARY ? (hasErrors ? 'header' : 'completedHeader') : 'activeHeader',
+            }),
+          )}
+        >
+          Expense Details
+        </li>
+        {(props.activeStep !== Step.SUMMARY || hasErrors) &&
+          stepOrder.map(step => (
+            <li
+              key={step}
+              className={cn(
+                stepListItemVariants({
+                  itemType:
+                    props.activeStep === step
+                      ? 'activeItem'
+                      : isExpenseFormStepCompleted(form, step)
+                        ? 'completedItem'
+                        : 'item',
+                }),
+              )}
+            >
+              <FormattedMessage {...StepTitles[step]} />
+            </li>
+          ))}
+        <li
+          className={cn(
+            "[--step-line-style:none] before:content-['2']",
+            stepListItemVariants({ itemType: props.activeStep === Step.SUMMARY ? 'activeHeader' : 'header' }),
+          )}
+        >
+          Summary
+        </li>
+      </ol>
+    </div>
+  );
+}
--- components/submit-expense/SubmittedExpense.tsx
+++ components/submit-expense/SubmittedExpense.tsx
@@ -1,127 +1,27 @@
 import React from 'react';
-import { gql, useMutation, useQuery } from '@apollo/client';
-import { includes, isEmpty } from 'lodash';
-import { ExternalLink } from 'lucide-react';
-import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+import { useQuery } from '@apollo/client';
 
-import { i18nGraphqlException } from '../../lib/errors';
 import { API_V2_CONTEXT } from '../../lib/graphql/helpers';
-import type {
-  CreatePrivateNoteMutation,
-  CreatePrivateNoteMutationVariables,
-  SubmittedExpenseFromDashboardQuery,
-  SubmittedExpenseFromDashboardQueryVariables,
-} from '../../lib/graphql/types/v2/graphql';
-import { CommentType, ExpenseStatus } from '../../lib/graphql/types/v2/graphql';
-import useLoggedInUser from '../../lib/hooks/useLoggedInUser';
+import type { ExpensePageQuery, ExpensePageQueryVariables } from '../../lib/graphql/types/v2/graphql';
 
+import ExpenseSummary from '../expenses/ExpenseSummary';
+import { expensePageQuery } from '../expenses/graphql/queries';
 import CreateExpenseFAQ from '../faqs/CreateExpenseFAQ';
-import PrivateInfoIcon from '../icons/PrivateInfoIcon';
-import Image from '../Image';
-import LinkCollective from '../LinkCollective';
 import Loading from '../Loading';
-import MessageBox from '../MessageBox';
 import MessageBoxGraphqlError from '../MessageBoxGraphqlError';
-import RichTextEditor from '../RichTextEditor';
-import StyledLink from '../StyledLink';
-import { Button } from '../ui/Button';
-import { useToast } from '../ui/useToast';
-
-const I18nMessages = defineMessages({
-  notePlaceholder: { defaultMessage: 'Add a note for the admins.', id: '4rMvYo' },
-});
 
 type SubmittedExpenseProps = {
   expenseId: number;
 };
 
 export function SubmittedExpense(props: SubmittedExpenseProps) {
-  const { toast } = useToast();
-  const intl = useIntl();
-  const { LoggedInUser } = useLoggedInUser();
-
-  const query = useQuery<SubmittedExpenseFromDashboardQuery, SubmittedExpenseFromDashboardQueryVariables>(
-    gql`
-      query SubmittedExpenseFromDashboard($expenseLegacyId: Int!) {
-        expense(expense: { legacyId: $expenseLegacyId }) {
-          id
-          legacyId
-          status
-          recurringExpense {
-            id
-          }
-          draft
-          host {
-            id
-            slug
-            legacyId
-            name
-          }
-          account {
-            id
-            slug
-            legacyId
-            name
-            isAdmin
-          }
-          requiredLegalDocuments
-        }
-      }
-    `,
-    {
-      context: API_V2_CONTEXT,
-      skip: !props.expenseId,
-      variables: {
-        expenseLegacyId: props.expenseId,
-      },
+  const query = useQuery<ExpensePageQuery, ExpensePageQueryVariables>(expensePageQuery, {
+    context: API_V2_CONTEXT,
+    skip: !props.expenseId,
+    variables: {
+      legacyExpenseId: props.expenseId,
     },
-  );
-
-  const [privateNote, setPrivateNote] = React.useState('');
-  const [lastPrivateNoteId, setLastPrivateNoteId] = React.useState(null);
-
-  const [createPrivateNote, createPrivateNoteMutation] = useMutation<
-    CreatePrivateNoteMutation,
-    CreatePrivateNoteMutationVariables
-  >(
-    gql`
-      mutation CreatePrivateNote($comment: CommentCreateInput!) {
-        createComment(comment: $comment) {
-          id
-        }
-      }
-    `,
-    {
-      context: API_V2_CONTEXT,
-    },
-  );
-
-  const onAddPrivateNoteClick = React.useCallback(async () => {
-    try {
-      const result = await createPrivateNote({
-        variables: {
-          comment: {
-            expense: {
-              legacyId: props.expenseId,
-            },
-            html: privateNote,
-            type: CommentType.PRIVATE_NOTE,
-          },
-        },
-      });
-      setPrivateNote('');
-      setLastPrivateNoteId(result.data.createComment.id);
-      toast({
-        variant: 'success',
-        message: <FormattedMessage defaultMessage="Note added!" id="N6X6du" />,
-      });
-    } catch (e) {
-      toast({
-        variant: 'error',
-        message: i18nGraphqlException(intl, e),
-      });
-    }
-  }, [createPrivateNote, privateNote, props.expenseId, intl, toast]);
+  });
 
   if (query.loading) {
     return <Loading />;
@@ -133,131 +33,26 @@ export function SubmittedExpense(props: SubmittedExpenseProps) {
 
   const expense = query.data.expense;
 
-  const isInvite = expense.status === ExpenseStatus.DRAFT && !expense.recurringExpense;
-
-  const requiresTaxDocuments = expense.account?.isAdmin && includes(expense.requiredLegalDocuments, 'US_TAX_FORM');
-
   return (
-    <div className="flex flex-col gap-8 overflow-scroll overflow-x-hidden">
-      <div className="flex-1">
-        <div className="mb-8 text-xl font-bold">
-          {isInvite ? (
-            <FormattedMessage id="InviteOnItsWay" defaultMessage="Your invite is on its way" />
-          ) : (
-            <FormattedMessage defaultMessage="Your expense has been submitted successfully!" id="K2coI/" />
-          )}
-        </div>
-
-        {isInvite ? (
-          <div className="mb-8 flex gap-2">
-            <Image
-              alt={intl.formatMessage({ id: 'Expense.Type.Invoice', defaultMessage: 'Invoice' })}
-              src="/static/images/pidgeon.png"
-              width={112}
-              height={112}
-            />
-            <div className="text-sm">
-              <FormattedMessage
-                id="Expense.InviteIsOnItsWay.Description"
-                defaultMessage="An invitation to submit this expense has been sent to {email}. Once they confirm and finish the process, it will appear on the expenses list."
-                values={{
-                  email: expense.draft?.payee?.email || expense.draft?.payee?.name,
-                }}
-              />
-            </div>
-          </div>
-        ) : null}
-
-        <h2 className="mb-3 font-bold">
-          <FormattedMessage defaultMessage="What's next?" id="1G5vLM" />
-        </h2>
-        <ol className="mb-4 list-inside list-decimal text-sm text-slate-700">
-          {requiresTaxDocuments && (
-            <li>
-              <FormattedMessage defaultMessage="Submit your tax form." id="D5d2Bp" />{' '}
-              <StyledLink
-                className="whitespace-nowrap"
-                href="https://docs.opencollective.com/help/expenses-and-getting-paid/tax-information"
-                openInNewTab
-              >
-                <FormattedMessage id="ContributeCard.SeeMore" defaultMessage="See More" />
-                <ExternalLink size={16} className="ml-1 inline align-text-top" />
-              </StyledLink>
-            </li>
-          )}
-          <li>
-            <FormattedMessage
-              defaultMessage="This expense needs to be approved by an admin of {collective}"
-              id="6FQMA7"
-              values={{
-                collective: <LinkCollective openInNewTab className="underline" collective={expense.account} />,
-              }}
-            />
-          </li>
-          {expense.host && (
-            <li>
-              <FormattedMessage
-                defaultMessage="After that, this expense needs to be reviewed and paid by an admin of {host}"
-                id="0BeZai"
-                values={{ host: <LinkCollective openInNewTab className="underline" collective={expense.host} /> }}
-              />
-            </li>
-          )}
-        </ol>
-
-        {!isInvite && (
-          <MessageBox my={4} type="info">
-            <FormattedMessage
-              defaultMessage="You can find and track your expense in your dashboard. <Link>Go to expense</Link>."
-              id="8us97Y"
-              values={{
-                Link: chunks => (
-                  <StyledLink
-                    className="whitespace-nowrap"
-                    href={`/dashboard/${LoggedInUser.collective.slug}/submitted-expenses?openExpenseId=${expense.legacyId}`}
-                    openInNewTab
-                  >
-                    {chunks}
-                    <ExternalLink size={16} className="ml-1 inline align-text-top" />
-                  </StyledLink>
-                ),
-              }}
-            />
-          </MessageBox>
-        )}
-
-        {!isInvite && (
-          <React.Fragment>
-            <div className="mb-3 font-bold">
-              <FormattedMessage
-                defaultMessage="Is there anything else you wish to communicate to the admins who will review this expense?"
-                id="0FP/Ii"
-              />{' '}
-              <PrivateInfoIcon />
-            </div>
-            <RichTextEditor
-              key={lastPrivateNoteId}
-              withBorders
-              version="simplified"
-              editorMinHeight={72}
-              fontSize="13px"
-              placeholder={intl.formatMessage(I18nMessages.notePlaceholder)}
-              onChange={e => setPrivateNote(e.target.value)}
-            />
-            <Button
-              loading={createPrivateNoteMutation.loading}
-              disabled={isEmpty(privateNote)}
-              className="mt-3"
-              variant="default"
-              onClick={onAddPrivateNoteClick}
-            >
-              <FormattedMessage defaultMessage="Add a note" id="dTW4m3" />
-            </Button>
-          </React.Fragment>
-        )}
+    <div className="flex flex-grow flex-wrap gap-8 px-4 sm:p-0">
+      <div className="flex-grow">
+        <ExpenseSummary
+          onDelete={() => {}}
+          onEdit={() => {}}
+          openFileViewer={() => {}}
+          enableKeyboardShortcuts={false}
+          drawerActionsContainer={null}
+          canEditTags={false}
+          isEditing={false}
+          isLoadingLoggedInUser={false}
+          showProcessButtons={false}
+          expense={expense}
+          host={expense.host}
+          isLoading={query.loading || !expense}
+          collective={expense.account}
+        />
       </div>
-
-      <div className="flex-1">
+      <div className="md:max-w-96">
         <CreateExpenseFAQ defaultOpen />
       </div>
     </div>
