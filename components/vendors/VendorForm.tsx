0edd4e3453956ca8a620ed2aac7f8e5fcdcfb686
Leo Kewitz <leo@opencollective.com>
2025-01-24 17:37:52 +0100

    fix: ignore image service images when updating vendors (#10942)

--- components/vendors/VendorForm.tsx
+++ components/vendors/VendorForm.tsx
@@ -16,6 +16,7 @@ import { UploadedFileKind } from '../../lib/graphql/types/v2/schema';
 import { useImageUploader } from '../../lib/hooks/useImageUploader';
 import { elementFromClass } from '../../lib/react-utils';
 import { cn, omitDeep } from '../../lib/utils';
+import { isImageServiceUrl } from '@/lib/image-utils';
 
 import Avatar from '../Avatar';
 import { useDrawerActionsContainer } from '../Drawer';
@@ -203,6 +204,7 @@ const VendorForm = ({ vendor, host, onSuccess, onCancel, isModal, supportsTaxFor
     const data = omitDeep(
       {
         ...pick(values, EDITABLE_FIELDS),
+        imageUrl: isImageServiceUrl(values.imageUrl) ? undefined : values.imageUrl,
         vendorInfo: {
           ...pick(values.vendorInfo, ['contact', 'notes', 'taxFormUrl', 'taxFormRequired', 'taxId', 'taxType']),
           taxType:
@@ -239,7 +241,7 @@ const VendorForm = ({ vendor, host, onSuccess, onCancel, isModal, supportsTaxFor
     { label: 'GST', value: 'GST' },
     { label: <FormattedMessage id="taxType.Other" defaultMessage="Other" />, value: 'OTHER' },
   ];
-  const initialValues = cloneDeep(pick(vendor, EDITABLE_FIELDS));
+  const initialValues = omitDeep(cloneDeep(pick(vendor, EDITABLE_FIELDS)), ['__typename']);
   if (initialValues.vendorInfo?.taxType && !['EIN', 'VAT', 'GST'].includes(initialValues.vendorInfo?.taxType)) {
     initialValues.vendorInfo['otherTaxType'] = initialValues.vendorInfo?.taxType;
     initialValues.vendorInfo.taxType = 'OTHER';
