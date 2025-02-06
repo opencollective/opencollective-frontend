f18f232fdb05d2cbb453dfba7d021d9382b1d29e
Henrique <hdiniz@users.noreply.github.com>
2025-02-06 09:33:48 -0300

    Fix incognito pm update (#10960)
    * Abort usePaymentIntent to avoid nedlessly generating payment intent
    
    * Use corret slug to generate setup intent
    
    * Add comment on purpose of abort controller in usePaymentIntent
    
    * Update lib/stripe/usePaymentIntent.ts
    
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>
    
    ---------
    
    Co-authored-by: Benjamin Piouffle <benjamin@opencollective.com>

--- lib/stripe/usePaymentIntent.ts
+++ lib/stripe/usePaymentIntent.ts
@@ -44,58 +44,76 @@ export default function usePaymentIntent({
   const [stripe, setStripe] = React.useState<Stripe | null>(null);
   const [paymentIntent, setPaymentIntent] = React.useState<StripePaymentIntent | null>(null);
 
+  const abort = React.useRef(new AbortController());
+
   React.useEffect(() => {
     async function load() {
       if (typeof window.Stripe === 'undefined') {
         await loadScriptAsync('https://js.stripe.com/v3/');
       }
 
-      const createPaymentIntentResp = await apolloClient.mutate({
-        mutation: createPaymentIntentMutation,
-        context: API_V2_CONTEXT,
-        variables: {
-          paymentIntent: {
-            amount,
-            fromAccount,
-            toAccount,
-            frequency,
-          },
-          guestInfo,
-        },
-        errorPolicy: 'all',
-      });
-
-      if (createPaymentIntentResp.errors?.length > 0) {
-        setError(new ApolloError({ graphQLErrors: createPaymentIntentResp.errors }));
-        setLoading(false);
-        return;
-      }
-
-      const { paymentIntentClientSecret, stripeAccountPublishableSecret, stripeAccount } =
-        createPaymentIntentResp.data?.createPaymentIntent ?? {};
-
-      const stripe = window.Stripe(stripeAccountPublishableSecret, stripeAccount ? { stripeAccount } : {});
-      stripe['stripeAccount'] = stripeAccount;
-      stripe['stripeAccountPublishableSecret'] = stripeAccountPublishableSecret;
-      setStripe(stripe);
-
+      const abortController = (abort.current = new AbortController());
       try {
+        const createPaymentIntentResp = await apolloClient.mutate({
+          mutation: createPaymentIntentMutation,
+          context: { ...API_V2_CONTEXT, fetchOptions: { signal: abort.current.signal } },
+          variables: {
+            paymentIntent: {
+              amount,
+              fromAccount,
+              toAccount,
+              frequency,
+            },
+            guestInfo,
+          },
+          errorPolicy: 'all',
+        });
+
+        if (!abortController.signal.aborted && createPaymentIntentResp.errors?.length > 0) {
+          setError(new ApolloError({ graphQLErrors: createPaymentIntentResp.errors }));
+          setLoading(false);
+          return;
+        }
+
+        const { paymentIntentClientSecret, stripeAccountPublishableSecret, stripeAccount } =
+          createPaymentIntentResp.data?.createPaymentIntent ?? {};
+
+        const stripe = window.Stripe(stripeAccountPublishableSecret, stripeAccount ? { stripeAccount } : {});
+        stripe['stripeAccount'] = stripeAccount;
+        stripe['stripeAccountPublishableSecret'] = stripeAccountPublishableSecret;
+
         const paymentIntentResult = await stripe.retrievePaymentIntent(paymentIntentClientSecret);
+        if (abortController.signal.aborted) {
+          return;
+        }
         if (paymentIntentResult.error) {
           setError(new Error('Payment Intent Retrieve error', { cause: paymentIntentResult.error }));
         } else {
           (paymentIntentResult.paymentIntent as StripePaymentIntent).stripeAccount = stripeAccount;
           setPaymentIntent(paymentIntentResult.paymentIntent as StripePaymentIntent);
         }
+        setStripe(stripe);
       } catch (e) {
-        setError(e);
+        if (e.networkError?.name !== 'AbortError') {
+          setError(e);
+        }
       } finally {
-        setLoading(false);
+        if (!abortController.signal.aborted) {
+          setLoading(false);
+        }
       }
     }
 
     if (skip) {
       return;
+    }
+
+    // If this hook is called again during component rerender and we still have a in-flight payment intent, abort it.
+    // Without the abort controller, we can get another payment intent created for a previous render that will be unused
+    // and will make the form render again with the setStripe, setPaymentIntent state changes. This also prevents generating
+    // too many incomplete payment intents on stripe
+    if (!abort.current.signal.aborted) {
+      abort.current.abort();
     }
 
     setLoading(true);
