227cd8575dcae488d64b6e6fe6b09bad67fc8167
Leo Kewitz <leo@opencollective.com>
2025-02-13 14:46:09 +0100

    refact: contribution information boxes

--- components/contribution-flow/StepProfileGuestForm.js
+++ components/contribution-flow/StepProfileGuestForm.js
@@ -4,19 +4,20 @@ import { set } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { isEmail } from 'validator';
 
+import { formatCurrency } from '../../lib/currency-utils';
+
 import Captcha, { isCaptchaEnabled } from '../Captcha';
-import Container from '../Container';
 import { Flex } from '../Grid';
 import I18nFormatters, { getI18nLink } from '../I18nFormatters';
 import PrivateInfoIcon from '../icons/PrivateInfoIcon';
 import Link from '../Link';
+import MessageBox from '../MessageBox';
 import StyledHr from '../StyledHr';
 import StyledInput from '../StyledInput';
 import StyledInputField from '../StyledInputField';
 import StyledInputLocation from '../StyledInputLocation';
 import { P, Span } from '../Text';
 
-import StepProfileInfoMessage from './StepProfileInfoMessage';
 import { getRequiredInformation } from './utils';
 
 export const validateGuestProfile = (stepProfile, stepDetails, tier, collective) => {
@@ -58,7 +59,31 @@ const StepProfileGuestForm = ({ stepDetails, onChange, data, isEmbed, onSignInCl
   );
 
   return (
-    <Container border="none" width={1} pb={3}>
+    <div className="flex flex-col gap-4">
+      <MessageBox type="info">
+        <h1 className="font-bold">
+          <FormattedMessage defaultMessage="Privacy Information" id="AyQ5sz" />
+        </h1>
+        <p>
+          <FormattedMessage
+            defaultMessage="When you contribute to {collectiveName}, your email address will be visible by its administrators and by {hostName}, the legal entity collecting this contribution."
+            id="ContributionFlow.StepProfileLoggedInForm.EmailInfo"
+            values={{
+              collectiveName: collective.name,
+              hostName: collective.host.name,
+            }}
+          />
+        </p>
+        <p className="mt-2">
+          <FormattedMessage
+            defaultMessage="<PrivacyPolicyLink>Read our privacy policy</PrivacyPolicyLink>."
+            id="ReadOurPrivacyPolicy"
+            values={{
+              PrivacyPolicyLink: getI18nLink({ href: '/privacypolicy', openInNewTab: true, as: Link }),
+            }}
+          />
+        </p>
+      </MessageBox>
       <StyledInputField
         htmlFor="email"
         label={<FormattedMessage defaultMessage="Your email" id="nONnTw" />}
@@ -97,7 +122,7 @@ const StepProfileGuestForm = ({ stepDetails, onChange, data, isEmbed, onSignInCl
           />
         )}
       </StyledInputField>
-      <StyledHr my="18px" borderColor="black.300" />
+      <StyledHr my="4px" borderColor="black.300" />
       <StyledInputField
         htmlFor="name"
         label={<FormattedMessage defaultMessage="Your name" id="vlKhIl" />}
@@ -128,12 +153,23 @@ const StepProfileGuestForm = ({ stepDetails, onChange, data, isEmbed, onSignInCl
         labelFontWeight="700"
         isPrivate
         required={requiredInformation.legalName && !data?.name}
-        mt={20}
         hint={
-          <FormattedMessage
-            defaultMessage="If different from your display name. Not public. Important for receipts, invoices, payments, and official documentation."
-            id="QLBxEF"
-          />
+          requiredInformation.legalName ? (
+            <FormattedMessage
+              defaultMessage="Required for legal purposes as your total anual contribution is more than {amount}. Your legal name is private and it can only be seen by {collectiveName} and {hostName}."
+              id="tLn/BI"
+              values={{
+                amount: formatCurrency(collective.policies.CONTRIBUTOR_INFO_THRESHOLDS.legalName, collective.currency),
+                hostName: collective.host.name,
+                collectiveName: collective.name,
+              }}
+            />
+          ) : (
+            <FormattedMessage
+              defaultMessage="Important for receipts, invoices, payments and official documentation if different from your name."
+              id="LdGP5g"
+            />
+          )
         }
       >
         {inputProps => (
@@ -148,15 +184,39 @@ const StepProfileGuestForm = ({ stepDetails, onChange, data, isEmbed, onSignInCl
       </StyledInputField>
       {requiredInformation.address && (
         <React.Fragment>
-          <Flex alignItems="center" my="14px">
+          <Flex alignItems="center">
             <P fontSize="24px" lineHeight="32px" fontWeight="500" mr={2}>
               <FormattedMessage id="collective.address.label" defaultMessage="Address" />
             </P>
             <Span mr={2} lineHeight="0">
               <PrivateInfoIcon className="text-muted-foreground" />
             </Span>
-            <StyledHr my="18px" borderColor="black.300" width="100%" />
+            <StyledHr my="16px" borderColor="black.300" width="100%" />
           </Flex>
+          <MessageBox type="info">
+            <h1 className="font-bold">
+              <FormattedMessage defaultMessage="Address Requirement" id="Mc7YHZ" />
+            </h1>
+            <p>
+              <FormattedMessage
+                defaultMessage="We require your physical address for legal purposes as your total anual contribution is more than {amount}. Your address is private and it can only be seen by {hostName}."
+                id="mp9ThO"
+                values={{
+                  amount: formatCurrency(collective.policies.CONTRIBUTOR_INFO_THRESHOLDS.address, collective.currency),
+                  hostName: collective.host.name,
+                }}
+              />
+            </p>
+            <p className="mt-2">
+              <FormattedMessage
+                defaultMessage="<PrivacyPolicyLink>Read our privacy policy</PrivacyPolicyLink>."
+                id="ReadOurPrivacyPolicy"
+                values={{
+                  PrivacyPolicyLink: getI18nLink({ href: '/privacypolicy', openInNewTab: true, as: Link }),
+                }}
+              />
+            </p>
+          </MessageBox>
           <StyledInputLocation
             autoDetectCountry
             location={data?.location}
@@ -167,19 +227,18 @@ const StepProfileGuestForm = ({ stepDetails, onChange, data, isEmbed, onSignInCl
         </React.Fragment>
       )}
       {isCaptchaEnabled() && (
-        <Flex mt="18px" justifyContent="center">
+        <Flex justifyContent="center">
           <Captcha onVerify={result => dispatchChange('captcha', result)} />
         </Flex>
       )}
-      <StepProfileInfoMessage isGuest hasLegalNameField />
-      <P color="black.500" fontSize="12px" mt={4} data-cy="join-conditions">
+      <P color="black.500" fontSize="12px" data-cy="join-conditions">
         <FormattedMessage
           defaultMessage="By contributing, you agree to our <TOSLink>Terms of Service</TOSLink> and <PrivacyPolicyLink>Privacy Policy</PrivacyPolicyLink>."
           id="Amj+Gh"
           values={I18nFormatters}
         />
       </P>
-    </Container>
+    </div>
   );
 };
 
--- components/contribution-flow/StepProfileLoggedInForm.js
+++ components/contribution-flow/StepProfileLoggedInForm.js
@@ -2,8 +2,13 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
-import { Box, Flex } from '../Grid';
+import { formatCurrency } from '../../lib/currency-utils';
+
+import { Flex } from '../Grid';
+import { getI18nLink } from '../I18nFormatters';
 import PrivateInfoIcon from '../icons/PrivateInfoIcon';
+import Link from '../Link';
+import MessageBox from '../MessageBox';
 import StyledHr from '../StyledHr';
 import StyledInput from '../StyledInput';
 import StyledInputField from '../StyledInputField';
@@ -11,7 +16,6 @@ import StyledInputLocation from '../StyledInputLocation';
 import { P, Span } from '../Text';
 
 import ContributeProfilePicker from './ContributeProfilePicker';
-import StepProfileInfoMessage from './StepProfileInfoMessage';
 import { getRequiredInformation } from './utils';
 
 export const NEW_ORGANIZATION_KEY = 'newOrg';
@@ -44,83 +48,141 @@ const StepProfileLoggedInForm = ({ profiles, onChange, collective, tier, data, s
 
   return (
     <Fragment>
-      <Box mb={4}>
+      <div className="flex flex-col gap-4">
+        <MessageBox type="info">
+          <h1 className="font-bold">
+            <FormattedMessage defaultMessage="Privacy Information" id="AyQ5sz" />
+          </h1>
+          <p>
+            <FormattedMessage
+              defaultMessage="When you contribute to {collectiveName}, your email address will be visible by its administrators and by {hostName}, the legal entity collecting this contribution."
+              id="ContributionFlow.StepProfileLoggedInForm.EmailInfo"
+              values={{
+                collectiveName: collective.name,
+                hostName: collective.host.name,
+              }}
+            />
+          </p>
+          <p className="mt-2">
+            <FormattedMessage
+              defaultMessage="<PrivacyPolicyLink>Read our privacy policy</PrivacyPolicyLink>."
+              id="ReadOurPrivacyPolicy"
+              values={{
+                PrivacyPolicyLink: getI18nLink({ href: '/privacypolicy', openInNewTab: true, as: Link }),
+              }}
+            />
+          </p>
+        </MessageBox>
         <ContributeProfilePicker
           profiles={profiles}
           tier={tier}
           selectedProfile={data}
           onChange={profile => onChange({ stepProfile: profile, stepPayment: null })}
         />
-      </Box>
-      {!isContributingFromSameHost && requiredInformation.legalName && (
-        <React.Fragment>
-          {!data?.isIncognito && (
+        {!isContributingFromSameHost && requiredInformation.legalName && (
+          <React.Fragment>
+            {!data?.isIncognito && (
+              <StyledInputField
+                htmlFor="name"
+                label={<FormattedMessage defaultMessage="Your name" id="vlKhIl" />}
+                labelFontSize="16px"
+                labelFontWeight="700"
+                hint={<FormattedMessage defaultMessage="This is your display name or alias." id="kFLEBd" />}
+              >
+                {inputProps => (
+                  <StyledInput
+                    {...inputProps}
+                    value={profileInfo.name}
+                    placeholder="Thomas Anderson"
+                    onChange={e => onChange({ stepProfile: { ...data, name: e.target.value } })}
+                    maxLength="255"
+                  />
+                )}
+              </StyledInputField>
+            )}
             <StyledInputField
-              htmlFor="name"
-              label={<FormattedMessage defaultMessage="Your name" id="vlKhIl" />}
+              htmlFor="legalName"
+              label={<FormattedMessage defaultMessage="Legal name" id="OozR1Y" />}
+              required={!profileInfo.name || requiredInformation.legalName}
+              hideOptionalLabel={requiredInformation.legalName}
               labelFontSize="16px"
               labelFontWeight="700"
-              hint={<FormattedMessage defaultMessage="This is your display name or alias." id="kFLEBd" />}
+              isPrivate
+              hint={
+                <FormattedMessage
+                  defaultMessage="Required for legal purposes as your total anual contribution is more than {amount}. Your legal name is private and it can only be seen by {collectiveName} and {hostName}."
+                  id="tLn/BI"
+                  values={{
+                    amount: formatCurrency(
+                      collective.policies.CONTRIBUTOR_INFO_THRESHOLDS.legalName,
+                      collective.currency,
+                    ),
+                    hostName: collective.host.name,
+                    collectiveName: collective.name,
+                  }}
+                />
+              }
             >
               {inputProps => (
                 <StyledInput
                   {...inputProps}
-                  value={profileInfo.name}
-                  placeholder="Thomas Anderson"
-                  onChange={e => onChange({ stepProfile: { ...data, name: e.target.value } })}
+                  value={profileInfo.legalName}
+                  placeholder={profileInfo.name}
+                  onChange={e => onChange({ stepProfile: { ...data, legalName: e.target.value } })}
                   maxLength="255"
                 />
               )}
             </StyledInputField>
-          )}
-          <StyledInputField
-            htmlFor="legalName"
-            label={<FormattedMessage defaultMessage="Legal name" id="OozR1Y" />}
-            required={!profileInfo.name}
-            labelFontSize="16px"
-            labelFontWeight="700"
-            isPrivate
-            mt={20}
-            hint={
-              <FormattedMessage
-                defaultMessage="If different from your display name. Not public. Important for receipts, invoices, payments, and official documentation."
-                id="QLBxEF"
-              />
-            }
-          >
-            {inputProps => (
-              <StyledInput
-                {...inputProps}
-                value={profileInfo.legalName}
-                placeholder={profileInfo.name}
-                onChange={e => onChange({ stepProfile: { ...data, legalName: e.target.value } })}
-                maxLength="255"
-              />
-            )}
-          </StyledInputField>
-        </React.Fragment>
-      )}
-      {!isContributingFromSameHost && requiredInformation.address && (
-        <React.Fragment>
-          <Flex alignItems="center" my="14px">
-            <P fontSize="24px" lineHeight="32px" fontWeight="500" mr={2}>
-              <FormattedMessage id="collective.address.label" defaultMessage="Address" />
-            </P>
-            <Span mr={2} lineHeight="0">
-              <PrivateInfoIcon />
-            </Span>
-            <StyledHr my="18px" borderColor="black.300" width="100%" />
-          </Flex>
-          <StyledInputLocation
-            autoDetectCountry
-            location={profileInfo.location}
-            onChange={value => onChange({ stepProfile: { ...data, location: value } })}
-            labelFontSize="16px"
-            labelFontWeight="700"
-          />
-        </React.Fragment>
-      )}
-      <StepProfileInfoMessage hasIncognito />
+          </React.Fragment>
+        )}
+        {!isContributingFromSameHost && requiredInformation.address && (
+          <React.Fragment>
+            <Flex alignItems="center" mt={2}>
+              <P fontSize="24px" lineHeight="32px" fontWeight="500" mr={2}>
+                <FormattedMessage id="collective.address.label" defaultMessage="Address" />
+              </P>
+              <Span mr={2} lineHeight="0">
+                <PrivateInfoIcon />
+              </Span>
+              <StyledHr my="16px" borderColor="black.300" width="100%" />
+            </Flex>
+            <MessageBox type="info">
+              <h1 className="font-bold">
+                <FormattedMessage defaultMessage="Address Requirement" id="Mc7YHZ" />
+              </h1>
+              <p>
+                <FormattedMessage
+                  defaultMessage="We require your physical address for legal purposes as your total anual contribution is more than {amount}. Your address is private and it can only be seen by {hostName}."
+                  id="mp9ThO"
+                  values={{
+                    amount: formatCurrency(
+                      collective.policies.CONTRIBUTOR_INFO_THRESHOLDS.address,
+                      collective.currency,
+                    ),
+                    hostName: collective.host.name,
+                  }}
+                />
+              </p>
+              <p className="mt-2">
+                <FormattedMessage
+                  defaultMessage="<PrivacyPolicyLink>Read our privacy policy</PrivacyPolicyLink>."
+                  id="ReadOurPrivacyPolicy"
+                  values={{
+                    PrivacyPolicyLink: getI18nLink({ href: '/privacypolicy', openInNewTab: true, as: Link }),
+                  }}
+                />
+              </p>
+            </MessageBox>
+            <StyledInputLocation
+              autoDetectCountry
+              location={profileInfo.location}
+              onChange={value => onChange({ stepProfile: { ...data, location: value } })}
+              labelFontSize="16px"
+              labelFontWeight="700"
+            />
+          </React.Fragment>
+        )}
+      </div>
     </Fragment>
   );
 };
@@ -132,8 +194,17 @@ StepProfileLoggedInForm.propTypes = {
   onChange: PropTypes.func,
   profiles: PropTypes.arrayOf(PropTypes.object),
   collective: PropTypes.shape({
+    name: PropTypes.string,
+    currency: PropTypes.string,
     host: PropTypes.shape({
       legacyId: PropTypes.number,
+      name: PropTypes.string,
+    }),
+    policies: PropTypes.shape({
+      CONTRIBUTOR_INFO_THRESHOLDS: PropTypes.shape({
+        legalName: PropTypes.number,
+        address: PropTypes.number,
+      }),
     }),
   }),
 };
