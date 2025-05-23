56fe7966e31ff9bd1dcea3115097312e8e8fc0c0
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2025-05-23 11:24:51 +0300

    fix: correct expense comment description (#11234)

--- components/expenses/PrivateCommentsMessage.js
+++ components/expenses/PrivateCommentsMessage.js
@@ -29,12 +29,12 @@ const PrivateCommentsMessage = ({ isAllowed, isLoading, ...props }) => {
             {isAllowed ? (
               <FormattedMessage
                 id="PrivateCommentsMessage.AllowedDetails"
-                defaultMessage="Expenses comments are private, because they sometimes contain confidential information such as payment details. Only the expense submitter and the admins can see them."
+                defaultMessage="Expenses comments are private, because they sometimes contain confidential information such as payment details. Only the expense submitter, the admins and the accountants can see them."
               />
             ) : (
               <FormattedMessage
                 id="PrivateCommentsMessage.NotAllowedDetails"
-                defaultMessage="You must be signed in as an admin or the expense submitter to read comments on an expense."
+                defaultMessage="You must be signed in as an admin, an accountant or the expense submitter to read comments on an expense."
               />
             )}
           </P>
