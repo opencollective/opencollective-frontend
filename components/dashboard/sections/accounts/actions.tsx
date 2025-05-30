549748b3321093c2ffd562f86cbe6431509263ab
Leo Kewitz <leo@opencollective.com>
2025-05-30 13:52:24 +0200

    Internal Transfer: Use CollectivePickerAsync (#11259)
    * fix: Formik error must be a flat object with dot notation keys
    
    * refact: use CollectivePickerAsync in InternalTransferModal
    
    * chore: update schemas

--- components/dashboard/sections/accounts/actions.tsx
+++ components/dashboard/sections/accounts/actions.tsx
@@ -34,7 +34,7 @@ export function useAccountActions<T extends DashboardAccountsQueryFieldsFragment
             showModal(
               InternalTransferModal,
               {
-                accountSlug: 'parent' in account ? account.parent.slug : account.slug,
+                parentAccount: 'parent' in account ? account.parent : account,
                 defaultFromAccount: account,
                 onCloseFocusRef,
               },
