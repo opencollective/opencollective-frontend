2964fc3f4013cdab084d1daa26e14f0232c1039a
Benjamin Piouffle <benjamin@opencollective.com>
2025-06-06 15:52:44 +0200

    remove draft-js

--- components/StyledMultiEmailInput.js
+++ components/StyledMultiEmailInput.js
@@ -1,50 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { themeGet } from '@styled-system/theme-get';
-import { Editor, EditorState } from 'draft-js';
 import { debounce, omit, uniq } from 'lodash';
 import { FormattedMessage } from 'react-intl';
-import styled from 'styled-components';
 
 import { getInputBorderColor } from '../lib/styled_components_utils';
 
+import { Textarea } from './ui/Textarea';
 import Container from './Container';
 import { Span } from './Text';
 
-const InputContainer = styled(Container)`
-  .DraftEditor-root {
-    padding: 0.5em;
-    cursor: text;
-    font-weight: 400;
-    border: 1px solid;
-    border-radius: 4px;
-    border-color: inherit;
-
-    &:hover,
-    &:focus {
-      border-color: ${themeGet('colors.primary.300')};
-    }
-  }
-
-  .public-DraftEditor-content {
-    min-height: 3em;
-    max-height: 18em;
-    overflow-y: auto;
-  }
-
-  .public-DraftEditor-content[contenteditable='false'] {
-    cursor: not-allowed;
-  }
-`;
-
 export default class StyledMultiEmailInput extends Component {
   static propTypes = {
-    /** Editor initial state */
-    initialState: PropTypes.instanceOf(EditorState),
+    /** Initial value */
+    value: PropTypes.string,
     /** Callback for state update like `({emails, invalids}) => void` */
     onChange: PropTypes.func,
-    /** Callback for when component is unmount. Useful to save editor state. */
-    onClose: PropTypes.func,
     /** On array of invalid emails */
     invalids: PropTypes.arrayOf(PropTypes.string),
     /** disabled */
@@ -58,15 +28,9 @@ export default class StyledMultiEmailInput extends Component {
     this.onBlur = this.onBlur.bind(this);
     this.onFocus = this.onFocus.bind(this);
     this.state = {
-      editorState: props.initialState || EditorState.createEmpty(),
+      value: props.value || '',
       showErrors: false,
     };
-  }
-
-  componentWillUnmount() {
-    if (this.props.onClose) {
-      this.props.onClose(this.state.editorState);
-    }
   }
 
   extractEmails(str) {
@@ -85,16 +49,16 @@ export default class StyledMultiEmailInput extends Component {
     );
   }
 
-  onChange(editorState) {
-    this.setState({ editorState });
+  onChange(e) {
+    const value = e.target.value;
+    this.setState({ value });
     if (this.props.onChange) {
-      this.onChangeParent(editorState);
+      this.onChangeParent(value);
     }
   }
 
-  onChangeParent(editorState) {
-    const stringContent = editorState.getCurrentContent().getPlainText();
-    const returnedState = this.extractEmails(stringContent);
+  onChangeParent(value) {
+    const returnedState = this.extractEmails(value);
     this.props.onChange(returnedState);
   }
 
@@ -110,20 +74,20 @@ export default class StyledMultiEmailInput extends Component {
     const { invalids, disabled } = this.props;
 
     return (
-      <InputContainer
+      <Container
         width="100%"
         bg={disabled ? 'black.50' : 'white.full'}
         fontSize="14px"
         borderColor={getInputBorderColor(invalids && invalids.length > 0)}
-        {...omit(this.props, ['invalids', 'onChange', 'initialState', 'onClose'])}
+        {...omit(this.props, ['invalids', 'onChange', 'value'])}
       >
-        <Editor
-          editorState={this.state.editorState}
+        <Textarea
+          value={this.state.value}
           onChange={this.onChange}
           onBlur={this.onBlur}
           onFocus={this.onFocus}
-          readOnly={disabled}
-          stripPastedStyles
+          disabled={disabled}
+          className={this.state.showErrors && invalids && invalids.length > 0 ? 'border-red-500' : ''}
         />
         {this.state.showErrors && invalids && invalids.length > 0 && (
           <Span className="multiemails-errors" display="block" color="red.500" pt={2} fontSize="10px">
@@ -133,7 +97,7 @@ export default class StyledMultiEmailInput extends Component {
             {invalids.join(', ')}
           </Span>
         )}
-      </InputContainer>
+      </Container>
     );
   }
 }
