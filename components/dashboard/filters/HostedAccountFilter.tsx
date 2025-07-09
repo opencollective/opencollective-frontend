568bfdbd832ce81757a6f7e8d263c3af9848f77b
Leo Kewitz <leo@opencollective.com>
2025-07-09 10:04:08 +0200

    fix: multiple queries with same name

--- components/dashboard/filters/HostedAccountFilter.tsx
+++ components/dashboard/filters/HostedAccountFilter.tsx
@@ -5,24 +5,15 @@ import { z } from 'zod';
 
 import type { FilterComponentProps, FilterConfig } from '../../../lib/filters/filter-types';
 import { API_V2_CONTEXT, gql } from '../../../lib/graphql/helpers';
-import type { AccountHoverCardFieldsFragment, AccountQuery } from '../../../lib/graphql/types/v2/graphql';
+import type { AccountFilterQuery, AccountHoverCardFieldsFragment } from '../../../lib/graphql/types/v2/graphql';
 import type { Account } from '../../../lib/graphql/types/v2/schema';
 import { isMulti } from '@/lib/filters/schemas';
 
 import { AccountHoverCard, accountHoverCardFields } from '../../AccountHoverCard';
 import Avatar from '../../Avatar';
 
+import { accountFilterQuery } from './AccountFilter';
 import ComboSelectFilter from './ComboSelectFilter';
-
-const accountQuery = gql`
-  query Account($slug: String!) {
-    account(slug: $slug) {
-      id
-      ...AccountHoverCardFields
-    }
-  }
-  ${accountHoverCardFields}
-`;
 
 const hostedAccountFilterSearchQuery = gql`
   query HostedAccountFilterSearch($searchTerm: String, $hostSlug: String, $orderBy: OrderByInput) {
@@ -45,7 +36,7 @@ export const AccountRenderer = ({
   };
   inOptionsList?: boolean; // For positioning the HoverCard to the right to prevent blocking options list
 }) => {
-  const { data } = useQuery<AccountQuery>(accountQuery, {
+  const { data } = useQuery<AccountFilterQuery>(accountFilterQuery, {
     variables: { slug: account.slug },
     fetchPolicy: 'cache-first',
     context: API_V2_CONTEXT,
