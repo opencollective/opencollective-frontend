0f9aca5d8569cd4c4e73a275c2b9fe8ec8882b96
Leo Kewitz <leo@opencollective.com>
2025-07-28 19:09:07 +0200

    Feat: Organization SetupGuide (#11350)
    * fix: multiple queries with same name
    
    * feat: add SetupGuide to OrganizationOverview
    
    * chore: update schemas and lang
    
    * fix: infinite loading for non-host accounts
    
    * fix: have only one category open at time
    
    * refact: move setup-guide logic to its own lib
    
    * feat: persist SetupGuide open state
    
    * chore: update schemas
    
    * fix: unexported component
    
    * fix: SetupGuide checks and items state change
    
    * chore: update schemas
    
    * fix: SetupGuide expanded state bug
    
    * fix: hide SetupGuide steps that don't make any sense

--- components/dashboard/sections/overview/OrgOverview.tsx
+++ components/dashboard/sections/overview/OrgOverview.tsx
@@ -1,54 +1,81 @@
-import React from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
 import { FormattedMessage } from 'react-intl';
 
 import { isHostAccount } from '@/lib/collective';
+import { API_V2_CONTEXT } from '@/lib/graphql/helpers';
+import useLoggedInUser from '@/lib/hooks/useLoggedInUser';
 
-// import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/Card';
-// import { Collapsible, CollapsibleContent } from '@/components/ui/Collapsible';
-// import { Button } from '../../../ui/Button';
+import { Collapsible, CollapsibleContent } from '@/components/ui/Collapsible';
+
+import { Button } from '../../../ui/Button';
 import { DashboardContext } from '../../DashboardContext';
 import DashboardHeader from '../../DashboardHeader';
 
 import { HostOverviewContent } from './HostOverviewContent';
 import { OrgOverviewContent } from './OrgOverviewContent';
+import { SetupGuideCard } from './SetupGuide';
+
+const editAccountSettingMutation = gql`
+  mutation UpdateSetupGuideState($account: AccountReferenceInput!, $key: AccountSettingsKey!, $value: JSON!) {
+    editAccountSetting(account: $account, key: $key, value: $value) {
+      id
+      settings
+    }
+  }
+`;
 
 export function OrgOverview() {
-  const { account } = React.useContext(DashboardContext);
-  // const [showSetupGuide, setShowSetupGuide] = React.useState(false);
+  const { account } = useContext(DashboardContext);
+  const { LoggedInUser, refetchLoggedInUser } = useLoggedInUser();
+  const [showSetupGuide, setShowSetupGuide] = useState(undefined);
+  const [editAccountSetting] = useMutation(editAccountSettingMutation, {
+    context: API_V2_CONTEXT,
+  });
+
+  useEffect(() => {
+    if (LoggedInUser && showSetupGuide === undefined && account) {
+      const showGuideKey = `id${account.legacyId}`;
+      const showGuide = LoggedInUser.collective.settings?.showSetupGuide?.[showGuideKey];
+      setShowSetupGuide(showGuide !== false ? true : false);
+    }
+  }, [LoggedInUser, account, showSetupGuide]);
+
+  const handleSetupGuideToggle = useCallback(
+    async (open: boolean) => {
+      setShowSetupGuide(open);
+
+      await editAccountSetting({
+        variables: {
+          account: { legacyId: LoggedInUser.collective.id },
+          key: `showSetupGuide.id${account.legacyId}`,
+          value: open,
+        },
+      }).catch(() => {});
+      await refetchLoggedInUser();
+    },
+    [account, LoggedInUser, editAccountSetting, refetchLoggedInUser],
+  );
 
   return (
     <div className="max-w-(--breakpoint-lg) space-y-6">
       <DashboardHeader
         title={<FormattedMessage id="AdminPanel.Menu.Overview" defaultMessage="Overview" />}
-        // actions={
-        //   <Button
-        //     size="sm"
-        //     variant="outline"
-        //     onClick={() => {
-        //       setShowSetupGuide(open => !open);
-        //     }}
-        //   >
-        //     {showSetupGuide ? (
-        //       <FormattedMessage defaultMessage="Hide setup guide" id="SetupGuide.HideSetupGuide" />
-        //     ) : (
-        //       <FormattedMessage defaultMessage="Show setup guide" id="SetupGuide.ShowSetupGuide" />
-        //     )}
-        //   </Button>
-        // }
+        actions={
+          <Button size="sm" variant="outline" onClick={() => handleSetupGuideToggle(!showSetupGuide)}>
+            {showSetupGuide ? (
+              <FormattedMessage defaultMessage="Hide setup guide" id="SetupGuide.HideSetupGuide" />
+            ) : (
+              <FormattedMessage defaultMessage="Show setup guide" id="SetupGuide.ShowSetupGuide" />
+            )}
+          </Button>
+        }
       />
-      {/* <Collapsible open={showSetupGuide}>
+      <Collapsible open={showSetupGuide}>
         <CollapsibleContent>
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-xl">
-                <FormattedMessage defaultMessage="Setup guide" id="SetupGuide.Title" />
-              </CardTitle>
-              <CardDescription>Get going with Open Collective!</CardDescription>
-            </CardHeader>
-            <CardContent>TBD</CardContent>
-          </Card>
+          <SetupGuideCard account={account} setOpen={handleSetupGuideToggle} />
         </CollapsibleContent>
-      </Collapsible> */}
+      </Collapsible>
 
       {isHostAccount(account) ? (
         <HostOverviewContent accountSlug={account.slug} />
