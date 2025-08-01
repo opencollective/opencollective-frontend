5fb87b18334f962425dc97692a45ec95187fea6a
Benjamin Piouffle <benjamin@opencollective.com>
2025-08-01 09:15:51 +0200

    test(StyledInputAmount): add unit coverage (#11390)

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ const nextJest = require('next/jest');
 const createJestConfig = nextJest({ dir: './' });
 
 module.exports = createJestConfig({
-  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
   testEnvironment: 'jsdom',
 });
