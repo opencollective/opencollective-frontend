c8156657e31c257c53da96d05076e81bf164f70f
Gustav Larsson <gustav.larsson@gmail.com>
2025-08-28 11:58:54 +0200

    Restore landing page content when env NEW_PRICING is false

--- components/home/TheFutureIsCollectiveSection.js
+++ components/home/TheFutureIsCollectiveSection.js
@@ -4,10 +4,10 @@ import { FormattedMessage } from 'react-intl';
 
 import NextIllustration from '../collectives/HomeNextIllustration';
 import Container from '../Container';
-import { Box } from '../Grid';
+import { Box, Flex } from '../Grid';
 import { I18nBold } from '../I18nFormatters';
 import Link from '../Link';
-import { MainDescription } from '../marketing/Text';
+import { MainDescription, MainTitle } from '../marketing/Text';
 import StyledButton from '../StyledButton';
 import StyledModal from '../StyledModal';
 import { Span } from '../Text';
@@ -17,29 +17,59 @@ const TheFutureIsCollective = () => {
 
   return (
     <Fragment>
-      <div className="mt-20 flex items-center justify-center px-4">
-        <div className="flex max-w-6xl flex-col items-center" flexDirection="column" alignItems="center">
-          <div>
-            <h1 className="text-center text-5xl font-bold tracking-tight text-balance text-oc sm:text-6xl md:text-7xl md:font-extrabold">
-              <FormattedMessage
-                id="home.collaborativeMoneyManagement"
-                defaultMessage="Collaborative Money Management"
-              />
-            </h1>
-          </div>
-          <div className="my-4 max-w-4xl sm:my-10">
-            {/* maxWidth={['288px', '608px', '768px', null, '896px']} */}
+      <Flex justifyContent="center" alignItems="center" px="16px" mt="40px">
+        <Flex flexDirection="column" alignItems="center">
+          <Box>
+            <MainTitle
+              fontSize={['52px', '64px', '72px']}
+              lineHeight={['56px', '80px', '88px']}
+              letterSpacing={['-0.04em', '0.012em', null]}
+              textAlign="center"
+            >
+              <FormattedMessage id="home.futureIsCollective" defaultMessage="The future is collective" />
+            </MainTitle>
+          </Box>
+          <Box my={[4, '40px']} maxWidth={['288px', '608px', '768px', null, '896px']}>
             <MainDescription textAlign="center">
               <FormattedMessage
-                id="home.collaborativeMoneyManagement.description"
-                defaultMessage="Open Collective provides organizations and groups with <b>participatory financial infrastructure</b>. When people can see where money comes from and where it goes, <b>trust can be built.</b> Trust paves the way for increased <b>financial collaboration and coordination.</b>"
+                id="home.futureIsCollective.description"
+                defaultMessage="We make it easy to <b>raise, manage, and disburse money with full transparency</b>. Start your open collective, apply to a Fiscal Host, and unlock funding for your community—free from red tape."
                 values={{
                   b: I18nBold,
                 }}
               />
             </MainDescription>
-          </div>
-
+          </Box>
+          <Box mb="40px" display="flex" flexDirection={['column', 'row']} alignItems="center">
+            <Link href="/create">
+              <StyledButton
+                minWidth={['288px', '125px']}
+                my={['12px', null, 0]}
+                mr={[0, '24px']}
+                buttonStyle="marketing"
+                whiteSpace="nowrap"
+                backgroundColor="primary.900"
+                fontSize="16px"
+                lineHeight="20px"
+              >
+                <FormattedMessage id="home.startNow" defaultMessage="Start Now" />
+              </StyledButton>
+            </Link>
+            <StyledButton
+              onClick={() => setShowModal(true)}
+              my={['12px', null, 0]}
+              minWidth={['288px', '196px']}
+              fontSize="16px"
+              lineHeight="20px"
+            >
+              <Span mr={'14px'} textDecoration="underline">
+                <FormattedMessage id="home.makeCommunitySection.watchVideo" defaultMessage="Watch Video" />
+              </Span>
+              <Span className="arrowIcon">
+                <RightArrow size="15" />
+              </Span>
+            </StyledButton>
+          </Box>
           <NextIllustration
             display={[null, 'none']}
             width={320}
@@ -68,8 +98,8 @@ const TheFutureIsCollective = () => {
             alt="The future is collective."
             src="/static/images/new-home/budget-illustration.png"
           />
-        </div>
-      </div>
+        </Flex>
+      </Flex>
       {showModal && (
         <StyledModal
           padding="0"
