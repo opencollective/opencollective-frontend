9e6ce731bdeab0873210d71895c341355a197079
Gustav Larsson <gustav.larsson@gmail.com>
2025-09-02 14:40:39 +0200

    Hide paypal settings if not enabled

--- components/edit-collective/sections/SendingMoney.tsx
+++ components/edit-collective/sections/SendingMoney.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
-import { FEATURES, requiresUpgrade } from '../../../lib/allowed-features';
+import hasFeature, { FEATURES, requiresUpgrade } from '../../../lib/allowed-features';
 
 import { DashboardContext } from '@/components/dashboard/DashboardContext';
 import { UpgradePlanCTA } from '@/components/platform-subscriptions/UpgradePlanCTA';
@@ -15,20 +15,22 @@ import SettingsSectionTitle from './SettingsSectionTitle';
 const SendingMoney = ({ collective }) => {
   const { account } = React.useContext(DashboardContext);
 
-  if (requiresUpgrade(account, FEATURES.TRANSFERWISE) && requiresUpgrade(account, FEATURES.PAYPAL_PAYOUTS)) {
-    // The TRANSFERWISE feature is used here, since they share the same Upgrade plan CTA messaging.
+  if (requiresUpgrade(account, FEATURES.TRANSFERWISE)) {
     return <UpgradePlanCTA featureKey={FEATURES.TRANSFERWISE} />;
   }
   const paypalAccount = collective.connectedAccounts?.find(c => c.service === 'paypal');
 
   return (
     <div className="flex flex-col gap-8">
-      <div>
-        <SettingsSectionTitle>
-          <FormattedMessage id="PayoutMethod.Type.Paypal" defaultMessage="PayPal" />
-        </SettingsSectionTitle>
-        <EditPayPalAccount collective={collective} connectedAccount={paypalAccount} variation="SENDING" />
-      </div>
+      {hasFeature(account, FEATURES.PAYPAL_PAYOUTS) && (
+        <div>
+          <SettingsSectionTitle>
+            <FormattedMessage id="PayoutMethod.Type.Paypal" defaultMessage="PayPal" />
+          </SettingsSectionTitle>
+          <EditPayPalAccount collective={collective} connectedAccount={paypalAccount} variation="SENDING" />
+        </div>
+      )}
+
       <div>
         <SettingsSectionTitle>Wise</SettingsSectionTitle>
         <EditTransferWiseAccount collective={collective} />
