f2f7d1093924e84aa99c69497ba0603a28387567
François Hodierne <francois@hodierne.net>
2025-10-16 11:19:13 +0200

    reorder imports

--- components/static-pages/index.js
+++ components/static-pages/index.js
@@ -1,8 +1,8 @@
+import about from './about.md';
 import hiring from './hiring.md';
 import pricing from './pricing.md';
 import privacypolicy from './privacypolicy.md';
 import tos from './tos.md';
 import widgets from './widgets.md';
-import about from './about.md';
 
 export default { privacypolicy, tos, widgets, hiring, pricing, about };
