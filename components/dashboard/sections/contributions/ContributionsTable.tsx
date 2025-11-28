167013aefb424fedab10fde2b349e9137139cf2d
Gustav Larsson <gustav.larsson@gmail.com>
2025-11-28 15:09:07 +0100

    Refactor useContributionActions to be standalone (triggers its own modals)

--- components/dashboard/sections/contributions/ContributionsTable.tsx
+++ components/dashboard/sections/contributions/ContributionsTable.tsx
@@ -1,44 +1,26 @@
 import React from 'react';
-import { useMutation, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { compact } from 'lodash';
-import { ArrowLeftRightIcon, LinkIcon, Pencil } from 'lucide-react';
 import { useRouter } from 'next/router';
-import type { IntlShape } from 'react-intl';
-import { FormattedMessage, useIntl } from 'react-intl';
 
-import type { GetActions } from '../../../../lib/actions/types';
 import { EMPTY_ARRAY } from '../../../../lib/constants/utils';
 import { API_V2_CONTEXT, gql } from '../../../../lib/graphql/helpers';
-import type { ContributionDrawerQuery, ManagedOrderFieldsFragment } from '../../../../lib/graphql/types/v2/graphql';
-import {
-  ContributionFrequency,
-  ExpectedFundsFilter,
-  OrderStatus,
-  PaymentMethodType,
-} from '../../../../lib/graphql/types/v2/schema';
-import useLoggedInUser from '../../../../lib/hooks/useLoggedInUser';
+import type { ManagedOrderFieldsFragment } from '../../../../lib/graphql/types/v2/graphql';
+import { ExpectedFundsFilter } from '../../../../lib/graphql/types/v2/schema';
 import type { useQueryFilterReturnType } from '../../../../lib/hooks/useQueryFilter';
-import type LoggedInUser from '../../../../lib/LoggedInUser';
-import { getWebsiteUrl } from '../../../../lib/utils';
 
-import ContributionConfirmationModal from '../../../ContributionConfirmationModal';
 import { ContributionDrawer } from '../../../contributions/ContributionDrawer';
-import { getTransactionsUrl } from '../../../contributions/ContributionTimeline';
-import { CopyID } from '../../../CopyId';
 import type { EditOrderActions } from '../../../EditOrderModal';
 import EditOrderModal from '../../../EditOrderModal';
-import Link from '../../../Link';
 import MessageBoxGraphqlError from '../../../MessageBoxGraphqlError';
-import { useModal } from '../../../ModalContext';
 import { managedOrderFragment } from '../../../recurring-contributions/graphql/queries';
 import { actionsColumn, DataTable } from '../../../table/DataTable';
-import { useToast } from '../../../ui/useToast';
 import { EmptyResults } from '../../EmptyResults';
 import { Filterbar } from '../../filters/Filterbar';
 import { Pagination } from '../../filters/Pagination';
 
+import { useContributionActions } from './actions';
 import { amount, beneficiary, contributionId, contributor, date, expectedAt, paymentMethod, status } from './columns';
-import CreatePendingContributionModal from './CreatePendingOrderModal';
 
 export const dashboardContributionsQuery = gql`
   query DashboardRecurringContributions(
@@ -112,171 +94,6 @@ export const getColumns = ({ onlyExpectedFunds }: { onlyExpectedFunds?: boolean 
     actionsColumn,
   ]);
 
-type GetContributionActionsOptions = {
-  LoggedInUser: LoggedInUser;
-  intl: IntlShape;
-  onUpdatePaymentMethodClick: (order: ManagedOrderFieldsFragment | ContributionDrawerQuery['order']) => void;
-  onResumeClick: (order: ManagedOrderFieldsFragment | ContributionDrawerQuery['order']) => void;
-  onEditAmountClick: (order: ManagedOrderFieldsFragment | ContributionDrawerQuery['order']) => void;
-  onMarkAsCompletedClick: (order: ManagedOrderFieldsFragment | ContributionDrawerQuery['order']) => void;
-  onMarkAsExpiredClick: (order: ManagedOrderFieldsFragment | ContributionDrawerQuery['order']) => void;
-  onCancelClick: (order: ManagedOrderFieldsFragment | ContributionDrawerQuery['order']) => void;
-  onEditExpectedFundsClick: (order: ManagedOrderFieldsFragment | ContributionDrawerQuery['order']) => void;
-  onEditAddedFundsClick: (order: ManagedOrderFieldsFragment | ContributionDrawerQuery['order']) => void;
-};
-
-export const getContributionActions: (
-  opts: GetContributionActionsOptions,
-) => GetActions<ManagedOrderFieldsFragment | ContributionDrawerQuery['order']> = opts => order => {
-  if (!order) {
-    return null;
-  }
-
-  const transactionsUrl = getTransactionsUrl(opts.LoggedInUser, order);
-  transactionsUrl.searchParams.set('orderId', order.legacyId.toString());
-
-  const actions: ReturnType<GetActions<any>> = {
-    primary: [
-      {
-        key: 'view-transactions',
-        label: (
-          <Link href={transactionsUrl.toString()} className="flex flex-row items-center gap-2.5">
-            <ArrowLeftRightIcon size={16} className="text-muted-foreground" />
-            <FormattedMessage defaultMessage="View transactions" id="DfQJQ6" />
-          </Link>
-        ),
-        onClick: () => {},
-      },
-    ],
-    secondary: [],
-  };
-
-  const isAdminOfOrder = opts.LoggedInUser.isAdminOfCollective(order.fromAccount);
-  const canUpdateActiveOrder =
-    order.frequency !== ContributionFrequency.ONETIME &&
-    ![
-      OrderStatus.PAUSED,
-      OrderStatus.PROCESSING,
-      OrderStatus.PENDING,
-      OrderStatus.CANCELLED,
-      OrderStatus.REFUNDED,
-      OrderStatus.REJECTED,
-    ].includes(order.status) &&
-    isAdminOfOrder;
-
-  const canResume = order.status === OrderStatus.PAUSED && order.permissions.canResume;
-  const canCancel =
-    isAdminOfOrder &&
-    ![OrderStatus.CANCELLED, OrderStatus.PAID, OrderStatus.REFUNDED, OrderStatus.REJECTED].includes(order.status) &&
-    order.frequency !== ContributionFrequency.ONETIME;
-  const canMarkAsCompleted =
-    [OrderStatus.PENDING, OrderStatus.EXPIRED].includes(order.status) && order.permissions.canMarkAsPaid;
-  const canMarkAsExpired = order.status === OrderStatus.PENDING && order.permissions.canMarkAsExpired;
-  const isExpectedFunds = !!order.pendingContributionData?.expectedAt;
-
-  if (canUpdateActiveOrder) {
-    actions.primary.push({
-      label: opts.intl.formatMessage({
-        defaultMessage: 'Update payment method',
-        id: 'subscription.menu.editPaymentMethod',
-      }),
-      onClick: () => opts.onUpdatePaymentMethodClick(order),
-      key: 'update-payment-method',
-    });
-  }
-
-  if (canResume) {
-    actions.primary.push({
-      label: opts.intl.formatMessage({ defaultMessage: 'Resume contribution', id: '51nF6S' }),
-      onClick: () => opts.onResumeClick(order),
-      key: 'resume-contribution',
-    });
-  }
-
-  if (canUpdateActiveOrder) {
-    actions.primary.push({
-      label: opts.intl.formatMessage({ defaultMessage: 'Update amount', id: 'subscription.menu.updateAmount' }),
-      onClick: () => opts.onEditAmountClick(order),
-      key: 'update-amount',
-    });
-  }
-
-  if (isExpectedFunds && (canMarkAsExpired || canMarkAsCompleted)) {
-    actions.primary.push({
-      label: opts.intl.formatMessage({ defaultMessage: 'Edit expected funds', id: 'hQAJH9' }),
-      onClick: () => opts.onEditExpectedFundsClick(order),
-      key: 'edit',
-    });
-  }
-
-  if (canMarkAsCompleted) {
-    actions.primary.push({
-      label: opts.intl.formatMessage({ defaultMessage: 'Mark as completed', id: 'order.markAsCompleted' }),
-      onClick: () => opts.onMarkAsCompletedClick(order),
-      'data-cy': 'MARK_AS_PAID-button',
-      key: 'mark-as-paid',
-    });
-  }
-
-  if (canMarkAsExpired) {
-    actions.primary.push({
-      label: opts.intl.formatMessage({ defaultMessage: 'Mark as expired', id: 'order.markAsExpired' }),
-      onClick: () => opts.onMarkAsExpiredClick(order),
-      'data-cy': 'MARK_AS_EXPIRED-button',
-      key: 'mark-as-expired',
-    });
-  }
-
-  if (canCancel) {
-    actions.secondary.push({
-      key: 'cancel-contribution',
-      label: opts.intl.formatMessage({
-        defaultMessage: 'Cancel contribution',
-        id: 'subscription.menu.cancelContribution',
-      }),
-      onClick: () => opts.onCancelClick(order),
-      'data-cy': 'recurring-contribution-menu-cancel-option',
-    });
-  }
-
-  if (order.paymentMethod?.type === PaymentMethodType.HOST) {
-    actions.primary.push({
-      key: 'edit-funds',
-      label: (
-        <React.Fragment>
-          <Pencil size={16} className="text-muted-foreground" />
-          {opts.intl.formatMessage({ defaultMessage: 'Edit funds', id: 'Kbjd3f' })}
-        </React.Fragment>
-      ),
-      onClick: () => opts.onEditAddedFundsClick(order),
-    });
-  }
-
-  const toAccount = order.toAccount;
-  const legacyId = order.legacyId;
-  const orderUrl = new URL(`${toAccount.slug}/orders/${legacyId}`, getWebsiteUrl());
-
-  actions.secondary.push({
-    key: 'copy-link',
-    label: (
-      <CopyID
-        Icon={null}
-        value={orderUrl}
-        tooltipLabel={<FormattedMessage defaultMessage="Copy link" id="CopyLink" />}
-        className=""
-      >
-        <div className="flex flex-row items-center gap-2.5">
-          <LinkIcon size={16} className="text-muted-foreground" />
-          <FormattedMessage defaultMessage="Copy link" id="CopyLink" />
-        </div>
-      </CopyID>
-    ),
-    onClick: () => {},
-  });
-
-  return actions;
-};
-
 export type ContributionsTableProps<FilterValues extends Record<string, unknown>> = {
   accountSlug: string;
   direction: 'INCOMING' | 'OUTGOING';
@@ -299,38 +116,7 @@ export default function ContributionsTable<FilterValues extends Record<string, u
   hostSlug,
   onRefetch,
 }: ContributionsTableProps<FilterValues>) {
-  const { toast } = useToast();
-  const { LoggedInUser } = useLoggedInUser();
-  const intl = useIntl();
   const router = useRouter();
-
-  const [expireOrder] = useMutation(
-    gql`
-      mutation ContributionsExpireOrder($orderId: Int) {
-        processPendingOrder(order: { legacyId: $orderId }, action: MARK_AS_EXPIRED) {
-          id
-          status
-          permissions {
-            id
-            canMarkAsPaid
-            canMarkAsExpired
-          }
-          activities {
-            nodes {
-              id
-            }
-          }
-        }
-      }
-    `,
-    {
-      context: API_V2_CONTEXT,
-    },
-  );
-
-  const [confirmCompletedOrder, setConfirmCompletedOrder] = React.useState(null);
-  const { showConfirmationModal } = useModal();
-  const [editingExpectedFunds, setEditingExpectedFunds] = React.useState(null);
 
   const selectedContributionId = router.query.orderId ? parseInt(router.query.orderId as string) : null;
 
@@ -405,56 +191,10 @@ export default function ContributionsTable<FilterValues extends Record<string, u
   const currentViewCount = views.find(v => v.id === queryFilter.activeViewId)?.count;
   const nbPlaceholders = currentViewCount < queryFilter.values.limit ? currentViewCount : queryFilter.values.limit;
 
-  const getActions = getContributionActions({
-    intl,
-    LoggedInUser,
-    onCancelClick: order => {
-      setEditOrder({ order, action: 'cancel' });
-    },
-    onEditAmountClick: order => {
-      setEditOrder({ order, action: 'editAmount' });
-    },
-    onResumeClick: order => {
-      setEditOrder({ order, action: 'editPaymentMethod' });
-    },
-    onUpdatePaymentMethodClick: order => {
-      setEditOrder({ order, action: 'editPaymentMethod' });
-    },
-    onMarkAsCompletedClick: order => {
-      setConfirmCompletedOrder(order);
-    },
-    onMarkAsExpiredClick: order => {
-      showConfirmationModal({
-        title: <FormattedMessage id="Order.MarkExpiredConfirm" defaultMessage="Mark this contribution as expired?" />,
-        description: (
-          <FormattedMessage
-            id="Order.MarkPaidExpiredDetails"
-            defaultMessage="This contribution will be marked as expired removed from Expected Funds. You can find this page by searching for its ID in the search bar or through the status filter in the Financial Contributions page."
-          />
-        ),
-        onConfirm: async () => {
-          await expireOrder({
-            variables: {
-              orderId: order.legacyId,
-            },
-          });
-          toast({
-            variant: 'success',
-            message: intl.formatMessage({
-              defaultMessage: 'The contribution has been marked as expired',
-              id: '46L6cy',
-            }),
-          });
-        },
-        confirmLabel: <FormattedMessage id="order.markAsExpired" defaultMessage="Mark as expired" />,
-      });
-    },
-    onEditExpectedFundsClick(order) {
-      setEditingExpectedFunds(order);
-    },
-    onEditAddedFundsClick(order) {
-      setEditOrder({ order, action: 'editAddedFunds' });
-    },
+  const getActions = useContributionActions({
+    accountSlug,
+    hostSlug,
+    refetchList: handleRefetch,
   });
 
   return (
@@ -501,23 +241,6 @@ export default function ContributionsTable<FilterValues extends Record<string, u
         orderId={selectedContributionId}
         getActions={getActions}
       />
-
-      {confirmCompletedOrder && (
-        <ContributionConfirmationModal
-          order={confirmCompletedOrder}
-          onClose={() => setConfirmCompletedOrder(false)}
-          onSuccess={handleRefetch}
-        />
-      )}
-
-      {editingExpectedFunds && (
-        <CreatePendingContributionModal
-          hostSlug={hostSlug || accountSlug}
-          edit={editingExpectedFunds}
-          onClose={() => setEditingExpectedFunds(null)}
-          onSuccess={handleRefetch}
-        />
-      )}
     </React.Fragment>
   );
 }
