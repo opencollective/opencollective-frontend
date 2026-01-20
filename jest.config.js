b0935eec290d115f2298d994530fb5a13f17d4ce
Benjamin Piouffle <benjamin@opencollective.com>
2026-01-20 10:33:17 +0100

    test(AuthenticatorSettings): fix for OTP update (#11823)

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,15 @@ const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({ dir: './' });
 
-module.exports = createJestConfig({
-  setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
-  testEnvironment: 'jsdom',
-});
+module.exports = async () => {
+  const nextConfig = await createJestConfig({
+    setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+    testEnvironment: 'jsdom',
+  })();
+
+  // Override transformIgnorePatterns to include @scure, @otplib, and @noble packages
+  // These packages use ES modules and need to be transformed
+  nextConfig.transformIgnorePatterns = ['^.+\\.module\\.(css|sass|scss)$', 'node_modules/(?!(@scure|@otplib|@noble)/)'];
+
+  return nextConfig;
+};
