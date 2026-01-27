4b867fbd7203fae57b8bdae0c53acdbdc4232dfa
François Hodierne <francois@hodierne.net>
2026-01-27 11:59:14 +0100

    fix: update pdf-parse import for v2 API

--- test/cypress/scripts/get-text-from-pdf-content.ts
+++ test/cypress/scripts/get-text-from-pdf-content.ts
@@ -1,8 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const pdf = require('pdf-parse');
+const { PDFParse } = require('pdf-parse');
 
 // imported using require
 // ts-unused-exports:disable-next-line
-export const getTextFromPdfContent = (pdfContent: string): Promise<string> => {
-  return pdf(pdfContent).then(({ text }) => text);
+export const getTextFromPdfContent = async (pdfContent: string): Promise<string> => {
+  const parser = new PDFParse({ data: pdfContent });
+  const result = await parser.getText();
+  await parser.destroy();
+  return result.text;
 };
