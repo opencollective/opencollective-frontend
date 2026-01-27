a4dcc4a0c8aaa08597409710c5d547a71720bec5
Leo Kewitz <leo@oficonsortium.org>
2026-01-27 16:24:21 +0100

    chore: update schemas

--- lib/graphql/types/v2/schema.ts
+++ lib/graphql/types/v2/schema.ts
@@ -10004,6 +10004,8 @@ export type Organization = Account & AccountWithContributions & AccountWithPlatf
   /** List of activities that the logged-in user is subscribed for this collective */
   activitySubscriptions?: Maybe<Array<Maybe<ActivitySubscription>>>;
   backgroundImageUrl?: Maybe<Scalars['String']['output']>;
+  /** Returns whether this organization can be a vendor of the specified host. This checks if the organization only transacted with this host and all its admins are also admins of the host. */
+  canBeVendorOf: Scalars['Boolean']['output'];
   /** Whether this account can have changelog updates */
   canHaveChangelogUpdates: Scalars['Boolean']['output'];
   /** Returns true if the remote user can start the process to resume contributions for account */
@@ -10177,6 +10179,12 @@ export type OrganizationActivitySubscriptionsArgs = {
 export type OrganizationBackgroundImageUrlArgs = {
   format?: InputMaybe<ImageFormat>;
   height?: InputMaybe<Scalars['Int']['input']>;
+};
+
+
+/** This represents an Organization account */
+export type OrganizationCanBeVendorOfArgs = {
+  host: AccountReferenceInput;
 };
 
 
