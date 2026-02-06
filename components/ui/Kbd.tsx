d3cb42edd610ac003baf761f961d225566f6799f
Leo Kewitz <leo@oficonsortium.org>
2026-02-06 10:58:35 +0100

    fix: unused export

--- components/ui/Kbd.tsx
+++ components/ui/Kbd.tsx
@@ -14,7 +14,10 @@ function Kbd({ className, ...props }: React.ComponentProps<'kbd'>) {
     />
   );
 }
+
 function KbdGroup({ className, ...props }: React.ComponentProps<'div'>) {
   return <kbd data-slot="kbd-group" className={cn('inline-flex items-center gap-1', className)} {...props} />;
 }
+
+// ts-unused-exports:disable-next-line
 export { Kbd, KbdGroup };
