c81e971972142e69e0e57ea51e31090193be8cf3
waterplea <alexander@inkin.ru>
2026-02-24 19:22:47 +0400

    refactor: drop react-text-mask in favor of Maskito

--- components/StyledInputMask.tsx
+++ components/StyledInputMask.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import MaskedInput from 'react-text-mask';
+import type { MaskitoOptions } from '@maskito/core';
+import { MASKITO_DEFAULT_OPTIONS } from '@maskito/core';
+import { useMaskito } from '@maskito/react';
 
 import StyledInput from './StyledInput';
 
-interface StyledInputMaskProps extends React.ComponentProps<typeof MaskedInput> {
-  render?: (ref: React.Ref<any>, props: any) => React.ReactNode;
+interface StyledInputMaskProps extends React.ComponentProps<typeof StyledInput> {
+  render?: (ref: React.Ref<unknown>, props: unknown) => React.ReactNode;
+  maskito?: MaskitoOptions;
 }
 
 const DefaultMaskRenderer = (ref, props) => <StyledInput ref={ref} {...props} />;
 
-const StyledInputMask = ({ render = DefaultMaskRenderer, ...props }: StyledInputMaskProps) => (
-  <MaskedInput render={render} {...props} />
-);
+const StyledInputMask = ({
+  render = DefaultMaskRenderer,
+  maskito = MASKITO_DEFAULT_OPTIONS,
+  ...props
+}: StyledInputMaskProps) => render(useMaskito({ options: maskito }), props);
 
 export default StyledInputMask;
