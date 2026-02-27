22360384a633f75d5e3047692258d4e132f2dca1
Leo Kewitz <leo@opencollective.com>
2026-02-27 17:30:53 +0100

    ExportCSVModal generate export tweaks (#11941)
    * fix: ExportCSVModal generate export tweaks
    
    * chore: update langs
    
    * fix: dayjs import

--- components/dashboard/sections/exports/index.tsx
+++ components/dashboard/sections/exports/index.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import { defineMessage, FormattedMessage, useIntl } from 'react-intl';
 import { z } from 'zod';
 
+import dayjs from '@/lib/dayjs';
 import { i18nGraphqlException } from '@/lib/errors';
 import { formatFileSize } from '@/lib/file-utils';
 import type { FilterComponentConfigs, Views } from '@/lib/filters/filter-types';
@@ -165,11 +166,24 @@ const getColumns = ({ intl }) => {
     },
     {
       accessorKey: 'createdAt',
-      header: intl.formatMessage({ defaultMessage: 'Date', id: 'expense.incurredAt' }),
+      header: intl.formatMessage({ defaultMessage: 'Requested At', id: 'RequestedAt' }),
       cell: ({ row }) => {
         const exportRequest = row.original;
         return (
-          <div className="text-sm text-muted-foreground">{new Date(exportRequest.createdAt).toLocaleString()}</div>
+          <div className="text-sm text-muted-foreground">
+            <div>{dayjs(exportRequest.createdAt).format('LLL')}</div>
+            {exportRequest.expiresAt && (
+              <div className="text-xs">
+                <FormattedMessage
+                  id="ContributePayment.expiresOn"
+                  defaultMessage="Expires on {expiryDate}"
+                  values={{
+                    expiryDate: dayjs(exportRequest.expiresAt).format('LL'),
+                  }}
+                />
+              </div>
+            )}
+          </div>
         );
       },
     },
