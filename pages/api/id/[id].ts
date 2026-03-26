cce5b86da3201217fa2689fc6042163f431e89f4
Henrique <hdiniz@users.noreply.github.com>
2026-03-26 11:31:40 -0300

    add permalink route as default

--- pages/api/id/[id].ts
+++ pages/api/id/[id].ts
@@ -1,7 +1,7 @@
-// maps frontend /id/:id to /api/id/:id
+// maps frontend /id/:id to /api/permalink/:id
 // next.js export
 // ts-unused-exports:disable-next-line
 export default async function handle(req, res) {
   const { id } = req.query;
-  res.redirect(`/api/id/${id}`);
+  res.redirect(`/api/permalink/${id}`);
 }
