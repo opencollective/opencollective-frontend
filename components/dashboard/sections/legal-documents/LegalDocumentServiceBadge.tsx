c432f49c2631e39c98b5d380e4ab2b0bea899945
Leo Kewitz <leo@oficonsortium.org>
2026-03-27 14:45:54 +0100

    refact: allow customizations on LegalDocuments and KYC badges

--- components/dashboard/sections/legal-documents/LegalDocumentServiceBadge.tsx
+++ components/dashboard/sections/legal-documents/LegalDocumentServiceBadge.tsx
@@ -4,11 +4,12 @@ import { startCase } from 'lodash';
 import type { LegalDocumentService } from '../../../../lib/graphql/types/v2/graphql';
 
 import { WebsiteName } from '../../../I18nFormatters';
+import type { BadgeProps } from '../../../ui/Badge';
 import { Badge } from '../../../ui/Badge';
 
-export const LegalDocumentServiceBadge = ({ service }: { service: LegalDocumentService }) => {
+export const LegalDocumentServiceBadge = ({ service, ...props }: { service: LegalDocumentService } & BadgeProps) => {
   return (
-    <Badge className="text-nowrap whitespace-nowrap">
+    <Badge className="text-nowrap whitespace-nowrap" {...props}>
       {service === 'OPENCOLLECTIVE' ? WebsiteName : startCase(service.toLowerCase())}
     </Badge>
   );
