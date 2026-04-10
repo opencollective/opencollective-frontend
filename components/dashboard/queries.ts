ec6488518da580b8424e7aefc5b1853b0d3f4359
Henrique <hdiniz@users.noreply.github.com>
2026-04-10 18:17:43 -0300

    Contribution categorization (#11929)
    * Contribution accounting rules
    
    * update lang
    
    * update graphql
    
    * Add missing enable toggle
    
    * update graphql
    
    * Rename categorization feature
    
    * Update graphql
    
    * Set enabled on rule when creating new one

--- components/dashboard/queries.ts
+++ components/dashboard/queries.ts
@@ -102,6 +102,7 @@ export const adminPanelQuery = gql`
         EXPECTED_FUNDS
         CHARGE_HOSTING_FEES
         KYC
+        ACCOUNTING_CATEGORIZATION_RULES
       }
       policies {
         id
