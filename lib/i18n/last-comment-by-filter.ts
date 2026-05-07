4fdcb4072f5b682c11015ec54f97e56f7bf8e0f5
Benjamin Piouffle <benjamin@oficonsortium.org>
2026-05-07 11:09:41 +0200

    feat(Dashboard): Unreplied filter for outoing payment requests

--- lib/i18n/last-comment-by-filter.ts
+++ lib/i18n/last-comment-by-filter.ts
@@ -2,6 +2,7 @@ import { defineMessages } from 'react-intl';
 
 export const LastCommentByFilterLabels = defineMessages({
   USER: { defaultMessage: 'Expense Submitter', id: 'AM5w9L' },
+  NON_FROM_ACCOUNT_ADMIN: { defaultMessage: 'Anyone but Payee admin', id: '+j25fv' },
   HOST_ADMIN: { defaultMessage: 'Fiscal Host Admin', id: '3yZ4rW' },
   NON_HOST_ADMIN: { defaultMessage: 'Anyone but Host Admin', id: 'UCmj6i' },
   COLLECTIVE_ADMIN: { defaultMessage: 'Collective Admin', id: 'ihHJtd' },
