5064032cddf9886c553f24007e8cbf737d7cd047
Henrique <hdiniz@users.noreply.github.com>
2026-06-11 09:11:11 -0300

    Put page behind preview feature

--- components/dashboard/sections/collectives/HostedCollectives.tsx
+++ components/dashboard/sections/collectives/HostedCollectives.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { useQuery } from '@apollo/client';
-import { compact, isString, omit } from 'lodash-es';
+import { compact, omit } from 'lodash-es';
 import { useRouter } from 'next/router';
 import { defineMessage, FormattedMessage, useIntl } from 'react-intl';
 import { z } from 'zod';
@@ -8,13 +8,16 @@ import { z } from 'zod';
 import { CollectiveType, HostedCollectiveTypes } from '../../../../lib/constants/collectives';
 import type { FilterComponentConfigs, FiltersToVariables } from '../../../../lib/filters/filter-types';
 import { integer, isMulti } from '../../../../lib/filters/schemas';
-import type { Account, Collective, HostedCollectivesQueryVariables } from '../../../../lib/graphql/types/v2/graphql';
+import type { Account, HostedCollectivesQueryVariables } from '../../../../lib/graphql/types/v2/graphql';
 import { HostFeeStructure } from '../../../../lib/graphql/types/v2/graphql';
+import useLoggedInUser from '../../../../lib/hooks/useLoggedInUser';
 import useQueryFilter from '../../../../lib/hooks/useQueryFilter';
 import formatCollectiveType from '../../../../lib/i18n/collective-type';
 import { formatHostFeeStructure } from '../../../../lib/i18n/host-fee-structure';
+import { PREVIEW_FEATURE_KEYS } from '../../../../lib/preview-features';
 
 import { Drawer } from '../../../Drawer';
+import { HostedAccountProfile } from '../../../hosted-account-overview/HostedAccountProfile';
 import MessageBoxGraphqlError from '../../../MessageBoxGraphqlError';
 import { DataTable } from '../../../table/DataTable';
 import { Button } from '../../../ui/Button';
@@ -134,11 +137,13 @@ const filters: FilterComponentConfigs<Schema> = {
   startsAt: startsAtDateFilter.filter,
 };
 
-const HostedCollectives = ({ accountSlug: hostSlug, subpath }: DashboardSectionProps) => {
+const HostedCollectivesList = ({ accountSlug: hostSlug }: DashboardSectionProps) => {
   const intl = useIntl();
   const router = useRouter();
+  const { LoggedInUser } = useLoggedInUser();
+  const hasAccountProfile = LoggedInUser?.hasPreviewFeatureEnabled(PREVIEW_FEATURE_KEYS.HOSTED_ACCOUNT_OVERVIEW);
   const [displayExportCSVModal, setDisplayExportCSVModal] = React.useState(false);
-  const [showCollectiveOverview, setShowCollectiveOverview] = React.useState<Account | undefined | string>(subpath[0]);
+  const [drawerCollective, setDrawerCollective] = React.useState<Account | undefined>(undefined);
   const accountTypes = [CollectiveType.COLLECTIVE];
 
   const { data: metadata, refetch: refetchMetadata } = useQuery(hostedCollectivesMetadataQuery, {
@@ -202,28 +207,15 @@ const HostedCollectives = ({ accountSlug: hostSlug, subpath }: DashboardSectionP
     fetchPolicy: typeof window !== 'undefined' ? 'cache-and-network' : 'cache-first',
   });
 
-  useEffect(() => {
-    if (subpath[0] !== ((showCollectiveOverview as Collective)?.id || showCollectiveOverview)) {
-      handleDrawer(subpath[0]);
-    }
-  }, [subpath[0]]);
-
-  const handleDrawer = (collective: Collective | string | undefined) => {
-    if (collective) {
-      pushSubpath(typeof collective === 'string' ? collective : collective.id);
-    } else {
-      pushSubpath(undefined);
-    }
-    setShowCollectiveOverview(collective);
-  };
-
   const handleEdit = () => {
     refetchMetadata();
     refetch();
   };
   const isUnhosted = queryFilter.values?.status === COLLECTIVE_STATUS.UNHOSTED;
   const hostedAccounts = data?.host?.hostedAccounts;
-  const onClickRow = row => handleDrawer(row.original);
+  const onClickRow = hasAccountProfile
+    ? row => pushSubpath(row.original.publicId)
+    : row => setDrawerCollective(row.original);
   return (
     <div className="flex flex-col gap-4">
       <DashboardHeader
@@ -279,7 +271,7 @@ const HostedCollectives = ({ accountSlug: hostSlug, subpath }: DashboardSectionP
                 onClickRow,
                 onEdit: handleEdit,
                 host: data?.host,
-                openCollectiveDetails: handleDrawer,
+                openCollectiveDetails: setDrawerCollective,
               } as unknown as HostedCollectivesDataTableMeta
             }
             onClickRow={onClickRow}
@@ -290,19 +282,18 @@ const HostedCollectives = ({ accountSlug: hostSlug, subpath }: DashboardSectionP
       )}
 
       <Drawer
-        open={Boolean(showCollectiveOverview)}
-        onClose={() => handleDrawer(null)}
+        open={Boolean(drawerCollective)}
+        onClose={() => setDrawerCollective(undefined)}
         className={'max-w-2xl'}
         showActionsContainer
         showCloseButton
       >
-        {showCollectiveOverview && (
+        {drawerCollective && (
           <CollectiveDetails
-            collective={isString(showCollectiveOverview) ? null : (showCollectiveOverview as any)}
-            collectiveId={isString(showCollectiveOverview) ? showCollectiveOverview : null}
+            collective={drawerCollective as any}
             host={data?.host}
-            onCancel={() => handleDrawer(null)}
-            openCollectiveDetails={handleDrawer}
+            onCancel={() => setDrawerCollective(undefined)}
+            openCollectiveDetails={setDrawerCollective}
             loading={loading}
             onEdit={handleEdit}
           />
@@ -310,6 +301,16 @@ const HostedCollectives = ({ accountSlug: hostSlug, subpath }: DashboardSectionP
       </Drawer>
     </div>
   );
+};
+
+const HostedCollectives = (props: DashboardSectionProps) => {
+  const { LoggedInUser } = useLoggedInUser();
+  const hasAccountProfile = LoggedInUser?.hasPreviewFeatureEnabled(PREVIEW_FEATURE_KEYS.HOSTED_ACCOUNT_OVERVIEW);
+  const collectiveId = props.subpath?.[0];
+  if (hasAccountProfile && collectiveId) {
+    return <HostedAccountProfile hostSlug={props.accountSlug} accountId={collectiveId} />;
+  }
+  return <HostedCollectivesList {...props} />;
 };
 
 export default HostedCollectives;
--- lib/preview-features.tsx
+++ lib/preview-features.tsx
@@ -19,6 +19,7 @@ export enum PREVIEW_FEATURE_KEYS {
   SIDEBAR_REORG_INCOMING_OUTGOING = 'SIDEBAR_REORG_INCOMING_OUTGOING',
   ASYNC_EXPORTS = 'ASYNC_EXPORTS',
   TABLE_QUICK_ACTIONS = 'TABLE_QUICK_ACTIONS',
+  HOSTED_ACCOUNT_OVERVIEW = 'HOSTED_ACCOUNT_OVERVIEW',
 }
 
 enum Categories {
@@ -282,5 +283,13 @@ export const previewFeatures: PreviewFeature[] = [
     publicBeta: true,
     alwaysEnableInDev: true,
     category: Categories.GENERAL,
+  },
+  {
+    key: PREVIEW_FEATURE_KEYS.HOSTED_ACCOUNT_OVERVIEW,
+    title: <FormattedMessage defaultMessage="Dashboard view of a hosted account profile" id="sDvTvc" />,
+    description: <FormattedMessage defaultMessage="Enable a dashboard hosted account profile page" id="cMqa3J" />,
+    publicBeta: false,
+    closedBetaAccessFor: [...PLATFORM_ACCOUNTS, ...OFICO_MEMBER_ORGANIZATIONS],
+    category: Categories.HOSTING,
   },
 ];
